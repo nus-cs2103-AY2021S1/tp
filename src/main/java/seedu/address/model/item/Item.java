b44d6583873926bee01b4995960eda4f0fe8e5ae
justacasul <zhengdl98@gmail.com>
2020-09-19 21:52:36 +0800

    Add Item class

---
+++ src/main/java/seedu/address/model/item/Item.java
@@ -0,0 +1,154 @@
+package seedu.address.model.item;
+
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+import seedu.address.model.tag.Tag;
+
+/**
+ * Represents a Person in the address book.
+ * Guarantees: details are present and not null, field values are validated, immutable.
+ */
+public class Item {
+
+    //item:
+    //int id
+    //string name
+    //int quantity
+    //string description
+    //list<integer> locationId
+    //list<integer> recipe ids that create this item
+    //boolean isDeleted
+
+
+    // Identity fields
+    private final int id;
+    private final String name;
+    private final int quantity;
+
+    // Data fields
+    private final String description;
+    private final List<Integer> locationId;
+    private final List<Integer> recipeId;
+    private final Set<Tag> tags = new HashSet<>();
+
+    /**
+     * Every field must be present and not null.
+     */
+    public Item(int id, String name, int quantity, String description, List<Integer> locationId,
+                List<Integer> recipeId, Set<Tag> tags) {
+        requireAllNonNull(id, name, quantity, description, locationId, recipeId, tags);
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.description = description;
+        this.locationId = locationId;
+        this.recipeId = recipeId;
+        this.tags.addAll(tags);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public List<Integer> getLocationId() {
+        return locationId;
+    }
+
+    public List<Integer> getRecipeId() {
+        return recipeId;
+    }
+
+    /**
+     * Returns an immutable tag set, which throws {@code UnsupportedOperationException}
+     * if modification is attempted.
+     */
+    public Set<Tag> getTags() {
+        return Collections.unmodifiableSet(tags);
+    }
+
+    /**
+     * Returns true if both persons of the same name have at least one other identity field that is the same.
+     * This defines a weaker notion of equality between two persons.
+     */
+    public boolean isSameItem(Item otherItem) {
+        if (otherItem == this) {
+            return true;
+        }
+
+        return otherItem != null
+                && otherItem.getId() == getId()
+                && otherItem.getName().equals(getName())
+                && otherItem.getDescription().equals(getDescription())
+                && (otherItem.getLocationId().equals(getLocationId()))
+                && (otherItem.getRecipeId().equals(getRecipeId()));
+    }
+
+    /**
+     * Returns true if both persons have the same identity and data fields.
+     * This defines a stronger notion of equality between two persons.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof Item)) {
+            return false;
+        }
+
+        Item otherItem = (Item) other;
+        return otherItem.getId() == getId()
+                && otherItem.getName().equals(getName())
+                && otherItem.getQuantity() == (getQuantity())
+                && otherItem.getDescription().equals(getDescription())
+                && otherItem.getLocationId().equals(getLocationId())
+                && otherItem.getRecipeId().equals(getRecipeId())
+                && otherItem.getTags().equals(getTags());
+    }
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(id, name, quantity, description, locationId, recipeId, tags);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append("Id:")
+                .append(getId())
+                .append(" Name: ")
+                .append(getName())
+                .append(" Quantity: ")
+                .append(getQuantity())
+                .append(" Description: ")
+                .append(getDescription())
+                .append(" Location Ids: ");
+        getLocationId().forEach(builder::append);
+        builder.append(" Recipe Ids: ");
+        getRecipeId().forEach(builder::append);
+        builder.append(" Tags: ");
+        getTags().forEach(builder::append);
+        return builder.toString();
+    }
+
+}
