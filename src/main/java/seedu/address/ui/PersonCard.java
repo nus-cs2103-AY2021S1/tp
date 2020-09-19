50636b02f9e18df10bf09ff9393bb80e44630952
blackonyyx <35429757+blackonyyx@users.noreply.github.com>
2020-09-19 20:16:46 +0800

    tutorial for remark

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -37,6 +37,8 @@ public class PersonCard extends UiPart<Region> {
     @FXML
     private Label address;
     @FXML
+    private Label remark;
+    @FXML
     private Label email;
     @FXML
     private FlowPane tags;
@@ -51,12 +53,13 @@ public class PersonCard extends UiPart<Region> {
         name.setText(person.getName().fullName);
         phone.setText(person.getPhone().value);
         address.setText(person.getAddress().value);
+        remark.setText(person.getRemark().value);
         email.setText(person.getEmail().value);
         person.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
                 .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
     }
-
+    
     @Override
     public boolean equals(Object other) {
         // short circuit if same object
