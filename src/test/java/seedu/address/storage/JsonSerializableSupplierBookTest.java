955170de90249eada8cf0c770cf23f467fe6dbe6
Choo Xing Yu <chooxingyu@hotmail.com>
2020-09-20 10:09:29 +0800

    Update test to remove unnecessary tests

--- src/test/java/seedu/address/storage/JsonSerializableSupplierBookTest.java
+++ src/test/java/seedu/address/storage/JsonSerializableSupplierBookTest.java
@@ -26,7 +26,6 @@ public class JsonSerializableSupplierBookTest {
         AddressBook typicalPersonsAddressBook = TypicalItems.getTypicalAddressBook();
         assertEquals(addressBookFromFile, typicalPersonsAddressBook);
     }
-     */
 
     @Test
     public void toModelType_invalidItemFile_throwsIllegalValueException() throws Exception {
@@ -35,7 +34,7 @@ public class JsonSerializableSupplierBookTest {
         assertThrows(IllegalValueException.class, dataFromFile::toModelType);
     }
 
-    /*
+
     @Test
     public void toModelType_duplicatePersons_throwsIllegalValueException() throws Exception {
         JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(DUPLICATE_PERSON_FILE,
