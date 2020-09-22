aae5594f2933f3cf34c12856e293cf5a38d4d4a4
kimberly ong <e0424807@u.nus.edu>
2020-09-22 13:01:45 +0800

    Rename getFilteredPersonList to getFilteredFlashcardList

--- src/main/java/seedu/address/logic/Logic.java
+++ src/main/java/seedu/address/logic/Logic.java
@@ -30,8 +30,8 @@ public interface Logic {
      */
     ReadOnlyAddressBook getAddressBook();
 
-    /** Returns an unmodifiable view of the filtered list of persons */
-    ObservableList<Flashcard> getFilteredPersonList();
+    /** Returns an unmodifiable view of the filtered list of flashcards */
+    ObservableList<Flashcard> getFilteredFlashcardList();
 
     /**
      * Returns the user prefs' address book file path.
