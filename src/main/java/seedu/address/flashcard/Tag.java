32b2c8459e3f0b3c49cea925f9140e83aaf2e030
joshtyf <tan.yin.feng@hotmail.com>
2020-09-22 23:04:16 +0800

    Add Tag.java

---
+++ src/main/java/seedu/address/flashcard/Tag.java
@@ -0,0 +1,16 @@
+package seedu.address.flashcard;
+
+/**
+ * Represents a tag.
+ */
+public class Tag {
+    final private String name;
+
+    public Tag(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
