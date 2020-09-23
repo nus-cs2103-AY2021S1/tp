dbe04bbd231d36c3b41f15569277ce4ea2c88453
Joven-Heng <62068282+Joven-Heng@users.noreply.github.com>
2020-09-23 13:50:29 +0800

    Fix CLI errors

--- src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
@@ -113,8 +113,8 @@ public class AddCommandParserTest {
                 + INVALID_TAG_DESC + VALID_TAG_FRIEND, Tag.MESSAGE_CONSTRAINTS);
 
         // two invalid values, only first invalid value reported
-        assertParseFailure(parser, INVALID_NAME_DESC + PHONE_DESC_BOB + EMAIL_DESC_BOB
-                , Name.MESSAGE_CONSTRAINTS);
+        assertParseFailure(parser, INVALID_NAME_DESC + PHONE_DESC_BOB + EMAIL_DESC_BOB,
+                Name.MESSAGE_CONSTRAINTS);
 
         // non-empty preamble
         assertParseFailure(parser, PREAMBLE_NON_EMPTY + NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB
