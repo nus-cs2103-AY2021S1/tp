6fe3606f1caec049784d1954bbb6a9632fe638ac
GeNiaaz <geniaaz@gmail.com>
2020-09-23 19:44:35 +0800

    Add Teammate class and associated classes. Remove Temp when class names finalised

---
+++ src/main/java/seedu/address/model/Teammate/exceptions/TeammateNotFoundException.java
@@ -0,0 +1,6 @@
+package seedu.address.model.Teammate.exceptions;
+
+/**
+ * Signals that the operation is unable to find the specified project.
+ */
+public class TeammateNotFoundException extends RuntimeException {}
