f0d53210b9594e75e7b5a8b42cb2b0f0d5b569fb
ChooJiaXin <e0426255@u.nus.edu>
2020-09-25 00:11:12 +0800

    Add JavaDocs and resolve checkstyle violations

--- src/main/java/seedu/address/model/person/Remind.java
+++ src/main/java/seedu/address/model/person/Remind.java
@@ -1,18 +1,35 @@
 package seedu.address.model.person;
 
+/**
+ * Represents a remind tag of an assignment.
+ * Guarantees: immutable; assignment is reminded as declared in {@link #isReminded()}
+ */
 public class Remind {
     private boolean isReminded;
 
+    /**
+     * Constructs a {@code Remind}.
+     */
     public Remind() {
         this.isReminded = false;
     }
 
+    /**
+     * Sets remind tag to true.
+     *
+     * @return Remind object with remind tag set to true
+     */
     public Remind setReminder() {
         Remind reminded = new Remind();
         reminded.isReminded = true;
         return reminded;
     }
 
+    /**
+     * Returns true if remind tag is set to true. Otherwise, returns false.
+     *
+     * @return true if remind tag is set to true. Otherwise, returns false
+     */
     public boolean isReminded() {
         return isReminded;
     }
--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -41,8 +41,6 @@ public class TypicalPersons {
             .withEmail("lydia@example.com").withAddress("little tokyo").build();
     public static final Person GEORGE = new PersonBuilder().withName("George Best").withPhone("9482442")
             .withEmail("anna@example.com").withAddress("4th street").build();
-    public static final Person HILARY = new PersonBuilder().withName("Hilary CLinton").withPhone("9445442")
-            .withEmail("hc@example.com").withAddress("East Coast Park").withRemindersSet().build();
 
     // Manually added
     public static final Person HOON = new PersonBuilder().withName("Hoon Meier").withPhone("8482424")
@@ -56,6 +54,10 @@ public class TypicalPersons {
     public static final Person BOB = new PersonBuilder().withName(VALID_NAME_BOB).withPhone(VALID_PHONE_BOB)
             .withEmail(VALID_EMAIL_BOB).withAddress(VALID_ADDRESS_BOB).withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND)
             .build();
+
+    // Manually added
+    public static final Person HILARY = new PersonBuilder().withName("Hilary CLinton").withPhone("9445442")
+            .withEmail("hc@example.com").withAddress("East Coast Park").withRemindersSet().build();
 
     public static final String KEYWORD_MATCHING_MEIER = "Meier"; // A keyword that matches MEIER
 
@@ -73,6 +75,6 @@ public class TypicalPersons {
     }
 
     public static List<Person> getTypicalPersons() {
-        return new ArrayList<>(Arrays.asList(ALICE, BENSON, CARL, DANIEL, ELLE, FIONA, GEORGE, HILARY));
+        return new ArrayList<>(Arrays.asList(ALICE, BENSON, CARL, DANIEL, ELLE, FIONA, GEORGE));
     }
 }
