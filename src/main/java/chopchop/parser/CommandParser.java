cc211307c6d209e4802e2d51b40fe1323a611015
zhiayang <zhiayang@nascentnoodle.com>
2020-09-24 23:40:30 +0800

    Extract named argument parsing into its own method

--- src/main/java/chopchop/parser/CommandParser.java
+++ src/main/java/chopchop/parser/CommandParser.java
@@ -7,84 +7,96 @@ import java.util.List;
 import java.util.HashMap;
 import java.util.Optional;
 import java.util.ArrayList;
-import java.util.stream.Collectors;
 
 import chopchop.util.StringView;
 
 public class CommandParser {
 
-	private Map<String, List<String>> parseNamedArguments(StringView input) {
+    private Map<String, List<String>> parseNamedArguments(StringView input) {
 
-		var ret = new HashMap<String, List<String>>();
-		while (input.size() > 0) {
+        var ret = new HashMap<String, List<String>>();
+        while (input.size() > 0) {
 
-			if (input.find('/') != 0) {
-				break;
-			}
+            if (input.find('/') != 0) {
+                break;
+            }
 
-			var currentArg = input.drop(1).bisect('/', input);
+            var currentArg = input.drop(1).bisect('/', input);
 
-			{
-				var argName = new StringView("");
-				var argValue = new StringView("");
+            {
+                var argName = new StringView("");
+                var argValue = new StringView("");
 
-				currentArg.bisect(argName, ' ', argValue);
-				var list = new ArrayList<String>();
-				list.add(argValue.toString().strip());
+                currentArg.bisect(argName, ' ', argValue);
+                if (argName.isEmpty()) {
+                    // return Optional.empty();
 
-				ret.merge(argName.toString().strip(), list, (oldVal, newVal) -> {
-					oldVal.addAll(newVal);
-					return oldVal;
-				});
-			}
+                }
 
-			if (input.isEmpty()) {
-				break;
-			}
+                var list = new ArrayList<String>();
+                list.add(argValue.toString().strip());
 
-			input = input.undrop(1);
-		}
+                ret.merge(argName.toString().strip(), list, (oldVal, newVal) -> {
+                    oldVal.addAll(newVal);
+                    return oldVal;
+                });
+            }
 
-		return ret;
-	}
+            if (input.isEmpty()) {
+                break;
+            }
 
-	public Optional<CommandArguments> parse(String input) {
+            input = input.undrop(1);
+        }
 
-		var sv = new StringView(input);
+        return ret;
+    }
 
-		var x = new StringView("");
-		var xs = new StringView("");
+    /**
+     * Parse an input string into its constituent components, including the name of the command, its target,
+     * and its arguments. See the documentation for {@link chopchop.parser.CommandArguments} for the recognised
+     * components of a given input string.
+     *
+     * @param input the input string to parse
+     * @return      the parsed components, iff parsing succeeded; an empty optional otherwise.
+     */
+    public Optional<CommandArguments> parse(String input) {
 
-		sv.bisect(x, ' ', xs);
+        var sv = new StringView(input);
 
-		/*
-			add recipe NAME [/ingredient INGREDIENT [/qty QTY1]...]... [/step STEP]...
-			delete recipe NAME
-			find recipe KEYWORDS [MORE_KEYWORDS]
-			list recipes
+        var x = new StringView("");
+        var xs = new StringView("");
 
-			add ingredient NAME [/qty QUANTITY] [/expiry DATE]
-			delete ingredient NAME [/qty QUANTITY]
-			find ingredient KEYWORDS [MORE_KEYWORDS]
-			list ingredients
-		*/
+        sv.bisect(x, ' ', xs);
 
-		var command = x.toString().strip();
-		xs.bisect(x, ' ', xs);
+        /*
+            add recipe NAME [/ingredient INGREDIENT [/qty QTY1]...]... [/step STEP]...
+            delete recipe NAME
+            find recipe KEYWORDS [MORE_KEYWORDS]
+            list recipes
 
-		var target = Optional.of(x)
-			.filter(s -> !s.isEmpty())
-			.map(StringView::toString)
-			.map(String::strip)
-			.orElse(null);
+            add ingredient NAME [/qty QUANTITY] [/expiry DATE]
+            delete ingredient NAME [/qty QUANTITY]
+            find ingredient KEYWORDS [MORE_KEYWORDS]
+            list ingredients
+        */
 
-		xs.bisect(x, '/', xs);
-		var theRest = x.toString().strip();
+        var command = x.toString().strip();
+        xs.bisect(x, ' ', xs);
 
-		xs = xs.undrop(1);
-		assert xs.at(0) == '/';
+        var target = Optional.of(x)
+            .filter(s -> !s.isEmpty())
+            .map(StringView::toString)
+            .map(String::strip)
+            .orElse(null);
 
-		var namedArgs = this.parseNamedArguments(xs);
-		return Optional.of(new CommandArguments(command, target, theRest, namedArgs));
-	}
+        xs.bisect(x, '/', xs);
+        var theRest = x.toString().strip();
+
+        xs = xs.undrop(1);
+        assert xs.at(0) == '/';
+
+        var namedArgs = this.parseNamedArguments(xs);
+        return Optional.of(new CommandArguments(command, target, theRest, namedArgs));
+    }
 }
