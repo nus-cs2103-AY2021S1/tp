d8585ca87a6b82f21c38283f0580be7471099d09
lll-jy <E0376997@u.nus.edu>
2020-09-26 20:04:01 +0800

    Adapt TaskTest.java for real Task class

--- src/test/java/seedu/address/model/task/TaskTest.java
+++ src/test/java/seedu/address/model/task/TaskTest.java
@@ -1,26 +1,15 @@
 package seedu.address.model.task;
 
 import org.junit.jupiter.api.Test;
-import seedu.address.model.tag.Tag;
 import static seedu.address.testutil.Assert.assertThrows;
 
 public class TaskTest {
 
     @Test
     public void constructor_null_throwsNullPointerException() {
-        assertThrows(NullPointerException.class, () -> new Tag(null));
+        assertThrows(NullPointerException.class, () -> new Task(null, null, null, 0, false));
     }
 
-    @Test
-    public void constructor_invalidTagName_throwsIllegalArgumentException() {
-        String invalidTagName = "";
-        assertThrows(IllegalArgumentException.class, () -> new Tag(invalidTagName));
-    }
-
-    @Test
-    public void isValidTagName() {
-        // null tag name
-        assertThrows(NullPointerException.class, () -> Tag.isValidTagName(null));
-    }
+    // TODO: invalid test
 
 }
