80c0153de44050b59c8f60a5483990542bdfd788
lll-jy <E0376997@u.nus.edu>
2020-09-26 16:13:17 +0800

    Change the Task constructor to most basic

--- src/main/java/seedu/address/model/task/Task.java
+++ src/main/java/seedu/address/model/task/Task.java
@@ -3,36 +3,24 @@ package seedu.address.model.task;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+/**
+ * Represents a Task of a project.
+ * Guarantees: details are present and not null, field values are validated, immutable.
+ */
 public class Task {
-    private String name;
-    private String description;
-    private LocalDate publishDate;
-    private LocalDateTime deadline;
-    private double progress;
+    private final String name;
+    private final String description;
+    private final LocalDate publishDate;
+    private final LocalDateTime deadline;
+    private final double progress;
+    private final boolean isDone;
 
-    public Task(String name, String description) {
-        initialize(name, description);
-    }
-
-    public Task(String name, String description, LocalDateTime deadline) {
-        initialize(name, description);
-        setDeadline(deadline);
-    }
-
-    public Task(String name, String description, LocalDate deadline) {
-        initialize(name, description);
-        setDeadline(deadline.atTime(23, 59));
-    }
-
-    private void initialize(String name, String description) {
+    public Task(String name, String description, LocalDateTime deadline, double progress, boolean isDone) {
         this.name = name;
         this.description = description;
         publishDate = LocalDate.now();
-        deadline = null;
-        progress = 0;
-    }
-
-    private void setDeadline(LocalDateTime deadline) {
         this.deadline = deadline;
+        this.progress = progress;
+        this.isDone = isDone;
     }
 }
