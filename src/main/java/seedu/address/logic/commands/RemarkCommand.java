8633d592eadd0c6b1cdcaff0b6883a7a4e745389
Jerryl Chong <jerrylchong@gmail.com>
2020-09-27 00:38:37 +0800

    Revert "Add Remark class"
    This reverts commit 6b685253b940d1ca85c55a1bdd20f223957d815f.

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -3,7 +3,6 @@ package seedu.address.logic.commands;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.person.Remark;
 
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
@@ -24,13 +23,13 @@ public class RemarkCommand extends Command {
     public static final String MESSAGE_ARGUMENTS = "Index: %1$d, Remark: %2$s";
 
     private final Index index;
-    private final Remark remark;
+    private final String remark;
 
     /**
      * @param index of the person in the filtered person list to edit the remark
      * @param remark of the person to be updated to
      */
-    public RemarkCommand(Index index, Remark remark) {
+    public RemarkCommand(Index index, String remark) {
         requireAllNonNull(index, remark);
 
         this.index = index;
