cd90aeaa58c422b85ca16366dc9b0d2a869eefb9
lll-jy <E0376997@u.nus.edu>
2020-09-26 19:31:03 +0800

    Include task field in parse_allFieldsPresent_sucess of AddCommandParserTest

--- src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
@@ -18,12 +18,16 @@ import static seedu.address.logic.commands.CommandTestUtil.PREAMBLE_NON_EMPTY;
 import static seedu.address.logic.commands.CommandTestUtil.PREAMBLE_WHITESPACE;
 import static seedu.address.logic.commands.CommandTestUtil.TAG_DESC_FRIEND;
 import static seedu.address.logic.commands.CommandTestUtil.TAG_DESC_HUSBAND;
+import static seedu.address.logic.commands.CommandTestUtil.TASK_DESC_DG;
+import static seedu.address.logic.commands.CommandTestUtil.TASK_DESC_MODEL;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TASK_DG;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TASK_MODEL;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.testutil.TypicalProjects.AMY;
@@ -45,33 +49,38 @@ public class AddCommandParserTest {
 
     @Test
     public void parse_allFieldsPresent_success() {
-        Project expectedProject = new ProjectBuilder(BOB).withTags(VALID_TAG_FRIEND).build();
+        Project expectedProject = new ProjectBuilder(BOB).withTags(VALID_TAG_FRIEND).withTasks(VALID_TASK_DG).build();
 
         // whitespace only preamble
         assertParseSuccess(parser, PREAMBLE_WHITESPACE + NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB
-                + ADDRESS_DESC_BOB + TAG_DESC_FRIEND, new AddCommand(expectedProject));
+                + ADDRESS_DESC_BOB + TAG_DESC_FRIEND + TASK_DESC_DG, new AddCommand(expectedProject));
 
         // multiple names - last name accepted
         assertParseSuccess(parser, NAME_DESC_AMY + NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB
-                + ADDRESS_DESC_BOB + TAG_DESC_FRIEND, new AddCommand(expectedProject));
+                + ADDRESS_DESC_BOB + TAG_DESC_FRIEND + TASK_DESC_DG, new AddCommand(expectedProject));
 
         // multiple phones - last phone accepted
         assertParseSuccess(parser, NAME_DESC_BOB + PHONE_DESC_AMY + PHONE_DESC_BOB + EMAIL_DESC_BOB
-                + ADDRESS_DESC_BOB + TAG_DESC_FRIEND, new AddCommand(expectedProject));
+                + ADDRESS_DESC_BOB + TAG_DESC_FRIEND + TASK_DESC_DG, new AddCommand(expectedProject));
 
         // multiple emails - last email accepted
         assertParseSuccess(parser, NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_AMY + EMAIL_DESC_BOB
-                + ADDRESS_DESC_BOB + TAG_DESC_FRIEND, new AddCommand(expectedProject));
+                + ADDRESS_DESC_BOB + TAG_DESC_FRIEND + TASK_DESC_DG, new AddCommand(expectedProject));
 
         // multiple addresses - last address accepted
         assertParseSuccess(parser, NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB + ADDRESS_DESC_AMY
-                + ADDRESS_DESC_BOB + TAG_DESC_FRIEND, new AddCommand(expectedProject));
+                + ADDRESS_DESC_BOB + TAG_DESC_FRIEND + TASK_DESC_DG, new AddCommand(expectedProject));
 
         // multiple tags - all accepted
         Project expectedProjectMultipleTags = new ProjectBuilder(BOB).withTags(VALID_TAG_FRIEND, VALID_TAG_HUSBAND)
                 .build();
         assertParseSuccess(parser, NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB + ADDRESS_DESC_BOB
-                + TAG_DESC_HUSBAND + TAG_DESC_FRIEND, new AddCommand(expectedProjectMultipleTags));
+                + TAG_DESC_HUSBAND + TAG_DESC_FRIEND + TASK_DESC_DG, new AddCommand(expectedProjectMultipleTags));
+
+        Project expectedProjectMultipleTasks = new ProjectBuilder(BOB).withTasks(VALID_TASK_DG, VALID_TASK_MODEL)
+                .build();
+        assertParseSuccess(parser, NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB + ADDRESS_DESC_BOB
+                + TAG_DESC_FRIEND + TASK_DESC_DG + TASK_DESC_MODEL, new AddCommand(expectedProjectMultipleTasks));
     }
 
     @Test
