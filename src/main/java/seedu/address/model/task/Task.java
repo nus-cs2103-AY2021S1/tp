1a00d1cd59762f8ed4f7ad123d53a5bde574a5a8
lll-jy <E0376997@u.nus.edu>
2020-09-26 15:51:00 +0800

    Add all fields of Task

--- src/main/java/seedu/address/model/task/Task.java
+++ src/main/java/seedu/address/model/task/Task.java
@@ -1,4 +1,16 @@
 package seedu.address.model.task;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Task {
+    private String name;
+    private String description;
+    private LocalDate publishDate;
+    private LocalDate startDate;
+    private LocalDateTime deadline;
+    private LocalDate lastDoneOn;
+    private int frequency;
+    private LocalDate endDate;
+    private double progress;
 }
