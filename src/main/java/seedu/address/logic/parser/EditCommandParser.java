b654be54f23a5bcb3e59a331e3dc7040f57972ab
lll-jy <E0376997@u.nus.edu>
2020-09-26 18:09:33 +0800

    Create parseTasks method for edit in main catalogue

--- src/main/java/seedu/address/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditCommandParser.java
@@ -16,8 +16,10 @@ import java.util.Set;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.EditCommand.EditProjectDescriptor;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TASK;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.tag.Tag;
+import seedu.address.model.task.Task;
 
 /**
  * Parses input arguments and creates a new EditCommand object
@@ -32,7 +34,8 @@ public class EditCommandParser implements Parser<EditCommand> {
     public EditCommand parse(String args) throws ParseException {
         requireNonNull(args);
         ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG);
+                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL,
+                        PREFIX_ADDRESS, PREFIX_TAG, PREFIX_TASK);
 
         Index index;
 
@@ -79,4 +82,18 @@ public class EditCommandParser implements Parser<EditCommand> {
         return Optional.of(ParserUtil.parseTags(tagSet));
     }
 
+    /**
+     * Parses {@code Collection<String> tasks} into a {@code Set<Task>} if {@code tasks} is non-empty.
+     * If {@code tasks} contain only one element which is an empty string, it will be parsed into a
+     * {@code Set<Task>} containing zero tasks.
+     */
+    private Optional<Set<Task>> parseTasksForEdit(Collection<String> tasks) {
+        assert tasks != null;
+
+        if (tasks.isEmpty()) {
+            return Optional.empty();
+        }
+        Collection<String> taskSet = tasks.size() == 1 && tasks.contains("") ? Collections.emptySet() : tasks;
+        return Optional.of(ParserUtil.parseTasks(taskSet));
+    }
 }
