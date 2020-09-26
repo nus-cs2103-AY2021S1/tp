ca825da9aebf933ffb9fc09b812a50218feae5d7
lll-jy <E0376997@u.nus.edu>
2020-09-26 15:30:57 +0800

    Delete teammate package

--- src/main/java/seedu/address/model/teammate/exceptions/TeammateNotFoundException.java
+++
@@ -1,6 +0,0 @@
-package seedu.address.model.teammate.exceptions;
-
-/**
- * Signals that the operation is unable to find the specified project.
- */
-public class TeammateNotFoundException extends RuntimeException {}
