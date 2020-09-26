1d74ba61a20a92d620fc01cb6c6733b064cb3ca7
lll-jy <E0376997@u.nus.edu>
2020-09-26 18:20:19 +0800

    Delete unused import of HashSet in AddCommandParser

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -8,7 +8,6 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Stream;
 
