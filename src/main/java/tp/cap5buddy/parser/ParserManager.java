0280304da36fc13b9257db677119ac973a6e8b16
MatthiasLHK <matthiasli1998@hotmail.com>
2020-09-27 17:02:27 +0800

    Add functions to split user input into command and info

--- src/main/java/tp/cap5buddy/parser/ParserManager.java
+++ src/main/java/tp/cap5buddy/parser/ParserManager.java
@@ -5,18 +5,22 @@ import java.util.Scanner;
 public class ParserManager {
     private String current_Input;
     private String command;
+    private String nonCommand;
+    private int count;
     public ParserManager() {
         this.current_Input = null;
         this.command = null;
+        this.nonCommand = null;
     }
 
     public String parse(String input) {
         this.current_Input = input;
         getCommand();
-        return this.command;
+        getNonCommand();
+        return this.command + " Non: " + this.nonCommand;
     }
 
-    public void getCommand() {
+    private void getCommand() {
         String command = "";
         Scanner sc = new Scanner(this.current_Input);
         while (sc.hasNext()) {
@@ -24,9 +28,25 @@ public class ParserManager {
             if (Prefix.isPrefix(now)) {
                 break;
             } else {
+                this.count++;
                 command += now;
             }
         }
         this.command = command;
+    }
+
+    private void getNonCommand() {
+        String nonCommand = "";
+        String[] input = this.current_Input.split(" ");
+        int limit = input.length;
+        for(int i = this.count; i < limit; i++) {
+            if (i == this.count) {
+                nonCommand += input[i].substring(2) + " ";
+            } else {
+                nonCommand += input[i] + " ";
+            }
+        }
+        this.nonCommand = nonCommand;
+        this.count = 0; // to reset the counter
     }
 }
