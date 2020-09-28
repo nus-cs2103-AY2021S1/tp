9791251a757cee75dd0d9ec89f48e3af8c7f5931
MatthiasLHK <matthiasli1998@hotmail.com>
2020-09-28 11:22:59 +0800

    Add JavaDocs

--- src/main/java/tp/cap5buddy/parser/ParserManager.java
+++ src/main/java/tp/cap5buddy/parser/ParserManager.java
@@ -1,13 +1,25 @@
 package tp.cap5buddy.parser;
 
+/**
+ * Represents the manager that handles all parser related actions and requests.
+ */
 public class ParserManager {
-    private String current_Input;
+    private String currentInput;
+
+    /**
+     * Represents the constructor that creates the manager object.
+     */
     public ParserManager() {
-        this.current_Input = null;
+        this.currentInput = null;
     }
 
+    /**
+     * Returns the String of the result after parsing the input.
+     * @param input user input.
+     * @return String result message.
+     */
     public String parse(String input) {
-        this.current_Input = input;
-        return this.current_Input;
+        this.currentInput = input;
+        return this.currentInput;
     }
 }
