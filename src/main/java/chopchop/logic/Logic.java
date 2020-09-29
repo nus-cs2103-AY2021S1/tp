808924a4ef107a96c2ce0e743888c4edde607916
Trav1sT <travtoh@gmail.com>
2020-09-29 20:38:30 +0800

    Create chopchop package Essentially a copy of seedu.address package

---
+++ src/main/java/chopchop/logic/Logic.java
@@ -0,0 +1,50 @@
+package chopchop.logic;
+
+import java.nio.file.Path;
+
+import javafx.collections.ObservableList;
+import chopchop.commons.core.GuiSettings;
+import chopchop.logic.commands.CommandResult;
+import chopchop.logic.commands.exceptions.CommandException;
+import chopchop.logic.parser.exceptions.ParseException;
+import chopchop.model.ReadOnlyAddressBook;
+import chopchop.model.person.Person;
+
+/**
+ * API of the Logic component
+ */
+public interface Logic {
+    /**
+     * Executes the command and returns the result.
+     * @param commandText The command as entered by the user.
+     * @return the result of the command execution.
+     * @throws CommandException If an error occurs during command execution.
+     * @throws ParseException If an error occurs during parsing.
+     */
+    CommandResult execute(String commandText) throws CommandException, ParseException;
+
+    /**
+     * Returns the AddressBook.
+     *
+     * @see chopchop.model.Model#getAddressBook()
+     */
+    ReadOnlyAddressBook getAddressBook();
+
+    /** Returns an unmodifiable view of the filtered list of persons */
+    ObservableList<Person> getFilteredPersonList();
+
+    /**
+     * Returns the user prefs' address book file path.
+     */
+    Path getAddressBookFilePath();
+
+    /**
+     * Returns the user prefs' GUI settings.
+     */
+    GuiSettings getGuiSettings();
+
+    /**
+     * Set the user prefs' GUI settings.
+     */
+    void setGuiSettings(GuiSettings guiSettings);
+}
---
+++ src/main/java/chopchop/logic/parser/exceptions/ParseException.java
@@ -0,0 +1,17 @@
+package chopchop.logic.parser.exceptions;
+
+import chopchop.commons.exceptions.IllegalValueException;
+
+/**
+ * Represents a parse error encountered by a parser.
+ */
+public class ParseException extends IllegalValueException {
+
+    public ParseException(String message) {
+        super(message);
+    }
+
+    public ParseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
