9ffd922aa22f225abca92d242537dddebc0f7a8f
joshtyf <tan.yin.feng@hotmail.com>
2020-09-30 21:55:08 +0800

    Add default Tag

--- src/test/java/seedu/address/testutil/FlashcardBuilder.java
+++ src/test/java/seedu/address/testutil/FlashcardBuilder.java
@@ -1,5 +1,6 @@
 package seedu.address.testutil;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -16,6 +17,7 @@ public class FlashcardBuilder {
 
     public static final String DEFAULT_QUESTION = "Question 0";
     public static final String DEFAULT_ANSWER = "Answer 0";
+    public static final Tag DEFAULT_TAG = new Tag("Tag 0");
 
     private Question question;
     private Answer answer;
@@ -27,7 +29,7 @@ public class FlashcardBuilder {
     public FlashcardBuilder() {
         question = new OpenEndedQuestion(DEFAULT_QUESTION);
         answer = new Answer(DEFAULT_ANSWER);
-        tags = new HashSet<>();
+        tags = new HashSet<>(Collections.singletonList(DEFAULT_TAG));
     }
 
     /**
