c949d03c3f4246f572b30bcff54856fe3282312c
joshtyf <tan.yin.feng@hotmail.com>
2020-09-30 19:12:36 +0800

    Add FindCommand case
    FindCommand case was previously commented out.

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -15,6 +15,7 @@ import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.ExitCommand;
 //import seedu.address.logic.commands.FindCommand;
+import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 // import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.commands.ListCommand;
@@ -64,8 +65,8 @@ public class AddressBookParser {
         case ClearCommand.COMMAND_WORD:
             return new ClearCommand();
 
-        // case FindCommand.COMMAND_WORD:
-        //    return new FindCommandParser().parse(arguments);
+        case FindCommand.COMMAND_WORD:
+            return new FindCommandParser().parse(arguments);
 
         case ListCommand.COMMAND_WORD:
             return new ListCommand();
