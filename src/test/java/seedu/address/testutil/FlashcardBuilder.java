bdbb015f8230ef6d4bf9f7bfc0329c61f32254e9
joshtyf <tan.yin.feng@hotmail.com>
2020-09-30 22:43:36 +0800

    Remove dead import statements

--- src/test/java/seedu/address/testutil/FlashcardBuilder.java
+++ src/test/java/seedu/address/testutil/FlashcardBuilder.java
@@ -1,8 +1,6 @@
 package seedu.address.testutil;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
