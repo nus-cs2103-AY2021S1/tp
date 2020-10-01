5d7fb43077a8d638138daea18eaf2324162fcb72
Chen Xihao <chenxihao0627@gmail.com>
2020-10-02 00:07:42 +0800

    Update code styling

--- src/test/java/nustorage/testutil/TypicalInventoryRecords.java
+++ src/test/java/nustorage/testutil/TypicalInventoryRecords.java
@@ -17,8 +17,11 @@ import nustorage.model.record.InventoryRecord;
 
 public class TypicalInventoryRecords {
 
-    public static final InventoryRecord INVENTORY_RECORD_A = new InventoryRecord(ITEM_NAME_1, QUANTITY_1, DATE_1, TIME_1);
-    public static final InventoryRecord INVENTORY_RECORD_B = new InventoryRecord(ITEM_NAME_2, QUANTITY_2, DATE_2, TIME_2);
-    public static final InventoryRecord INVENTORY_RECORD_C = new InventoryRecord(ITEM_NAME_3, QUANTITY_3, DATE_1, TIME_2);
+    public static final InventoryRecord INVENTORY_RECORD_A =
+            new InventoryRecord(ITEM_NAME_1, QUANTITY_1, DATE_1, TIME_1);
+    public static final InventoryRecord INVENTORY_RECORD_B =
+            new InventoryRecord(ITEM_NAME_2, QUANTITY_2, DATE_2, TIME_2);
+    public static final InventoryRecord INVENTORY_RECORD_C =
+            new InventoryRecord(ITEM_NAME_3, QUANTITY_3, DATE_1, TIME_2);
 
 }
