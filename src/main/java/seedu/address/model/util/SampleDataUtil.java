fa4e10c9ef0e8853cb49788b3b03d7c2c7ad8325
joshtyf <tan.yin.feng@hotmail.com>
2020-10-02 20:02:38 +0800

    Make SampleDataUtil use Choice class

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -5,6 +5,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import seedu.address.flashcard.Answer;
+import seedu.address.flashcard.Choice;
 import seedu.address.flashcard.Flashcard;
 import seedu.address.flashcard.MultipleChoiceQuestion;
 import seedu.address.flashcard.Tag;
@@ -16,26 +17,41 @@ import seedu.address.model.ReadOnlyQuickCache;
  */
 public class SampleDataUtil {
     public static Flashcard[] getSampleFlashcards() {
+        Choice choice1 = new Choice("Software Engineering module for NUS students");
+        Choice choice2 = new Choice("A non-compulsory module for NUS Computer Science students");
+        Choice choice3 = new Choice("A module that can be S/Ued");
+        Choice choice4 = new Choice("A module that doesn't involve a brown field project");
+        Choice choice5 = new Choice("Computer Organization module for NUS students");
+        Choice choice6 = new Choice("A non-compulsory module for NUS Computer Science students");
+        Choice choice7 = new Choice("A module that can be S/Ued");
+        Choice choice8 = new Choice("A module that doesn't involve assembly language");
+        Choice choice9 = new Choice("Introductory module to computer networks for NUS students");
+        Choice choice10 = new Choice("A non-compulsory module for NUS Computer Science students");
+        Choice choice11 = new Choice("A module that can be S/Ued");
+        Choice choice12 = new Choice("A module that doesn't involve TCP");
         return new Flashcard[] {
             new Flashcard(new MultipleChoiceQuestion("What is CS2103T?",
-                    "Software Engineering module for NUS students",
-                    "A non-compulsory module for NUS Computer Science students",
-                    "A module that can be S/Ued",
-                    "A module that doesn't involve a brown field project"),
+                    choice1,
+                    choice2,
+                    choice3,
+                    choice4
+                    ),
                     new Answer("Software Engineering module for NUS students"),
                     getTagSet("MCQ", "Good Question")),
             new Flashcard(new MultipleChoiceQuestion("What is CS2100?",
-                    "Computer Organization module for NUS students",
-                        "A non-compulsory module for NUS Computer Science students",
-                        "A module that can be S/Ued",
-                        "A module that doesn't involve assembly language"),
+                    choice5,
+                        choice6,
+                        choice7,
+                        choice8
+                        ),
                         new Answer("Computer Organization module for NUS students"),
                         getTagSet("MCQ", "Assembly")),
             new Flashcard(new MultipleChoiceQuestion("What is CS2105?",
-                        "Introductory module to computer networks for NUS students",
-                        "A non-compulsory module for NUS Computer Science students",
-                        "A module that can be S/Ued",
-                        "A module that doesn't involve TCP"),
+                        choice9,
+                        choice10,
+                        choice11,
+                        choice12
+                        ),
                         new Answer("Introductory module to computer networks for NUS students"),
                         getTagSet("MCQ", "TCPforLife"))
         };
