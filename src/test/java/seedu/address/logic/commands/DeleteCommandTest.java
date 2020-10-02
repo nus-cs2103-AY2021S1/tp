21b59f1e148a42407055788cfaf5244aef0f5c5b
minzzelo <siminnnx@gmail.com>
2020-10-02 13:44:07 +0800

    Minor semantic change

--- src/test/java/seedu/address/logic/commands/DeleteCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteCommandTest.java
@@ -68,7 +68,7 @@ public class DeleteCommandTest {
         showFlashcardAtIndex(model, INDEX_FIRST_FLASHCARD);
 
         Index outOfBoundIndex = INDEX_SECOND_FLASHCARD;
-        // ensures that outOfBoundIndex is still in bounds of address book list
+        // ensures that outOfBoundIndex is still in bounds of flashcard list
         assertTrue(outOfBoundIndex.getZeroBased() < model.getAddressBook().getFlashcardList().size());
 
         DeleteCommand deleteCommand = new DeleteCommand(outOfBoundIndex);
