8d3b3f4878176bcd26f47d2ce9ad4a3c71c05797
joshtyf <tan.yin.feng@hotmail.com>
2020-10-02 19:28:58 +0800

    Add Choice.java

---
+++ src/main/java/seedu/address/flashcard/Choice.java
@@ -0,0 +1,37 @@
+package seedu.address.flashcard;
+
+/**
+ * Represents a MCQ choice.
+ */
+public class Choice {
+    final private String content;
+
+    public Choice(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return content;
+    }
+
+    /**
+     * Compares this choice to the specified object.
+     * The result is true if and only if the argument is not null and is a
+     * Choice object that has the same contents as this object
+     * @param o the object to compare this Choice against
+     * @return {@code true} if the given object represents a Choice equivalent to this choice, false otherwise
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+        if (o instanceof Choice) {
+            Choice other = (Choice) o;
+            return content.equals(other.content);
+        } else {
+            return false;
+        }
+    }
+}
