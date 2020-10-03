644db2b71eca0268c6edf485058182642d7c8164
duckmoon99 <e0407816@u.nus.edu>
2020-10-03 22:26:33 +0800

    Add Order exceptions

---
+++ src/main/java/seedu/address/model/order/exceptions/DuplicateOrderItemException.java
@@ -0,0 +1,5 @@
+package seedu.address.model.order.exceptions;
+
+public class DuplicateOrderItemException extends RuntimeException {
+    public DuplicateOrderItemException() { super("Operation would result in duplicate order item"); }
+}
---
+++ src/main/java/seedu/address/model/order/exceptions/OrderItemNotFoundException.java
@@ -0,0 +1,7 @@
+package seedu.address.model.order.exceptions;
+
+/**
+ * Signals that the operation is unable to find the specified food.
+ */
+public class OrderItemNotFoundException {
+}
