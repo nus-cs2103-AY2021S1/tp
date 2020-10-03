3d6e220f6ba1a3e27e45a57dbabfe189c3819b86
lll-jy <E0376997@u.nus.edu>
2020-10-03 15:12:29 +0800

    Add test case for LeaveCommand

---
+++ src/test/java/seedu/address/logic/commands/LeaveCommandTest.java
@@ -0,0 +1,54 @@
+package seedu.address.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.showProjectAtIndex;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.project.Project;
+import seedu.address.model.project.Status;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PROJECT;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PROJECT;
+import static seedu.address.testutil.TypicalIndexes.INDEX_THIRD_PROJECT;
+import static seedu.address.testutil.TypicalProjects.getTypicalMainCatalogue;
+
+/**
+ * Contains integration tests (interaction with the Model, UndoCommand and RedoCommand) and unit tests for
+ * {@code StartCommand}.
+ */
+public class LeaveCommandTest {
+
+    private Model model = new ModelManager(getTypicalMainCatalogue(), new UserPrefs());
+
+    @Test
+    public void execute_inProjectScope_success() {
+        Project projectToStart = model.getFilteredProjectList().get(INDEX_FIRST_PROJECT.getZeroBased());
+        model.enter(projectToStart);
+        LeaveCommand leaveCommand = new LeaveCommand();
+
+        String expectedMessage = LeaveCommand.MESSAGE_LEAVE_SUCCESS;
+
+        ModelManager expectedModel = new ModelManager(model.getProjectCatalogue(), new UserPrefs());
+
+        assertCommandSuccess(leaveCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_inCatalogueScope_throwsCommandException() {
+        LeaveCommand leaveCommand = new LeaveCommand();
+        assertCommandFailure(leaveCommand, model, String.format(Messages.MESSAGE_INVALID_SCOPE_COMMAND,
+                Status.PROJECT, Status.CATALOGUE));
+    }
+
+    @Test
+    public void equals() {
+        assertEquals(new LeaveCommand(), new LeaveCommand());
+    }
+}
