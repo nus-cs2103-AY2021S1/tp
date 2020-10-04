762dfb304d9051db055a0a5c2e71578b66ebd318
Tan Zhuo Yao <e0406711@u.nus.edu>
2020-10-04 17:24:25 +0800

    Modify code style based on CheckstyleTest test

--- src/test/java/seedu/address/logic/commands/FilterCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FilterCommandTest.java
@@ -1,20 +1,24 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.commons.core.Messages.MESSAGE_FLASHCARDS_LISTED_OVERVIEW;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalFlashcards.FLASHCARD_1;
+import static seedu.address.testutil.TypicalFlashcards.FLASHCARD_2;
+
+import java.util.Arrays;
+import java.util.Collections;
+
 import org.junit.jupiter.api.Test;
+
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.flashcard.CategoryEqualsKeywordsPredicate;
 import seedu.address.testutil.TypicalFlashcards;
 
-import java.util.Arrays;
-import java.util.Collections;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static seedu.address.commons.core.Messages.MESSAGE_FLASHCARDS_LISTED_OVERVIEW;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.TypicalFlashcards.FLASHCARD_1;
-import static seedu.address.testutil.TypicalFlashcards.FLASHCARD_2;
 
 /**
  * Contains integration tests (interaction with the Model) for {@code FilterCommand}.
--- src/test/java/seedu/address/logic/parser/FilterCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FilterCommandParserTest.java
@@ -1,14 +1,15 @@
 package seedu.address.logic.parser;
 
-import org.junit.jupiter.api.Test;
-import seedu.address.logic.commands.FilterCommand;
-import seedu.address.model.flashcard.CategoryEqualsKeywordsPredicate;
-
-import java.util.Arrays;
-
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.FilterCommand;
+import seedu.address.model.flashcard.CategoryEqualsKeywordsPredicate;
 
 public class FilterCommandParserTest {
 
