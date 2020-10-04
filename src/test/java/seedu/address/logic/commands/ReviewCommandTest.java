32b102f9c14e74aac1b9695e63a4982d6a06ce99
kimberly ong <e0424807@u.nus.edu>
2020-10-04 15:35:30 +0800

    Update ReviewCommandTest to test for empty flashcard list

--- src/test/java/seedu/address/logic/commands/ReviewCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ReviewCommandTest.java
@@ -1,5 +1,6 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.ReviewCommand.MESSAGE_INITIATE_REVIEW_SUCCESS;
 import static seedu.address.testutil.TypicalFlashcards.getTypicalAddressBook;
@@ -18,6 +19,13 @@ public class ReviewCommandTest {
     public void execute_review_success() {
         CommandResult expectedCommandResult = new CommandResult(MESSAGE_INITIATE_REVIEW_SUCCESS, false, false, true);
         assertCommandSuccess(new ReviewCommand(), model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_emptyFlashcardList_throwsCommandException() {
+        Model model = new ModelManager();
+        ReviewCommand reviewCommand = new ReviewCommand();
+        assertCommandFailure(reviewCommand, model, ReviewCommand.MESSAGE_INITIATE_REVIEW_ERROR);
     }
 }
 
