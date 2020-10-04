028db2b93f7d0c5ee16c99f68075c9a74b02db8f
Tan Zhuo Yao <e0406711@u.nus.edu>
2020-10-04 17:08:40 +0800

    Refactor FilterCommand to support multiple category inputs using regex "c/"

--- src/main/java/seedu/address/logic/commands/FilterCommand.java
+++ src/main/java/seedu/address/logic/commands/FilterCommand.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CATEGORY;
 
 import seedu.address.commons.core.Messages;
 import seedu.address.model.Model;
@@ -16,9 +17,7 @@ public class FilterCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Filters and lists flashcards belonging to the category input by user.\n"
             + "Parameters: CATEGORY\n"
-            + "Example: " + COMMAND_WORD + " SDLC";
-
-    public static final String MESSAGE_FILTER_FLASHCARD_SUCCESS = "Filtered flashcards";
+            + "Example: " + PREFIX_CATEGORY + COMMAND_WORD + " SDLC";
 
     private final CategoryEqualsKeywordsPredicate predicate;
 
