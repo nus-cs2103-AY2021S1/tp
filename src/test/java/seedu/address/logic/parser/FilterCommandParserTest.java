e909aa3420a1f616861f515827cfc91f79cfd537
Tan Zhuo Yao <e0406711@u.nus.edu>
2020-10-04 16:32:36 +0800

    Add FilterCommandParserTest

---
+++ src/test/java/seedu/address/logic/parser/FilterCommandParserTest.java
@@ -0,0 +1,34 @@
+package seedu.address.logic.parser;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.logic.commands.FilterCommand;
+import seedu.address.model.flashcard.CategoryEqualsKeywordsPredicate;
+import seedu.address.model.flashcard.NameContainsKeywordsPredicate;
+
+import java.util.Arrays;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+public class FilterCommandParserTest {
+
+    private FilterCommandParser parser = new FilterCommandParser();
+
+    @Test
+    public void parse_emptyArg_throwsParseException() {
+        assertParseFailure(parser, "     ",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FilterCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_validArgs_returnsFilterCommand() {
+        // no leading and trailing whitespaces
+        FilterCommand expectedFilterCommand =
+                new FilterCommand(new CategoryEqualsKeywordsPredicate(Arrays.asList("SDLC", "RevisionHistory")));
+        assertParseSuccess(parser, "SDLC RevisionHistory", expectedFilterCommand);
+
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, " \n SDLC \n \t RevisionHistory  \t", expectedFilterCommand);
+    }
+}
