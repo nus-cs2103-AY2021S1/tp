244f3c121c225dc9ef2f5a9ebdf8a6f5c5681194
Christopher-LM <>
2020-10-05 22:19:27 +0800

    Fix CheckStyle

--- src/main/java/seedu/address/model/CalendarVenue.java
+++ src/main/java/seedu/address/model/CalendarVenue.java
@@ -9,8 +9,8 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
  */
 public class CalendarVenue {
 
-    public static final String MESSAGE_CONSTRAINTS = "Calendar Venue should only contain alphanumeric characters" +
-            " and spaces, and it should not be blank";
+    public static final String MESSAGE_CONSTRAINTS = "Calendar Venue should only contain alphanumeric characters"
+            + " and spaces, and it should not be blank";
 
     /*
      * The first character of the CalendarVenue must not be a whitespace,
