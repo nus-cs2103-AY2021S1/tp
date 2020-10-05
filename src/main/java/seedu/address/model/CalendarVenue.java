f7744a8a3c453eb63d3abc356e57c372de3c3c96
Christopher-LM <>
2020-10-05 22:13:44 +0800

    Fix JavaDoc for CalendarVenue

--- src/main/java/seedu/address/model/CalendarVenue.java
+++ src/main/java/seedu/address/model/CalendarVenue.java
@@ -4,7 +4,7 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
 /**
- * Represents a property's address in the property book.
+ * Represents a Calendar's venue in the Calendar book.
  * Guarantees: immutable; is valid as declared in {@link #isValidCalendarTime(String)}
  */
 public class CalendarVenue {
