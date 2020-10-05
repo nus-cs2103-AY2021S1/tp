5f32abd8a015a7f976ab4037eae1e12a177c3949
MatthiasLHK <matthiasli1998@hotmail.com>
2020-10-05 16:38:07 +0800

    Reset master

--- src/main/java/tp/cap5buddy/logic/commands/AddZoomLinkCommand.java
+++ src/main/java/tp/cap5buddy/logic/commands/AddZoomLinkCommand.java
@@ -40,7 +40,6 @@ public class AddZoomLinkCommand extends Command {
         Module updatedModule = module.addZoomLink(this.zoomLink);
         modules.updateModule(index, updatedModule);
         String successMessage = createSuccessMessage(updatedModule.getName());
-        System.out.println("link is" + updatedModule.getLink());
         return new ResultCommand(successMessage, isExit());
     }
 
