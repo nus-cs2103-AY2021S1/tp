bc46cffa3317cd90de18721471cb1d45f233be0e
Christopher-LM <>
2020-10-05 21:41:40 +0800

    Add CalendarVenue

--- src/main/java/seedu/address/model/CalendarVenue.java
+++ src/main/java/seedu/address/model/CalendarVenue.java
@@ -1,4 +1,57 @@
 package seedu.address.model;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a property's address in the property book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
+ */
 public class CalendarVenue {
+
+    public static final String MESSAGE_CONSTRAINTS = "Addresses can take any values, and it should not be blank";
+
+    /*
+     * The first character of the CalendarVenue must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
+    public static final String VALIDATION_REGEX = "[^\\s].*";
+
+    public final String value;
+
+    /**
+     * Constructs an {@code CalendarVenue}.
+     *
+     * @param venue A valid CalendarVenue.
+     */
+    public CalendarVenue(String venue) {
+        requireNonNull(venue);
+        checkArgument(isValidAddress(venue), MESSAGE_CONSTRAINTS);
+        value = venue;
+    }
+
+    /**
+     * Returns true if a given string is a valid email.
+     */
+    public static boolean isValidAddress(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof CalendarVenue // instanceof handles nulls
+                && value.equals(((CalendarVenue) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
 }
