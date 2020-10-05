c2dcb50fa33b928cf5b17471829122b54b0f8492
Ashley-Lau <ashleylau451@gmail.com>
2020-10-05 20:20:23 +0800

    Add test cases for stock except serial number

--- src/test/java/seedu/stock/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/stock/logic/commands/CommandTestUtil.java
@@ -1,8 +1,19 @@
 package seedu.stock.logic.commands;
 
-
 /**
  * Contains helper methods for testing commands.
  */
 public class CommandTestUtil {
+
+    public static final String VALID_NAME_APPLE = "Apple Juice";
+    public static final String VALID_NAME_BANANA = "Banana Bun";
+    public static final String VALID_SERIALNUMBER_APPLE = "Ntuc1";
+    public static final String VALID_SERIALNUMBER_BANANA = "Fairprice1";
+    public static final String VALID_SOURCE_APPLE = "Ntuc";
+    public static final String VALID_SOURCE_BANANA = "Fairprice";
+    public static final String VALID_QUANTITY_APPLE = "2000";
+    public static final String VALID_QUANTITY_BANANA = "1000";
+    public static final String VALID_LOCATION_APPLE = "Fruit Section, Subsection C";
+    public static final String VALID_LOCATION_BANANA = "Fruits section, Subsection B";
+
 }
