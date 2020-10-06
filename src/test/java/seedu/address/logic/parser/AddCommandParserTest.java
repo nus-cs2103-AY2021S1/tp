83e7e75fef65c6d9044d9bdb336a2cf958997705
Jerry Ho <jerryho@u.nus.edu>
2020-10-06 11:11:32 +0800

    Remove test case related to tag

--- src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
@@ -60,11 +60,6 @@ public class AddCommandParserTest {
         // multiple addresses - last address accepted
         assertParseSuccess(parser, NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB + ADDRESS_DESC_AMY
                 + ADDRESS_DESC_BOB, new AddCommand(expectedPerson));
-
-        // multiple tags - all accepted
-        Person expectedPersonMultipleTags = new PersonBuilder(BOB).build();
-        assertParseSuccess(parser, NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB + ADDRESS_DESC_BOB,
-                new AddCommand(expectedPersonMultipleTags));
     }
 
     @Test
