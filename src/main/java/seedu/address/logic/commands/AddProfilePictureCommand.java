e423eb9aa23673835f614505537b4af1c99ec44a
afroneth <knjh30@gmail.com>
2020-10-06 21:30:49 +0800

    Change checkstyle

--- src/main/java/seedu/address/logic/commands/AddProfilePictureCommand.java
+++ src/main/java/seedu/address/logic/commands/AddProfilePictureCommand.java
@@ -10,7 +10,6 @@ import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.patient.Name;
 import seedu.address.model.patient.Patient;
 import seedu.address.model.patient.ProfilePicture;
 import seedu.address.storage.StorageManager;
