7f82edfe06ed59df54efd6ec80ba1ba6b4292cc8
Junhua <wenjh1998@gmail.com>
2020-10-07 13:47:49 +0800

    Update SampleDataUtil

--- src/main/java/jimmy/mcgymmy/model/util/SampleDataUtil.java
+++ src/main/java/jimmy/mcgymmy/model/util/SampleDataUtil.java
@@ -28,15 +28,9 @@ public class SampleDataUtil {
             new Food(new Name("Fried Chicken"), new Protein(200), new Fat(100),
                     new Carbohydrate(10),
                     getTagSet("neighbours")),
-            new Food(new Name("David Li"), new Protein(132), new Fat(321),
-                    new Carbohydrate(125),
+            new Food(new Name("Nasi Lemak"), new Protein(20), new Fat(17),
+                    new Carbohydrate(31),
                     getTagSet("family")),
-            new Food(new Name("Irfan Ibrahim"), new Protein(145), new Fat(432),
-                    new Carbohydrate(125),
-                    getTagSet("classmates")),
-            new Food(new Name("Roy Balakrishnan"), new Protein(452), new Fat(254),
-                    new Carbohydrate(564),
-                    getTagSet("colleagues"))
         };
     }
 
