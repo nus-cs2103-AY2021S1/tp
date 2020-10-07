4cc2d733886625e44fad4b97d165bfc4c41359ba
Junhua <wenjh1998@gmail.com>
2020-10-08 01:08:39 +0800

    Fix Phrasing in DuplicateFoodException

--- src/main/java/jimmy/mcgymmy/model/food/exceptions/DuplicateFoodException.java
+++ src/main/java/jimmy/mcgymmy/model/food/exceptions/DuplicateFoodException.java
@@ -1,11 +1,11 @@
 package jimmy.mcgymmy.model.food.exceptions;
 
 /**
- * Signals that the operation will result in duplicate Persons (Persons are considered duplicates if they have the same
- * identity).
+ * Signals that the operation will result in duplicate Food items (Food items are considered duplicates if they have the same
+ * name and nutrition content).
  */
 public class DuplicateFoodException extends RuntimeException {
     public DuplicateFoodException() {
-        super("Operation would result in duplicate persons");
+        super("Operation would result in duplicate food items");
     }
 }
