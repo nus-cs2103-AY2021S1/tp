c0fefce5af60b6b8e15f1cd912417251cccc05d9
NiMadeNai <e0424663@u.nus.edu>
2020-10-07 16:56:42 +0800

    edit indentation

--- src/main/java/seedu/stock/ui/MainWindow.java
+++ src/main/java/seedu/stock/ui/MainWindow.java
@@ -63,8 +63,6 @@ public class MainWindow extends UiPart<Stage> {
         // Configure the UI
         setWindowDefaultSize(logic.getGuiSettings());
 
-//        setAccelerators();
-
         helpWindow = new HelpWindow();
     }
 
