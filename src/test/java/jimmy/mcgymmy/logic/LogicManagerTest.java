9cefb3352007ce6dce1eae898c9dcd9742685735
Junhua <wenjh1998@gmail.com>
2020-10-07 21:25:56 +0800

    Update addressbook to McGymmy in more classes

--- src/test/java/jimmy/mcgymmy/logic/LogicManagerTest.java
+++ src/test/java/jimmy/mcgymmy/logic/LogicManagerTest.java
@@ -39,10 +39,10 @@ public class LogicManagerTest {
 
     @BeforeEach
     public void setUp() {
-        JsonMcGymmyStorage addressBookStorage =
+        JsonMcGymmyStorage mcGymmyStorage =
                 new JsonMcGymmyStorage(temporaryFolder.resolve("mcGymmy.json"));
         JsonUserPrefsStorage userPrefsStorage = new JsonUserPrefsStorage(temporaryFolder.resolve("userPrefs.json"));
-        StorageManager storage = new StorageManager(addressBookStorage, userPrefsStorage);
+        StorageManager storage = new StorageManager(mcGymmyStorage, userPrefsStorage);
         logic = new LogicManager(model, storage);
     }
 
