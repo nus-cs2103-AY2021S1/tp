b909b99048458e9070a570fe437c2531f3a07825
AudreyFelicio <audreyanwar@gmail.com>
2020-10-08 03:25:22 +0800

    Add unfiltered list tests

--- src/test/java/seedu/stock/logic/commands/UpdateCommandTest.java
+++ src/test/java/seedu/stock/logic/commands/UpdateCommandTest.java
@@ -1,11 +1,15 @@
 package seedu.stock.logic.commands;
 
+import static seedu.stock.logic.commands.CommandTestUtil.VALID_LOCATION_APPLE;
+import static seedu.stock.logic.commands.CommandTestUtil.VALID_NAME_APPLE;
+import static seedu.stock.logic.commands.CommandTestUtil.VALID_SOURCE_APPLE;
 import static seedu.stock.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.stock.testutil.TypicalStocks.getTypicalStockBook;
 import static seedu.stock.testutil.TypicalStocks.getTypicalStocksSerialNumbers;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.stock.commons.core.index.Index;
 import seedu.stock.logic.commands.UpdateCommand.UpdateStockDescriptor;
 import seedu.stock.model.Model;
 import seedu.stock.model.ModelManager;
@@ -38,4 +42,44 @@ public class UpdateCommandTest {
 
         assertCommandSuccess(updateCommand, model, expectedMessage, expectedModel);
     }
+
+    @Test
+    public void execute_someFieldsSpecifiedUnfilteredList_success() {
+        Index indexLastPerson = Index.fromOneBased(model.getFilteredStockList().size());
+        Stock lastStock = model.getFilteredStockList().get(indexLastPerson.getZeroBased());
+
+        StockBuilder stockInList = new StockBuilder(lastStock);
+        Stock updatedStock = stockInList.withName(VALID_NAME_APPLE).withSource(VALID_SOURCE_APPLE)
+                .withLocation(VALID_LOCATION_APPLE).build();
+
+        UpdateStockDescriptor descriptor = new UpdateStockDescriptorBuilder().withName(VALID_NAME_APPLE)
+                .withSource(VALID_SOURCE_APPLE).withLocation(VALID_LOCATION_APPLE)
+                .withSerialNumber(updatedStock.getSerialNumber().toString()).build();
+        UpdateCommand editCommand = new UpdateCommand(descriptor);
+
+        String expectedMessage = String.format(UpdateCommand.MESSAGE_UPDATE_STOCK_SUCCESS, "\n" + updatedStock);
+
+        Model expectedModel = new ModelManager(new StockBook(model.getStockBook()), new UserPrefs(),
+                model.getSerialNumberSetsBook());
+        expectedModel.setStock(lastStock, updatedStock);
+
+        assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_noFieldSpecifiedUnfilteredList_success() {
+        Stock updatedStock = model.getFilteredStockList().get(0);
+        UpdateStockDescriptor serialNumberOnly = new UpdateStockDescriptorBuilder()
+                .withSerialNumber(updatedStock.getSerialNumber().toString()).build();
+        UpdateCommand updateCommand = new UpdateCommand(serialNumberOnly);
+
+        String expectedMessage = String.format(UpdateCommand.MESSAGE_UPDATE_STOCK_SUCCESS, "\n" + updatedStock);
+
+        Model expectedModel = new ModelManager(new StockBook(model.getStockBook()), new UserPrefs(),
+                model.getSerialNumberSetsBook());
+
+        assertCommandSuccess(updateCommand, model, expectedMessage, expectedModel);
+    }
+    
+    
 }
