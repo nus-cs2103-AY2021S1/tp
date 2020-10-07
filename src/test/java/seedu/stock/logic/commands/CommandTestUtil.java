5f74acf4950b2ce4110d9d6984a04f16639c4bd0
Ashley-Lau <ashleylau451@gmail.com>
2020-10-08 00:03:45 +0800

    Improve code

--- src/test/java/seedu/stock/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/stock/logic/commands/CommandTestUtil.java
@@ -1,10 +1,20 @@
 package seedu.stock.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.stock.logic.parser.CliSyntax.PREFIX_LOCATION;
 import static seedu.stock.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.stock.logic.parser.CliSyntax.PREFIX_QUANTITY;
 import static seedu.stock.logic.parser.CliSyntax.PREFIX_SERIAL_NUMBER;
 import static seedu.stock.logic.parser.CliSyntax.PREFIX_SOURCE;
+import static seedu.stock.testutil.Assert.assertThrows;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import seedu.stock.logic.commands.exceptions.CommandException;
+import seedu.stock.model.Model;
+import seedu.stock.model.StockBook;
+import seedu.stock.model.stock.Stock;
 
 /**
  * Contains helper methods for testing commands.
@@ -43,5 +53,48 @@ public class CommandTestUtil {
     public static final String PREAMBLE_WHITESPACE = "\t  \r  \n";
     public static final String PREAMBLE_NON_EMPTY = "NonEmptyPreamble";
     public static final String DEFAULT_SERIAL_NUMBER = "0";
+
+    /**
+     * Executes the given {@code command}, confirms that <br>
+     * - the returned {@link CommandResult} matches {@code expectedCommandResult} <br>
+     * - the {@code actualModel} matches {@code expectedModel}
+     */
+    public static void assertCommandSuccess(Command command, Model actualModel, CommandResult expectedCommandResult,
+                                            Model expectedModel) {
+        try {
+            CommandResult result = command.execute(actualModel);
+            assertEquals(expectedCommandResult, result);
+            assertEquals(expectedModel, actualModel);
+        } catch (CommandException ce) {
+            throw new AssertionError("Execution of command should not fail.", ce);
+        }
+    }
+
+    /**
+     * Convenience wrapper to {@link #assertCommandSuccess(Command, Model, CommandResult, Model)}
+     * that takes a string {@code expectedMessage}.
+     */
+    public static void assertCommandSuccess(Command command, Model actualModel, String expectedMessage,
+                                            Model expectedModel) {
+        CommandResult expectedCommandResult = new CommandResult(expectedMessage);
+        assertCommandSuccess(command, actualModel, expectedCommandResult, expectedModel);
+    }
+
+    /**
+     * Executes the given {@code command}, confirms that <br>
+     * - a {@code CommandException} is thrown <br>
+     * - the CommandException message matches {@code expectedMessage} <br>
+     * - the stock book, filtered stock list and selected stock in {@code actualModel} remain unchanged
+     */
+    public static void assertCommandFailure(Command command, Model actualModel, String expectedMessage) {
+        // we are unable to defensively copy the model for comparison later, so we can
+        // only do so by copying its components.
+        StockBook expectedAddressBook = new StockBook(actualModel.getStockBook());
+        List<Stock> expectedFilteredList = new ArrayList<>(actualModel.getFilteredStockList());
+
+        assertThrows(CommandException.class, expectedMessage, () -> command.execute(actualModel));
+        assertEquals(expectedAddressBook, actualModel.getStockBook());
+        assertEquals(expectedFilteredList, actualModel.getFilteredStockList());
+    }
 
 }
