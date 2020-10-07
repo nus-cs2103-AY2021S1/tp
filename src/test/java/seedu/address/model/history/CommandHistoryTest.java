32f5f17b74d305efe247c9106b52be3936d5b66d
Michaeliaaa <michaelia0816@me.com>
2020-10-07 16:13:48 +0800

    Add testing for peek features

--- src/test/java/seedu/address/model/history/CommandHistoryTest.java
+++ src/test/java/seedu/address/model/history/CommandHistoryTest.java
@@ -11,4 +11,29 @@ public class CommandHistoryTest {
         CommandHistory.clearHistory();
         assertEquals("Here are your command history:", CommandHistory.getCommandHistory());
     }
+
+    @Test
+    void peekNext() {
+        CommandHistory.clearHistory();
+        CommandHistory.addUsedCommand("list");
+        CommandHistory.addUsedCommand("history");
+        CommandHistory.addUsedCommand("clear");
+        assertEquals("clear", CommandHistory.peekNext());
+        assertEquals("history", CommandHistory.peekNext());
+        assertEquals("list", CommandHistory.peekNext());
+    }
+
+    @Test
+    void peekPrev() {
+        CommandHistory.clearHistory();
+        CommandHistory.addUsedCommand("list");
+        CommandHistory.addUsedCommand("history");
+        CommandHistory.addUsedCommand("clear");
+        assertEquals("clear", CommandHistory.peekNext());
+        assertEquals("history", CommandHistory.peekNext());
+        assertEquals("list", CommandHistory.peekNext());
+        assertEquals("history", CommandHistory.peekPrev());
+        assertEquals("clear", CommandHistory.peekPrev());
+        assertEquals("", CommandHistory.peekPrev());
+    }
 }
