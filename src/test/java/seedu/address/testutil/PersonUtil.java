079797b13f8227deeb606b29a1b7dcb7eb2fe172
li-s <lishuo980@gmail.com>
2020-10-07 16:19:17 +0800

    Remove editCommand from Logic

--- src/test/java/seedu/address/testutil/PersonUtil.java
+++ src/test/java/seedu/address/testutil/PersonUtil.java
@@ -5,13 +5,13 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
-
-import java.util.Set;
+//
+//import java.util.Set;
 
 import seedu.address.logic.commands.AddCommand;
-import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
+//import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
 import seedu.address.model.person.Person;
-import seedu.address.model.tag.Tag;
+//import seedu.address.model.tag.Tag;
 
 /**
  * A utility class for Person.
@@ -40,9 +40,11 @@ public class PersonUtil {
         return sb.toString();
     }
 
+    /*
     /**
      * Returns the part of command string for the given {@code EditPersonDescriptor}'s details.
      */
+    /*
     public static String getEditPersonDescriptorDetails(EditPersonDescriptor descriptor) {
         StringBuilder sb = new StringBuilder();
         descriptor.getName().ifPresent(name -> sb.append(PREFIX_NAME).append(name.fullName).append(" "));
@@ -59,4 +61,5 @@ public class PersonUtil {
         }
         return sb.toString();
     }
+     */
 }
