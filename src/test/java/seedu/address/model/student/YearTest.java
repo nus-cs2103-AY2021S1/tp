8c238781d51993778da43bfc394e1d108e498f64
AlexCQY <alex_chua@u.nus.edu>
2020-10-07 13:15:16 +0800

    Add Year Tests

---
+++ src/test/java/seedu/address/model/student/YearTest.java
@@ -0,0 +1,38 @@
+package seedu.address.model.student;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+
+public class YearTest {
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Year(null));
+    }
+
+    @Test
+    public void constructor_invalidPhone_throwsIllegalArgumentException() {
+        String invalidYear = "";
+        assertThrows(IllegalArgumentException.class, () -> new Year(invalidYear));
+    }
+
+    @Test
+    public void isValidYear() {
+        // null year
+        assertThrows(NullPointerException.class, () -> Year.isValidYear(null));
+
+        // invalid year numbers
+        assertFalse(Year.isValidYear("")); // empty string
+        assertFalse(Year.isValidYear(" ")); // spaces only
+        assertFalse(Year.isValidYear("abc")); // non-numeric
+        assertFalse(Year.isValidYear("!@#$")); // characters
+
+        // valid year numbers
+        assertTrue(Year.isValidYear("0"));
+        assertTrue(Year.isValidYear("11")); // double digits
+        assertTrue(Year.isValidYear("92312432423")); // long number
+    }
+}
