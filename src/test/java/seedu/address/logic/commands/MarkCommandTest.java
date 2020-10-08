c628a41fa4c806a2b4706571fb9884e19158cd73
JoshuaChewJianXiang <joshuachewabc@gmail.com>
2020-10-08 21:15:07 +0800

    Fix lexocographical ordering for static imports for MarkCommandTest

--- src/test/java/seedu/address/logic/commands/MarkCommandTest.java
+++ src/test/java/seedu/address/logic/commands/MarkCommandTest.java
@@ -2,8 +2,8 @@ package seedu.address.logic.commands;
 
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.TypicalStudents.getTypicalTaskmaster;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_STUDENT;
+import static seedu.address.testutil.TypicalStudents.getTypicalTaskmaster;
 
 import org.junit.jupiter.api.Test;
 
