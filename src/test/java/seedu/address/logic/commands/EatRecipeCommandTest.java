acaf888ace74ae76c6994d3d8e9e5ed1c6c2383b
TianYong-Goh <goh_ty@users.noreply.github.com>
2020-10-08 16:17:27 +0800

    Update checkstyle

--- src/test/java/seedu/address/logic/commands/EatRecipeCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EatRecipeCommandTest.java
@@ -10,6 +10,7 @@ import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_RECIPE;
 import static seedu.address.testutil.TypicalRecipes.getTypicalWishfulShrinking;
 
 import org.junit.jupiter.api.Test;
+
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.model.Model;
