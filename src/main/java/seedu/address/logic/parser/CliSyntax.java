2f9918af0d13f9c0fbe9692b00b5e07bf07fdcc2
YING-GAO-PC\Ying Gao <ongyg65@gmail.com>
2020-10-08 16:17:48 +0800

    Add prefixes for admin details for implementing add and edit

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -10,10 +10,23 @@ public class CliSyntax {
     public static final Prefix PREFIX_PHONE = new Prefix("p/");
     public static final Prefix PREFIX_SCHOOL = new Prefix("s/");
     public static final Prefix PREFIX_YEAR = new Prefix("y/");
-    public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
-    public static final Prefix PREFIX_TAG = new Prefix("t/");
+    public static final Prefix PREFIX_VENUE = new Prefix("v/");
+    public static final Prefix PREFIX_TIME = new Prefix("t/");
+    public static final Prefix PREFIX_FEE = new Prefix("f/");
+    public static final Prefix PREFIX_PAYMENT = new Prefix("d/");
+    public static final Prefix PREFIX_DETAILS = new Prefix("a/");
 
-    public static final Prefix[] COMPULSORY_PREFIXES = new Prefix[] {PREFIX_NAME, PREFIX_PHONE,
-        PREFIX_SCHOOL, PREFIX_YEAR};
+    public static final Prefix[] COMPULSORY_PREFIXES =
+            new Prefix[] {PREFIX_NAME, PREFIX_PHONE, PREFIX_SCHOOL, PREFIX_YEAR,
+                PREFIX_VENUE, PREFIX_TIME, PREFIX_FEE, PREFIX_PAYMENT};
+
+    /**
+     * Returns a list of all CLI syntax definitions.
+     */
+    public static Prefix[] valuesOf() {
+        return new Prefix[] {PREFIX_NAME, PREFIX_PHONE, PREFIX_SCHOOL,
+            PREFIX_YEAR, PREFIX_VENUE, PREFIX_TIME, PREFIX_DETAILS,
+            PREFIX_FEE, PREFIX_PAYMENT};
+    }
 
 }
