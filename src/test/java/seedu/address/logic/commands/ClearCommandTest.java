4c6c3582319c89862ff0f3d6f8253f4516d28c79
dianneloh9 <dianneloh9@gmail.com>
2020-10-11 03:51:49 +0800

    Add PropertyBook methods to ModelManager

--- src/test/java/seedu/address/logic/commands/ClearCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ClearCommandTest.java
@@ -9,6 +9,7 @@ import seedu.address.model.AddressBook;
 import seedu.address.model.BidBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
+import seedu.address.model.PropertyBook;
 import seedu.address.model.UserPrefs;
 
 public class ClearCommandTest {
@@ -23,8 +24,8 @@ public class ClearCommandTest {
 
     @Test
     public void execute_nonEmptyAddressBook_success() {
-        Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs(), new BidBook());
-        Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs(), new BidBook());
+        Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs(), new BidBook(), new PropertyBook());
+        Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs(), new BidBook(), new PropertyBook());
         expectedModel.setAddressBook(new AddressBook());
 
         assertCommandSuccess(new ClearCommand(), model, ClearCommand.MESSAGE_SUCCESS, expectedModel);
