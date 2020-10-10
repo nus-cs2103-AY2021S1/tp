bb70415835221f58e8a4c544be2eefa3f6cff732
Xue Yong <xueyong26@hotmail.com>
2020-10-10 17:36:48 +0800

    Fix sizes to better accomodate information

--- src/main/java/chopchop/ui/RecipeCard.java
+++ src/main/java/chopchop/ui/RecipeCard.java
@@ -1,9 +1,11 @@
 package chopchop.ui;
 
+import chopchop.model.recipe.Recipe;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
+import seedu.address.ui.PersonCard;
 
 /**
  * An UI component that displays information of a {@code Recipe}.
@@ -15,11 +17,30 @@ public class RecipeCard extends UiPart<Region> {
     private Button cardPane
     @FXML
     private Label name;
+
     /**
      * Creates a {@code RecipeCard} with the given {@code Recipe}.
      */
     public RecipeCard(Recipe recipe) {
+        super(FXML);
         this.recipe = recipe;
-        name.setText(recipe.getName());
+        name.setText(recipe.getName().toString());
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof RecipeCard)) {
+            return false;
+        }
+
+        // state check
+        RecipeCard card = (RecipeCard) other;
+        return recipe.equals(card.recipe);
     }
 }
