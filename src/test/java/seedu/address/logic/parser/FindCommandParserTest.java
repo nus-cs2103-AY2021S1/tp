a400d6f34bf93720ffb2329b3849b39da7e97f96
royleochan <royleochan@gmail.com>
2020-10-11 02:24:41 +0800

    Update test cases

--- src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
@@ -2,16 +2,15 @@ package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
-//import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
-//
-//import java.util.ArrayList;
-//import java.util.Arrays;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.FindCommand;
-//import seedu.address.model.person.PersonHasTagsAndNamePredicate;
-//import seedu.address.model.tag.Tag;
+import seedu.address.model.person.PersonHasTagsAndNamePredicate;
+import seedu.address.model.tag.Tag;
 
 public class FindCommandParserTest {
 
@@ -21,21 +20,45 @@ public class FindCommandParserTest {
     public void parse_emptyArg_throwsParseException() {
         assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
     }
-    // TODO: Check why this test fails
-    //
+
+    @Test
+    public void parse_validArgs_returnsFindCommand() {
+        // no leading and trailing whitespaces
+        ArrayList<String> nameList = new ArrayList<>();
+        nameList.add("Alice");
+        ArrayList<Tag> tagList = new ArrayList<>();
+        tagList.add(new Tag("tag"));
+        FindCommand expectedFindCommand =
+                new FindCommand(new PersonHasTagsAndNamePredicate(nameList, tagList));
+        assertParseSuccess(parser, " n/Alice t/tag", expectedFindCommand);
+
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, " \n n/Alice \n t/tag\t", expectedFindCommand);
+    }
+
+    //TODO: Fix this test case
     //    @Test
-    //    public void parse_validArgs_returnsFindCommand() {
+    //    public void parse_multipleNames_returnsFindCommand() {
     //        // no leading and trailing whitespaces
     //        ArrayList<String> nameList = new ArrayList<>();
     //        nameList.add("Alice");
+    //        nameList.add("Bob");
+    //        nameList.add("Candy");
     //        ArrayList<Tag> tagList = new ArrayList<>();
     //        tagList.add(new Tag("tag"));
     //        FindCommand expectedFindCommand =
     //                new FindCommand(new PersonHasTagsAndNamePredicate(nameList, tagList));
-    //        assertParseSuccess(parser, " n/Alice t/tag", expectedFindCommand);
-    //
-    //        // multiple whitespaces between keywords
-    //        assertParseSuccess(parser, " \n n/Alice \n \t n/Bob  t/tag\t", expectedFindCommand);
+    //        assertParseSuccess(parser, " n/Alice n/Bob n/Candy t/tag", expectedFindCommand);
     //    }
 
+    @Test
+    public void parse_singleNameAndNoTag_returnsFindCommand() {
+        // no leading and trailing whitespaces
+        ArrayList<String> nameList = new ArrayList<>();
+        nameList.add("Alice");
+        ArrayList<Tag> tagList = new ArrayList<>();
+        FindCommand expectedFindCommand =
+                new FindCommand(new PersonHasTagsAndNamePredicate(nameList, tagList));
+        assertParseSuccess(parser, " n/Alice ", expectedFindCommand);
+    }
 }
