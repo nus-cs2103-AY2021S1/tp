6157fd0f03b414f2fb38b769407d7bf783984fe9
YING-GAO-PC\Ying Gao <ongyg65@gmail.com>
2020-10-10 14:26:06 +0800

    Add JSON-adaptable questions

---
+++ src/main/java/seedu/address/storage/JsonAdaptedQuestion.java
@@ -0,0 +1,57 @@
+package seedu.address.storage;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.student.Question;
+
+/**
+ * Jackson-friendly version of {@code Question}.
+ */
+public class JsonAdaptedQuestion {
+
+    private final String question;
+
+    /**
+     * Constructs a {@code JsonAdaptedQuestion} with the given {@code question}.
+     */
+    @JsonCreator
+    public JsonAdaptedQuestion(String question) {
+        this.question = question;
+    }
+
+    /**
+     * Converts a given {@code Question} into this class for Jackson use.
+     */
+    public JsonAdaptedQuestion(Question source) {
+        int status = source.isResolved ? 1 : 0;
+        this.question = String.format("%1d | %2s", status, source.question);
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    /**
+     * Converts this Jackson-friendly adapted question object into the model's {@code Question} object.
+     *
+     * @throws IllegalValueException if there were any data constraints violated in the adapted detail.
+     */
+    public Question toModelType() throws IllegalValueException {
+        Pattern pattern = Pattern.compile("(?<isResolved>[01])(\\s\\|\\s)(?<question>.*)");
+        Matcher matcher = pattern.matcher(question);
+        if (!matcher.matches()) {
+            throw new IllegalValueException(Question.MESSAGE_CONSTRAINTS);
+        }
+
+        String questionDetail = matcher.group("question");
+        if (!Question.isValidQuestion(questionDetail)) {
+            throw new IllegalValueException(Question.MESSAGE_CONSTRAINTS);
+        }
+
+        boolean status = Integer.parseInt(matcher.group("isResolved")) != 0;
+        return new Question(questionDetail, status);
+    }
+}
---
+++ src/test/java/seedu/address/storage/JsonAdaptedQuestionTest.java
@@ -0,0 +1,49 @@
+package seedu.address.storage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.student.Question;
+
+public class JsonAdaptedQuestionTest {
+
+    @Test
+    public void toModelType_invalidString_throwsIllegalArgumentException() {
+        JsonAdaptedQuestion question = new JsonAdaptedQuestion("0 |   ");
+        assertThrows(IllegalValueException.class, question::toModelType);
+
+        question = new JsonAdaptedQuestion("1 | ");
+        assertThrows(IllegalValueException.class, question::toModelType);
+
+        question = new JsonAdaptedQuestion("- | test");
+        assertThrows(IllegalValueException.class, question::toModelType);
+
+        question = new JsonAdaptedQuestion("2 | test");
+        assertThrows(IllegalValueException.class, question::toModelType);
+
+        question = new JsonAdaptedQuestion("1 test");
+        assertThrows(IllegalValueException.class, question::toModelType);
+
+        question = new JsonAdaptedQuestion("0 ");
+        assertThrows(IllegalValueException.class, question::toModelType);
+
+        question = new JsonAdaptedQuestion("");
+        assertThrows(IllegalValueException.class, question::toModelType);
+    }
+
+    @Test
+    public void toModelType_validString_success() throws IllegalValueException {
+        String testString = "Hi";
+        JsonAdaptedQuestion question = new JsonAdaptedQuestion("0 | " + testString);
+        Question modelQuestion = new Question(testString, false);
+        assertEquals(question.toModelType(), modelQuestion);
+
+        testString = "test! | 131?";
+        question = new JsonAdaptedQuestion("1 | " + testString);
+        modelQuestion = new Question(testString, true);
+        assertEquals(question.toModelType(), modelQuestion);
+
+    }
+}
