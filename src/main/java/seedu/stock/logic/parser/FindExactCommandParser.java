8c04d1ac33b0547083d68a857bb40a9bfbe9d77a
amylzting <amylimzting@gmail.com>
2020-10-10 21:59:29 +0800

    Remove unused import

--- src/main/java/seedu/stock/logic/parser/FindExactCommandParser.java
+++ src/main/java/seedu/stock/logic/parser/FindExactCommandParser.java
@@ -9,7 +9,6 @@ import static seedu.stock.logic.parser.CliSyntax.PREFIX_SOURCE;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
