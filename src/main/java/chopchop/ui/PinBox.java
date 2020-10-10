cf2b6a41688d71cb46ebba0ab7bbf407ee917541
Xue Yong <xueyong26@hotmail.com>
2020-10-10 21:06:27 +0800

    Update pin box

---
+++ src/main/java/chopchop/ui/PinBox.java
@@ -0,0 +1,24 @@
+package chopchop.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.Region;
+
+/**
+ * The UI component that is responsible for displaying pinned information.
+ */
+public class PinBox extends UiPart<Region> {
+
+    private static final String FXML = "PinBox.fxml";
+
+    @FXML
+    private TextArea pins;
+
+    /**
+     * Creates a {@code PinBox}.
+     */
+    public PinBox() {
+        super(FXML);
+        pins.setText("Favourites");
+    }
+}
