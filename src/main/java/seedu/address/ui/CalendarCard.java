ea71872fe27afbbb0b2a5323041814dc39a74137
Harsha Vardhan <munharsha@gmail.com>
2020-10-10 15:23:12 +0800

    Update CalendarVenue.java, CalendarCard.java, CalendarListPanel.java

--- src/main/java/seedu/address/ui/CalendarCard.java
+++ src/main/java/seedu/address/ui/CalendarCard.java
@@ -1,4 +1,74 @@
 package seedu.address.ui;
 
-public class CalendarCard {
+import java.util.Comparator;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import seedu.address.model.calendar.CalendarMeeting;
+import seedu.address.model.person.Person;
+
+/**
+ * An UI component that displays information of a {@code Person}.
+ */
+public class CalendarCard extends UiPart<Region> {
+
+    private static final String FXML = "PersonListCard.fxml";
+
+    /**
+     * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
+     * As a consequence, UI elements' variable names cannot be set to such keywords
+     * or an exception will be thrown by JavaFX during runtime.
+     *
+     * @see <a href="https://github.com/se-edu/addressbook-level4/issues/336">The issue on AddressBook level 4</a>
+     */
+
+    public final CalendarMeeting meeting;
+
+    @FXML
+    private HBox cardPane;
+    @FXML
+    private Label calenderBidderId;
+    @FXML
+    private Label calenderPropertyId;
+    @FXML
+    private Label time;
+    @FXML
+    private Label venue;
+    @FXML
+    private FlowPane tags;
+
+    /**
+     * Creates a {@code PersonCode} with the given {@code Person} and index to display.
+     */
+    public CalendarCard(CalendarMeeting meeting, int displayedIndex) {
+        super(FXML);
+        this.meeting = meeting;
+        id.setText(displayedIndex + ". ");
+        name.setText(person.getName().fullName);
+        phone.setText(person.getPhone().value);
+        person.getTags().stream()
+                .sorted(Comparator.comparing(tag -> tag.tagName))
+                .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof PersonCard)) {
+            return false;
+        }
+
+        // state check
+        PersonCard card = (PersonCard) other;
+        return id.getText().equals(card.id.getText())
+                && person.equals(card.person);
+    }
 }
