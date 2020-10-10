a8397677fb6bca9e29dc0e3119dc437abc6700d4
Harsha Vardhan <munharsha@gmail.com>
2020-10-10 15:22:34 +0800

    Update ReadOnlyMeetingManager.java, CalendarTime.java

--- src/main/java/seedu/address/model/ReadOnlyMeetingManager.java
+++ src/main/java/seedu/address/model/ReadOnlyMeetingManager.java
@@ -1,4 +1,16 @@
 package seedu.address.model;
 
-public class ReadOnlyMeetingManager {
+import javafx.collections.ObservableList;
+import seedu.address.model.calendar.CalendarMeeting;
+
+/**
+ * Unmodifiable view of a calendar of meeting
+ */
+public interface ReadOnlyMeetingManager {
+
+    /**
+     * Returns an unmodifiable view of the meeting list.
+     * This list will not contain any duplicate meetings.
+     */
+    ObservableList<CalendarMeeting> getMeetingList();
 }
