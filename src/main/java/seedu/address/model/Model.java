14ea4caf57cdd261476cfd83eac2a37ce044c6f9
Lim Zi Yang <limziyang8@gmail.com>
2020-10-11 15:02:50 +0800

    Fix checkstyle

--- src/main/java/seedu/address/model/Model.java
+++ src/main/java/seedu/address/model/Model.java
@@ -6,8 +6,6 @@ import java.util.function.Predicate;
 import javafx.collections.ObservableList;
 import seedu.address.commons.core.GuiSettings;
 import seedu.address.model.account.Account;
-import seedu.address.model.account.entry.Expense;
-import seedu.address.model.account.entry.Revenue;
 import seedu.address.model.person.Person;
 
 /**
