0ead4265ce684694f96126f325a0c58d359c4c8c
Thai Duong <htduong292001@gmail.com>
2020-10-11 18:11:16 +0800

    Fix checkstyle

--- src/test/java/jimmy/mcgymmy/logic/commands/TagCommandTest.java
+++ src/test/java/jimmy/mcgymmy/logic/commands/TagCommandTest.java
@@ -1,6 +1,5 @@
 package jimmy.mcgymmy.logic.commands;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import jimmy.mcgymmy.commons.core.Messages;
@@ -18,9 +17,9 @@ import jimmy.mcgymmy.testutil.TypicalIndexes;
 class TagCommandTest {
 
     private Model model = new ModelManager(TypicalFoods.getTypicalMcGymmy(), new UserPrefs());
-    private final String tag_1 = "hello";
-    private final String tag_2 = "goodbye";
-    private final String tag_3 = "seeYouAgain";
+    private String tag1 = "hello";
+    private String tag2 = "goodbye";
+    private String tag3 = "seeYouAgain";
 
     @Test
     public void execute_validIndexUnfilteredList_success() {
@@ -29,13 +28,13 @@ class TagCommandTest {
         TagCommand tagCommand = new TagCommand();
         tagCommand.setParameters(
                 new CommandParserTestUtil.ParameterStub<>("", TypicalIndexes.INDEX_FIRST_FOOD),
-                new CommandParserTestUtil.ParameterStub<>("t", new Tag(tag_1))
+                new CommandParserTestUtil.ParameterStub<>("t", new Tag(tag1))
         );
 
-        String expectedMessage = String.format(TagCommand.MESSAGE_SUCCESS, tag_1);
+        String expectedMessage = String.format(TagCommand.MESSAGE_SUCCESS, tag1);
 
         ModelManager expectedModel = new ModelManager(model.getMcGymmy(), new UserPrefs());
-        expectedModel.setFood(index, new FoodBuilder(foodToTag).withTags(tag_1).build());
+        expectedModel.setFood(index, new FoodBuilder(foodToTag).withTags(tag1).build());
 
         CommandTestUtil.assertCommandSuccess(tagCommand, model, expectedMessage, expectedModel);
     }
@@ -46,7 +45,7 @@ class TagCommandTest {
         TagCommand tagCommand = new TagCommand();
         tagCommand.setParameters(
                 new CommandParserTestUtil.ParameterStub<>("", outOfBoundIndex),
-                new CommandParserTestUtil.ParameterStub<>("t", new Tag(tag_1))
+                new CommandParserTestUtil.ParameterStub<>("t", new Tag(tag1))
         );
         CommandTestUtil.assertCommandFailure(tagCommand, model, Messages.MESSAGE_INVALID_FOOD_DISPLAYED_INDEX);
     }
@@ -60,13 +59,13 @@ class TagCommandTest {
         TagCommand tagCommand = new TagCommand();
         tagCommand.setParameters(
                 new CommandParserTestUtil.ParameterStub<>("", TypicalIndexes.INDEX_FIRST_FOOD),
-                new CommandParserTestUtil.ParameterStub<>("t", new Tag(tag_3))
+                new CommandParserTestUtil.ParameterStub<>("t", new Tag(tag3))
         );
 
-        String expectedMessage = String.format(TagCommand.MESSAGE_SUCCESS, tag_3);
+        String expectedMessage = String.format(TagCommand.MESSAGE_SUCCESS, tag3);
         ModelManager expectedModel = new ModelManager(model.getMcGymmy(), new UserPrefs());
         expectedModel.setFood(Index.fromZeroBased(0),
-                new FoodBuilder(foodToTag).withTags(tag_3).build());
+                new FoodBuilder(foodToTag).withTags(tag3).build());
 
         CommandTestUtil.assertCommandSuccess(tagCommand, model, expectedMessage, expectedModel);
     }
@@ -78,17 +77,17 @@ class TagCommandTest {
         TagCommand tagCommand = new TagCommand();
         tagCommand.setParameters(
             new CommandParserTestUtil.ParameterStub<>("", TypicalIndexes.INDEX_FIRST_FOOD),
-            new CommandParserTestUtil.ParameterStub<>("t", new Tag(tag_2))
+            new CommandParserTestUtil.ParameterStub<>("t", new Tag(tag2))
         );
 
-        String expectedMessage = String.format(TagCommand.MESSAGE_SUCCESS, tag_2);
+        String expectedMessage = String.format(TagCommand.MESSAGE_SUCCESS, tag2);
 
         ModelManager expectedModel = new ModelManager(model.getMcGymmy(), new UserPrefs());
-        expectedModel.setFood(index, new FoodBuilder(foodToTag).withTags(tag_2).build());
+        expectedModel.setFood(index, new FoodBuilder(foodToTag).withTags(tag2).build());
 
         // add tag for the first time
         CommandTestUtil.assertCommandSuccess(tagCommand, model, expectedMessage, expectedModel);
         CommandTestUtil.assertCommandFailure(tagCommand, model,
-            String.format(TagCommand.MESSAGE_DUPLICATE_TAG, tag_2, foodToTag.getName().fullName));
+            String.format(TagCommand.MESSAGE_DUPLICATE_TAG, tag2, foodToTag.getName().fullName));
     }
 }
