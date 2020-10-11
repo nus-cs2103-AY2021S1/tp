c8b34337053196cef233ee1a8b8867b780edf2a0
jonasngs <jonasngs30@gmail.com>
2020-10-11 17:14:02 +0800

    Implement Module class and related classes

--- src/main/java/seedu/address/model/ModuleList.java
+++ src/main/java/seedu/address/model/ModuleList.java
@@ -5,16 +5,16 @@ import static java.util.Objects.requireNonNull;
 import java.util.List;
 
 import javafx.collections.ObservableList;
-import seedu.address.model.person.Person;
-import seedu.address.model.person.UniquePersonList;
+import seedu.address.model.module.Module;
+import seedu.address.model.module.UniqueModuleList;
 
 /**
  * Wraps all data at the address-book level
  * Duplicates are not allowed (by .isSamePerson comparison)
  */
-public class ModuleList implements ReadOnlyAddressBook {
+public class ModuleList implements ReadOnlyModuleList {
 
-    private final UniquePersonList persons;
+    private final UniqueModuleList modules;
 
     /*
      * The 'unusual' code block below is a non-static initialization block, sometimes used to avoid duplication
@@ -24,7 +24,7 @@ public class ModuleList implements ReadOnlyAddressBook {
      *   among constructors.
      */
     {
-        persons = new UniquePersonList();
+        modules = new UniqueModuleList();
     }
 
     public ModuleList() {}
@@ -32,7 +32,7 @@ public class ModuleList implements ReadOnlyAddressBook {
     /**
      * Creates an AddressBook using the Persons in the {@code toBeCopied}
      */
-    public ModuleList(ReadOnlyAddressBook toBeCopied) {
+    public ModuleList(ReadOnlyModuleList toBeCopied) {
         this();
         resetData(toBeCopied);
     }
@@ -40,81 +40,79 @@ public class ModuleList implements ReadOnlyAddressBook {
     //// list overwrite operations
 
     /**
-     * Replaces the contents of the person list with {@code persons}.
-     * {@code persons} must not contain duplicate persons.
+     * * Resets the existing data of this {@code ModuleList} with {@code newData}.
      */
-    public void setPersons(List<Person> persons) {
-        this.persons.setPersons(persons);
-    }
-
-    /**
-     * Resets the existing data of this {@code AddressBook} with {@code newData}.
-     */
-    public void resetData(ReadOnlyAddressBook newData) {
+    public void resetData(ReadOnlyModuleList newData) {
         requireNonNull(newData);
 
-        setPersons(newData.getPersonList());
+        setModule(newData.getModuleList());
     }
 
     //// person-level operations
 
     /**
-     * Returns true if a person with the same identity as {@code person} exists in the address book.
+     * Returns true if a module with the same identity as {@code module} exists in the module list.
      */
-    public boolean hasPerson(Person person) {
-        requireNonNull(person);
-        return persons.contains(person);
+    public boolean hasModule(Module module) {
+        requireNonNull(module);
+        return modules.contains(module);
     }
 
     /**
-     * Adds a person to the address book.
-     * The person must not already exist in the address book.
+     * Adds a module to the module list.
+     * The module must not already exist in the module list.
      */
-    public void addPerson(Person p) {
-        persons.add(p);
+    public void addModule(Module m) {
+        modules.add(m);
+    }
+    /**
+     * Replaces the contents of the module list with {@code module}.
+     * {@code module} must not contain duplicate module.
+     */
+    public void setModule(List<Module> modules) {
+        this.modules.setModules(modules);
     }
 
     /**
-     * Replaces the given person {@code target} in the list with {@code editedPerson}.
-     * {@code target} must exist in the address book.
-     * The person identity of {@code editedPerson} must not be the same as another existing person in the address book.
+     * Replaces the given module {@code target} in the list with {@code editedModule}.
+     * {@code target} must exist in the modulelist.
+     * The module identity of {@code editedModule} must not be the same as another existing module in the modulelist.
      */
-    public void setPerson(Person target, Person editedPerson) {
-        requireNonNull(editedPerson);
-
-        persons.setPerson(target, editedPerson);
+    public void setModule(Module target, Module editedModule) {
+        requireNonNull(editedModule);
+        modules.setModule(target, editedModule);
     }
 
     /**
-     * Removes {@code key} from this {@code AddressBook}.
-     * {@code key} must exist in the address book.
+     * Removes {@code key} from this {@code ModuleList}.
+     * {@code key} must exist in the modulelist.
      */
-    public void removePerson(Person key) {
-        persons.remove(key);
+    public void removeModule(Module key) {
+        modules.remove(key);
     }
 
     //// util methods
 
     @Override
     public String toString() {
-        return persons.asUnmodifiableObservableList().size() + " persons";
+        return modules.asUnmodifiableObservableList().size() + " modules";
         // TODO: refine later
     }
 
     @Override
-    public ObservableList<Person> getPersonList() {
-        return persons.asUnmodifiableObservableList();
+    public ObservableList<Module> getModuleList() {
+        return modules.asUnmodifiableObservableList();
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof ModuleList // instanceof handles nulls
-                && persons.equals(((ModuleList) other).persons));
+                && modules.equals(((ModuleList) other).modules));
     }
 
     @Override
     public int hashCode() {
-        return persons.hashCode();
+        return modules.hashCode();
     }
 }
