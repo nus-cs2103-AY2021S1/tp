ac2a9a697e8d8f9162aa2078220f7b4fc106f9ea
lll-jy <E0376997@u.nus.edu>
2020-10-11 13:14:12 +0800

    Update AssignCommandParser to parse assign commands

--- src/main/java/seedu/address/logic/parser/AssignCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AssignCommandParser.java
@@ -2,26 +2,33 @@ package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.catalogue.DeleteCommand;
+import seedu.address.logic.commands.project.AssignCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
- * Parses input arguments and creates a new DeleteCommand object
+ * Parses input arguments and creates a new AssignCommand object
  */
-public class AssignCommandParser implements Parser<DeleteCommand> {
+public class AssignCommandParser implements Parser<AssignCommand> {
 
     /**
      * Parses the given {@code String} of arguments in the context of the DeleteCommand
      * and returns a DeleteCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
-    public DeleteCommand parse(String args) throws ParseException {
+    public AssignCommand parse(String args) throws ParseException {
         try {
-            Index index = ParserUtil.parseIndex(args);
-            return new DeleteCommand(index);
+            String input = args.trim();
+            int divider = input.indexOf(" ");
+            if (divider <= 0) {
+                throw new ParseException("");
+            }
+            String indexString = input.substring(0, divider);
+            Index index = ParserUtil.parseIndex(indexString);
+            String name = input.substring(divider + 1);
+            return new AssignCommand(index, name);
         } catch (ParseException pe) {
             throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE), pe);
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, AssignCommand.MESSAGE_USAGE), pe);
         }
     }
 
