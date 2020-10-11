baedf7d9af92b56da2450e02ac5ab637753b6a83
Sean Tay <seantaysl@gmail.com>
2020-10-11 18:31:38 +0800

    Fix styling issues

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -10,10 +10,10 @@ import seedu.address.commons.core.index.Index;
 import seedu.address.commons.util.StringUtil;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.Email;
+import seedu.address.model.person.Module;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Phone;
 import seedu.address.model.tag.Tag;
-import seedu.address.model.person.Module;
 
 /**
  * Contains utility methods used for parsing strings in the various *Parser classes.
