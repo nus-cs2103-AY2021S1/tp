33ec178fbb386ccfc37d029a16c4e2b8c69b30e7
Hou-Rui <houruinus@gmail.com>
2020-10-11 16:22:50 +0800

    JsonAdaptedLeave.java: Add Jackson-friendly Leave

--- src/main/java/com/eva/storage/JsonAdaptedLeave.java
+++ src/main/java/com/eva/storage/JsonAdaptedLeave.java
@@ -1,2 +1,32 @@
-package com.eva.storage;public class JsonAdaptedLeave {
+package com.eva.storage;
+
+import com.eva.commons.util.DateUtil;
+import com.eva.model.person.staff.leave.Leave;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Jackson-friendly version of {@link Leave}.
+ */
+public class JsonAdaptedLeave {
+    private final String startDate;
+    private final String endDate;
+
+    @JsonCreator
+    public JsonAdaptedLeave(
+            @JsonProperty("startDate") String startDate,
+            @JsonProperty("endDate") String endDate
+    ) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public JsonAdaptedLeave(Leave leave) {
+        this.startDate = DateUtil.dateToString(leave.getStartDate());
+        this.endDate = DateUtil.dateToString(leave.getEndDate());
+    }
+
+    public Leave toModelType() {
+        return new Leave(startDate, endDate);
+    }
 }
