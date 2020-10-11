c4c9107bc6e8c8242ba8543de069e70e556a7d92
CodyChew <chewchengyap@gmail.com>
2020-10-11 23:46:41 +0800

    Add OpenCase Command.
    Let's create an abstract OpenCommand to accomodate for open case and
    open document. The OpenCaseCommand extends from OpenCommand.
    An OpenCommandParser parses the arguments to return the correct
    OpenCommand object given an input type and index.

---
+++ src/main/java/seedu/address/logic/commands/OpenCaseCommand.java
@@ -0,0 +1,36 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.investigationcase.Case;
+
+public class OpenCaseCommand extends OpenCommand {
+
+    public static final String MESSAGE_OPEN_CASE_SUCCESS = "Opened Case: %1$s";
+
+    public OpenCaseCommand(Index targetIndex) {
+        super(targetIndex);
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Case> lastShownList = model.getFilteredCaseList();
+
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_CASE_DISPLAYED_INDEX);
+        }
+
+        Case caseToOpen = lastShownList.get(targetIndex.getZeroBased());
+
+        //update state here - to be done after State is merged
+
+        return new CommandResult(String.format(MESSAGE_OPEN_CASE_SUCCESS, caseToOpen));
+    }
+}
