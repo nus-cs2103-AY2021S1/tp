3d414a835721fa24cf7a8d89c82c212db32bdc16
Sean Tay <seantaysl@gmail.com>
2020-10-11 16:35:02 +0800

    Add TypicalModules class

---
+++ src/test/java/seedu/address/testutil/TypicalModules.java
@@ -0,0 +1,12 @@
+package seedu.address.testutil;
+
+import seedu.address.model.person.Module;
+
+/**
+ * A utility class containing a list of {@code Module} objects to be used in tests.
+ */
+public class TypicalModules {
+    public static final Module CS2100 = new Module("CS2100");
+    public static final Module CS2103T = new Module("CS2103T");
+    public static final Module CS2040 = new Module("CS2040");
+}
