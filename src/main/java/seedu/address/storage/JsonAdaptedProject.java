4c09e32c97bf4e612d2cd88bb553579b98cc6dee
lll-jy <E0376997@u.nus.edu>
2020-10-11 19:36:42 +0800

    Rename a field in JsonAdaptedProject

--- src/main/java/seedu/address/storage/JsonAdaptedProject.java
+++ src/main/java/seedu/address/storage/JsonAdaptedProject.java
@@ -31,7 +31,7 @@ class JsonAdaptedProject {
     private final String repoUrl;
     private final String projectDescription;
     private final List<JsonAdaptedTag> projectTagged = new ArrayList<>();
-    private final List<JsonAdaptedTask> occupied = new ArrayList<>();
+    private final List<JsonAdaptedTask> projectOccupied = new ArrayList<>();
 
     /**
      * Constructs a {@code JsonAdaptedProject} with the given project details.
@@ -51,7 +51,7 @@ class JsonAdaptedProject {
             this.projectTagged.addAll(projectTagged);
         }
         if (projectOccupied != null) {
-            this.occupied.addAll(projectOccupied);
+            this.projectOccupied.addAll(projectOccupied);
         }
     }
 
@@ -66,7 +66,7 @@ class JsonAdaptedProject {
         projectTagged.addAll(source.getProjectTags().stream()
                 .map(JsonAdaptedTag::new)
                 .collect(Collectors.toList()));
-        occupied.addAll(source.getTasks().stream()
+        projectOccupied.addAll(source.getTasks().stream()
                 .map(JsonAdaptedTask::new)
                 .collect(Collectors.toList()));
     }
@@ -82,7 +82,7 @@ class JsonAdaptedProject {
             projectProjectTags.add(projectTag.toModelType());
         }
         final List<Task> projectTasks = new ArrayList<>();
-        for (JsonAdaptedTask task : occupied) {
+        for (JsonAdaptedTask task : projectOccupied) {
             projectTasks.add(task.toModelType());
         }
 
