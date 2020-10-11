902871cc8c29ce35f25496119444f1050286782f
Thai Duong <htduong292001@gmail.com>
2020-10-11 20:48:54 +0800

    Store mcgymmy data in a separate data folder

--- src/main/java/jimmy/mcgymmy/model/UserPrefs.java
+++ src/main/java/jimmy/mcgymmy/model/UserPrefs.java
@@ -14,7 +14,7 @@ import jimmy.mcgymmy.commons.core.GuiSettings;
 public class UserPrefs implements ReadOnlyUserPrefs {
 
     private GuiSettings guiSettings = new GuiSettings();
-    private Path mcGymmyFilePath = Paths.get("mcgymmy.json");
+    private Path mcGymmyFilePath = Paths.get("data", "mcgymmy.json");
 
     /**
      * Creates a {@code UserPrefs} with default values.
