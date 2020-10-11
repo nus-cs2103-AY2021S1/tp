34a12632ed76cd3653a4658f2ce1e8252e4c7129
lll-jy <E0376997@u.nus.edu>
2020-10-11 12:43:07 +0800

    Modify execute of AssignCommand

---
+++ src/main/java/seedu/address/logic/commands/catalogue/ClearCommand 2.java
@@ -0,0 +1,25 @@
+package seedu.address.logic.commands.catalogue;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.model.MainCatalogue;
+import seedu.address.model.Model;
+
+/**
+ * Clears the main catalogue.
+ */
+public class ClearCommand extends Command {
+
+    public static final String COMMAND_WORD = "clear";
+    public static final String MESSAGE_SUCCESS = "ProjectDescription book has been cleared!";
+
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.setProjectCatalogue(new MainCatalogue());
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+}
--- src/main/java/seedu/address/logic/commands/project/AssignCommand.java
+++ src/main/java/seedu/address/logic/commands/project/AssignCommand.java
@@ -7,7 +7,9 @@ import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.project.Participation;
 import seedu.address.model.project.Project;
+import seedu.address.model.task.Task;
 
 import java.util.List;
 
@@ -24,26 +26,30 @@ public class AssignCommand extends Command {
             + "Parameters: INDEX (must be a positive integer), NAME (must be present in the project)\n"
             + "Example: " + COMMAND_WORD + " 1 Lucas";
 
-    public static final String MESSAGE_ASSIGN_TASK_SUCCESS = "Assigns task: %1$s to ";
+    public static final String MESSAGE_ASSIGN_TASK_SUCCESS = "Assigns task: %1$s to %s";
 
     private final Index targetIndex;
+    private final Participation assignee;
 
-    public AssignCommand(Index targetIndex) {
+    public AssignCommand(Index targetIndex, Participation assignee) {
         this.targetIndex = targetIndex;
+        this.assignee = assignee;
     }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
-        List<Project> lastShownList = model.getFilteredProjectList();
+        Project project = model.getFilteredProjectList().get(0);
+        List<Task> lastShownTaskList = project.getFilteredTaskList();
 
-        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+        if (targetIndex.getZeroBased() >= lastShownTaskList.size()) {
             throw new CommandException(Messages.MESSAGE_INVALID_PROJECT_DISPLAYED_INDEX);
         }
 
-        Project projectToDelete = lastShownList.get(targetIndex.getZeroBased());
-        model.deleteProject(projectToDelete);
-        return new CommandResult(String.format(MESSAGE_ASSIGN_TASK_SUCCESS, projectToDelete));
+        Task taskToAssociate = lastShownTaskList.get(targetIndex.getZeroBased());
+        assignee.addTask(taskToAssociate);
+
+        return new CommandResult(String.format(MESSAGE_ASSIGN_TASK_SUCCESS, taskToAssociate, assignee));
     }
 
     @Override
