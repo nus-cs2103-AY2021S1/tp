17dd2da6b002a2b85f570318434671d8465847ae
Xue Yong <xueyong26@hotmail.com>
2020-10-11 19:00:08 +0800

    Add Ingredient Card

---
+++ src/main/java/chopchop/ui/IngredientCard.java
@@ -0,0 +1,48 @@
+package chopchop.ui;
+
+
+import chopchop.model.ingredient.Ingredient;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Region;
+
+import java.awt.event.ActionEvent;
+
+public class IngredientCard extends UiPart<Region>{
+
+    private static final String FXML = "IngredientCard.fxml";
+
+    public final Ingredient ingredient;
+
+    @FXML
+    private Button cardPane;
+
+    @FXML
+    private Label name;
+
+    /**
+     * Creates a {@code RecipeCard} with the given {@code Recipe}.
+     */
+    public IngredientCard(Ingredient ingredient) {
+        super(FXML);
+        this.ingredient = ingredient;
+        name.setText(ingredient.toString());
+    }
+
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+        // instanceof handles nulls
+        if (!(other instanceof IngredientCard)) {
+            return false;
+        }
+        // state check
+        IngredientCard card = (IngredientCard) other;
+        return ingredient.equals(card.ingredient);
+    }
+}
