ff771ad2484a1aa7f4919a6254d19f60a291244d
zhiayang <zhiayang@nascentnoodle.com>
2020-10-12 21:53:54 +0800

    Remove redundant method in CommonParser

--- src/main/java/chopchop/logic/parser/commands/CommonParser.java
+++ src/main/java/chopchop/logic/parser/commands/CommonParser.java
@@ -15,19 +15,6 @@ import chopchop.logic.parser.CommandArguments;
 public class CommonParser {
 
     /**
-     * Parse an item reference from the given string.
-     */
-    public static Result<ItemReference> parseItemReference(StringView ref) {
-        if (ref.startsWith("$")) {
-            return ref.drop(1)
-                .parseInt()
-                .map(i -> ItemReference.ofOneIndex(i));
-        } else {
-            return Result.of(ItemReference.ofName(ref.toString()));
-        }
-    }
-
-    /**
      * Finds the first named argument in the given {@code args} that isn't part of {@code knownArgs}
      * and returns it so we can print a nice error message. If all names are part of the given list,
      * then an empty optional is returned.
