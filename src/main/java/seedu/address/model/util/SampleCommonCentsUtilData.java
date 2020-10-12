5b5adeb33bedfb38ea41d4c5dfad951eb31f917c
Lim Zi Yang <limziyang8@gmail.com>
2020-10-12 19:45:11 +0800

    Update SampleCommonCentsUtilData

--- src/main/java/seedu/address/model/util/SampleCommonCentsUtilData.java
+++ src/main/java/seedu/address/model/util/SampleCommonCentsUtilData.java
@@ -20,35 +20,34 @@ import seedu.address.model.tag.Tag;
  * Contains utility methods for populating {@code CommonCents} with sample data.
  */
 public class SampleCommonCentsUtilData {
-    public static Account[] getSampleAccount() {
-        Account acc1 = new Account(new Name("Lulu - store acc"));
-        RevenueList acc1Profits = new RevenueList();
-        acc1Profits.add(new Revenue(new Description("earrings sales"),
+    public static Account getSampleAccount() {
+        Account acc = new Account(new Name("General"));
+        RevenueList accProfits = new RevenueList();
+        accProfits.add(new Revenue(new Description("earrings sales"),
                 new Amount("13.50"), getTagSet("craft", "accessories")));
-        acc1Profits.add(new Revenue(new Description("phone cases sales"),
+        accProfits.add(new Revenue(new Description("phone cases sales"),
                 new Amount("15.70"), getTagSet("phone", "accessories")));
-        ExpenseList acc1ExpenseList = new ExpenseList();
-        acc1ExpenseList.add(new Expense(new Description("Watercolours"),
+        ExpenseList accExpenseList = new ExpenseList();
+        accExpenseList.add(new Expense(new Description("Watercolours"),
                 new Amount("12.10"), getTagSet("art", "colours")));
-        acc1ExpenseList.add(new Expense(new Description("canvas"),
+        accExpenseList.add(new Expense(new Description("canvas"),
                 new Amount("20.15"), getTagSet("art")));
-
+        /*
         Account acc2 = new Account(new Name("Lulu - nonbiz acc"));
         RevenueList acc2Profits = new RevenueList();
         ExpenseList acc2ExpenseList = new ExpenseList();
-        acc1ExpenseList.add(new Expense(new Description("lunch"),
+        accExpenseList.add(new Expense(new Description("lunch"),
                 new Amount("5.45"), getTagSet("food", "hawker")));
-        acc1ExpenseList.add(new Expense(new Description("dinner at Morganfield's"),
+        accExpenseList.add(new Expense(new Description("dinner at Morganfield's"),
                 new Amount("24.45"), getTagSet("food", "restaurant", "Morganfields")));
-
-        return new Account[] {acc1, acc2};
+        */
+        return acc;
     }
 
     public static ReadOnlyCommonCents getSampleCommonCents() {
         CommonCents sampleCc = new CommonCents();
-        for (Account account : getSampleAccount()) {
-            sampleCc.addAccount(account);
-        }
+        sampleCc.addAccount(getSampleAccount());
+
         return sampleCc;
     }
 
