c28c7c5a05385a2c78985cac79a67c1242c7e29e
AudreyFelicio <audreyanwar@gmail.com>
2020-10-12 23:29:02 +0800

    Add suggestion for exit command

--- src/main/java/seedu/stock/logic/parser/SuggestionCommandParser.java
+++ src/main/java/seedu/stock/logic/parser/SuggestionCommandParser.java
@@ -1,5 +1,6 @@
 package seedu.stock.logic.parser;
 
+import static seedu.stock.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.stock.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 import static seedu.stock.logic.parser.CliSyntax.PREFIX_INCREMENT_QUANTITY;
 import static seedu.stock.logic.parser.CliSyntax.PREFIX_LOCATION;
@@ -13,11 +14,13 @@ import java.util.List;
 
 import seedu.stock.commons.util.SuggestionUtil;
 import seedu.stock.logic.commands.CommandWords;
+import seedu.stock.logic.commands.ExitCommand;
 import seedu.stock.logic.commands.SuggestionCommand;
 import seedu.stock.logic.parser.exceptions.ParseException;
 
 public class SuggestionCommandParser implements Parser<SuggestionCommand> {
 
+    private static final String MESSAGE_SUGGESTION = "Do you mean \n";
     private String faultyCommandWord;
 
     public SuggestionCommandParser(String faultyCommandWord) {
@@ -47,8 +50,20 @@ public class SuggestionCommandParser implements Parser<SuggestionCommand> {
         if (suggestedCommandWord.equals(faultyCommandWord)) {
             throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
         }
-        toBeDisplayed.append(suggestedCommandWord);
+        toBeDisplayed.append(String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_SUGGESTION));
+
+        switch (suggestedCommandWord) {
+        case ExitCommand.COMMAND_WORD:
+            generateExitSuggestion(toBeDisplayed, argMultimap);
+            break;
+        default:
+            throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
+        }
 
         return new SuggestionCommand(toBeDisplayed.toString());
+    }
+
+    private void generateExitSuggestion(StringBuilder toBeDisplayed, ArgumentMultimap argMultimap) {
+        toBeDisplayed.append(CommandWords.EXIT_COMMAND_WORD);
     }
 }
