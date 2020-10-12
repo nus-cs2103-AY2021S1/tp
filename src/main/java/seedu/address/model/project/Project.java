d2bd27f6634d81b185795f1bbb043427d2b07a0a
GeNiaaz <geniaaz@gmail.com>
2020-10-12 21:14:58 +0800

    Add import statement

--- src/main/java/seedu/address/model/project/Project.java
+++ src/main/java/seedu/address/model/project/Project.java
@@ -2,6 +2,7 @@ package seedu.address.model.project;
 
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
