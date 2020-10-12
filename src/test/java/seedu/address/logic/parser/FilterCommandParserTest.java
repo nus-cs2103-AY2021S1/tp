85ba972b7cf8b366988ec8897ddd757cdac0c45d
Tian Fang <tianfangsg@gmail.com>
2020-10-12 16:00:39 +0800

    Add test cases files

---
+++ src/test/java/seedu/address/logic/parser/FilterCommandParserTest.java
@@ -0,0 +1,22 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.project.FilterCommand;
+
+class FilterCommandParserTest {
+
+    private FilterCommandParser parser = new FilterCommandParser();
+    private static final String MESSAGE_INVALID_FORMAT =
+        String.format(MESSAGE_INVALID_COMMAND_FORMAT, FilterCommand.MESSAGE_USAGE);
+
+    @Test
+    public void parse_missingParts_failure() {
+        // empty user input
+        assertParseFailure(parser, "", MESSAGE_INVALID_FORMAT);
+    }
+    //TODO: will add more when meeting filter is implemented
+}
---
+++ src/test/java/seedu/address/testutil/TypicalTasks.java
@@ -0,0 +1,9 @@
+package seedu.address.testutil;
+
+import seedu.address.model.task.Task;
+
+public class TypicalTasks {
+    public static final String VALID_TASK_DEADLINE= "31-12-2020 10:00:00";
+    public static final String VALID_TASK_NAME = "group meeting on milestone v1.4";
+
+}
