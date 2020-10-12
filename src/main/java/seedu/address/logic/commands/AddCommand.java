bade191f1db40d483d5dc8a13b5f19902d1978c0
Liu-2001 <1034368684@qq.com>
2020-10-12 21:36:34 +0800

    remove unnecessary imports

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -9,8 +9,6 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_CALORIES;
 
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.ExerciseModel;
-import seedu.address.model.Model;
-import seedu.address.model.ModelForExercise;
 import seedu.address.model.exercise.Exercise;
 
 /**
