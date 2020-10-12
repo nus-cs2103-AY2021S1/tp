b9829961b82012322eb9eea44bd51c29d5bdad51
halcon-blanco <saadh98@gmail.com>
2020-10-12 20:37:31 +0800

    Fix bug of incorrect fxml file name in DeliverCard.java

--- src/main/java/seedu/address/ui/DeliveryCard.java
+++ src/main/java/seedu/address/ui/DeliveryCard.java
@@ -8,7 +8,7 @@ import seedu.address.model.delivery.Delivery;
  */
 public class DeliveryCard extends UiPart<Region> {
 
-    private static final String FXML = "ItemListCard.fxml";
+    private static final String FXML = "DeliveryListCard.fxml";
 
     /**
      * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
