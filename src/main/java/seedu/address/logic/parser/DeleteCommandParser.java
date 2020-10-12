ae9a81a1e44e58b8052603e0a867c0b7008d6fcf
Jargonx <neilgupta98@yahoo.com.sg>
2020-10-12 21:39:26 +0800

    Check for State before Deleting Case

--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -1,5 +1,6 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.commons.core.Messages.MESSAGE_INCORRECT_MAIN_PAGE;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.commands.Command.TYPE_CASE;
 import static seedu.address.logic.parser.AddressBookParser.BASIC_COMMAND_FORMAT;
@@ -10,6 +11,7 @@ import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.casecommands.DeleteCaseCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.logic.state.StateManager;
 
 /**
  * Parses input arguments and creates a new DeleteCommand object
@@ -22,24 +24,28 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public DeleteCommand parse(String args) throws ParseException {
+        Index index;
+        final Matcher matcher = BASIC_COMMAND_FORMAT.matcher(args.trim());
+        if (!matcher.matches()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
+        }
+        final String deleteType = matcher.group("commandWord");
+        final String indexString = matcher.group("arguments");
         try {
-            final Matcher matcher = BASIC_COMMAND_FORMAT.matcher(args.trim());
-            if (!matcher.matches()) {
-                throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
-            }
-            final String deleteType = matcher.group("commandWord");
-            final String indexString = matcher.group("arguments");
-            Index index = ParserUtil.parseIndex(indexString);
-
-            switch(deleteType) {
-            case TYPE_CASE:
-                return new DeleteCaseCommand(index);
-            default:
-                throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
-            }
+            index = ParserUtil.parseIndex(indexString);
         } catch (ParseException pe) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE), pe);
+        }
+
+        switch(deleteType) {
+        case TYPE_CASE:
+            if (StateManager.atCasePage()) {
+                throw new ParseException(MESSAGE_INCORRECT_MAIN_PAGE);
+            }
+            return new DeleteCaseCommand(index);
+        default:
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
         }
     }
 
