d9a9eb94d1a7c99f7087d73d2b62db58afb302fc
Tian Fang <tianfangsg@gmail.com>
2020-10-12 11:20:46 +0800

    Update Message usage

--- src/main/java/seedu/address/logic/commands/project/FilterCommand.java
+++ src/main/java/seedu/address/logic/commands/project/FilterCommand.java
@@ -1,5 +1,9 @@
 package seedu.address.logic.commands.project;
 
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TASK_FILTER_BY_ASSIGNEE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TASK_FILTER_BY_DEADLINE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TASK_FILTER_BY_NAME;
+
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
@@ -8,12 +12,19 @@ import seedu.address.model.Model;
 public class FilterCommand extends Command {
     public static final String COMMAND_WORD = "filter";
 
-    public static final String MESSAGE_NOT_IMPLEMENTED_YET = "Remark command not implemented yet";
+    public static final String MESSAGE_NOT_IMPLEMENTED_YET = "Filter command not implemented yet";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
         + ": Filter and show tasks with given predicate\n"
-        + "Parameters: INDEX (must be a positive integer), NAME (must be present in the project)\n"
-        + "Example: " + COMMAND_WORD + " 1 Lucas";
+        + "Parameters: ("
+        + PREFIX_TASK_FILTER_BY_ASSIGNEE + "ASSIGNEE NAME )||("
+        + PREFIX_TASK_FILTER_BY_DEADLINE + "DEADLINE )||("
+        + PREFIX_TASK_FILTER_BY_NAME + "NAME)\n"
+        + "Example: " + COMMAND_WORD + " "
+        + PREFIX_TASK_FILTER_BY_ASSIGNEE + "ASSIGNEE NAME )||("
+        + PREFIX_TASK_FILTER_BY_DEADLINE + "DEADLINE )||("
+        + PREFIX_TASK_FILTER_BY_NAME + "NAME)\n";
+
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
