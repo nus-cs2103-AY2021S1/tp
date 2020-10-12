e6f15b7918cae0e11c757bc0c0f619057284c4c6
AlexCQY <alex_chua@u.nus.edu>
2020-10-12 15:26:55 +0800

    fix checkstyle

--- src/test/java/seedu/address/logic/commands/ScheduleCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ScheduleCommandTest.java
@@ -1,16 +1,11 @@
 package seedu.address.logic.commands;
 
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-
-import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 
-
+import org.junit.jupiter.api.Test;
 
 public class ScheduleCommandTest {
 
