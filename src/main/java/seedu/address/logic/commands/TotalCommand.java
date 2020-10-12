80977e1836e3066746c504bc9e095e880f5f8aa1
duckmoon99 <e0407816@u.nus.edu>
2020-10-12 22:10:40 +0800

    Add Total and Submit commands 2

--- src/main/java/seedu/address/logic/commands/TotalCommand.java
+++ src/main/java/seedu/address/logic/commands/TotalCommand.java
@@ -1,2 +1,20 @@
-package seedu.address.logic.commands;public class TotalCommand {
+package seedu.address.logic.commands;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.order.Order;
+
+public class TotalCommand extends Command {
+
+    public static final String COMMAND_WORD = "total";
+
+    public static final String MESSAGE_RESULT = "Total is $%.2f.";
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        Order order = new Order();
+        order.setOrderItems(model.getFilteredOrderItemList());
+        double total = order.getTotal();
+        return new CommandResult(String.format(MESSAGE_RESULT, total));
+    }
 }
