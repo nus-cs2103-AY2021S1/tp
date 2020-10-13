214d5cccf848219cf20507823fe4ed6d78829c58
Thai Duong <htduong292001@gmail.com>
2020-10-13 21:11:24 +0800

    Update Sample data

--- src/main/java/jimmy/mcgymmy/model/util/SampleDataUtil.java
+++ src/main/java/jimmy/mcgymmy/model/util/SampleDataUtil.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import jimmy.mcgymmy.model.McGymmy;
 import jimmy.mcgymmy.model.ReadOnlyMcGymmy;
+import jimmy.mcgymmy.model.date.Date;
 import jimmy.mcgymmy.model.food.Carbohydrate;
 import jimmy.mcgymmy.model.food.Fat;
 import jimmy.mcgymmy.model.food.Food;
@@ -20,17 +21,17 @@ public class SampleDataUtil {
     public static Food[] getSamplePersons() {
         return new Food[] {
             new Food(new Name("Chicken Rice"), new Protein(200), new Fat(300),
-                    new Carbohydrate(100),
-                    getTagSet("Lunch")),
+                    new Carbohydrate(100), getTagSet("Lunch"),
+                    new Date("20/4/2020")),
             new Food(new Name("Rice Bowl"), new Protein(300), new Fat(200),
-                    new Carbohydrate(100),
-                    getTagSet("Lunch", "Dinner")),
+                    new Carbohydrate(100), getTagSet("Lunch", "Dinner"),
+                    new Date("2/9/2020")),
             new Food(new Name("Fried Chicken"), new Protein(200), new Fat(100),
-                    new Carbohydrate(10),
-                    getTagSet("Tasty")),
+                    new Carbohydrate(10), getTagSet("Tasty"),
+                    new Date("23/7/2019")),
             new Food(new Name("Nasi Lemak"), new Protein(20), new Fat(17),
-                    new Carbohydrate(31),
-                    getTagSet("Lunch")),
+                    new Carbohydrate(31), getTagSet("Lunch"),
+                    Date.currentDate()),
         };
     }
 
