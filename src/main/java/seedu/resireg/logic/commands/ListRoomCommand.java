3e6317b6097cad09abcf348902cc33b2860de8cc
Jing Yen Loh <lohjingyen@gmail.com>
2020-10-13 13:36:27 +0800

    Implement viewing of allocated rooms

--- src/main/java/seedu/resireg/logic/commands/ListRoomCommand.java
+++ src/main/java/seedu/resireg/logic/commands/ListRoomCommand.java
@@ -9,32 +9,51 @@ import seedu.resireg.model.Model;
  */
 public class ListRoomCommand extends Command {
     public static final String COMMAND_WORD = "rooms";
-    public static final String COMMAND_VACANT_FLAG = "--vacant";
+    public static final String COMMAND_VACANT_FLAG = "vacant";
+    public static final String COMMAND_ALLOCATED_FLAG = "allocated";
 
     public static final String MESSAGE_SUCCESS = "Listed all rooms";
     public static final String MESSAGE_VACANT_SUCCESS = "Listed all vacant rooms";
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all rooms within the system. If the "
+    public static final String MESSAGE_ALLOCATED_SUCCESS = "Listed all allocated rooms";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all rooms within the system. If the --"
             + COMMAND_VACANT_FLAG
-            + " parameter is specified, find only rooms that are vacant, meaning no students are assigned to them.\n"
-            + "Parameters: [" + COMMAND_VACANT_FLAG + "]...\n"
+            + " flag is specified, find only rooms that are vacant, meaning no students are assigned to them. "
+            + "Otherwise, if the --" + COMMAND_ALLOCATED_FLAG + " flag is specified, "
+            + "find only rooms that have students assigned to them.\n"
+            + "Parameters: [--" + COMMAND_VACANT_FLAG + "|" + COMMAND_ALLOCATED_FLAG + "]\n"
             + "Example: " + COMMAND_WORD;
 
-    private final boolean shouldDisplayOnlyVacant;
+    private final ListRoomFilter filter;
 
-    public ListRoomCommand(boolean shouldDisplayOnlyVacant) {
-        this.shouldDisplayOnlyVacant = shouldDisplayOnlyVacant;
+    public ListRoomCommand(ListRoomFilter filter) {
+        this.filter = filter;
     }
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
 
-        if (shouldDisplayOnlyVacant) {
-            model.updateFilteredRoomList(room -> !room.hasStudent());
-            return new ToggleCommandResult(MESSAGE_VACANT_SUCCESS, TabView.ROOMS);
-        } else {
+        String message = MESSAGE_SUCCESS;
+        switch (filter) {
+        case ALL: {
             model.updateFilteredRoomList(Model.PREDICATE_SHOW_ALL_ROOMS);
-            return new ToggleCommandResult(MESSAGE_SUCCESS, TabView.ROOMS);
+            message = MESSAGE_SUCCESS;
+            break;
         }
+        case VACANT: {
+            model.updateFilteredRoomList(Model.PREDICATE_SHOW_VACANT_ROOMS);
+            message = MESSAGE_VACANT_SUCCESS;
+            break;
+        }
+        case ALLOCATED: {
+            model.updateFilteredRoomList(Model.PREDICATE_SHOW_ALLOCATED_ROOMS);
+            message = MESSAGE_ALLOCATED_SUCCESS;
+            break;
+        }
+        default:
+            break;
+        }
+        return new ToggleCommandResult(message, TabView.ROOMS);
     }
 }
--- src/main/java/seedu/resireg/logic/parser/ListRoomCommandParser.java
+++ src/main/java/seedu/resireg/logic/parser/ListRoomCommandParser.java
@@ -3,6 +3,7 @@ package seedu.resireg.logic.parser;
 import static seedu.resireg.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import seedu.resireg.logic.commands.ListRoomCommand;
+import seedu.resireg.logic.commands.ListRoomFilter;
 import seedu.resireg.logic.parser.exceptions.ParseException;
 
 public class ListRoomCommandParser implements Parser<ListRoomCommand> {
@@ -11,9 +12,18 @@ public class ListRoomCommandParser implements Parser<ListRoomCommand> {
         String trimmedArgs = args.strip();
 
         if (trimmedArgs.isEmpty()) {
-            return new ListRoomCommand(false);
-        } else if (trimmedArgs.equalsIgnoreCase(ListRoomCommand.COMMAND_VACANT_FLAG)) {
-            return new ListRoomCommand(true);
+            return new ListRoomCommand(ListRoomFilter.ALL);
+        }
+        if (!trimmedArgs.startsWith("--")) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListRoomCommand.MESSAGE_USAGE));
+        }
+        trimmedArgs = trimmedArgs.substring(2); // "strip away the leading "--"
+        if (trimmedArgs.equalsIgnoreCase(ListRoomCommand.COMMAND_VACANT_FLAG)) {
+            return new ListRoomCommand(ListRoomFilter.VACANT);
+        }
+        if (trimmedArgs.equalsIgnoreCase(ListRoomCommand.COMMAND_ALLOCATED_FLAG)) {
+            return new ListRoomCommand(ListRoomFilter.ALLOCATED);
         }
         throw new ParseException(
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListRoomCommand.MESSAGE_USAGE));
