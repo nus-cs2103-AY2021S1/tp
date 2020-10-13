54f8c60e6f60752ed143c24185f5193e7b50798f
Ebolaeater <53635104+Ebolaeater@users.noreply.github.com>
2020-10-14 02:48:07 +0800

    Fixed add command

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -2,10 +2,15 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.List;
+
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.parser.ParserUtil;
 import seedu.address.model.Model;
 import seedu.address.model.food.Food;
 import seedu.address.model.order.OrderItem;
+
 //TODO change to fit vendor better
 /**
  * Adds a person to the address book.
@@ -18,27 +23,28 @@ public class AddCommand extends Command {
             + "Parameters: "
             + " Index of FoodItem in Menu "
             + " [Quantity of Food to add]";
-    // To remove
-    public static final String MESSAGE_DUPLICATE_VENDOR = "This vendor already exists in the address book";
-    public static final String MESSAGE_SUCCESS = "%1$s has been added to your Order";
-    private final OrderItem toAdd;
+
+    public static final String MESSAGE_ADD_FIRST_SUCCESS = "%1$s has been added to your Order";
+    public static final String MESSAGE_ADD_QUANTITY_SUCCESS = "%1$s has been added to your Order";
+
+    private final Index addIndex;
     private final int quantity;
 
     /**
      * Creates an AddCommand to add the specified {@code Food}
      */
-    public AddCommand(Food food) {
-        requireNonNull(food);
-        toAdd = new OrderItem(food, 1);
+    public AddCommand(Index index) {
+        requireNonNull(index);
+        this.addIndex = index;
         this.quantity = 1;
     }
 
     /**
      * Creates an AddCommand to add the specified {@code Food}
      */
-    public AddCommand(Food food, int quantity) {
-        requireNonNull(food);
-        toAdd = new OrderItem(food, quantity);
+    public AddCommand(Index index, int quantity) {
+        requireNonNull(index);
+        this.addIndex = index;
         this.quantity = quantity;
     }
 
@@ -46,20 +52,50 @@ public class AddCommand extends Command {
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
-        model.addOrderItem(toAdd);
-        return new CommandResult(String.format(MESSAGE_SUCCESS, getAddMessage()));
+        List<OrderItem> orderItemList = model.getFilteredOrderItemList(); //Order: list of orderitems
+        // Todo: This index value will be that of the chosen vendor. As of now the first menu on the list is chosen
+        List<Food> menu = model.getFilteredFoodList(0); // Menu: list of food
+        int menuSize;
+
+        try {
+            menuSize = menu.size();
+        } catch (NullPointerException ex) {
+            throw new NullPointerException("Menu file has problems");
+        }
+
+        if (addIndex.getZeroBased() >= menuSize) {
+            // index provided larger than size of menu
+            throw new CommandException(ParserUtil.MESSAGE_INVALID_ORDERITEM_DISPLAYED_INDEX);
+        }
+
+        if (!OrderItem.isValidQuantity(quantity)) {
+            throw new CommandException(ParserUtil.MESSAGE_INVALID_ORDERITEM_DISPLAYED_QUANTITY);
+        }
+
+        String addMessage;
+        Food toAdd = menu.get(addIndex.getZeroBased()); //get the food from the menu
+        OrderItem orderItem = new OrderItem(toAdd, quantity); //check if this orederitem is already in the order
+        if (!orderItemList.contains(orderItem)) {
+            // completely new addition to the order
+            model.addOrderItem(orderItem);
+            addMessage = String.format(MESSAGE_ADD_FIRST_SUCCESS, orderItem);
+        } else {
+            // increment the current order by the specified quantity
+            int index = orderItemList.indexOf(orderItem);
+            OrderItem original = orderItemList.get(index);
+            int newQuantity = original.getQuantity() + this.quantity;
+            original.setQuantity(newQuantity);
+            model.setOrderItem(original, original);
+            addMessage = String.format(MESSAGE_ADD_QUANTITY_SUCCESS, orderItem);
+        }
+        return new CommandResult(addMessage);
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof AddCommand // instanceof handles nulls
-                && toAdd.equals(((AddCommand) other).toAdd));
+                && addIndex.equals(((AddCommand) other).addIndex));
     }
 
-    public String getAddMessage() {
-        final StringBuilder builder = new StringBuilder();
-        builder.append(toAdd.getName()).append(" x ").append(quantity);
-        return builder.toString();
-    }
 }
--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -23,6 +23,14 @@ public class ParserUtil {
 
     public static final String MESSAGE_INVALID_INDEX = "%s is not a non-zero unsigned integer.";
     public static final String MESSAGE_INVALID_QUANTITY = "Quantity given is invalid.";
+    public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
+    public static final String MESSAGE_INVALID_ARG_LENGTHS = "Incorrect number of arguments provided.";
+    public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
+    public static final String MESSAGE_INVALID_VENDOR_DISPLAYED_INDEX = "The vendor index provided is invalid";
+    public static final String MESSAGE_VENDORS_LISTED_OVERVIEW = "%1$d vendors listed!";
+    public static final String MESSAGE_INVALID_ORDERITEM_DISPLAYED_INDEX = "The order item index provided is invalid";
+    public static final String MESSAGE_INVALID_ORDERITEM_DISPLAYED_QUANTITY = "The order item quantity "
+            + "provided is invalid";
 
 
     /**
