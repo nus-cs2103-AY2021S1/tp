7f24280b4c191af85edaf625a61e7a25d7226350
Liu-2001 <1034368684@qq.com>
2020-10-13 18:06:56 +0800

    update macther

--- src/main/java/seedu/address/logic/parser/ExerciseBookParser.java
+++ src/main/java/seedu/address/logic/parser/ExerciseBookParser.java
@@ -6,7 +6,11 @@ import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import seedu.address.logic.commands.*;
+import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.commands.ArchiveCommand;
+import seedu.address.logic.commands.CommandForExercise;
+import seedu.address.logic.commands.HelpCommand;
+import seedu.address.logic.commands.UpdateExerciseCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 /**
  * Parses user input.
