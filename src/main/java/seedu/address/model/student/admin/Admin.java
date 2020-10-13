9aff7dd3bebb781138a4822b2068a28abed46b25
hogantan <hogan_tan@hotmail.sg>
2020-10-13 14:52:56 +0800

    Update GUI

--- src/main/java/seedu/address/model/student/admin/Admin.java
+++ src/main/java/seedu/address/model/student/admin/Admin.java
@@ -69,6 +69,18 @@ public class Admin {
         return placeholder;
     }
 
+    /**
+     * Get additional details of student formatted for GUI use.
+     * @return formatted additional details.
+     */
+    public String getFormattedDetails() {
+        String result = "";
+        for (AdditionalDetail detail: details) {
+            result = result + "- " + detail.toString() + "\n";
+        }
+        return result;
+    }
+
     @Override
     public int hashCode() {
         return Objects.hash(classVenue, classTime, fee, paymentDate, details);
