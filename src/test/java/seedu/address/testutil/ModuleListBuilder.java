1365aec7d251f5a285aa64362e1af5f365714e67
michael-setia <michaelsetia55@gmail.com>
2020-10-13 16:52:11 +0800

    Refactor ModuleListBuilder class

--- src/test/java/seedu/address/testutil/ModuleListBuilder.java
+++ src/test/java/seedu/address/testutil/ModuleListBuilder.java
@@ -1,12 +1,12 @@
 package seedu.address.testutil;
 
 import seedu.address.model.ModuleList;
-import seedu.address.model.person.Person;
+import seedu.address.model.module.Module;
 
 /**
- * A utility class to help with building Addressbook objects.
+ * A utility class to help with building ModuleList objects.
  * Example usage: <br>
- *     {@code AddressBook ab = new AddressBookBuilder().withPerson("John", "Doe").build();}
+ *     {@code ModuleList moduleList = new ModuleList().withModule(CS2103T).build();}
  */
 public class ModuleListBuilder {
 
@@ -21,10 +21,10 @@ public class ModuleListBuilder {
     }
 
     /**
-     * Adds a new {@code Person} to the {@code AddressBook} that we are building.
+     * Adds a new {@code Module} to the {@code ModuleList} that we are building.
      */
-    public ModuleListBuilder withPerson(Person person) {
-        // moduleList.addPerson(person);
+    public ModuleListBuilder withModule(Module module) {
+        moduleList.addModule(module);
         return this;
     }
 
