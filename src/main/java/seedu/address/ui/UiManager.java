3bb45dfbacc7a5ebd9ce03ddf564ca44e408dc61
Ma-Yueran <mayuerantom@outlook.com>
2020-10-15 00:07:48 +0800

    Delete old ui classes that are no longer used

--- src/main/java/seedu/address/ui/UiManager.java
+++ src/main/java/seedu/address/ui/UiManager.java
@@ -18,7 +18,7 @@ public class UiManager implements Ui {
 
     public static final String ALERT_DIALOG_PANE_FIELD_ID = "alertDialogPane";
 
-    private static final Logger logger = LogsCenter.getLogger(OldUiManager.class);
+    private static final Logger logger = LogsCenter.getLogger(UiManager.class);
     private static final String ICON_APPLICATION = "/images/hello_file_icon.png";
 
     private Logic logic;
