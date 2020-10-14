9cf3ef3799c18ae491771672c9aaed4b90d4ea06
jetnew <notesjet@gmail.com>
2020-10-14 11:19:54 +0800

    Fix checkstyle

--- src/main/java/seedu/resireg/logic/parser/DeallocateCommandParser.java
+++ src/main/java/seedu/resireg/logic/parser/DeallocateCommandParser.java
@@ -1,13 +1,11 @@
 package seedu.resireg.logic.parser;
 
 import static seedu.resireg.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.resireg.logic.parser.CliSyntax.PREFIX_ROOM_INDEX;
 import static seedu.resireg.logic.parser.CliSyntax.PREFIX_STUDENT_INDEX;
 
 import java.util.stream.Stream;
 
 import seedu.resireg.commons.core.index.Index;
-import seedu.resireg.logic.commands.AllocateCommand;
 import seedu.resireg.logic.commands.DeallocateCommand;
 import seedu.resireg.logic.parser.exceptions.ParseException;
 
