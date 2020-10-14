9dbc2ad698a2ead97f2b60f9254e383b405a440c
Chen Xihao <chenxihao0627@gmail.com>
2020-10-14 17:04:26 +0800

    Restore changes to interface and updated javadoc

--- src/main/java/nustorage/model/ReadOnlyFinanceAccount.java
+++ src/main/java/nustorage/model/ReadOnlyFinanceAccount.java
@@ -1,17 +1,12 @@
 package nustorage.model;
 
 
-import java.util.List;
-import java.util.Optional;
-import java.util.function.Predicate;
-
 import javafx.collections.ObservableList;
-import nustorage.commons.core.index.Index;
 import nustorage.model.record.FinanceRecord;
 
 
 /**
- * Unmodifiable view of an address book
+ * Unmodifiable view of a finance account
  */
 public interface ReadOnlyFinanceAccount {
 
@@ -27,29 +22,5 @@ public interface ReadOnlyFinanceAccount {
      * This list will not contain any duplicate persons.
      */
     ObservableList<FinanceRecord> asUnmodifiableObservableList();
-
-
-    boolean hasRecord(FinanceRecord record);
-
-
-    void addRecord(FinanceRecord record);
-
-
-    void setFinanceRecord(FinanceRecord target, FinanceRecord newRecord);
-
-
-    Optional<FinanceRecord> removeRecord(Index targetIndex);
-
-
-    int count();
-
-
-    double netProfit();
-
-
-    List<FinanceRecord> filterRecords(Predicate<FinanceRecord> filter);
-
-
-    boolean isEmpty();
 
 }
