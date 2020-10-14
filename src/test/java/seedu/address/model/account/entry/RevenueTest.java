4bc7a247999a2195e9aad23b580b0b514f219267
Cheok Su Anne <e0424761@u.nus.edu>
2020-10-14 17:20:33 +0800

    Add RevenueTest

---
+++ src/test/java/seedu/address/model/account/entry/RevenueTest.java
@@ -0,0 +1,45 @@
+package seedu.address.model.account.entry;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.model.tag.Tag;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static seedu.address.testutil.Assert.assertThrows;
+
+class RevenueTest {
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Revenue(null, null, null));
+    }
+
+    @Test
+    public void constructor_invalidDescription_throwsIllegalArgumentException() {
+        String invalidDescription = "";
+        Amount amount = new Amount("15.00");
+        Set<Tag> tags = new HashSet<>();
+        tags.add(new Tag("craft"));
+        tags.add(new Tag("art"));
+
+        assertThrows(IllegalArgumentException.class, () -> new Revenue(
+                new Description(invalidDescription), amount, tags));
+    }
+
+    @Test
+    public void constructor_invalidAmount_throwsIllegalArgumentException() {
+        Description description = new Description("bought craft supplies");
+        String amount = "";
+        Set<Tag> tags = new HashSet<>();
+        tags.add(new Tag("craft"));
+        tags.add(new Tag("art"));
+
+        assertThrows(IllegalArgumentException.class, () -> new Expense(description, new Amount(amount), tags));
+    }
+
+
+
+
+}
