2e5bc9e8c8a13ec332479ecd4f21792e5fbe470c
murtubak <murtubakchickenss@gmail.com>
2020-10-15 00:40:06 +0800

    Update Assignment and GradeTracker class

--- src/main/java/seedu/address/model/module/grade/Assignment.java
+++ src/main/java/seedu/address/model/module/grade/Assignment.java
@@ -4,18 +4,13 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
 /**
- * Represents a Person's name in the address book.
+ * Represents an assignment in the Grade Tracker.
  * Guarantees: immutable; is valid as declared in {@link #isValidAssignmentName(String)} (String)}
  */
 public class Assignment {
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Names should only contain alphanumeric characters and spaces, and it should not be blank";
-
-    /*
-     * The first character of the address must not be a whitespace,
-     * otherwise " " (a blank string) becomes a valid input.
-     */
+            "Assignment names should only contain alphanumeric characters and spaces, and it should not be blank";
     public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
 
     public final String assignmentName;
