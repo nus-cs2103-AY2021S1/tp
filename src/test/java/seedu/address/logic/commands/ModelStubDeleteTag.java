3e8d4f866cf46196d8a6c959edbbea31a55c519b
li-s <lishuo980@gmail.com>
2020-10-14 14:02:37 +0800

    Add JUnit test for Untag Command

---
+++ src/test/java/seedu/address/logic/commands/ModelStubDeleteTag.java
@@ -0,0 +1,57 @@
+package seedu.address.logic.commands;
+
+import seedu.address.model.tag.Tag;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import static java.util.Objects.requireNonNull;
+
+public class ModelStubDeleteTag extends ModelStub {
+    private List<Tag> tagList;
+
+    ModelStubDeleteTag(List<Tag> tagList) {
+        requireNonNull(tagList);
+        this.tagList = tagList;
+    }
+
+    @Override
+    public boolean hasTag(Tag tag) {
+        requireNonNull(tag);
+        return this.tagList.contains(tag);
+    }
+
+    @Override
+    public void deleteTag(Tag tag) {
+        requireNonNull(tag);
+        this.tagList.remove(tag);
+    }
+
+    public List<Tag> findFilteredTagList(Predicate<Tag> predicate) {
+        for (Tag tag : tagList) {
+            if (predicate.test(tag)) {
+                return List.of(tag);
+            }
+        }
+        return new ArrayList<>();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+
+        }
+
+        if (other instanceof ModelStubDeleteTag) {
+            if (tagList.isEmpty() && ((ModelStubDeleteTag) other).tagList.isEmpty()) {
+                return true;
+            } else if (this.tagList.equals(((ModelStubDeleteTag) other).tagList)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
--- src/test/java/seedu/address/logic/commands/UntagCommandTest.java
+++ src/test/java/seedu/address/logic/commands/UntagCommandTest.java
@@ -2,102 +2,82 @@ package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_CS2101;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_CS2103;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.logic.commands.CommandTestUtil.showTagAtIndex;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_TAG;
-import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_TAG;
-import static seedu.address.testutil.TypicalTags.getTypicalAddressBook;
+import static seedu.address.testutil.Assert.assertThrows;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import seedu.address.commons.core.Messages;
-import seedu.address.commons.core.index.Index;
-import seedu.address.model.Model;
-import seedu.address.model.ModelManager;
-import seedu.address.model.UserPrefs;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.tag.Tag;
+import seedu.address.model.tag.TagName;
+import seedu.address.testutil.TagBuilder;
+
+import java.util.ArrayList;
 
 /**
  * Contains integration tests (interaction with the Model, UndoCommand and RedoCommand) and unit tests for
  * {@code UntagCommand}.
  */
 public class UntagCommandTest {
+    private ModelStub modelStub;
 
-    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    @BeforeEach
+    public void setUp() {
+        Tag validTag = new TagBuilder().build();
+        ArrayList<Tag> tagList = new ArrayList<>();
+        tagList.add(validTag);
+
+        modelStub = new ModelStubDeleteTag(tagList);
+    }
 
     @Test
     public void execute_validTagName_success() {
-//        Tag tagToDelete = model.getFilteredTagList().get(INDEX_FIRST_TAG.getZeroBased());
-//        UntagCommand untagCommand = new UntagCommand(INDEX_FIRST_TAG);
-//
-//        String expectedMessage = String.format(UntagCommand.MESSAGE_UNTAG_TAG_SUCCESS, tagToDelete);
-//
-//        ModelManager expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
-//        expectedModel.deleteTag(tagToDelete);
-//
-//        assertCommandSuccess(untagCommand, model, expectedMessage, expectedModel);
+        Tag tagToDelete = new TagBuilder().build();
+        UntagCommand untagCommand = new UntagCommand(tagToDelete.getTagName());
+
+        String expectedMessage = String.format(UntagCommand.MESSAGE_UNTAG_TAG_SUCCESS, tagToDelete.getTagName());
+
+        ModelStub expectedModel = new ModelStubDeleteTag(new ArrayList<Tag>());
+
+        assertCommandSuccess(untagCommand, modelStub, expectedMessage, expectedModel);
     }
-//
-//    @Test
-//    public void execute_invalidIndexUnfilteredList_throwsCommandException() {
-//        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredTagList().size() + 1);
-//        UntagCommand untagCommand = new UntagCommand(outOfBoundIndex);
-//
-//        assertCommandFailure(untagCommand, model, Messages.MESSAGE_INVALID_TAG_DISPLAYED_INDEX);
-//    }
-//
-//    @Test
-//    public void execute_validIndexFilteredList_success() {
-//        showTagAtIndex(model, INDEX_FIRST_TAG);
-//
-//        Tag tagToDelete = model.getFilteredTagList().get(INDEX_FIRST_TAG.getZeroBased());
-//        UntagCommand untagCommand = new UntagCommand(INDEX_FIRST_TAG);
-//
-//        String expectedMessage = String.format(untagCommand.MESSAGE_UNTAG_TAG_SUCCESS, tagToDelete);
-//
-//        Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
-//        expectedModel.deleteTag(tagToDelete);
-//        showNoTag(expectedModel);
-//
-//        assertCommandSuccess(untagCommand, model, expectedMessage, expectedModel);
-//    }
-//
-//    @Test
-//    public void execute_invalidIndexFilteredList_throwsCommandException() {
-//        showTagAtIndex(model, INDEX_FIRST_TAG);
-//
-//        Index outOfBoundIndex = INDEX_SECOND_TAG;
-//        // ensures that outOfBoundIndex is still in bounds of address book list
-//        assertTrue(outOfBoundIndex.getZeroBased() < model.getAddressBook().getTagList().size());
-//
-//        UntagCommand untagCommand = new UntagCommand(outOfBoundIndex);
-//
-//        assertCommandFailure(untagCommand, model, Messages.MESSAGE_INVALID_TAG_DISPLAYED_INDEX);
-//    }
-//
-//    @Test
-//    public void equals() {
-//        UntagCommand deleteFirstCommand = new UntagCommand(INDEX_FIRST_TAG);
-//        UntagCommand deleteSecondCommand = new UntagCommand(INDEX_SECOND_TAG);
-//
-//        // same object -> returns true
-//        assertTrue(deleteFirstCommand.equals(deleteFirstCommand));
-//
-//        // same values -> returns true
-//        UntagCommand deleteFirstCommandCopy = new UntagCommand(INDEX_FIRST_TAG);
-//        assertTrue(deleteFirstCommand.equals(deleteFirstCommandCopy));
-//
-//        // different types -> returns false
-//        assertFalse(deleteFirstCommand.equals(1));
-//
-//        // null -> returns false
-//        assertFalse(deleteFirstCommand.equals(null));
-//
-//        // different person -> returns false
-//        assertFalse(deleteFirstCommand.equals(deleteSecondCommand));
-//    }
-//
+
+    @Test
+    public void execute_tagNotInModel_throwsCommandException() {
+        TagName nonExistTagName = new TagName("jalksdjlfkjsdlakjflsjlj");
+        UntagCommand untagCommand = new UntagCommand(nonExistTagName);
+
+        assertThrows(CommandException.class, String.format(UntagCommand.MESSAGE_TAG_NOT_FOUND, nonExistTagName),
+                () -> untagCommand.execute(modelStub));
+    }
+
+    @Test
+    public void equals() {
+        TagName validTag1 = new TagName(VALID_NAME_CS2101);
+        TagName validTag2 = new TagName(VALID_NAME_CS2103);
+        UntagCommand deleteFirstCommand = new UntagCommand(validTag1);
+        UntagCommand deleteSecondCommand = new UntagCommand(validTag2);
+
+        // same object -> returns true
+        assertTrue(deleteFirstCommand.equals(deleteFirstCommand));
+
+        // same values -> returns true
+        UntagCommand deleteFirstCommandCopy = new UntagCommand(new TagName(VALID_NAME_CS2101));
+        assertTrue(deleteFirstCommand.equals(deleteFirstCommandCopy));
+
+        // different types -> returns false
+        assertFalse(deleteFirstCommand.equals(new TagName(VALID_NAME_CS2101)));
+
+        // null -> returns false
+        assertFalse(deleteFirstCommand.equals(null));
+
+        // different tag -> returns false
+        assertFalse(deleteFirstCommand.equals(deleteSecondCommand));
+    }
+
 //    /**
 //     * Updates {@code model}'s filtered list to show no one.
 //     */
