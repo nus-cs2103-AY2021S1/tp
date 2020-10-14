d92f18bf6ecca342a59566d0240865e4bd7c7a7e
Kacalamaka <heinrich2001.ang@gmail.com>
2020-10-15 00:18:32 +0800

    Fix Bug in Retag Command User now can retag with the same old and new tag without being prompt error message

--- src/main/java/seedu/address/logic/commands/RetagCommand.java
+++ src/main/java/seedu/address/logic/commands/RetagCommand.java
@@ -67,7 +67,7 @@ public class RetagCommand extends Command {
 
         // Check if newTagName is in tag list
         List<Tag> newTagList = model.findFilteredTagList(new TagNameEqualsKeywordPredicate(newTagName));
-        if (!newTagList.isEmpty()) {
+        if (!newTagList.isEmpty() && !newTagName.equals(oldTagName)) {
             throw new CommandException(String.format(MESSAGE_DUPLICATE_TAG));
         }
 
