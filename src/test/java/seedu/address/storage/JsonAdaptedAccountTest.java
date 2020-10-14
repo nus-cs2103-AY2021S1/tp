976cc1ca33fe810483c57fc2cab7d3a63a60922e
lettuceman <manle.9a4@gmail.com>
2020-10-14 17:42:50 +0800

    Add tests for JsonAdaptedRevenue, some tests for JsonAdaptedAccount

---
+++ src/test/java/seedu/address/storage/JsonAdaptedAccountTest.java
@@ -0,0 +1,21 @@
+package seedu.address.storage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.commons.util.JsonUtil;
+import seedu.address.model.account.Account;
+
+public class JsonAdaptedAccountTest {
+
+    @Test
+    public void toModelType_validTypicalAccount_success() {
+        Account 
+    }
+}
--- src/test/java/seedu/address/storage/JsonAdaptedRevenueTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedRevenueTest.java
@@ -1,7 +1,7 @@
 package seedu.address.storage;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static seedu.address.storage.JsonAdaptedExpense.MISSING_FIELD_MESSAGE_FORMAT;
+import static seedu.address.storage.JsonAdaptedRevenue.MISSING_FIELD_MESSAGE_FORMAT;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import java.util.ArrayList;
@@ -13,62 +13,62 @@ import org.junit.jupiter.api.Test;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.account.entry.Amount;
 import seedu.address.model.account.entry.Description;
-import seedu.address.model.account.entry.Expense;
-import seedu.address.testutil.ExpenseBuilder;
+import seedu.address.model.account.entry.Revenue;
+import seedu.address.testutil.RevenueBuilder;
 
-public class JsonAdaptedExpenseTest {
+public class JsonAdaptedRevenueTest {
     private static final String INVALID_DESCRIPTION = " ";
     private static final String INVALID_AMOUNT = "a1";
     private static final String INVALID_TAG = "#friend";
 
     private static final String VALID_DESCRIPTION = "bought cooking utensils";
     private static final String VALID_AMOUNT = "1000.39";
-    private static final List<JsonAdaptedTag> VALID_TAGS = new ExpenseBuilder().build().getTags().stream()
+    private static final List<JsonAdaptedTag> VALID_TAGS = new RevenueBuilder().build().getTags().stream()
         .map(JsonAdaptedTag::new)
         .collect(Collectors.toList());
 
 
     @Test
-    public void toModelType_validExpenseDetails_returnsExpense() throws Exception {
-        Expense modelExpense = new ExpenseBuilder().build();
-        JsonAdaptedExpense adaptedExpense = new JsonAdaptedExpense(modelExpense);
-        assertEquals(modelExpense, adaptedExpense.toModelType());
+    public void toModelType_validRevenueDetails_returnsRevenue() throws Exception {
+        Revenue modelRevenue = new RevenueBuilder().build();
+        JsonAdaptedRevenue adaptedRevenue = new JsonAdaptedRevenue(modelRevenue);
+        assertEquals(modelRevenue, adaptedRevenue.toModelType());
     }
 
     @Test
     public void toModelType_invalidDescription_throwsIllegalValueException() {
-        JsonAdaptedExpense adaptedExpense = new JsonAdaptedExpense(INVALID_DESCRIPTION, VALID_AMOUNT, VALID_TAGS);
+        JsonAdaptedRevenue adaptedRevenue = new JsonAdaptedRevenue(INVALID_DESCRIPTION, VALID_AMOUNT, VALID_TAGS);
         String expectedMessage = Description.MESSAGE_CONSTRAINTS;
-        assertThrows(IllegalValueException.class, expectedMessage, adaptedExpense::toModelType);
+        assertThrows(IllegalValueException.class, expectedMessage, adaptedRevenue::toModelType);
     }
 
     @Test
     public void toModelType_nullDescription_throwsIllegalValueException() {
-        JsonAdaptedExpense adaptedExpense = new JsonAdaptedExpense(null, VALID_AMOUNT, VALID_TAGS);
+        JsonAdaptedRevenue adaptedRevenue = new JsonAdaptedRevenue(null, VALID_AMOUNT, VALID_TAGS);
         String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Description.class.getSimpleName());
-        assertThrows(IllegalValueException.class, expectedMessage, adaptedExpense::toModelType);
+        assertThrows(IllegalValueException.class, expectedMessage, adaptedRevenue::toModelType);
     }
 
     @Test
     public void toModelType_invalidAmount_throwsIllegalValueException() {
-        JsonAdaptedExpense adaptedExpense = new JsonAdaptedExpense(VALID_DESCRIPTION, INVALID_AMOUNT, VALID_TAGS);
+        JsonAdaptedRevenue adaptedRevenue = new JsonAdaptedRevenue(VALID_DESCRIPTION, INVALID_AMOUNT, VALID_TAGS);
         String expectedMessage = Amount.MESSAGE_CONSTRAINTS;
-        assertThrows(IllegalValueException.class, expectedMessage, adaptedExpense::toModelType);
+        assertThrows(IllegalValueException.class, expectedMessage, adaptedRevenue::toModelType);
     }
 
     @Test
     public void toModelType_nullAmount_throwsIllegalValueException() {
-        JsonAdaptedExpense adaptedExpense = new JsonAdaptedExpense(VALID_DESCRIPTION, null, VALID_TAGS);
+        JsonAdaptedRevenue adaptedRevenue = new JsonAdaptedRevenue(VALID_DESCRIPTION, null, VALID_TAGS);
         String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Amount.class.getSimpleName());
-        assertThrows(IllegalValueException.class, expectedMessage, adaptedExpense::toModelType);
+        assertThrows(IllegalValueException.class, expectedMessage, adaptedRevenue::toModelType);
     }
 
     @Test
     public void toModelType_invalidTag_throwsIllegalValueException() {
         List<JsonAdaptedTag> invalidTags = new ArrayList<>(VALID_TAGS);
         invalidTags.add(new JsonAdaptedTag(INVALID_TAG));
-        JsonAdaptedExpense adaptedExpense = new JsonAdaptedExpense(VALID_DESCRIPTION, VALID_AMOUNT, invalidTags);
-        assertThrows(IllegalValueException.class, adaptedExpense::toModelType);
+        JsonAdaptedRevenue adaptedRevenue = new JsonAdaptedRevenue(VALID_DESCRIPTION, VALID_AMOUNT, invalidTags);
+        assertThrows(IllegalValueException.class, adaptedRevenue::toModelType);
     }
 
 }
