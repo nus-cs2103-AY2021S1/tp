9102d7875c569416e331adec6def698bd710e8c0
jordanyoong <37181485+jordanyoong@users.noreply.github.com>
2020-10-14 23:48:52 +0800

    Fix more Checkstyle

--- src/test/java/seedu/address/logic/commands/DeleteCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteCommandTest.java
@@ -33,8 +33,8 @@ public class DeleteCommandTest {
 
     @Test
     public void constructor_nullEntry_throwsNullPointerException() {
-        assertThrows(NullPointerException.class, () -> new DeleteCommand(INDEX_FIRST_PERSON, expense).
-                execute(modelStub, activeAccountStub));
+        assertThrows(NullPointerException.class, () -> new DeleteCommand(INDEX_FIRST_PERSON, expense)
+                .execute(modelStub, activeAccountStub));
     }
 
     @Test
@@ -55,14 +55,14 @@ public class DeleteCommandTest {
 
     @Test
     public void execute_validIndexExpense_failure() {
-        assertThrows(CommandException.class, () -> new DeleteCommand(INDEX_TENTH_PERSON, expense).
-                execute(modelStub, activeAccount));
+        assertThrows(CommandException.class, () -> new DeleteCommand(INDEX_TENTH_PERSON, expense)
+                .execute(modelStub, activeAccount));
     }
 
     @Test
     public void execute_validIndexRevenue_failure() {
-        assertThrows(CommandException.class, () -> new DeleteCommand(INDEX_TENTH_PERSON, revenue).
-                execute(modelStub, activeAccount));
+        assertThrows(CommandException.class, () -> new DeleteCommand(INDEX_TENTH_PERSON, revenue)
+                .execute(modelStub, activeAccount));
     }
 
 }
