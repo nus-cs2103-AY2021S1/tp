797ffc42abdd261e6c67cdc120ba51df890fcdbe
Wincent <wincenttsjoi@gmail.com>
2020-10-15 19:00:15 +0800

    Add deliveryname test

--- src/test/java/seedu/address/model/delivery/DeliveryNameTest.java
+++ src/test/java/seedu/address/model/delivery/DeliveryNameTest.java
@@ -1,8 +1,9 @@
 package seedu.address.model.delivery;
 
 import org.junit.jupiter.api.Test;
-import seedu.address.model.item.Name;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
 public class DeliveryNameTest {
@@ -11,29 +12,29 @@ public class DeliveryNameTest {
     public void constructor_null_throwsNullPointerException() {
         assertThrows(NullPointerException.class, () -> new DeliveryName(null));
     }
-//
-//    @Test
-//    public void constructor_invalidName_throwsIllegalArgumentException() {
-//        String invalidName = "";
-//        assertThrows(IllegalArgumentException.class, () -> new Name(invalidName));
-//    }
-//
-//    @Test
-//    public void isValidName() {
-//        // null name
-//        assertThrows(NullPointerException.class, () -> Name.isValidName(null));
-//
-//        // invalid name
-//        assertFalse(Name.isValidName("")); // empty string
-//        assertFalse(Name.isValidName(" ")); // spaces only
-//        assertFalse(Name.isValidName("^")); // only non-alphanumeric characters
-//        assertFalse(Name.isValidName("chicken*")); // contains non-alphanumeric characters
-//
-//        // valid name
-//        assertTrue(Name.isValidName("premium chicken")); // alphabets only
-//        assertTrue(Name.isValidName("12345")); // numbers only
-//        assertTrue(Name.isValidName("5th grade waygu beef")); // alphanumeric characters
-//        assertTrue(Name.isValidName("Kurobuta Pork")); // with capital letters
-//        assertTrue(Name.isValidName("Japanese 5th grade waygu beef")); // long names
-//    }
+
+    @Test
+    public void constructor_invalidName_throwsIllegalArgumentException() {
+        String invalidName = "";
+        assertThrows(IllegalArgumentException.class, () -> new DeliveryName(invalidName));
+    }
+
+    @Test
+    public void isValidName() {
+        // null name
+        assertThrows(NullPointerException.class, () -> DeliveryName.isValidName(null));
+
+        // invalid name
+        assertFalse(DeliveryName.isValidName("")); // empty string
+        assertFalse(DeliveryName.isValidName(" ")); // spaces only
+        assertFalse(DeliveryName.isValidName("^")); // only non-alphanumeric characters
+        assertFalse(DeliveryName.isValidName("damith*")); // contains non-alphanumeric characters
+
+        // valid name
+        assertTrue(DeliveryName.isValidName("marissa")); // alphabets only
+        assertTrue(DeliveryName.isValidName("12345")); // numbers only
+        assertTrue(DeliveryName.isValidName("5th son of obama")); // alphanumeric characters
+        assertTrue(DeliveryName.isValidName("TRUMP")); // with capital letters
+        assertTrue(DeliveryName.isValidName("hubert blaine wolfeschlegelsteinhausenbergerdorff sr")); // long names
+    }
 }
