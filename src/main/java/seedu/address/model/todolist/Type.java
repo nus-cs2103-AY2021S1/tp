2b67b8c795556688b59f7c80647050565923337c
michael-setia <michaelsetia55@gmail.com>
2020-10-16 00:19:49 +0800

    Add Task class and its dependencies

---
+++ src/main/java/seedu/address/model/todolist/Type.java
@@ -0,0 +1,13 @@
+package seedu.address.model.todolist;
+
+/**
+ * Represents the type of the task.
+ */
+public enum Type {
+    ASSIGNMENT,
+    LAB,
+    TUTORIAL,
+    PROJECT,
+    STUDY,
+    DAILY
+}
