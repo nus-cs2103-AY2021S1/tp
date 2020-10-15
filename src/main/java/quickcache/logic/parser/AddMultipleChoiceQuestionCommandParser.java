69e3f9e97c52ad115852d71108c122b95971e61f
FH-30 <finxictus@gmail.com>
2020-10-15 16:33:20 +0800

    Remove Unnecessary print

--- src/main/java/quickcache/logic/parser/AddMultipleChoiceQuestionCommandParser.java
+++ src/main/java/quickcache/logic/parser/AddMultipleChoiceQuestionCommandParser.java
@@ -59,7 +59,6 @@ public class AddMultipleChoiceQuestionCommandParser implements Parser<AddMultipl
                 throw new ParseException("Answer must be smaller than number of choices");
             }
         } catch (NumberFormatException e) {
-            System.out.println("hello");
             throw new ParseException("Answer must be integer");
         }
 
