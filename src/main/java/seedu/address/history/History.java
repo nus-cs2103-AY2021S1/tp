8c1409ef533d79d129e37ddf438395f13a2ea503
Choo Xing Yu <chooxingyu@hotmail.com>
2020-10-15 17:53:05 +0800

    Update History javadocs

--- src/main/java/seedu/address/history/History.java
+++ src/main/java/seedu/address/history/History.java
@@ -6,18 +6,21 @@ public interface History {
 
     /**
      * Adds input to history.
+     * Removes oldest history when there is an overflow.
      * @param command input for adding to history.
      */
     void addToHistory(String command);
 
     /**
      * Provides previous command, if any.
+     * Decreases the currentCommandIndex by 1.
      * @return an Optional of the previous command.
      */
     Optional<String> previousCommand();
 
     /**
      * Provides next command, if any.
+     * Increments the currentCommandIndex by 1.
      * @return an Optional of the next command.
      */
     Optional<String> nextCommand();
