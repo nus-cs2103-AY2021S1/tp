5c93e49bf44b2f33a633f07a962f3fb4628fdf20
KendrewC <kendrewchanys@gmail.com>
2020-10-15 16:00:39 +0800

    Minor code styling updates

--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -21,13 +21,13 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
             String trimArgs = args.trim();
             String[] argList = trimArgs.split(" ");
             if (argList.length == 0 || argList.length > 2) {
-                throw new ParseException("");
+                throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
             }
             Index index = ParserUtil.parseIndex(argList[0]);
             if (argList.length == 1) {
                 return new DeleteCommand(index);
             } else {
-                int quantity = Integer.valueOf(argList[1]);
+                int quantity = ParserUtil.parseQuantity(argList[1]);
                 return new DeleteCommand(index, quantity);
             }
         } catch (ParseException pe) {
