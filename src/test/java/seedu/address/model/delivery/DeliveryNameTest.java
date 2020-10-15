8fc21cfe5f0d8271b5eb7baaf51750ad173ef488
Wincent <wincenttsjoi@gmail.com>
2020-10-15 18:44:20 +0800

    Remove checkstyle

---
+++ src/test/java/seedu/address/model/delivery/DeliveryNameTest.java
@@ -0,0 +1,34 @@
+package seedu.address.model.delivery;
+
+public class DeliveryNameTest {
+
+//    @Test
+//    public void constructor_null_throwsNullPointerException() {
+//        assertThrows(NullPointerException.class, () -> new Name(null));
+//    }
+//
+//    @Test
+//    public void constructor_invalidName_throwsIllegalArgumentException() {
+//        String invalidName = "";
+//        assertThrows(IllegalArgumentException.class, () -> new Name(invalidName));
+//    }
+//
+//    @Test
+//    public void isValidName() {
+//        // null name
+//        assertThrows(NullPointerException.class, () -> Name.isValidName(null));
+//
+//        // invalid name
+//        assertFalse(Name.isValidName("")); // empty string
+//        assertFalse(Name.isValidName(" ")); // spaces only
+//        assertFalse(Name.isValidName("^")); // only non-alphanumeric characters
+//        assertFalse(Name.isValidName("chicken*")); // contains non-alphanumeric characters
+//
+//        // valid name
+//        assertTrue(Name.isValidName("premium chicken")); // alphabets only
+//        assertTrue(Name.isValidName("12345")); // numbers only
+//        assertTrue(Name.isValidName("5th grade waygu beef")); // alphanumeric characters
+//        assertTrue(Name.isValidName("Kurobuta Pork")); // with capital letters
+//        assertTrue(Name.isValidName("Japanese 5th grade waygu beef")); // long names
+//    }
+}
