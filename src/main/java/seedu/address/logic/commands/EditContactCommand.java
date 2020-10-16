fb61eee9e0b2d2a4f97f810ceceb0ac529d742b8
jonasngs <jonasngs30@gmail.com>
2020-10-16 13:49:14 +0800

    Implement EditContactCommand and EditContactParser

--- src/main/java/seedu/address/logic/commands/EditContactCommand.java
+++ src/main/java/seedu/address/logic/commands/EditContactCommand.java
@@ -1,4 +1,189 @@
 package seedu.address.logic.commands;
 
-public class EditContactCommand {
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TELEGRAM;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_CONTACTS;
+
+import java.util.List;
+import java.util.Optional;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.util.CollectionUtil;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.contact.Contact;
+import seedu.address.model.contact.Email;
+import seedu.address.model.contact.Name;
+import seedu.address.model.contact.Telegram;
+
+/**
+ * Edits the details of an existing contact in the contact list.
+ */
+public class EditContactCommand extends Command {
+
+    public static final String COMMAND_WORD = "editcontact";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the details of the contact identified "
+            + "by the index number used in the displayed contact list. "
+            + "Existing values will be overwritten by the input values.\n"
+            + "Parameters: INDEX (must be a positive integer) "
+            + "[" + PREFIX_NAME + "NAME] "
+            + "[" + PREFIX_EMAIL + "EMAIL] "
+            + "[" + PREFIX_TELEGRAM + "TELEGRAM]...\n"
+            + "Example: " + COMMAND_WORD + " 1 "
+            + PREFIX_EMAIL + "johndoe@example.com";
+
+    public static final String MESSAGE_EDIT_CONTACT_SUCCESS = "Edited Contact: %1$s";
+    public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided.";
+    public static final String MESSAGE_DUPLICATE_CONTACT = "This contact already exists in the contact list.";
+
+    private final Index index;
+    private final EditContactDescriptor editContactDescriptor;
+
+    /**
+     * @param index of the contact in the filtered contact list to edit
+     * @param editContactDescriptor details to edit the contact with
+     */
+    public EditContactCommand(Index index, EditContactDescriptor editContactDescriptor) {
+        requireNonNull(index);
+        requireNonNull(editContactDescriptor);
+
+        this.index = index;
+        this.editContactDescriptor = new EditContactDescriptor(editContactDescriptor);
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Contact> lastShownList = model.getFilteredContactList();
+
+        if (index.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_CONTACT_DISPLAYED_INDEX);
+        }
+
+        Contact contactToEdit = lastShownList.get(index.getZeroBased());
+        Contact editedContact = createEditedContact(contactToEdit, editContactDescriptor);
+
+        if (!contactToEdit.isSameContact(editedContact) && model.hasContact(editedContact)) {
+            throw new CommandException(MESSAGE_DUPLICATE_CONTACT);
+        }
+
+        model.setContact(contactToEdit, editedContact);
+        model.updateFilteredContactList(PREDICATE_SHOW_ALL_CONTACTS);
+        return new CommandResult(String.format(MESSAGE_EDIT_CONTACT_SUCCESS, editedContact));
+    }
+
+    /**
+     * Creates and returns a {@code Contact} with the details of {@code contactToEdit}
+     * edited with {@code editContactDescriptor}.
+     */
+    private static Contact createEditedContact(Contact contactToEdit, EditContactDescriptor editContactDescriptor) {
+        assert contactToEdit != null;
+
+        Name updatedName = editContactDescriptor.getName().orElse(contactToEdit.getName());
+        Email updatedEmail = editContactDescriptor.getEmail().orElse(contactToEdit.getEmail());
+        Telegram updatedTelegram = editContactDescriptor.getTelegram().orElse(contactToEdit.getTelegramUsername());
+
+        return new Contact(updatedName, updatedEmail, updatedTelegram);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof EditCommand)) {
+            return false;
+        }
+
+        // state check
+        EditContactCommand e = (EditContactCommand) other;
+        return index.equals(e.index)
+                && editContactDescriptor.equals(e.editContactDescriptor);
+    }
+
+    /**
+     * Stores the details to edit the contact with. Each non-empty field value will replace the
+     * corresponding field value of the contact.
+     */
+    public static class EditContactDescriptor {
+        private Name name;
+        private Email email;
+        private Telegram telegram;
+
+        public EditContactDescriptor() {}
+
+        /**
+         * Copy constructor.
+         * A defensive copy of {@code tags} is used internally.
+         */
+        public EditContactDescriptor(EditContactDescriptor toCopy) {
+            setName(toCopy.name);
+            setEmail(toCopy.email);
+            setTelegram(toCopy.telegram);
+        }
+
+        /**
+         * Returns true if at least one field is edited.
+         */
+        public boolean isAnyFieldEdited() {
+            return CollectionUtil.isAnyNonNull(name, email, telegram);
+        }
+
+        public void setName(Name name) {
+            this.name = name;
+        }
+
+        public Optional<Name> getName() {
+            return Optional.ofNullable(name);
+        }
+
+        public void setEmail(Email email) {
+            this.email = email;
+        }
+
+        public Optional<Email> getEmail() {
+            return Optional.ofNullable(email);
+        }
+
+        public void setTelegram(Telegram telegram) {
+            this.telegram = telegram;
+        }
+
+        public Optional<Telegram> getTelegram() {
+            return Optional.ofNullable(telegram);
+        }
+
+
+        @Override
+        public boolean equals(Object other) {
+            // short circuit if same object
+            if (other == this) {
+                return true;
+            }
+
+            // instanceof handles nulls
+            if (!(other instanceof EditContactDescriptor)) {
+                return false;
+            }
+
+            // state check
+            EditContactDescriptor e = (EditContactDescriptor) other;
+
+            return getName().equals(e.getName())
+                    && getEmail().equals(e.getEmail())
+                    && getTelegram().equals(e.getTelegram());
+        }
+    }
+
+    @Override
+    public boolean isExit() {
+        return false;
+    }
 }
