f6138b281a8c5d8cdc2e0e5553f905064c26844b
GilbertTan19 <Gilberttan95.gt@gmail.com>
2020-10-16 21:43:07 +0800

    Refactor question to include answer

--- src/main/java/quickcache/model/flashcard/Question.java
+++ src/main/java/quickcache/model/flashcard/Question.java
@@ -10,9 +10,21 @@ public interface Question {
     String MESSAGE_CONSTRAINTS =
             "Questions should only contain alphanumeric characters and spaces, and it should not be blank";
 
+    public static final String VALIDATION_REGEX = "[^\\s].*";
+
     String getValue();
 
     String getFormatQuestion();
 
     Optional<Choice[]> getChoices();
+
+    Answer getAnswer();
+
+    Answer getAnswerOrIndex();
+
+    boolean checkAnswer(Answer answer);
+
+    public static boolean isValidQuestion(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
 }
