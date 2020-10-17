38b6ce5c38eee3b77b1ec8047bddba8f1558f018
Junhua <wenjh1998@gmail.com>
2020-10-18 03:45:43 +0800

    Update with more TestCases

--- src/test/java/jimmy/mcgymmy/logic/commands/ExportCommandTest.java
+++ src/test/java/jimmy/mcgymmy/logic/commands/ExportCommandTest.java
@@ -6,6 +6,7 @@ import java.nio.file.Path;
 
 import org.junit.jupiter.api.Test;
 
+import jimmy.mcgymmy.logic.parser.CommandParserTestUtil.OptionalParameterStub;
 import jimmy.mcgymmy.logic.parser.CommandParserTestUtil.ParameterStub;
 import jimmy.mcgymmy.model.Model;
 import jimmy.mcgymmy.model.ModelManager;
@@ -16,7 +17,18 @@ class ExportCommandTest {
     public void directoryNotFoundError() {
         ExportCommand exportCommand = new ExportCommand();
         ParameterStub<Path> parameterStub = new ParameterStub<Path>("", Path.of("c:\\Invalid String"));
-        exportCommand.setPathParameter(parameterStub);
+        OptionalParameterStub<String> optionalparameterStub = new OptionalParameterStub<String>("o", "mcgymmy.json");
+        exportCommand.setPathParameter(parameterStub, optionalparameterStub);
+        Model model = new ModelManager();
+        assertCommandFailure(exportCommand, model, String.format(ExportCommand.MESSAGE_FAILURE, "c:\\Invalid String"));
+    }
+
+    @Test
+    public void invalidOutputName() {
+        ExportCommand exportCommand = new ExportCommand();
+        ParameterStub<Path> parameterStub = new ParameterStub<Path>("", Path.of("c:\\Invalid String"));
+        OptionalParameterStub<String> optionalparameterStub = new OptionalParameterStub<String>("o", "");
+        exportCommand.setPathParameter(parameterStub, optionalparameterStub);
         Model model = new ModelManager();
         assertCommandFailure(exportCommand, model, String.format(ExportCommand.MESSAGE_FAILURE, "c:\\Invalid String"));
     }
