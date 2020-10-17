b33ba9ec5f9b72de73638645a990dcd4b884104d
Andrea <andreaxtan611@gmail.com>
2020-10-17 11:19:39 +0800

    Fix checkstyle and add some tests

--- src/test/java/seedu/address/logic/parser/ListCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ListCommandParserTest.java
@@ -1,20 +1,14 @@
 package seedu.address.logic.parser;
 
-import org.junit.jupiter.api.Test;
-import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.ListCommand;
-
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
-import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.ListCommand;
 
 public class ListCommandParserTest {
     private ListCommandParser parser = new ListCommandParser();
-
-//    @Test
-//    public void parse_validArgs_returnsListCommand() {
-//        assertParseSuccess(parser, "2", new ListCommand(Index.fromZeroBased(2)));
-//    }
 
     @Test
     public void parse_invalidArgs_throwsParseException() {
