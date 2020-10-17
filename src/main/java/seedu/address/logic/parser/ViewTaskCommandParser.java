d2893435e57e556033402e3a70fa521e6b670f05
lll-jy <E0376997@u.nus.edu>
2020-10-17 15:36:44 +0800

    Delete descriptor part in ViewTask

--- src/main/java/seedu/address/logic/parser/ViewTaskCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ViewTaskCommandParser.java
@@ -2,34 +2,33 @@ package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.global.EditCommand;
-import seedu.address.logic.commands.project.EditTaskCommand;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DEADLINE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PROJECT_DESCRIPTION;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PROJECT_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TASK_IS_DONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TASK_PROGRESS;
-import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.task.Task;
 
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Optional;
 import java.util.Set;
 
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.project.ViewTaskCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.task.Task;
 /**
- * Parses input arguments and creates a new EditCommand object
+ * Parses input arguments and creates a new ViewCommand object
  */
-public class ViewTaskCommandParser implements Parser<EditTaskCommand> {
+public class ViewTaskCommandParser implements Parser<ViewTaskCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the EditCommand
-     * and returns an EditCommand object for execution.
+     * Parses the given {@code String} of arguments in the context of the ViewCommand
+     * and returns an ViewCommand object for execution.
      *
      * @throws ParseException if the user input does not conform the expected format
      */
-    public EditTaskCommand parse(String args) throws ParseException {
+    public ViewTaskCommand parse(String args) throws ParseException {
         requireNonNull(args);
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_PROJECT_NAME, PREFIX_DEADLINE, PREFIX_PROJECT_DESCRIPTION,
@@ -40,34 +39,10 @@ public class ViewTaskCommandParser implements Parser<EditTaskCommand> {
         try {
             index = ParserUtil.parseIndex(argMultimap.getPreamble());
         } catch (ParseException pe) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, EditTaskCommand.MESSAGE_USAGE), pe);
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ViewTaskCommand.MESSAGE_USAGE), pe);
         }
 
-        EditTaskCommand.EditTaskDescriptor editTaskDescriptor = new EditTaskCommand.EditTaskDescriptor();
-        if (argMultimap.getValue(PREFIX_PROJECT_NAME).isPresent()) {
-            editTaskDescriptor.setTaskName(ParserUtil.parseTaskBasicInformation(
-                    argMultimap.getValue(PREFIX_PROJECT_NAME).get()));
-        }
-        if (argMultimap.getValue(PREFIX_DEADLINE).isPresent()) {
-            editTaskDescriptor.setDeadline(ParserUtil.parseDeadline(argMultimap.getValue(PREFIX_DEADLINE).get()));
-        }
-        if (argMultimap.getValue(PREFIX_PROJECT_DESCRIPTION).isPresent()) {
-            editTaskDescriptor.setTaskDescription(ParserUtil.parseTaskBasicInformation(argMultimap
-                    .getValue(PREFIX_PROJECT_DESCRIPTION).get()));
-        }
-        if (argMultimap.getValue(PREFIX_TASK_PROGRESS).isPresent()) {
-            editTaskDescriptor.setProgress(Double.parseDouble(ParserUtil.parseTaskBasicInformation(argMultimap
-                    .getValue(PREFIX_TASK_PROGRESS).get())));
-        }
-        if (argMultimap.getValue(PREFIX_TASK_IS_DONE).isPresent()) {
-            editTaskDescriptor.setIsDone(Boolean.parseBoolean(ParserUtil.parseTaskBasicInformation(argMultimap
-                    .getValue(PREFIX_TASK_IS_DONE).get())));
-        }
-        if (!editTaskDescriptor.isAnyFieldEdited()) {
-            throw new ParseException(EditCommand.MESSAGE_NOT_EDITED);
-        }
-
-        return new EditTaskCommand(index, editTaskDescriptor);
+        return new ViewTaskCommand(index);
     }
 
     /**
@@ -75,7 +50,7 @@ public class ViewTaskCommandParser implements Parser<EditTaskCommand> {
      * If {@code tasks} contain only one element which is an empty string, it will be parsed into a
      * {@code Set<Task>} containing zero tasks.
      */
-    private Optional<Set<Task>> parseTasksForEdit(Collection<String> tasks) {
+    private Optional<Set<Task>> parseTasksForView(Collection<String> tasks) {
         assert tasks != null;
 
         if (tasks.isEmpty()) {
