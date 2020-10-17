d831720c2af63bfac9498c0d39a904fb3ddbf39e
lll-jy <E0376997@u.nus.edu>
2020-10-17 14:34:02 +0800

    Add task-on-view of a project

--- src/main/java/seedu/address/model/project/Project.java
+++ src/main/java/seedu/address/model/project/Project.java
@@ -9,6 +9,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -39,6 +40,9 @@ public class Project {
     private final Set<Task> tasks = new HashSet<>();
     private final Set<Meeting> meetings = new HashSet<>();
 
+    // Display helper
+    private Optional<Task> taskOnView;
+
     /**
      * Every field must be present and not null.
      */
@@ -55,6 +59,7 @@ public class Project {
         this.listOfParticipations.putAll(listOfParticipations);
         this.tasks.addAll(tasks);
         this.meetings.addAll(meetings);
+        this.taskOnView = Optional.empty();
     }
 
     public ProjectName getProjectName() {
@@ -87,6 +92,12 @@ public class Project {
     }
     public void updateTaskFilter(Predicate<Task> predicate) {
         this.taskFilter = predicate;
+    }
+    public Optional<Task> getTaskOnView() {
+        return taskOnView;
+    }
+    public void updateTaskOnView(Task t) {
+        taskOnView = Optional.of(t);
     }
 
     /**
