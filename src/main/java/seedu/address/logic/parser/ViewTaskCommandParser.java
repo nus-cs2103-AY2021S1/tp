90dd98955afd2c58ee8193679f41f4825ddf0659
lll-jy <E0376997@u.nus.edu>
2020-10-17 15:41:46 +0800

    Parse ViewTaskCommandParser

--- src/main/java/seedu/address/logic/parser/ViewTaskCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ViewTaskCommandParser.java
@@ -2,21 +2,10 @@ package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_DEADLINE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PROJECT_DESCRIPTION;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PROJECT_NAME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TASK_IS_DONE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TASK_PROGRESS;
-
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Optional;
-import java.util.Set;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.project.ViewTaskCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.task.Task;
 /**
  * Parses input arguments and creates a new ViewCommand object
  */
@@ -30,33 +19,15 @@ public class ViewTaskCommandParser implements Parser<ViewTaskCommand> {
      */
     public ViewTaskCommand parse(String args) throws ParseException {
         requireNonNull(args);
-        ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(args, PREFIX_PROJECT_NAME, PREFIX_DEADLINE, PREFIX_PROJECT_DESCRIPTION,
-                        PREFIX_TASK_PROGRESS, PREFIX_TASK_IS_DONE);
 
         Index index;
 
         try {
-            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+            index = ParserUtil.parseIndex(args.trim());
         } catch (ParseException pe) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ViewTaskCommand.MESSAGE_USAGE), pe);
         }
 
         return new ViewTaskCommand(index);
-    }
-
-    /**
-     * Parses {@code Collection<String> tasks} into a {@code Set<Task>} if {@code tasks} is non-empty.
-     * If {@code tasks} contain only one element which is an empty string, it will be parsed into a
-     * {@code Set<Task>} containing zero tasks.
-     */
-    private Optional<Set<Task>> parseTasksForView(Collection<String> tasks) {
-        assert tasks != null;
-
-        if (tasks.isEmpty()) {
-            return Optional.empty();
-        }
-        Collection<String> taskSet = tasks.size() == 1 && tasks.contains("") ? Collections.emptySet() : tasks;
-        return Optional.of(ParserUtil.parseTasks(taskSet));
     }
 }
