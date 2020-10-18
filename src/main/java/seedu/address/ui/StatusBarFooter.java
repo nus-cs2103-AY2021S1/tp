273a27d15e8dafce1eab6d4b8f79df77942c1ad5
Wincent <wincenttsjoi@gmail.com>
2020-10-18 19:33:35 +0800

    Add corner datetime

--- src/main/java/seedu/address/ui/StatusBarFooter.java
+++ src/main/java/seedu/address/ui/StatusBarFooter.java
@@ -2,10 +2,17 @@ package seedu.address.ui;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
+import javafx.util.Pair;
+import javafx.util.Duration;
 
 /**
  * A ui for the status bar that is displayed at the footer of the application.
@@ -17,13 +24,60 @@ public class StatusBarFooter extends UiPart<Region> {
     @FXML
     private Label saveLocationStatus;
 
+    private Pair<String, LocalDateTime> displayString;
+
     /**
      * Creates a {@code StatusBarFooter} with the given {@code Path}.
      */
     public StatusBarFooter(Path inventoryLocation, Path deliveryLocation) {
         super(FXML);
-        saveLocationStatus.setText(Paths.get(".").resolve(inventoryLocation).toString()
-                + " and " + Paths.get(".").resolve(deliveryLocation).toString());
+        this.displayString = null;
+        initialize();
+//        saveLocationStatus.setText(Paths.get(".").resolve(inventoryLocation).toString()
+//                + " and " + Paths.get(".").resolve(deliveryLocation).toString());
+    }
+
+    /**
+     * Sets the display text on status footer. Default display time of 3 seconds
+     *
+     * @param display the displayed text.
+     */
+    public void setDisplayString(String display) {
+        setDisplayString(display, 3);
+    }
+
+    /**
+     * Sets the display text on status footer.
+     *
+     * @param display the displayed text.
+     * @param seconds the amount of seconds to display
+     */
+    public void setDisplayString(String display, int seconds) {
+        this.displayString = new Pair<>(display, LocalDateTime.now().plusSeconds(seconds));
+    }
+
+    private void initialize() {
+        //Reference: https://stackoverflow.com/questions/42383857/javafx-live-time-and-date/42384436
+        Timeline clock = new Timeline(new KeyFrame(Duration.ZERO, e -> {
+            LocalDateTime currentTime = LocalDateTime.now();
+            checkDisplayValidity();
+            if (displayString == null) {
+                saveLocationStatus.setText(currentTime.format(DateTimeFormatter.ofPattern("dd MMMM yyyy HH:mm:ss")));
+            } else {
+                saveLocationStatus.setText(currentTime.format(DateTimeFormatter.ofPattern("dd MMMM yyyy HH:mm:ss"))
+                        + " - " + displayString.getKey());
+            }
+        }), new KeyFrame(Duration.seconds(1)));
+        clock.setCycleCount(Animation.INDEFINITE);
+        clock.play();
+    }
+
+    private void checkDisplayValidity() {
+        if (displayString != null) {
+            if (displayString.getValue().isBefore(LocalDateTime.now())) {
+                displayString = null;
+            }
+        }
     }
 
 }
