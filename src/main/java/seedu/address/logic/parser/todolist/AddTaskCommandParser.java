286f2a81af42cece42879925c4032c297dc4fb25
michael-setia <michaelsetia55@gmail.com>
2020-10-18 15:00:11 +0800

    Fix a typo in JavaDoc

--- src/main/java/seedu/address/logic/parser/todolist/AddTaskCommandParser.java
+++ src/main/java/seedu/address/logic/parser/todolist/AddTaskCommandParser.java
@@ -27,7 +27,7 @@ import seedu.address.model.task.TaskName;
 public class AddTaskCommandParser implements Parser<AddTaskCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the AddCommand
+     * Parses the given {@code String} of arguments in the context of the AddTaskCommand
      * and returns an AddCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
