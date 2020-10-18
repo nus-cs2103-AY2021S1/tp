476018b5c7dda0e0254bf17f52fa5033681f2ba9
Wincent <wincenttsjoi@gmail.com>
2020-10-18 21:09:26 +0800

    checkstyle main

--- src/main/java/seedu/address/logic/commands/deliverycommand/DeliveryClearCommand.java
+++ src/main/java/seedu/address/logic/commands/deliverycommand/DeliveryClearCommand.java
@@ -1,13 +1,16 @@
 package seedu.address.logic.commands.deliverycommand;
 
+import static java.util.Objects.requireNonNull;
+
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.commands.results.CommandResult;
 import seedu.address.model.Model;
 import seedu.address.model.deliverymodel.DeliveryBook;
 import seedu.address.model.deliverymodel.DeliveryModel;
 
-import static java.util.Objects.requireNonNull;
-
+/**
+ * Clears the delivery book.
+ */
 public class DeliveryClearCommand extends DeliveryCommand {
 
     public static final String COMMAND_WORD = "clear-d";
--- src/main/java/seedu/address/logic/commands/itemcommand/ItemCommand.java
+++ src/main/java/seedu/address/logic/commands/itemcommand/ItemCommand.java
@@ -2,5 +2,8 @@ package seedu.address.logic.commands.itemcommand;
 
 import seedu.address.logic.commands.Command;
 
+/**
+ * Parent class of all item commands.
+ */
 public abstract class ItemCommand extends Command {
 }
