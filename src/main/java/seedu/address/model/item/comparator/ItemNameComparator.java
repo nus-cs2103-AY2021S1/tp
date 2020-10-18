bf86d426c2f86b314dcc789368058ed15fa54357
Choo Xing Yu <chooxingyu@hotmail.com>
2020-10-18 21:41:23 +0800

    Adhere itemName comaparator to better abstraction

--- src/main/java/seedu/address/model/item/comparator/ItemNameComparator.java
+++ src/main/java/seedu/address/model/item/comparator/ItemNameComparator.java
@@ -7,6 +7,6 @@ import seedu.address.model.item.Item;
 public class ItemNameComparator implements Comparator<Item> {
     @Override
     public int compare(Item o1, Item o2) {
-        return o1.getName().fullName.compareTo(o2.getName().fullName);
+        return o1.getName().compareTo(o2.getName());
     }
 }
