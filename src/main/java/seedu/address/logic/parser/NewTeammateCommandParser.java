264ca500fe94da70dbc43a03e8b66339f48c7bab
GeNiaaz <geniaaz1009@gmail.com>
2020-10-18 12:59:45 +0800

    Delete unnecessary code

--- src/main/java/seedu/address/logic/parser/NewTeammateCommandParser.java
+++ src/main/java/seedu/address/logic/parser/NewTeammateCommandParser.java
@@ -70,34 +70,3 @@ public class NewTeammateCommandParser implements Parser<NewTeammateCommand> {
     }
 
 }
-    //        try {
-    //            String input = args.trim();
-    //
-    //            final int lowerBound = 0;
-    //            int upperBound = input.indexOf(" ");
-    //            if (upperBound <= 0) {
-    //                throw new ParseException("");
-    //            }
-    //
-    //            String name = input.substring(lowerBound, upperBound);
-    //            String afterNameSubstring = input.substring(upperBound + 1);
-    //
-    //            upperBound = afterNameSubstring.indexOf(" ");
-    //            String gitUserName = afterNameSubstring.substring(lowerBound, upperBound);
-    //            String afterGitUserNameSubstring = afterNameSubstring.substring(upperBound + 1);
-    //
-    //            upperBound = afterGitUserNameSubstring.indexOf(" ");
-    //            String phone = afterGitUserNameSubstring.substring(lowerBound, upperBound);
-    //            String afterPhoneSubstring = afterGitUserNameSubstring.substring(upperBound + 1);
-    //
-    //            upperBound = afterPhoneSubstring.indexOf(" ");
-    //            String email = afterPhoneSubstring.substring(lowerBound, upperBound);
-    //
-    //            String address = afterPhoneSubstring.substring(upperBound + 1);
-    //
-    //
-    //            return new NewTeammateCommand(name, gitUserName, phone, email, address);
-    //        } catch (ParseException | StringIndexOutOfBoundsException e) {
-    //            throw new ParseException(
-    //                String.format(MESSAGE_INVALID_COMMAND_FORMAT, NewTeammateCommand.MESSAGE_USAGE), e);
-    //        }
