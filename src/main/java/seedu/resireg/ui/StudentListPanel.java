db08d8e969cc6cc76c707d4af93f23baaeafc592
jetnew <notesjet@gmail.com>
2020-10-19 00:40:13 +0800

    Bug fix for UI Student and Panel list updates

--- src/main/java/seedu/resireg/ui/StudentListPanel.java
+++ src/main/java/seedu/resireg/ui/StudentListPanel.java
@@ -74,6 +74,7 @@ public class StudentListPanel extends UiPart<Region> {
                         relatedRoom.getFloor(),
                         relatedRoom.getRoomNumber()).getRoot());
             }
+            studentListView.refresh();
         }
     }
 }
