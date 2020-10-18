f57bee671a876e9de27dcc65931d3ad8df481cbb
MatthiasLHK <matthiasli1998@hotmail.com>
2020-10-19 01:25:47 +0800

    Add TodolistParsers

---
+++ src/main/java/seedu/address/logic/parser/todolistparsers/DeleteTodoParser.java
@@ -0,0 +1,11 @@
+package seedu.address.logic.parser.todolistparsers;
+
+import seedu.address.logic.parser.Parser;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+public class DeleteTodoParser implements Parser<DeleteTodoCommand> {
+    @Override
+    public DeleteTodoCommand parse(String userInput) throws ParseException {
+        return null;
+    }
+}
---
+++ src/main/java/seedu/address/logic/parser/todolistparsers/ViewTodoParser.java
@@ -0,0 +1,10 @@
+package seedu.address.logic.parser.todolistparsers;
+
+import seedu.address.logic.parser.exceptions.ParseException;
+
+public class ViewTodoParser implements Parser<ViewTodoCommand> {
+    @Override
+    public ViewTodoCommand parse(String userInput) throws ParseException {
+        return null;
+    }
+}
