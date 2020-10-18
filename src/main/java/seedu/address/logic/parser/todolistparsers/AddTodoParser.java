5554c7ce3ff68742c14006e026315753023b488a
MatthiasLHK <matthiasli1998@hotmail.com>
2020-10-18 20:38:43 +0800

    Create TodolistParsers

---
+++ src/main/java/seedu/address/logic/parser/todolistparsers/AddTodoParser.java
@@ -0,0 +1,12 @@
+package seedu.address.logic.parser.todolistparsers;
+
+import seedu.address.logic.parser.Parser;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+public class AddTodoParser implements Parser<AddTodoCommand> {
+
+    @Override
+    public AddTodoCommand parse(String userInput) throws ParseException {
+        return null;
+    }
+}
---
+++ src/main/java/seedu/address/logic/parser/todolistparsers/EditTodoParser.java
@@ -0,0 +1,11 @@
+package seedu.address.logic.parser.todolistparsers;
+
+import seedu.address.logic.parser.Parser;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+public class EditTodoParser implements Parser<EditTodoCommand> {
+    @Override
+    public EditTodoCommand parse(String userInput) throws ParseException {
+        return null;
+    }
+}
