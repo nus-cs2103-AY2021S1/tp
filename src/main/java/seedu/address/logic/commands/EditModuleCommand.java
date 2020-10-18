5a60510e16af80f37ea94c68a48be0e9e310f21a
davidcaiqifan <e0406982@u.nus.edu>
2020-10-18 19:36:49 +0800

    Checkstyle cleanup 2

--- src/main/java/seedu/address/logic/commands/EditModuleCommand.java
+++ src/main/java/seedu/address/logic/commands/EditModuleCommand.java
@@ -5,7 +5,11 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ZOOM_LINK;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_MODULES;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 import seedu.address.commons.util.CollectionUtil;
 import seedu.address.logic.commands.exceptions.CommandException;
