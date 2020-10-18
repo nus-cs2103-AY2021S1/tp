7357bc68aa6e67f04b9be506dbafc5df02c51b0c
Josiah Khoo <josiahkhooshaoqi@gmail.com>
2020-10-18 19:02:26 +0800

    Add javadocs to ExportCommandParser

--- src/main/java/quickcache/logic/parser/ExportCommandParser.java
+++ src/main/java/quickcache/logic/parser/ExportCommandParser.java
@@ -6,8 +6,17 @@ import quickcache.commons.core.Messages;
 import quickcache.logic.commands.ExportCommand;
 import quickcache.logic.parser.exceptions.ParseException;
 
+/**
+ * Parses input arguments and creates a new ExportCommand object.
+ */
 public class ExportCommandParser implements Parser<ExportCommand> {
 
+    /**
+     * Parses the given {@code String} of arguments in the context of the ExportCommand
+     * and returns an ExportCommand object for execution.
+     *
+     * @throws ParseException If the given user input does not conform to the expected format.
+     */
     @Override
     public ExportCommand parse(String args) throws ParseException {
         requireNonNull(args);
