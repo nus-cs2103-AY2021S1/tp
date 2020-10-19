b9b80d276b991a5976d413671f3cc0b4e8df40ae
chewypiano <gimikamil@gmail.com>
2020-10-20 02:23:07 +0800

    Add tag, name parameters to find command

--- src/main/java/jimmy/mcgymmy/logic/commands/FindCommand.java
+++ src/main/java/jimmy/mcgymmy/logic/commands/FindCommand.java
@@ -5,8 +5,10 @@ import static java.util.Objects.requireNonNull;
 import java.util.Arrays;
 
 import jimmy.mcgymmy.commons.core.Messages;
-import jimmy.mcgymmy.logic.parser.parameter.Parameter;
+import jimmy.mcgymmy.logic.parser.parameter.OptionalParameter;
 import jimmy.mcgymmy.logic.predicate.FoodContainsKeywordsPredicate;
+import jimmy.mcgymmy.logic.predicate.NameContainsKeywordsPredicate;
+import jimmy.mcgymmy.logic.predicate.TagContainsKeywordsPredicate;
 import jimmy.mcgymmy.model.Model;
 
 /**
@@ -17,22 +19,38 @@ public class FindCommand extends Command {
     public static final String COMMAND_WORD = "find";
     public static final String SHORT_DESCRIPTION = "Filter the displayed list by a given keyword.";
 
-    private Parameter<FoodContainsKeywordsPredicate> predicateParameter = this.addParameter(
+    private OptionalParameter<FoodContainsKeywordsPredicate> foodPredicateParameter = this.addOptionalParameter(
             "keyword",
             "",
             "keywords (case-insensitive).",
             "chicken beef mutton", (s) -> new FoodContainsKeywordsPredicate(Arrays.asList(s.split("\\s+")))
     );
+    private OptionalParameter<NameContainsKeywordsPredicate> namePredicateParameter = this.addOptionalParameter(
+            "name",
+            "n",
+            "Name of the Food",
+            "Cereal", (s) -> new NameContainsKeywordsPredicate(Arrays.asList(s.split("\\s+")))
+    );
+    private OptionalParameter<TagContainsKeywordsPredicate> tagPredicateParameter = this.addOptionalParameter(
+            "tag",
+            "t",
+            "Tag associated with the Food",
+            "Lunch", (s) -> new TagContainsKeywordsPredicate(Arrays.asList(s.split("\\s+")))
+    );
 
-    void setParameters(Parameter<FoodContainsKeywordsPredicate> predicateParameter) {
-        this.predicateParameter = predicateParameter;
+    void setParameters(OptionalParameter<FoodContainsKeywordsPredicate> foodPredicateParameter,
+                       OptionalParameter<NameContainsKeywordsPredicate> namePredicateParameter,
+                       OptionalParameter<TagContainsKeywordsPredicate> tagPredicateParameter) {
+        this.foodPredicateParameter = foodPredicateParameter;
+        this.namePredicateParameter = namePredicateParameter;
+        this.tagPredicateParameter = tagPredicateParameter;
     }
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        FoodContainsKeywordsPredicate predicate = predicateParameter.consume();
-        model.updateFilteredFoodList(predicate);
+        FoodContainsKeywordsPredicate foodPredicate = foodPredicateParameter.getValue().orElse(null);
+        model.updateFilteredFoodList(foodPredicate);
         return new CommandResult(
                 String.format(Messages.MESSAGE_FOOD_LISTED_OVERVIEW, model.getFilteredFoodList().size()));
     }
