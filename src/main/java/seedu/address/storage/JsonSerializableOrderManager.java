b6ae8d93433da55193ae6bc82becc7d02943865e
KendrewC <kendrewchanys@gmail.com>
2020-10-19 23:39:40 +0800

    Add OrderManagerStorage and Related Tests

--- src/main/java/seedu/address/storage/JsonSerializableOrderManager.java
+++ src/main/java/seedu/address/storage/JsonSerializableOrderManager.java
@@ -1,16 +1,17 @@
 package seedu.address.storage;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRootName;
+
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.order.OrderItem;
 import seedu.address.model.order.OrderManager;
 import seedu.address.model.order.ReadOnlyOrderManager;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * An Immutable OrderManager that is serializable to JSON format.
