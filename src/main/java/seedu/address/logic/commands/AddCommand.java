021c5bafda439277b07596eda9069c12e67dbb55
koonweee <jeremytkw98@gmail.com>
2020-10-20 01:28:26 +0800

    Fix error messages and default json

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -33,7 +33,7 @@ public class AddCommand extends Command {
             + PREFIX_FEED_TIME + "1400 ";
 
     public static final String MESSAGE_SUCCESS = "New animal added: %1$s";
-    public static final String MESSAGE_DUPLICATE_ANIMAL = "An animal with this ID already exists in the address book";
+    public static final String MESSAGE_DUPLICATE_ANIMAL = "An animal with this ID already exists";
 
     private final Animal toAdd;
 
