7906242b848818abf2ccf64f2e796b818931ec15
Harsha Vardhan <munharsha@gmail.com>
2020-10-19 16:21:37 +0800

    Add list meeting command

--- src/main/java/seedu/address/logic/commands/ListMeetingCommand.java
+++ src/main/java/seedu/address/logic/commands/ListMeetingCommand.java
@@ -10,7 +10,7 @@ import seedu.address.model.Model;
  */
 public class ListMeetingCommand extends Command {
 
-    public static final String COMMAND_WORD = "list";
+    public static final String COMMAND_WORD = "list-m";
 
     public static final String MESSAGE_SUCCESS = "Listed all meetings";
 
--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -18,6 +18,7 @@ import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.ListMeetingCommand;
 import seedu.address.logic.commands.bidcommands.AddBidCommand;
 import seedu.address.logic.commands.bidcommands.DeleteBidCommand;
 import seedu.address.logic.commands.bidcommands.EditBidCommand;
@@ -127,6 +128,9 @@ public class AddressBookParser {
 
         case EditMeetingCommand.COMMAND_WORD:
             return new EditMeetingCommandParser().parse(arguments);
+
+        case ListMeetingCommand.COMMAND_WORD:
+            return new ListMeetingCommand();
 
         // -------------------- PROPERTY ------------------- //
         case AddPropertyCommand.COMMAND_WORD:
