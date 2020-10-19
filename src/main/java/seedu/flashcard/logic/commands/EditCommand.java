e1d2c1e9a18424b4784473104131c24f66e2f407
kimberly ong <e0424807@u.nus.edu>
2020-10-19 12:01:19 +0800

    Update EditCommand#createEditedFlashcard to get flashcard's favourite status

--- src/main/java/seedu/flashcard/logic/commands/EditCommand.java
+++ src/main/java/seedu/flashcard/logic/commands/EditCommand.java
@@ -93,7 +93,8 @@ public class EditCommand extends Command {
         Answer updatedAnswer = editFlashcardDescriptor.getAnswer().orElse(flashcardToEdit.getAnswer());
         Category updatedCategory = editFlashcardDescriptor.getCategory().orElse(flashcardToEdit.getCategory());
         Note updatedNote = editFlashcardDescriptor.getNote().orElse(flashcardToEdit.getNote());
-        return new Flashcard(updatedQuestion, updatedAnswer, updatedCategory, updatedNote);
+        boolean isFavourite = flashcardToEdit.isFavourite();
+        return new Flashcard(updatedQuestion, updatedAnswer, updatedCategory, updatedNote, isFavourite);
     }
 
     @Override
