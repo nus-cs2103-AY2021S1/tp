96b415f938363f8edcbb863c03680377f647b98a
aizatazhar <aizat.azharr@gmail.com>
2020-10-19 18:06:20 +0800

    Add custom style for non-specified fields

--- src/main/java/seedu/address/ui/AnimalCard.java
+++ src/main/java/seedu/address/ui/AnimalCard.java
@@ -16,6 +16,8 @@ import seedu.address.model.feedtime.FeedTimeComparator;
 public class AnimalCard extends UiPart<Region> {
 
     private static final String FXML = "AnimalListCard.fxml";
+    private static final String NOT_SPECIFIED_STYLE = "-fx-font-size: 11px; -fx-text-fill: #e06060;"
+            + "-fx-background-color: transparent; -fx-padding: 0;";
 
     /**
      * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
@@ -56,14 +58,28 @@ public class AnimalCard extends UiPart<Region> {
         name.setText(animal.getName().fullName);
         identity.setText(animal.getId().value);
         species.setText(animal.getSpecies().value);
-        animal.getMedicalConditions().stream()
-                .sorted(Comparator.comparing(medicalCondition -> medicalCondition.medicalConditionName))
-                .forEach(medicalCondition -> medicalConditions.getChildren()
-                        .add(new Label(medicalCondition.medicalConditionName)));
-        animal.getFeedTimes().stream()
-                .sorted(new FeedTimeComparator())
-                .forEach(feedTime -> feedTimes.getChildren()
-                        .add(new Label(feedTime.feedTime + " hrs")));
+
+        if (!animal.getFeedTimes().isEmpty()) {
+            animal.getMedicalConditions().stream()
+                    .sorted(Comparator.comparing(medicalCondition -> medicalCondition.medicalConditionName))
+                    .forEach(medicalCondition -> medicalConditions.getChildren()
+                            .add(new Label(medicalCondition.medicalConditionName)));
+        } else {
+            Label label = new Label("Not specified");
+            label.setStyle(NOT_SPECIFIED_STYLE);
+            medicalConditions.getChildren().add(label);
+        }
+
+        if (!animal.getFeedTimes().isEmpty()) {
+            animal.getFeedTimes().stream()
+                    .sorted(new FeedTimeComparator())
+                    .forEach(feedTime -> feedTimes.getChildren()
+                            .add(new Label(feedTime.feedTime + " hrs")));
+        } else {
+            Label label = new Label("Not specified");
+            label.setStyle(NOT_SPECIFIED_STYLE);
+            feedTimes.getChildren().add(label);
+        }
     }
 
     @Override
