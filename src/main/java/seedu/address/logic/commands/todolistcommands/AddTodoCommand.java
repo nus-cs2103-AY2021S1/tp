495a5257951e4a485fb74d68200255ee95e126c2
michael-setia <michaelsetia55@gmail.com>
2020-10-19 21:24:18 +0800

    Integrate add command with the new parser

--- src/main/java/seedu/address/logic/commands/todolistcommands/AddTodoCommand.java
+++ src/main/java/seedu/address/logic/commands/todolistcommands/AddTodoCommand.java
@@ -1,15 +1,66 @@
 package seedu.address.logic.commands.todolistcommands;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PRIORITY;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.task.Task;
 
+/**
+ * Adds a task to the todo list.
+ */
 public class AddTodoCommand extends Command {
+
     public static final String COMMAND_WORD = "addtodo";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a task to the todo list. "
+        + "Parameters: "
+        + PREFIX_NAME + "TASK NAME "
+        + PREFIX_TAG + "TAG [OPTIONAL] "
+        + PREFIX_PRIORITY + "PRIORITY [OPTIONAL] "
+        + PREFIX_DATE + "DATE/DEADLINE [OPTIONAL] "
+        + "Example: " + COMMAND_WORD + " "
+        + PREFIX_NAME + "Do Lab Sheet 1 "
+        + PREFIX_TAG + "LAB "
+        + PREFIX_PRIORITY + "HIGH "
+        + PREFIX_DATE + "2020-10-16 ";
+
+    public static final String MESSAGE_SUCCESS = "New task added: %1$s";
+    public static final String MESSAGE_DUPLICATE_TASK = "This task already exists in the todo list";
+
+    private final Task toAdd;
+
+    /**
+     * Creates an AddTaskCommand to add the specified {@code Task}
+     */
+    public AddTodoCommand(Task task) {
+        requireNonNull(task);
+        toAdd = task;
+    }
+
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        return null;
+        requireNonNull(model);
+
+        if (model.hasTask(toAdd)) {
+            throw new CommandException(MESSAGE_DUPLICATE_TASK);
+        }
+
+        model.addTask(toAdd);
+        return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+            || (other instanceof AddTodoCommand // instanceof handles nulls
+            && toAdd.equals(((AddTodoCommand) other).toAdd));
     }
 
     @Override
@@ -17,3 +68,4 @@ public class AddTodoCommand extends Command {
         return false;
     }
 }
+
--- src/main/java/seedu/address/logic/parser/todolistparsers/AddTodoParser.java
+++ src/main/java/seedu/address/logic/parser/todolistparsers/AddTodoParser.java
@@ -1,13 +1,68 @@
 package seedu.address.logic.parser.todolistparsers;
 
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PRIORITY;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+
+import java.util.stream.Stream;
+
 import seedu.address.logic.commands.todolistcommands.AddTodoCommand;
+import seedu.address.logic.parser.ArgumentMultimap;
+import seedu.address.logic.parser.ArgumentTokenizer;
 import seedu.address.logic.parser.Parser;
+import seedu.address.logic.parser.ParserUtil;
+import seedu.address.logic.parser.Prefix;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.tag.Tag;
+import seedu.address.model.task.Date;
+import seedu.address.model.task.Priority;
+import seedu.address.model.task.Task;
+import seedu.address.model.task.TaskName;
 
+/**
+ * Parses input arguments and creates a new AddTaskCommand object
+ */
 public class AddTodoParser implements Parser<AddTodoCommand> {
+    /**
+     * Parses the given {@code String} of arguments in the context of the AddTaskCommand
+     * and returns an AddCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public AddTodoCommand parse(String args) throws ParseException {
+        Task task;
+        ArgumentTokenizer tokenizer = new ArgumentTokenizer(args, PREFIX_NAME, PREFIX_TAG, PREFIX_PRIORITY,
+            PREFIX_DATE);
+        ArgumentMultimap argMultimap = tokenizer.tokenize();
+        if (!arePrefixesPresent(argMultimap, PREFIX_NAME)
+            || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddTodoCommand.MESSAGE_USAGE));
+        }
+        TaskName taskName = ParserUtil.parseTaskName(argMultimap.getValue(PREFIX_NAME).get());
+        task = new Task(taskName);
 
-    @Override
-    public AddTodoCommand parse(String userInput) throws ParseException {
-        return null;
+        if (argMultimap.getValue(PREFIX_TAG).isPresent()) {
+            Tag taskTag = ParserUtil.parseTag(argMultimap.getValue(PREFIX_TAG).get());
+            task = task.setTag(taskTag);
+        }
+        if (argMultimap.getValue(PREFIX_PRIORITY).isPresent()) {
+            Priority priority = ParserUtil.parseTaskPriority(argMultimap.getValue(PREFIX_PRIORITY).get());
+            task = task.setPriority(priority);
+        }
+        if (argMultimap.getValue(PREFIX_DATE).isPresent()) {
+            Date date = ParserUtil.parseTaskDate(argMultimap.getValue(PREFIX_DATE).get());
+            task = task.setDate(date);
+        }
+        return new AddTodoCommand(task);
     }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
+
 }
