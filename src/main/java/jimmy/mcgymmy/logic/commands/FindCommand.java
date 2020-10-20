6ed4b815ce22a90da571cd7d17d71db1e7381158
chewypiano <gimikamil@gmail.com>
2020-10-20 12:42:39 +0800

    Repurpose FindCommand to accomodate Optional Parameters

--- src/main/java/jimmy/mcgymmy/logic/commands/FindCommand.java
+++ src/main/java/jimmy/mcgymmy/logic/commands/FindCommand.java
@@ -3,13 +3,16 @@ package jimmy.mcgymmy.logic.commands;
 import static java.util.Objects.requireNonNull;
 
 import java.util.Arrays;
+import java.util.function.Predicate;
 
 import jimmy.mcgymmy.commons.core.Messages;
 import jimmy.mcgymmy.logic.parser.parameter.OptionalParameter;
+import jimmy.mcgymmy.logic.predicate.DatePredicate;
 import jimmy.mcgymmy.logic.predicate.FoodContainsKeywordsPredicate;
 import jimmy.mcgymmy.logic.predicate.NameContainsKeywordsPredicate;
 import jimmy.mcgymmy.logic.predicate.TagContainsKeywordsPredicate;
 import jimmy.mcgymmy.model.Model;
+import jimmy.mcgymmy.model.food.Food;
 
 /**
  * Finds and lists all persons in mcgymmy whose name contains any of the argument keywords.
@@ -37,13 +40,21 @@ public class FindCommand extends Command {
             "Tag associated with the Food",
             "Lunch", (s) -> new TagContainsKeywordsPredicate(Arrays.asList(s.split("\\s+")))
     );
+    private OptionalParameter<DatePredicate> datePredicateParameter = this.addOptionalParameter(
+            "tag",
+            "t",
+            "Tag associated with the Food",
+            "Lunch", (s) -> new DatePredicate(s)
+    );
 
     void setParameters(OptionalParameter<FoodContainsKeywordsPredicate> foodPredicateParameter,
                        OptionalParameter<NameContainsKeywordsPredicate> namePredicateParameter,
-                       OptionalParameter<TagContainsKeywordsPredicate> tagPredicateParameter) {
+                       OptionalParameter<TagContainsKeywordsPredicate> tagPredicateParameter,
+                       OptionalParameter<DatePredicate> datePredicateParameter) {
         this.foodPredicateParameter = foodPredicateParameter;
         this.namePredicateParameter = namePredicateParameter;
         this.tagPredicateParameter = tagPredicateParameter;
+        this.datePredicateParameter = datePredicateParameter;
     }
 
     @Override
@@ -52,7 +63,37 @@ public class FindCommand extends Command {
         FoodContainsKeywordsPredicate foodPredicate = foodPredicateParameter.getValue().orElse(null);
         NameContainsKeywordsPredicate namePredicate = namePredicateParameter.getValue().orElse(null);
         TagContainsKeywordsPredicate tagPredicate = tagPredicateParameter.getValue().orElse(null);
-        model.updateFilteredFoodList(foodPredicate);
+        DatePredicate datePredicate = datePredicateParameter.getValue().orElse(null);
+
+        Predicate<Food> combinedPredicate = null;
+        if (foodPredicate != null) {
+            combinedPredicate = foodPredicate;
+        }
+        if (namePredicate != null) {
+            if (combinedPredicate != null) {
+                combinedPredicate = combinedPredicate.and(namePredicate);
+            } else {
+                combinedPredicate = namePredicate;
+            }
+        }
+        if (tagPredicate != null) {
+            if (combinedPredicate != null) {
+                combinedPredicate = combinedPredicate.and(tagPredicate);
+            } else {
+                combinedPredicate = tagPredicate;
+            }
+        }
+        if (datePredicate != null) {
+            if (combinedPredicate != null) {
+                combinedPredicate = combinedPredicate.and(datePredicate);
+            } else {
+                combinedPredicate = datePredicate;
+            }
+        }
+        if (combinedPredicate == null) {
+            combinedPredicate = food -> false;
+        }
+        model.updateFilteredFoodList(combinedPredicate);
         return new CommandResult(
                 String.format(Messages.MESSAGE_FOOD_LISTED_OVERVIEW, model.getFilteredFoodList().size()));
     }
