d5ed74f9728ed8c45512b3fbfd4710a6bf70c7c0
michael-setia <michaelsetia55@gmail.com>
2020-10-20 16:33:00 +0800

    Rename some commands name

--- src/main/java/seedu/address/logic/commands/todolistcommands/AddTaskCommand.java
+++ src/main/java/seedu/address/logic/commands/todolistcommands/AddTaskCommand.java
@@ -5,7 +5,7 @@ import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
-public class AddTodoCommand extends Command {
+public class AddTaskCommand extends Command {
     public static final String COMMAND_WORD = "addtodo";
     @Override
     public CommandResult execute(Model model) throws CommandException {
