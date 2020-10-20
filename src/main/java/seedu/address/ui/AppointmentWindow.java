2e41cb85b898431ec955367123d94d469ea1774a
Peh Jun Siang <austin9090@hotmail.com>
2020-10-21 02:27:56 +0800

    Style Appointment Window

--- src/main/java/seedu/address/ui/AppointmentWindow.java
+++ src/main/java/seedu/address/ui/AppointmentWindow.java
@@ -1,5 +1,6 @@
 package seedu.address.ui;
 
+import java.util.Comparator;
 import java.util.logging.Logger;
 
 import javafx.collections.FXCollections;
@@ -7,11 +8,12 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.patient.Patient;
 
-public class AppointmentWindow extends UiPart<Stage>{
+public class AppointmentWindow extends UiPart<Stage> {
 
     private final Logger logger = LogsCenter.getLogger(getClass());
     private static final String FXML = "AppointmentWindow.fxml";
@@ -23,19 +25,36 @@ public class AppointmentWindow extends UiPart<Stage>{
     @FXML
     private Label patientName;
 
+    @FXML
+    private Label patientNric;
+
+    @FXML
+    private Label patientAddress;
+
+    @FXML
+    private FlowPane patientAllergies;
+
+
     public AppointmentWindow() {
         this(new Stage());
     }
 
-    public AppointmentWindow(Stage root){
+    public AppointmentWindow(Stage root) {
         super(FXML, root);
         appointmentTable.setItems(appointmentDescriptions);
     }
 
     public void setPatientAppointments(Patient patient) {
         patientName.setText(patient.getName().fullName);
+        patientAddress.setText(patient.getAddress().value);
+        patientNric.setText(patient.getNric().value);
+        patient.getAllergies().stream()
+                .sorted(Comparator.comparing(allergy -> allergy.allergyName))
+                .forEach(allergy -> patientAllergies.getChildren().add(new Label(allergy.allergyName)));
+
         patient.getAppointments().forEach(appointment -> appointmentDescriptions.add(
-                new AppointmentDescription(appointment.toString(), "Empty description")));
+                new AppointmentDescription(appointment.getAppointmentTimeString(), appointment
+                        .getAppointmentDescription())));
     }
 
     public void show() {
