a51c2b844299f388e5b719037589e1ca3df36d69
lettuceman <manle.9a4@gmail.com>
2020-10-20 16:46:51 +0800

    Fix bug when Find has no parameters

---
+++ src/test/java/seedu/address/logic/commands/FindCommandTest.java
@@ -0,0 +1,10 @@
+package seedu.address.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class FindCommandTest {
+}
