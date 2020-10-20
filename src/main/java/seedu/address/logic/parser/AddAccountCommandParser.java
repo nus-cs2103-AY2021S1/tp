b0af5b02a845ce86c84570f1218485970d9be712
Lim Zi Yang <limziyang8@gmail.com>
2020-10-20 20:23:39 +0800

    Fix checkstyles

--- src/main/java/seedu/address/logic/parser/AddAccountCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddAccountCommandParser.java
@@ -3,8 +3,6 @@ package seedu.address.logic.parser;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.util.CliSyntax.PREFIX_NAME;
 
-import java.util.stream.Stream;
-
 import seedu.address.logic.commands.AddAccountCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.logic.parser.util.ArgumentMultimap;
