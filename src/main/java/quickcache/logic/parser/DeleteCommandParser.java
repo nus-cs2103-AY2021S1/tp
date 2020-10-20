57a6ef0c159f261e2535fe23a9559fdcaee964e2
joshtyf <tan.yin.feng@hotmail.com>
2020-10-20 14:47:35 +0800

    Make DeleteCommandParser parse tag prefix

--- src/main/java/quickcache/logic/parser/DeleteCommandParser.java
+++ src/main/java/quickcache/logic/parser/DeleteCommandParser.java
@@ -5,6 +5,11 @@ import quickcache.commons.core.index.Index;
 import quickcache.logic.commands.DeleteCommand;
 import quickcache.logic.parser.exceptions.ParseException;
 
+import java.util.List;
+
+import static quickcache.logic.parser.CliSyntax.PREFIX_QUESTION;
+import static quickcache.logic.parser.CliSyntax.PREFIX_TAG;
+
 /**
  * Parses input arguments and creates a new DeleteCommand object
  */
@@ -17,13 +22,32 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public DeleteCommand parse(String args) throws ParseException {
-        try {
-            Index index = ParserUtil.parseIndex(args);
-            return new DeleteCommand(index);
-        } catch (ParseException pe) {
-            throw new ParseException(
-                    String.format(Messages.MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE), pe);
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_TAG);
+
+        if (isTagPrefixPresent(argMultimap)) {
+            if (!argMultimap.getPreamble().isEmpty()) {
+                // there shouldn't be a preamble together with the tag prefix
+                // TODO: make sure the exception message is correct
+                throw new ParseException(String.format(Messages.MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
+            }
+            return new DeleteCommand(argMultimap.getAllValues(PREFIX_TAG));
+        } else {
+            try {
+                Index index = ParserUtil.parseIndex(args);
+                return new DeleteCommand(index);
+            } catch (ParseException pe) {
+                throw new ParseException(
+                        String.format(Messages.MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE), pe);
+            }
         }
+    }
+
+    /**
+     * Returns true if the tag prefix has at least one value.
+     */
+    private static boolean isTagPrefixPresent(ArgumentMultimap argumentMultimap) {
+        return argumentMultimap.getValue(PREFIX_TAG).isPresent();
     }
 
 }
