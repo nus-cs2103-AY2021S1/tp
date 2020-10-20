cac758b8323fc93f57f595202677e96cd6af6afa
davidcaiqifan <e0406982@u.nus.edu>
2020-10-20 19:11:11 +0800

    Implement storage for todolist component

---
+++ src/main/java/seedu/address/storage/JsonAdaptedTask.java
@@ -0,0 +1,94 @@
+package seedu.address.storage;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.contact.Contact;
+import seedu.address.model.contact.Email;
+import seedu.address.model.contact.Name;
+import seedu.address.model.contact.Telegram;
+import seedu.address.model.tag.Tag;
+import seedu.address.model.task.*;
+
+import java.util.PriorityQueue;
+
+/**
+ * Jackson-friendly version of {@link Contact}.
+ */
+public class JsonAdaptedTask {
+
+    public static final String MISSING_FIELD_MESSAGE_FORMAT = "Task's %s field is missing!";
+
+    private final String name;
+    private final JsonAdaptedTag tag;
+    private final String priority;
+    private final String date;
+    private final String status;
+
+    /**
+     * Constructs a {@code JsonAdaptedContact} with the given contact details.
+     */
+    @JsonCreator
+    public JsonAdaptedTask(@JsonProperty("name") String name, @JsonProperty("tag") JsonAdaptedTag tag,
+                              @JsonProperty("priority") String priority,
+                           @JsonProperty("date") String date, @JsonProperty("status") String status) {
+        this.name = name;
+        this.tag = tag;
+        this.priority = priority;
+        this.date = date;
+        this.status = status;
+    }
+
+    /**
+     * Converts a given {@code Task} into this class for Jackson use.
+     */
+    public JsonAdaptedTask(Task source) {
+        name = source.getName().get().getValue();
+        tag = new JsonAdaptedTag(source.getTag().get());
+        priority = source.getPriority().get().toString();
+        date = source.getDate().get().toString();
+        status = source.getStatus().get().toString();
+    }
+
+    /**
+     * Converts this Jackson-friendly adapted contact object into the model's {@code Contact} object.
+     *
+     * @throws IllegalValueException if there were any data constraints violated in the adapted contact.
+     */
+    public Task toModelType() throws IllegalValueException {
+        if (name == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT,
+                    TaskName.class.getSimpleName()));
+        }
+        if (!TaskName.isValidTaskName(name)) {
+            throw new IllegalValueException(TaskName.MESSAGE_CONSTRAINTS);
+        }
+        final TaskName modelTaskName = new TaskName(name);
+        final Tag modelTag = tag.toModelType();
+
+        if (priority == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT,
+                    Priority.class.getSimpleName()));
+        }
+        if (!Priority.isValidPriority(priority)) {
+            throw new IllegalValueException(Priority.MESSAGE_CONSTRAINTS);
+        }
+        final Priority modelPriority = Priority.getPriority(priority);
+        if (date == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT,
+                    Date.class.getSimpleName()));
+        }
+        if (!Date.isValidDate(date)) {
+            throw new IllegalValueException(Date.MESSAGE_CONSTRAINTS);
+        }
+        final Date modelDate = new Date(date);
+        if (status == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT,
+                    Status.class.getSimpleName()));
+        }
+        final Status modelStatus = Status.getStatus(status);
+        return new Task(modelTaskName, modelTag, modelPriority, modelDate, modelStatus);
+    }
+
+}
