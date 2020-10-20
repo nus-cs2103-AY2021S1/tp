28476fc681c3c2345fdd3dd77c8e43f98bb9f7ff
michael-setia <michaelsetia55@gmail.com>
2020-10-20 17:36:02 +0800

    Undo rename

--- src/main/java/seedu/address/logic/commands/todolistcommands/DeleteTodoCommand.java
+++ src/main/java/seedu/address/logic/commands/todolistcommands/DeleteTodoCommand.java
@@ -5,7 +5,7 @@ import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
-public class DeleteTaskCommand extends Command {
+public class DeleteTodoCommand extends Command {
     public static final String COMMAND_WORD = "deletetodo";
     @Override
     public CommandResult execute(Model model) throws CommandException {
