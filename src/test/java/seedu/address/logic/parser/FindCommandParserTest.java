d355ec597369ee22f4878de85b7ec958989451b5
Andrea <andreaxtan611@gmail.com>
2020-10-21 16:59:20 +0800

    Add find command parser tests

--- src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.FindCommand;
-import seedu.address.model.assignment.NameContainsKeywordsPredicate;
+import seedu.address.model.assignment.*;
 
 public class FindCommandParserTest {
 
@@ -21,14 +21,41 @@ public class FindCommandParserTest {
     }
 
     @Test
-    public void parse_validArgs_returnsFindCommand() {
-        // no leading and trailing whitespaces
-        FindCommand expectedFindCommand =
-                new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList("CS1231S", "Homework")));
-        assertParseSuccess(parser, "CS1231S Homework", expectedFindCommand);
+    public void parse_moreThanOnePrefix_throwsParseException() {
+        assertParseFailure(parser, " n/CS1231S Homework mod/CS2100", String.format(
+                MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MORE_THAN_ONE_PREFIX_MESSAGE));
+    }
 
-        // multiple whitespaces between keywords
-        //assertParseSuccess(parser, " \n CS1231S \n \t Homework  \t", expectedFindCommand);
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, " d/2345 hello", String.format(
+                MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.INVALID_DATE_OR_TIME_MESSAGE));
+        assertParseFailure(parser, " mod/CS386", ModuleCode.MESSAGE_CONSTRAINTS);
+        assertParseFailure(parser, " priority/NO", Priority.MESSAGE_CONSTRAINTS);
+        assertParseFailure(parser, " preamble mod/CS2100", String.format(
+                MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, " preamble n/assignment 2", String.format(
+                MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, " preamble d/1200", String.format(
+                MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, " preamble priority/MEDIUM", String.format(
+                MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_validArgs_returnsFindCommand() {
+        FindCommand expectedFindCommandByName =
+                new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList("CS1231S", "Homework")));
+        FindCommand expectedFindCommandByModuleCode =
+                new FindCommand(new ModuleCodeContainsKeywordsPredicate(Arrays.asList("CS1231S", "CS2100")));
+        FindCommand expectedFindCommandByDeadline =
+                new FindCommand(new DeadlineContainsKeywordsPredicate(Arrays.asList("1200", "24-10-2020")));
+        FindCommand expectedFindCommandByPriority =
+                new FindCommand(new PriorityContainsKeywordsPredicate(Arrays.asList("HIGH", "LOW")));
+        assertParseSuccess(parser, " n/CS1231S Homework", expectedFindCommandByName);
+        assertParseSuccess(parser, " mod/CS1231S CS2100", expectedFindCommandByModuleCode);
+        assertParseSuccess(parser, " d/1200 24-10-2020", expectedFindCommandByDeadline);
+        assertParseSuccess(parser, " priority/HIGH LOW", expectedFindCommandByPriority);
     }
 
 }
