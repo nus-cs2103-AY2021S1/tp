912eff11f5a76483552137585c6611d311a9bc6b
joshtyf <tan.yin.feng@hotmail.com>
2020-10-21 10:30:43 +0800

    Add an InvalidTag

--- src/test/java/quickcache/testutil/TypicalTags.java
+++ src/test/java/quickcache/testutil/TypicalTags.java
@@ -4,4 +4,5 @@ import quickcache.model.flashcard.Tag;
 
 public class TypicalTags {
     public static final Tag testTag = new Tag("test");
+    public static final Tag invalidTag = new Tag("invalid");
 }
