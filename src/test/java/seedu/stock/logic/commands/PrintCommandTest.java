4a60e8401261b641fec1ca99a7c0ea0a2d316a79
Ashley-Lau <ashleylau451@gmail.com>
2020-10-21 18:50:40 +0800

    Fix printcommandtest

--- src/test/java/seedu/stock/logic/commands/PrintCommandTest.java
+++ src/test/java/seedu/stock/logic/commands/PrintCommandTest.java
@@ -1,14 +1,13 @@
 package seedu.stock.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.stock.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.stock.testutil.TypicalStocks.getTypicalSerialNumberSetsBook;
 import static seedu.stock.testutil.TypicalStocks.getTypicalStockBook;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.stock.logic.commands.exceptions.CommandException;
 import seedu.stock.model.Model;
 import seedu.stock.model.ModelManager;
 import seedu.stock.model.UserPrefs;
@@ -19,12 +18,11 @@ import seedu.stock.model.UserPrefs;
 public class PrintCommandTest {
     private Model model = new ModelManager(getTypicalStockBook(), new UserPrefs(), getTypicalSerialNumberSetsBook());
 
-//    @Test
-//    public void execute_stockAcceptedByModel_addSuccessful() throws CommandException {
-//        CommandResult commandResult = new PrintCommand().execute(model);
-//
-//        assertEquals(PrintCommand.MESSAGE_SUCCESS, commandResult.getFeedbackToUser());
-//    }
+    @Test
+    public void execute_stockAcceptedByModel_addSuccessful() {
+        Model expectedModel = new ModelManager(model.getStockBook(), new UserPrefs(), getTypicalSerialNumberSetsBook());
+        assertCommandSuccess(new PrintCommand(), model, PrintCommand.MESSAGE_SUCCESS, expectedModel);
+    }
 
     @Test
     public void equals() {
