84cc3d2850cf484b36d777f6c3a0816492bcd73f
EthanTheGoondu <ethannoahrozario@gmail.com>
2020-10-22 01:03:23 +0800

    Add studentId to JsonAdaptedStudent

--- src/main/java/seedu/address/storage/JsonAdaptedStudent.java
+++ src/main/java/seedu/address/storage/JsonAdaptedStudent.java
@@ -28,6 +28,7 @@ class JsonAdaptedStudent {
     private final String phone;
     private final String email;
     private final List<JsonAdaptedTag> tagged = new ArrayList<>();
+    private final String studentId;
 
     /**
      * Constructs a {@code JsonAdaptedStudent} with the given student details.
@@ -35,10 +36,12 @@ class JsonAdaptedStudent {
     @JsonCreator
     public JsonAdaptedStudent(@JsonProperty("name") String name, @JsonProperty("phone") String phone,
                              @JsonProperty("email") String email,
-                             @JsonProperty("tagged") List<JsonAdaptedTag> tagged) {
+                             @JsonProperty("tagged") List<JsonAdaptedTag> tagged,
+                              @JsonProperty("studentId") String studentId) {
         this.name = name;
         this.phone = phone;
         this.email = email;
+        this.studentId = studentId;
         if (tagged != null) {
             this.tagged.addAll(tagged);
         }
@@ -51,6 +54,7 @@ class JsonAdaptedStudent {
         name = source.getName().fullName;
         phone = source.getPhone().value;
         email = source.getEmail().value;
+        studentId = source.getStudentId().toString();
         tagged.addAll(source.getTags().stream()
                 .map(JsonAdaptedTag::new)
                 .collect(Collectors.toList()));
@@ -91,8 +95,16 @@ class JsonAdaptedStudent {
         }
         final Email modelEmail = new Email(email);
 
+        if (studentId == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, StudentId.class.getSimpleName()));
+        }
+        if (!StudentId.isValidStudentId(studentId)) {
+            throw new IllegalValueException(StudentId.MESSAGE_CONSTRAINTS);
+        }
+        final StudentId modelStudentId = new StudentId(studentId);
+
         final Set<Tag> modelTags = new HashSet<>(personTags);
-        return new Student(modelName, modelPhone, modelEmail, modelTags, new StudentId("Test")); // Todo finish
+        return new Student(modelName, modelPhone, modelEmail, modelTags, modelStudentId);
     }
 
 }
