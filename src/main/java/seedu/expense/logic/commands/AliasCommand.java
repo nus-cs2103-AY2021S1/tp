8f55f72f07b366d11580142d19a83c7a726fd041
Marcus <60178730+marcus2k@users.noreply.github.com>
2020-10-22 00:13:51 +0800

    Add AliasCommand

---
+++ src/main/java/seedu/expense/logic/commands/AliasCommand.java
@@ -0,0 +1,71 @@
+package seedu.expense.logic.commands;
+
+import seedu.expense.logic.commands.exceptions.CommandException;
+import seedu.expense.model.Model;
+import seedu.expense.model.alias.AliasEntry;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Edits the details of an existing expense in the expense book.
+ */
+public class AliasCommand extends Command {
+
+    public static final String COMMAND_WORD = "alias";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Gives a command a customised alias. "
+            + "Alias must not be the same as the default command words and the 'alias' command cannot have an alias. "
+            + "Parameters: "
+            + "[Alias you want to set for a specific command] "
+            + "[Command's current alias or if none, default command word]"
+            + "Example: " + COMMAND_WORD + " find " + "get ";
+
+    public static final String MESSAGE_EDIT_ALIAS_SUCCESS = "Edited alias: %s becomes %s";
+
+    private final String previousAlias;
+    private final String newAlias;
+
+    /**
+     * @param previousAlias   of a command, or default keyword
+     * @param newAlias      to set to a command
+     */
+    public AliasCommand(String previousAlias, String newAlias) {
+        requireNonNull(previousAlias);
+        requireNonNull(newAlias);
+
+        this.previousAlias = previousAlias;
+        this.newAlias = newAlias;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        String actualCommand = model.getAliasMap().getValue(previousAlias);
+        try {
+            model.setAlias(new AliasEntry(previousAlias, actualCommand),
+                    new AliasEntry(newAlias, actualCommand));
+        } catch (IllegalArgumentException e) {
+            throw new CommandException(e.getMessage());
+        }
+        return new CommandResult(String.format(MESSAGE_EDIT_ALIAS_SUCCESS, previousAlias, newAlias));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof EditCommand)) {
+            return false;
+        }
+
+        // state check
+        AliasCommand e = (AliasCommand) other;
+        return previousAlias.equals(e.previousAlias)
+                && newAlias.equals(e.newAlias);
+    }
+
+}
