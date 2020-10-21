99e3ca6241a7572b98a5ec23fcce2af9a54227dd
jzwoo <jianzhe@Woos-MacBook-Pro-2.local>
2020-10-22 03:03:49 +0800

    Updated findmod command to follow find command

--- src/test/java/seedu/address/logic/parser/FindModCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindModCommandParserTest.java
@@ -8,13 +8,16 @@ import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSucces
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
+import java.util.function.Predicate;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.FindModCommand;
-import seedu.address.model.module.ModuleCodeContainsKeywordsPredicate;
-import seedu.address.model.module.ModuleInstructorsContainsKeywordsPredicate;
-import seedu.address.model.module.ModuleNameContainsKeywordsPredicate;
+import seedu.address.model.module.Module;
+import seedu.address.model.module.predicates.ModuleCodeContainsKeywordsPredicate;
+import seedu.address.model.module.predicates.ModuleInstructorsContainsKeywordsPredicate;
+import seedu.address.model.module.predicates.ModuleNameContainsKeywordsPredicate;
 
 class FindModCommandParserTest {
     private FindModCommandParser parser = new FindModCommandParser();
@@ -31,7 +34,7 @@ class FindModCommandParserTest {
     @Test
     public void parse_argsWithDuplicateParameters_throwsParseException() {
         assertParseFailure(parser, " m/CS2103 n/Software n/Engineering i/ ",
-                MESSAGE_DUPLICATE_PREFIX);
+                String.format(MESSAGE_DUPLICATE_PREFIX, "n/"));
     }
     @Test
     public void parse_argsWithModuleParameterAsSentence_throwsParseException() {
@@ -45,7 +48,8 @@ class FindModCommandParserTest {
                 new ModuleNameContainsKeywordsPredicate(Arrays.asList("Software", "Engineering"));
         ModuleInstructorsContainsKeywordsPredicate instructorPredicate =
                 new ModuleInstructorsContainsKeywordsPredicate(Collections.singletonList("Damith"));
-        FindModCommand expectedFindCommand = new FindModCommand(codePredicate, namePredicate, instructorPredicate);
+        List<Predicate<Module>> predicateList = Arrays.asList(codePredicate, namePredicate, instructorPredicate);
+        FindModCommand expectedFindCommand = new FindModCommand(predicateList);
 
         // no leading and trailing whitespaces
         assertParseSuccess(parser, " m/CS2103 n/Software Engineering i/Damith", expectedFindCommand);
--- src/test/java/seedu/address/model/module/ModuleNameContainsKeywordsPredicateTest.java
+++ src/test/java/seedu/address/model/module/ModuleNameContainsKeywordsPredicateTest.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.model.module.predicates.ModuleNameContainsKeywordsPredicate;
 import seedu.address.testutil.ModuleBuilder;
 
 class ModuleNameContainsKeywordsPredicateTest {
