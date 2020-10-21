a6dce2ec43aa7fd665450e2cdafb2d6d2a6cb5fb
florenciamartina <e0407741@u.nus.edu>
2020-10-21 21:29:35 +0800

    Fix typo

--- src/main/java/seedu/address/logic/parser/ListCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ListCommandParser.java
@@ -9,7 +9,7 @@ import seedu.address.logic.parser.exceptions.ParseException;
 public class ListCommandParser implements Parser<ListCommand> {
     /**
      * Parses the given {@code String} of arguments in the context of the ListCommand
-     * and returns an RemarkCommand object for execution.
+     * and returns an ListCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
     public ListCommand parse(String args) throws ParseException {
