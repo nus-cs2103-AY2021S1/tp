256f22d71d114076ffacdf4a1ec6c7197aea69a6
michael-setia <michaelsetia55@gmail.com>
2020-10-22 02:10:37 +0800

    Add assertion and logging

--- src/main/java/seedu/address/logic/commands/todolistcommands/AddTaskCommand.java
+++ src/main/java/seedu/address/logic/commands/todolistcommands/AddTaskCommand.java
@@ -1,11 +1,13 @@
 package seedu.address.logic.commands.todolistcommands;
 
-import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PRIORITY;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
+import java.util.logging.Logger;
+
+import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
@@ -34,24 +36,31 @@ public class AddTaskCommand extends Command {
     public static final String MESSAGE_SUCCESS = "New task added: %1$s";
     public static final String MESSAGE_DUPLICATE_TASK = "This task already exists in the todo list";
 
+    private final Logger logger = LogsCenter.getLogger(AddTaskCommand.class);
+
     private final Task toAdd;
 
     /**
      * Creates an AddTaskCommand to add the specified {@code Task}
      */
     public AddTaskCommand(Task task) {
-        requireNonNull(task);
+        assert task != null;
+        logger.info("Adding a task: \n" + task.toString());
         toAdd = task;
     }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        requireNonNull(model);
+        assert model != null;
 
         if (model.hasTask(toAdd)) {
+            logger.warning("Detected duplicate task. Task will not be added to the list.");
             throw new CommandException(MESSAGE_DUPLICATE_TASK);
         }
 
+        assert toAdd != null;
+
+        logger.info("Adding task to the list.");
         model.addTask(toAdd);
         return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd));
     }
--- src/main/java/seedu/address/logic/parser/todolistparsers/AddTaskParser.java
+++ src/main/java/seedu/address/logic/parser/todolistparsers/AddTaskParser.java
@@ -6,8 +6,10 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PRIORITY;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
+import java.util.logging.Logger;
 import java.util.stream.Stream;
 
+import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.commands.todolistcommands.AddTaskCommand;
 import seedu.address.logic.parser.ArgumentMultimap;
 import seedu.address.logic.parser.ArgumentTokenizer;
@@ -25,6 +27,9 @@ import seedu.address.model.task.TaskName;
  * Parses input arguments and creates a new AddTaskCommand object
  */
 public class AddTaskParser implements Parser<AddTaskCommand> {
+
+    private final Logger logger = LogsCenter.getLogger(AddTaskParser.class);
+
     /**
      * Parses the given {@code String} of arguments in the context of the AddTaskCommand
      * and returns an AddCommand object for execution.
@@ -32,12 +37,16 @@ public class AddTaskParser implements Parser<AddTaskCommand> {
      */
     @Override
     public AddTaskCommand parse(String userInput) throws ParseException {
+        assert userInput != null;
+        logger.info("User input is: " + userInput);
+
         Task task;
         ArgumentTokenizer tokenizer = new ArgumentTokenizer(userInput, PREFIX_NAME, PREFIX_TAG, PREFIX_PRIORITY,
             PREFIX_DATE);
         ArgumentMultimap argMultimap = tokenizer.tokenize();
         if (!arePrefixesPresent(argMultimap, PREFIX_NAME)
             || !argMultimap.getPreamble().isEmpty()) {
+            logger.warning("User input does not contain prefix for name which is compulsory for creating task.");
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddTaskCommand.MESSAGE_USAGE));
         }
 
@@ -47,17 +56,23 @@ public class AddTaskParser implements Parser<AddTaskCommand> {
         task = new Task(taskName);
 
         if (argMultimap.getValue(PREFIX_TAG).isPresent()) {
+            logger.info("Tag field is present");
             Tag taskTag = ParserUtil.parseTag(argMultimap.getValue(PREFIX_TAG).get());
             task = task.setTag(taskTag);
         }
         if (argMultimap.getValue(PREFIX_PRIORITY).isPresent()) {
+            logger.info("Priority field is present.");
             Priority priority = ParserUtil.parseTaskPriority(argMultimap.getValue(PREFIX_PRIORITY).get());
             task = task.setPriority(priority);
         }
         if (argMultimap.getValue(PREFIX_DATE).isPresent()) {
+            logger.info("Date field is present.");
             Date date = ParserUtil.parseTaskDate(argMultimap.getValue(PREFIX_DATE).get());
             task = task.setDate(date);
         }
+
+        assert task != null;
+
         return new AddTaskCommand(task);
     }
 
