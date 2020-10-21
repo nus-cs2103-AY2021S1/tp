58dd4e67b06f158b47ce698e6db71596395429e2
Junhua <wenjh1998@gmail.com>
2020-10-21 20:33:28 +0800

    Add 2 more Test cases

--- src/test/java/jimmy/mcgymmy/logic/commands/ClearCommandTest.java
+++ src/test/java/jimmy/mcgymmy/logic/commands/ClearCommandTest.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Test;
 
 import jimmy.mcgymmy.model.Model;
 import jimmy.mcgymmy.model.ModelManager;
-
+import jimmy.mcgymmy.model.food.Name;
+import jimmy.mcgymmy.testutil.FoodBuilder;
+import jimmy.mcgymmy.testutil.TypicalFoods;
 
 
 public class ClearCommandTest {
@@ -17,6 +19,31 @@ public class ClearCommandTest {
         CommandTestUtil.assertCommandSuccess(new ClearCommand(), model, ClearCommand.MESSAGE_SUCCESS, expectedModel);
     }
 
-    //ToDo: Add relevent non empty success that uses predicates
+    @Test
+    public void execute_oneFoodMcGymmy_success() {
+        Model model = new ModelManager();
+        model.addFood(new FoodBuilder().withName(new Name("Pizza")).build());
+        Model expectedModel = new ModelManager();
+        ClearCommand clearCommand = new ClearCommand();
+        CommandTestUtil.assertCommandSuccess(clearCommand, model, ClearCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    @Test
+    public void execute_partialClearMcGymmy_success() {
+        Model model = new ModelManager();
+        model.addFood(TypicalFoods.APPLE);
+        model.addFood(TypicalFoods.CRISPY_FRIED_FISH);
+        model.updateFilteredFoodList((food) -> food.getName().fullName.contains(TypicalFoods.APPLE.getName().fullName));
+
+        Model expectedModel = new ModelManager();
+        expectedModel.addFood(TypicalFoods.CRISPY_FRIED_FISH);
+        expectedModel.updateFilteredFoodList((food) -> food.getName()
+                .fullName.contains(TypicalFoods.APPLE.getName().fullName));
+
+
+        ClearCommand clearCommand = new ClearCommand();
+        CommandTestUtil.assertCommandSuccess(clearCommand, model, ClearCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
 
 }
