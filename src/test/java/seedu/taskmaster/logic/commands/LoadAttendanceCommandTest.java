ba631c3ce1b2a5d51c45b8f96d5bbd217770833d
JoshuaChewJianXiang <joshuachewabc@gmail.com>
2020-10-22 23:33:52 +0800

    Add more assertions to LoadAttendanceCommandTest.java

--- src/test/java/seedu/taskmaster/logic/commands/LoadAttendanceCommandTest.java
+++ src/test/java/seedu/taskmaster/logic/commands/LoadAttendanceCommandTest.java
@@ -61,6 +61,7 @@ public class LoadAttendanceCommandTest {
             File file = new File(filepath.toString());
             file.delete();
         }
+        assert(!isExistingFile);
 
         Model model = new ModelManager(SampleDataUtil.getSampleTaskmaster(), new UserPrefs());
         StorageCommand loadCommand = new LoadAttendanceCommand(filename);
@@ -77,6 +78,7 @@ public class LoadAttendanceCommandTest {
         if (!isExistingFile) {
             throw new AssertionError("Sample load file for existing file test not found.");
         }
+        assert(isExistingFile);
 
         StorageCommand loadCommand = new LoadAttendanceCommand(filename);
         loadCommand.initialiseStorage(storage);
