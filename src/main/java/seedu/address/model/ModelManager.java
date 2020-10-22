6bda858d1dd7e15cf4cb26cfbcceca16cf68fddc
Andrea <andreaxtan611@gmail.com>
2020-10-22 14:37:21 +0800

    Remove unused import

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -12,7 +12,6 @@ import javafx.collections.transformation.FilteredList;
 import seedu.address.commons.core.GuiSettings;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.assignment.Assignment;
-import seedu.address.model.lesson.Lesson;
 import seedu.address.model.task.Task;
 import seedu.address.timetable.TimetableData;
 
