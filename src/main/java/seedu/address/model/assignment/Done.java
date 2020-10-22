792b6566f2dbc8376c89192f0d82562fd7559f95
Jerry Ho <jerryho@u.nus.edu>
2020-10-22 17:02:17 +0800

    Add Done

---
+++ src/main/java/seedu/address/model/assignment/Done.java
@@ -0,0 +1,40 @@
+package seedu.address.model.assignment;
+
+/**
+ * Represents a done tag of an assignment.
+ * Guarantees: immutable; assignment is marked as done as declared in {@link #isMarkedDone()}
+ */
+public class Done {
+    private boolean isMarkedDone;
+
+    /**
+     * Constructs a {@code Remind}.
+     */
+    public Done() {
+        this.isMarkedDone = false;
+    }
+
+    public Done(boolean isMarkedDone) {
+        this.isMarkedDone= isMarkedDone;
+    }
+
+    /**
+     * Sets done tag to true.
+     *
+     * @return Done object with done tag set to true
+     */
+    public Done markAsDone() {
+        Done markedDone = new Done();
+        markedDone.isMarkedDone = true;
+        return markedDone;
+    }
+
+    /**
+     * Returns true if done tag is set to true. Otherwise, returns false.
+     *
+     * @return true if done tag is set to true. Otherwise, returns false
+     */
+    public boolean isMarkedDone() {
+        return isMarkedDone;
+    }
+}
