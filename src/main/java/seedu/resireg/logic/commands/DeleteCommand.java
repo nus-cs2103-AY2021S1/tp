516c15b2dc272d2269e071cefc5dbf96e669b70f
Lysire <slackertux13.37@gmail.com>
2020-10-23 15:31:12 +0800

    Add testing support for undo and redo

--- src/main/java/seedu/resireg/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/resireg/logic/commands/DeleteCommand.java
@@ -12,7 +12,7 @@ import seedu.resireg.model.allocation.Allocation;
 import seedu.resireg.model.student.Student;
 
 /**
- * Deletes a student identified using it's displayed index from the address book.
+ * Deletes a student identified using its displayed index from the address book.
  */
 public class DeleteCommand extends Command {
 
---
+++ src/test/java/seedu/resireg/logic/commands/RedoCommandTest.java
@@ -0,0 +1,47 @@
+package seedu.resireg.logic.commands;
+
+import static seedu.resireg.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.resireg.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.resireg.logic.commands.CommandTestUtil.deleteFirstStudent;
+import static seedu.resireg.testutil.TypicalStudents.getTypicalAddressBook;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import seedu.resireg.model.Model;
+import seedu.resireg.model.ModelManager;
+import seedu.resireg.model.UserPrefs;
+
+class RedoCommandTest {
+
+    private final Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    private final Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @BeforeEach
+    public void setUp() {
+        // set up undo/redo history
+        deleteFirstStudent(model);
+        deleteFirstStudent(model);
+        model.undoResiReg();
+        model.undoResiReg();
+
+        deleteFirstStudent(expectedModel);
+        deleteFirstStudent(expectedModel);
+        expectedModel.undoResiReg();
+        expectedModel.undoResiReg();
+    }
+
+    @Test
+    public void execute() {
+        // multiple redoable states in model
+        expectedModel.redoResiReg();
+        assertCommandSuccess(new RedoCommand(), model, RedoCommand.MESSAGE_SUCCESS, expectedModel);
+
+        // single redoable state in model
+        expectedModel.redoResiReg();
+        assertCommandSuccess(new RedoCommand(), model, RedoCommand.MESSAGE_SUCCESS, expectedModel);
+
+        // no redoable state in model
+        assertCommandFailure(new RedoCommand(), model, RedoCommand.MESSAGE_FAILURE);
+    }
+}
