1e4225f3c001bb9beb7d0fe8fdc5e1e8e96aa3c3
zhiayang <zhiayang@nascentnoodle.com>
2020-10-24 13:01:52 +0800

    Add tests for StringView

--- src/test/java/chopchop/commons/util/StringViewTest.java
+++ src/test/java/chopchop/commons/util/StringViewTest.java
@@ -4,8 +4,11 @@ package chopchop.commons.util;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static chopchop.testutil.Assert.assertThrows;
 
 public class StringViewTest {
 
@@ -17,5 +20,35 @@ public class StringViewTest {
 
         assertEquals(3, sv1.find("def"));
         assertEquals(7, sv1.find(new StringView("ghi")));
+        assertEquals(1, sv1.find('b'));
+
+        assertTrue(sv2.startsWith("owo"));
+        assertTrue(sv2.startsWith(new StringView("owo")));
+
+        assertEquals('o', sv2.at(2));
+        assertEquals('a', sv1.front());
+        assertEquals('A', sv2.back());
+
+        assertThrows(AssertionError.class, () -> new StringView("").front());
+        assertThrows(AssertionError.class, () -> new StringView("").back());
+
+        assertEquals("owo", sv2.take(3).toString());
+        assertEquals("AYAYA", sv2.takeLast(5).toString());
+
+        assertEquals("owo uwu AYAYA", sv2.take(400).toString());
+        assertEquals("owo uwu AYAYA", sv2.takeLast(400).toString());
+
+        assertEquals(new StringView("ayaya").uppercase().toString(), "AYAYA");
+        assertEquals(new StringView("AYAYA").lowercase().toString(), "ayaya");
+
+        assertTrue(new StringView("asdf").equals("asdf"));
+        assertEquals(sv1, sv1);
+        assertEquals(sv1, new StringView(sv1.toString()));
+        assertNotEquals(sv1, 3);
+
+        assertFalse(sv1.startsWith("owo"));
+        assertFalse(sv2.startsWith(new StringView("kekw")));
+
+        assertEquals("asdf".hashCode(), new StringView("asdf").hashCode());
     }
 }
