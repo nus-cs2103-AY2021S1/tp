210623c5b8599b381f885f703daf549177617014
Elgoh <elgoh3@gmail.com>
2020-10-24 15:16:10 +0800

    Update DuplicateInventoryRecordException.java to fit style guide

--- src/main/java/nustorage/model/record/exceptions/DuplicateInventoryRecordException.java
+++ src/main/java/nustorage/model/record/exceptions/DuplicateInventoryRecordException.java
@@ -1,8 +1,8 @@
 package nustorage.model.record.exceptions;
 
 /**
- * Signals that the operation will result in duplicate InventoryRecord (InventoryRecords are considered duplicates if they have the same
- * name).
+ * Signals that the operation will result in duplicate InventoryRecord
+ * (InventoryRecords are considered duplicates if they have the same name).
  */
 public class DuplicateInventoryRecordException extends RuntimeException {
     public DuplicateInventoryRecordException() {
