14c8cdbc70e61fdea84425e453a9e51b192a7986
halcon-blanco <saadh98@gmail.com>
2020-10-24 14:02:29 +0800

    Increase quantity font size, make stats more pronounced, add labels for quantity and supplier

--- src/main/java/seedu/address/ui/ItemCard.java
+++ src/main/java/seedu/address/ui/ItemCard.java
@@ -77,9 +77,9 @@ public class ItemCard extends UiPart<Region> {
             if (percentage > 100) {
                 quantityStats.setFill(Color.LIGHTSKYBLUE);
             } else if (percentage > 20) {
-                quantityStats.setFill(Color.GREEN);
+                quantityStats.setFill(Color.LIGHTGREEN);
             } else if (percentage <= 20) {
-                quantityStats.setFill(Color.RED);
+                quantityStats.setFill(Color.web("#f24e6c")); // light red
             }
         } else {
             if (itemMetric.isPresent()) {
