76b3c545cb23fefc3511cab6ed384ea3aeb33288
eugene3231 <47620989+eugene3231@users.noreply.github.com>
2020-10-24 12:10:23 +0800

    Add assert and examples of defensive programming

--- src/main/java/seedu/address/logic/commands/RedoCommand.java
+++ src/main/java/seedu/address/logic/commands/RedoCommand.java
@@ -19,6 +19,7 @@ public class RedoCommand extends Command {
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
+        // Defensive programming
         if (!model.canRedoCliniCal()) {
             throw new CommandException(MESSAGE_FAILURE);
         }
