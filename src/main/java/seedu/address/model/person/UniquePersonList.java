b0463b327274520584045f412bd188759d2a90fa
lll-jy <E0376997@u.nus.edu>
2020-10-24 15:21:17 +0800

    Fix checkstyle fail

--- src/main/java/seedu/address/model/person/UniquePersonList.java
+++ src/main/java/seedu/address/model/person/UniquePersonList.java
@@ -9,7 +9,6 @@ import java.util.stream.Stream;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-
 import seedu.address.model.person.exceptions.DuplicatePersonException;
 import seedu.address.model.person.exceptions.PersonNotFoundException;
 
