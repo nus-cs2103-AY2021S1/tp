948ee98e6565d8654743168c9743066f59b70026
hjl99 <hejl20150101@gmail.com>
2020-10-24 20:42:44 +0800

    Improve style in IngredientContainsKeywordPredicate.java

--- src/main/java/chopchop/model/attributes/IngredientsContainsKeywordsPredicate.java
+++ src/main/java/chopchop/model/attributes/IngredientsContainsKeywordsPredicate.java
@@ -5,7 +5,6 @@ import chopchop.model.recipe.Recipe;
 
 import java.util.List;
 import java.util.function.Predicate;
-import java.util.stream.Stream;
 
 /**
  * Tests that an item's {@code Name} matches any of the keywords given.
@@ -19,16 +18,14 @@ public class IngredientsContainsKeywordsPredicate implements Predicate<Recipe> {
 
     @Override
     public boolean test(Recipe recipe) {
-        Stream<String> indNameList = recipe.getIngredients()
-                .stream().map(indRef -> indRef.getName());
-
         // Returns recipes whose ingredient name list containing any of these keywords
         /*return indNameList.anyMatch(indName -> this.keywords.stream()
                 .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(indName, keyword)));*/
 
         // Returns recipes whose ingredient name list contains all these keywords
         return this.keywords.stream().allMatch(keyword -> recipe.getIngredients()
-                .stream().map(indRef -> indRef.getName())
+                .stream()
+                .map(indRef -> indRef.getName())
                 .anyMatch(indName -> StringUtil.containsWordIgnoreCase(indName, keyword)));
     }
 
