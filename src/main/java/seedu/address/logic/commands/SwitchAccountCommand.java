31a96a3363d5482dd5f95bddb1c0693497a627d6
Lim Zi Yang <limziyang8@gmail.com>
2020-10-24 15:36:43 +0800

    Add SwitchAccountCommand tests

--- src/main/java/seedu/address/logic/commands/SwitchAccountCommand.java
+++ src/main/java/seedu/address/logic/commands/SwitchAccountCommand.java
@@ -23,8 +23,8 @@ public class SwitchAccountCommand extends Command {
             + "Parameters: INDEX (must be a positive integer). "
             + "Example: " + COMMAND_WORD + " 1";
 
-    private static final String MESSAGE_SWITCH_ACCOUNT_SUCCESS = "Account switch! Current account: %1$s";
-    private static final String MESSAGE_ACTIVE_ACCOUNT = "You are already on the account!";
+    public static final String MESSAGE_SWITCH_ACCOUNT_SUCCESS = "Account switch! Current account: %1$s";
+    public static final String MESSAGE_ACTIVE_ACCOUNT = "You are already on the account!";
 
     private final Index targetIndex;
 
@@ -38,7 +38,7 @@ public class SwitchAccountCommand extends Command {
     @Override
     public CommandResult execute(Model model, ActiveAccount activeAccount) throws CommandException {
         requireAllNonNull(model, activeAccount);
-        assert true;
+
         List<Account> accounts = model.getFilteredAccountList();
 
         int index = targetIndex.getZeroBased();
