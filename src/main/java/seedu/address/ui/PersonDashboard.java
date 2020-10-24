e89fb177074cd95c07d8c1a4654ef9412fbc992b
lll-jy <E0376997@u.nus.edu>
2020-10-25 07:32:21 +0800

    Create PersonDashboard

---
+++ src/main/java/seedu/address/ui/PersonDashboard.java
@@ -0,0 +1,94 @@
+package seedu.address.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import seedu.address.model.project.Project;
+
+import java.util.Comparator;
+import java.util.Optional;
+
+/**
+ * An UI component that displays information of a {@code Project}.
+ */
+public class PersonDashboard extends UiPart<Region> {
+
+    private static final String FXML = "ProjectDashboard.fxml";
+
+    /**
+     * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
+     * As a consequence, UI elements' variable names cannot be set to such keywords
+     * or an exception will be thrown by JavaFX during runtime.
+     *
+     * @see <a href="https://github.com/se-edu/addressbook-level4/issues/336">The issue on MainCatalogue level 4</a>
+     */
+
+    public final Project project;
+
+    @FXML
+    private HBox dashboardPane;
+    @FXML
+    private Label projectName;
+    @FXML
+    private Label deadline;
+    @FXML
+    private Label projectDescription;
+    @FXML
+    private Label repoUrl;
+    @FXML
+    private Label header1;
+    @FXML
+    private Label header2;
+    @FXML
+    private Label header3;
+    @FXML
+    private FlowPane tags;
+    @FXML
+    private FlowPane teammates;
+    @FXML
+    private FlowPane tasks;
+    @FXML
+    private FlowPane meetings;
+
+    /**
+     * Creates a {@code ProjectDashboardCode} with the given {@code Project} and index to display.
+     */
+    public PersonDashboard(Optional<Project> project) {
+        super(FXML);
+        this.project = project.get();
+        projectName.setText(this.project.getProjectName().fullProjectName);
+        deadline.setText("Project deadline: " + this.project.getDeadline().toString());
+        projectDescription.setText("Project description: " + this.project.getProjectDescription().value);
+        repoUrl.setText("Project repourl: " + this.project.getRepoUrl().value);
+        header1.setText("Tasks: ");
+        this.project.getFilteredTaskList().stream()
+                .sorted(Comparator.comparing(task -> task.taskName))
+                .forEach(task -> tasks.getChildren().add(new Label(task.taskName)));
+        header2.setText("Teammates: ");
+        this.project.getTeammates().stream()
+                .forEach(person -> teammates.getChildren().add(
+                        new Label(person.getPerson().getPersonName().toString())));
+        header3.setText("Meetings: ");
+        this.project.getMeetings()
+                .forEach(meeting -> meetings.getChildren().add(new Label(meeting.getMeetingName())));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof PersonDashboard)) {
+            return false;
+        }
+
+        // state check
+        PersonDashboard card = (PersonDashboard) other;
+        return project.equals(card.project);
+    }
+}
