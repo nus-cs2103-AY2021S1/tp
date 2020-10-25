7030e71a65072ffdc0977b2300f31dbfcc1a97fb
Constance <constancensq@gmail.com>
2020-10-25 12:08:29 +0800

    Refine address class

--- src/main/java/seedu/pivot/model/investigationcase/caseperson/Address.java
+++ src/main/java/seedu/pivot/model/investigationcase/caseperson/Address.java
@@ -5,17 +5,9 @@ import static seedu.pivot.commons.util.AppUtil.checkArgument;
 
 /**
  * Represents a Person's address in PIVOT.
- * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
+ * Guarantees: immutable; is always valid
  */
 public class Address {
-
-    public static final String MESSAGE_CONSTRAINTS = "Addresses can take any values, and it should not be blank";
-
-    /*
-     * The first character of the address must not be a whitespace,
-     * otherwise " " (a blank string) becomes a valid input.
-     */
-    public static final String VALIDATION_REGEX = "[^\\s].*";
 
     public final String value;
 
@@ -26,15 +18,7 @@ public class Address {
      */
     public Address(String address) {
         requireNonNull(address);
-        checkArgument(isValidAddress(address), MESSAGE_CONSTRAINTS);
         value = address;
-    }
-
-    /**
-     * Returns true if a given string is a valid email.
-     */
-    public static boolean isValidAddress(String test) {
-        return test.isBlank() || test.matches(VALIDATION_REGEX);
     }
 
     @Override
