8335990ca5b5c6366ed60392fa19db56096cad30
ktaekwon000 <ktaekwon000@gmail.com>
2020-10-26 00:37:19 +0800

    Remove errors that appeared if users clicked on Calendar GUI

--- src/main/java/seedu/address/ui/CalendarDisplay.java
+++ src/main/java/seedu/address/ui/CalendarDisplay.java
@@ -5,9 +5,13 @@ import static seedu.address.commons.util.VEventUtil.appsToVEventsMapper;
 import javafx.collections.ListChangeListener.Change;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.input.MouseDragEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TouchEvent;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
 import jfxtras.icalendarfx.VCalendar;
+import jfxtras.scene.control.agenda.Agenda;
 import jfxtras.scene.control.agenda.icalendar.ICalendarAgenda;
 import seedu.address.model.appointment.Appointment;
 
@@ -43,17 +47,19 @@ public class CalendarDisplay extends UiPart<Region> {
     private static void disableMouseInteraction(ICalendarAgenda agenda) {
         agenda.setAllowDragging(false);
         agenda.setAllowResize(false);
-        agenda.setActionCallback(null);
-        agenda.setNewAppointmentCallback(null);
-        agenda.setSelectedOneAppointmentCallback(null);
-        agenda.setNewAppointmentDrawnCallback(null);
-        agenda.setAppointmentChangedCallback(null);
-        agenda.setOnMouseClicked(null);
-        agenda.setOnMousePressed(null);
+        agenda.setActionCallback((Agenda.Appointment n) -> null);
+        agenda.setNewAppointmentCallback((Agenda.LocalDateTimeRange n) -> null);
+        agenda.setSelectedOneAppointmentCallback((Agenda.Appointment n) -> null);
+        agenda.setNewAppointmentDrawnCallback((Agenda.Appointment a) -> null);
+        agenda.setAppointmentChangedCallback((Agenda.Appointment a) -> null);
+        agenda.setOnMouseClicked((MouseEvent e) -> {});
+        agenda.setOnMousePressed((MouseEvent e) -> {});
         agenda.setAllowDragging(false);
-        agenda.setOnTouchPressed(null);
-        agenda.setOnMouseEntered(null);
-        agenda.setOnMouseExited(null);
+        agenda.setOnMouseDragEntered((MouseDragEvent e) -> {});
+        agenda.setOnMouseDragExited((MouseDragEvent e) -> {});
+        agenda.setOnTouchPressed((TouchEvent e) -> {});
+        agenda.setOnMouseEntered((MouseEvent e) -> {});
+        agenda.setOnMouseExited((MouseEvent e) -> {});
     }
 
 }
