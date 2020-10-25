73d1f00df12c21390c4e5eb45879454f90b35acf
Nikhilalalalala <uvnikhi@gmail.com>
2020-10-25 21:32:21 +0800

    Add assertion

--- src/test/java/com/eva/model/person/applicant/ApplicationStatusTest.java
+++ src/test/java/com/eva/model/person/applicant/ApplicationStatusTest.java
@@ -1,6 +1,5 @@
 package com.eva.model.person.applicant;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
