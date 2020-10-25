187507ec2bf1374faab6a7b92114e7b3c216f719
Tian Fang <tianfangsg@gmail.com>
2020-10-25 18:24:35 +0800

    Change Deadline to contain LocalDateTime instead of String

--- src/main/java/seedu/address/model/project/Deadline.java
+++ src/main/java/seedu/address/model/project/Deadline.java
@@ -3,32 +3,55 @@ package seedu.address.model.project;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
+import java.time.LocalDateTime;
+
 /**
  * Represents a Project's deadline in the main catalogue.
  * Guarantees: immutable; is valid as declared in {@link #isValidDeadline(String)}
  */
-public class Deadline {
+public class Deadline implements Comparable<Deadline> {
     public static final String MESSAGE_CONSTRAINTS =
             "Deadline should only be in the format of dd-MM-yyyy HH:mm:ss, "
                     + "and the time should only be in the format of 24-Hour";
     public static final String VALIDATION_REGEX = "(\\d{2}-\\d{2}-\\d{4} \\d{2}:\\d{2}:\\d{2})";
-    public final String value;
+    private final LocalDateTime dateTime;
 
     /**
      * Constructs a {@code Deadline}.
      *
-     * @param deadline A valid phone number.
+     * @param deadline A valid deadline string.
      */
     public Deadline(String deadline) {
         requireNonNull(deadline);
         checkArgument(isValidDeadline(deadline), MESSAGE_CONSTRAINTS);
-        value = deadline;
+        dateTime = convertIntoDateTime(deadline);
+    }
+
+    /**
+     * Converts a valid deadline string into LocalDateTime.
+     * @param deadline  the deadline string to be converted
+     * @return  an LocalDateTime object that corresponds to the deadline string
+     */
+    public LocalDateTime convertIntoDateTime(String deadline) {
+        assert isValidDeadline(deadline);
+        String[] dateTime = deadline.split(" ");
+        String dateString = dateTime[0];
+        String timeString = dateTime[1];
+        String[] date = dateString.split("-");
+        String[] time = timeString.split("-");
+        int day = Integer.parseInt(date[0]);
+        int month = Integer.parseInt(date[1]);
+        int year = Integer.parseInt(date[2]);
+        int hour = Integer.parseInt(time[0]);
+        int minute = Integer.parseInt(time[1]);
+        int second = Integer.parseInt(time[2]);
+        return LocalDateTime.of(year, month, day, hour, minute, second);
     }
 
     /**
      * Returns true if a given string is a valid deadline.
      */
-    public static boolean isValidDeadline(String test) { //todo: check if the deadline is later than the starting time
+    public static boolean isValidDeadline(String test) {
         boolean isValidDate = false;
         boolean isValidTime = false;
         if (test.matches(VALIDATION_REGEX)) {
@@ -81,21 +104,29 @@ public class Deadline {
         boolean isValidSecond = Integer.parseInt(second) <= 59 && Integer.parseInt(second) >= 0;
         return isValidHour && isValidMinute && isValidSecond;
     }
+    public LocalDateTime getDateTime() {
+        return this.dateTime;
+    }
+
     @Override
     public String toString() {
-        return value;
+        return dateTime.toString();
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof Deadline // instanceof handles nulls
-                && value.equals(((Deadline) other).value)); // state check
+                && dateTime.equals(((Deadline) other).dateTime)); // state check
     }
 
     @Override
     public int hashCode() {
-        return value.hashCode();
+        return dateTime.hashCode();
     }
 
+    @Override
+    public int compareTo(Deadline deadline) {
+        return this.dateTime.compareTo(deadline.getDateTime());
+    }
 }
