8cf1a092cccddb8f688c592e7ec981cb115bf2a4
lettuceman <manle.9a4@gmail.com>
2020-10-25 14:48:49 +0800

    Add FindCommandTest and FindCommandParser test

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -8,6 +8,6 @@ public class Messages {
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command!";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_DISPLAYED_INDEX = "The index provided is out of bounds!";
-    public static final String MESSAGE_ENTRIES_LISTED_OVERVIEW = "Entries updated!";
-
+    public static final String MESSAGE_ENTRIES_UPDATED = "Entries updated!";
+    public static final String MESSAGE_EMPTY_FILTERED_LIST = "No entries found!";
 }
--- src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
@@ -1,34 +1,76 @@
-//package seedu.address.logic.parser;
-//
-//import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-//import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
-//import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
-//
-//import java.util.Arrays;
-//
-//import org.junit.jupiter.api.Test;
-//
-//import seedu.address.logic.commands.FindCommand;
-//import seedu.address.model.account.NameContainsKeywordsPredicate;
-//
-//public class FindCommandParserTest {
-//
-//    private FindCommandParser parser = new FindCommandParser();
-//
-//    @Test
-//    public void parse_emptyArg_throwsParseException() {
-//        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
-//    }
-//
-//    @Test
-//    public void parse_validArgs_returnsFindCommand() {
-//        // no leading and trailing whitespaces
-//        FindCommand expectedFindCommand =
-//                new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList("Alice", "Bob")));
-//        assertParseSuccess(parser, "Alice Bob", expectedFindCommand);
-//
-//        // multiple whitespaces between keywords
-//        assertParseSuccess(parser, " \n Alice \n \t Bob  \t", expectedFindCommand);
-//    }
-//
-//}
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.commons.core.category.Category.EXPENSE_STRING;
+import static seedu.address.commons.core.category.Category.REVENUE_STRING;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.logic.parser.util.CliSyntax.PREFIX_CATEGORY;
+import static seedu.address.logic.parser.util.CliSyntax.PREFIX_KEYWORDS;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.FindCommand;
+import seedu.address.model.account.entry.ExpenseDescriptionContainsKeywordsPredicate;
+import seedu.address.model.account.entry.RevenueDescriptionContainsKeywordsPredicate;
+
+public class FindCommandParserTest {
+    private static final String WHITE_SPACE = " ";
+    private static final String KEYWORDS = "pots flower rent";
+    private static final String INVALID_USER_INPUT_NO_KEYWORDS = WHITE_SPACE + PREFIX_KEYWORDS + WHITE_SPACE;
+    private static final String VALID_USER_INPUT_NO_CATEGORY = WHITE_SPACE + PREFIX_KEYWORDS + KEYWORDS;
+    private static final String VALID_USER_INPUT_WITH_EXPENSE_CATEGORY =
+        WHITE_SPACE + PREFIX_CATEGORY + EXPENSE_STRING + VALID_USER_INPUT_NO_CATEGORY;
+    private static final String VALID_USER_INPUT_WITH_REVENUE_CATEGORY =
+        WHITE_SPACE + PREFIX_CATEGORY + REVENUE_STRING + VALID_USER_INPUT_NO_CATEGORY;
+    private static final String EMPTY_KEYWORDS_MESSAGE = String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+        FindCommand.EMPTY_KEYWORD_LIST_MESSAGE);
+    private FindCommandParser parser = new FindCommandParser();
+
+    @Test
+    public void parse_emptyArg_throwsParseException() {
+        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+            FindCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_validArgs_returnsFindCommandWithBothPredicates() {
+        List<String> keywordList = Arrays.asList("pots", "flower", "rent");
+        ExpenseDescriptionContainsKeywordsPredicate expensePredicate = new
+            ExpenseDescriptionContainsKeywordsPredicate(keywordList);
+        RevenueDescriptionContainsKeywordsPredicate revenuePredicate = new
+            RevenueDescriptionContainsKeywordsPredicate(keywordList);
+        FindCommand expectedFindCommand = new FindCommand(expensePredicate, revenuePredicate);
+
+        assertParseSuccess(parser, VALID_USER_INPUT_NO_CATEGORY, expectedFindCommand);
+    }
+
+    @Test
+    public void parse_validArgs_returnsFindCommandWithExpensePredicate() {
+        List<String> keywordList = Arrays.asList("pots", "flower", "rent");
+        ExpenseDescriptionContainsKeywordsPredicate expensePredicate = new
+            ExpenseDescriptionContainsKeywordsPredicate(keywordList);
+        FindCommand expectedFindCommand = new FindCommand(expensePredicate);
+
+        assertParseSuccess(parser, VALID_USER_INPUT_WITH_EXPENSE_CATEGORY, expectedFindCommand);
+    }
+
+    @Test
+    public void parse_validArgs_returnsFindCommandWithRevenuePredicate() {
+        List<String> keywordList = Arrays.asList("pots", "flower", "rent");
+        RevenueDescriptionContainsKeywordsPredicate revenuePredicate = new
+            RevenueDescriptionContainsKeywordsPredicate(keywordList);
+        FindCommand expectedFindCommand = new FindCommand(revenuePredicate);
+
+        assertParseSuccess(parser, VALID_USER_INPUT_WITH_REVENUE_CATEGORY, expectedFindCommand);
+    }
+
+    @Test
+    public void parse_emptyKeywordList_throwsParseException() {
+        assertParseFailure(parser, INVALID_USER_INPUT_NO_KEYWORDS, EMPTY_KEYWORDS_MESSAGE);
+    }
+
+}
