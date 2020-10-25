710eaf33e5f2c8dac65a694d7e0104c9355ade8e
Chen Xihao <chenxihao0627@gmail.com>
2020-10-25 11:45:04 +0800

    Adapt test cases

--- src/test/java/nustorage/testutil/TypicalInventoryRecords.java
+++ src/test/java/nustorage/testutil/TypicalInventoryRecords.java
@@ -9,16 +9,19 @@ import static nustorage.logic.commands.CommandTestUtil.ITEM_NAME_3;
 import static nustorage.logic.commands.CommandTestUtil.QUANTITY_1;
 import static nustorage.logic.commands.CommandTestUtil.QUANTITY_2;
 import static nustorage.logic.commands.CommandTestUtil.QUANTITY_3;
+import static nustorage.logic.commands.CommandTestUtil.ID_A;
+import static nustorage.logic.commands.CommandTestUtil.ID_B;
+import static nustorage.logic.commands.CommandTestUtil.ID_C;
 
 import nustorage.model.record.InventoryRecord;
 
 public class TypicalInventoryRecords {
 
     public static final InventoryRecord INVENTORY_RECORD_A =
-            new InventoryRecord(ITEM_NAME_1, QUANTITY_1, DATE_TIME_A);
+            new InventoryRecord(ITEM_NAME_1, QUANTITY_1, DATE_TIME_A, ID_A);
     public static final InventoryRecord INVENTORY_RECORD_B =
-            new InventoryRecord(ITEM_NAME_2, QUANTITY_2, DATE_TIME_B);
+            new InventoryRecord(ITEM_NAME_2, QUANTITY_2, DATE_TIME_B, ID_B);
     public static final InventoryRecord INVENTORY_RECORD_C =
-            new InventoryRecord(ITEM_NAME_3, QUANTITY_3, DATE_TIME_C);
+            new InventoryRecord(ITEM_NAME_3, QUANTITY_3, DATE_TIME_C, ID_C);
 
 }
