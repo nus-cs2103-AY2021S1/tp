375bf2e83be17b83b8be55842693f2d55183ab60
Nahoyhp <phyohan@hotmail.com>
2020-10-25 22:05:34 +0800

    Add in CaloriesGraph UI, with dummy data.

--- src/main/java/seedu/address/ui/CaloriesGraph.java
+++ src/main/java/seedu/address/ui/CaloriesGraph.java
@@ -1,35 +1,42 @@
 package seedu.address.ui;
 
+
 import javafx.fxml.FXML;
-import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.chart.XYChart.Series;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.Region;
 
 public class CaloriesGraph extends UiPart<Region> {
     private static final String FXML = "CaloriesGraph.fxml";
 
     @FXML
-    private LineChart<String, Number> graph;
+    private Pane pane;
 
     /**
      * Creates a {@code CaloriesGraph} with the given {@code HashMap}.
      */
     public CaloriesGraph() {
         super(FXML);
-        graph = generateGraph();
+        fillInfo();
     }
 
-    private static LineChart<String, Number> generateGraph() {
-        CategoryAxis xAxis = new CategoryAxis();
-        xAxis.setLabel("Month");
+    private void fillInfo() {
+        NumberAxis xAxis = new NumberAxis();
         NumberAxis yAxis = new NumberAxis();
-        yAxis.setLabel("Value");
+        LineChart<Number, Number> lineChart = new LineChart<Number, Number>(xAxis, yAxis);
 
-        LineChart<String, Number> lineChart = new LineChart<>(xAxis, yAxis);
-
-        return lineChart;
+        Series<Number, Number> series = new Series<Number, Number>();
+        series.setName("No of schools in an year");
+        series.getData().add(new XYChart.Data(1970, 15));
+        series.getData().add(new XYChart.Data(1980, 30));
+        series.getData().add(new XYChart.Data(1990, 60));
+        series.getData().add(new XYChart.Data(2000, 120));
+        series.getData().add(new XYChart.Data(2013, 240));
+        series.getData().add(new XYChart.Data(2014, 300));
+        lineChart.getData().add(series);
+        pane.getChildren().add(lineChart);
     }
 }
