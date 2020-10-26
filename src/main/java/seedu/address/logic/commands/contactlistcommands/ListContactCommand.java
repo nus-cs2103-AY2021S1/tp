5d453fcebcfd8bca19e6f7081345170a86ad01bd
jonasngs <jonasngs30@gmail.com>
2020-10-26 11:48:37 +0800

    Resolve checkstyle issues

--- src/main/java/seedu/address/logic/commands/contactlistcommands/ListContactCommand.java
+++ src/main/java/seedu/address/logic/commands/contactlistcommands/ListContactCommand.java
@@ -4,13 +4,13 @@ package seedu.address.logic.commands.contactlistcommands;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_CONTACTS;
 
+import java.util.logging.Logger;
+
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-
-import java.util.logging.Logger;
 
 /**
  * Lists all the existing contacts in the contact list to the user.
