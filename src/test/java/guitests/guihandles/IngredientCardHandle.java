ba699b2ca7eee5f27740237cc6f63ecabecfe205
Xue Yong <xueyong26@hotmail.com>
2020-10-27 02:23:11 +0800

    Add more handles for GUI

---
+++ src/test/java/guitests/guihandles/IngredientCardHandle.java
@@ -0,0 +1,35 @@
+package guitests.guihandles;
+
+
+import chopchop.model.ingredient.Ingredient;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+
+/**
+ * A handle to the {@code IngredientCard} in the GUI.
+ */
+public class IngredientCardHandle extends NodeHandle<Node> {
+    private static final String NAME_FIELD_ID = "#name";
+
+    private final Label nameLabel;
+
+    /**
+     * Constructs a {@code IngredientCard} with the given {@code ingredientCardNode}.
+     */
+    public IngredientCardHandle(Node ingredientCardNode) {
+        super(ingredientCardNode);
+        nameLabel = getChildNode(NAME_FIELD_ID);
+    }
+
+    /**
+     * Retrieves the name of the ingredient in the ingredient card.
+     */
+    public String getName() {
+        return nameLabel.getText();
+    }
+
+    public boolean equals(Ingredient ingredient) {
+        // Well we gotta change this depending on how we make our ingredient card more presentable uwu
+        return getName().equals(ingredient.getName());
+    }
+}
