1a0f31101902a4d92c06c216fc445ca1b06f52e8
StopTakingAllTheNames <ong.yinggao@gmail.com>
2020-10-26 12:34:25 +0800

    Update help message

--- src/main/java/seedu/address/logic/commands/QuestionCommand.java
+++ src/main/java/seedu/address/logic/commands/QuestionCommand.java
@@ -10,11 +10,12 @@ public abstract class QuestionCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds, resolves or deletes a question "
             + "from a student in Reeve. "
             + "Supported actions: \n"
-            + PREFIX_DETAIL_TEXT + " QUESTION: adds an unresolved question.\n"
-            + "Examples:\n" + COMMAND_WORD + "add 1 " + PREFIX_DETAIL_TEXT + "1 + 1 = ?\n"
-            + PREFIX_DETAIL_INDEX + "QUESTION_INDEX " + PREFIX_DETAIL_TEXT + "SOLUTION: marks a question as solved.\n"
-            + "Examples:\n" + COMMAND_WORD + "solve 1 " + PREFIX_DETAIL_INDEX + " 1 "
+            + "add INDEX " + PREFIX_DETAIL_TEXT + "QUESTION: adds an unresolved question.\n"
+            + "Example: " + COMMAND_WORD + " add 1 " + PREFIX_DETAIL_TEXT + "How do birds fly\n"
+            + "solve INDEX " + PREFIX_DETAIL_INDEX + "QUESTION_INDEX "
+            + PREFIX_DETAIL_TEXT + "SOLUTION: marks a question as solved.\n"
+            + "Example: " + COMMAND_WORD + " solve 1 " + PREFIX_DETAIL_INDEX + " 1 "
             + PREFIX_DETAIL_TEXT + "Read your textbook.\n"
-            + PREFIX_DETAIL_INDEX + "QUESTION_INDEX: deletes a question.\n"
-            + "Examples:\n" + COMMAND_WORD + "delete 2 " + PREFIX_DETAIL_INDEX + " 1 \n";
+            + "delete INDEX " + PREFIX_DETAIL_INDEX + "QUESTION_INDEX: deletes a question.\n"
+            + "Example: " + COMMAND_WORD + " delete 2 " + PREFIX_DETAIL_INDEX + " 1 \n";
 }
