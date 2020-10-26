75a0008aa39b3ab3f7df855de74c5b2b11ff0d31
zhiayang <zhiayang@nascentnoodle.com>
2020-10-26 12:45:53 +0800

    Fix a small bug where the button styles wouldn't be applied

--- src/main/java/chopchop/ui/DisplayController.java
+++ src/main/java/chopchop/ui/DisplayController.java
@@ -77,6 +77,9 @@ public class DisplayController extends UiPart<Region> {
                 resetButtons();
             }
         });
+
+        resetButtons();
+
         if (!logic.getFilteredRecipeList().isEmpty()) {
             displayRecipeList();
         } else if (!logic.getFilteredIngredientList().isEmpty()) {
