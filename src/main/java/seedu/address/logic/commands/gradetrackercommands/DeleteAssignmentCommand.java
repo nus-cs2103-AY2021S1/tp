90ec7936850ae3ddaf38085729aea29805c57485
murtubak <murtubakchickenss@gmail.com>
2020-10-26 15:55:40 +0800

    Update to fit command style to fit other delete commands

--- src/main/java/seedu/address/logic/commands/gradetrackercommands/DeleteAssignmentCommand.java
+++ src/main/java/seedu/address/logic/commands/gradetrackercommands/DeleteAssignmentCommand.java
@@ -1,7 +1,6 @@
 package seedu.address.logic.commands.gradetrackercommands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_INDEX;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 
 import java.util.List;
@@ -20,29 +19,27 @@ public class DeleteAssignmentCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Deletes the assignment at the specified position in the specified module.\n"
-            + "Parameters: "
-            + PREFIX_NAME + "MODULE NAME "
-            + PREFIX_INDEX + " ASSIGNMENT POSITION "
-            + "Example: " + COMMAND_WORD + " "
-            + PREFIX_NAME + "CS2100 "
-            + PREFIX_INDEX + "2";
+            + "Parameters: INDEX (must be a positive integer) "
+            + "[" + PREFIX_NAME + "NAME] "
+            + "Example: " + COMMAND_WORD + " 1 "
+            + PREFIX_NAME + "CS2100";
 
     public static final String MESSAGE_DELETE_ASSIGNMENT_SUCCESS = "Deleted assignment %1$s from module: %2$s";
     public static final String MESSAGE_ASSIGNMENT_NOT_DELETED =
             "Assignment to be deleted or module specified is invalid.";
 
-    private final String targetModule;
     private final Index targetIndex;
+    private final String targetModule;
 
     /**
      * Creates a DeleteAssignmentCommand to remove the specified assignment in the specified module.
      *
-     * @param targetModule the module to remove the assignment from.
      * @param targetIndex the index of the assignment to be removed.
+     * @param targetModule the module to remove the assignment from.
      */
-    public DeleteAssignmentCommand(String targetModule, Index targetIndex) {
-        this.targetModule = targetModule;
+    public DeleteAssignmentCommand(Index targetIndex, String targetModule) {
         this.targetIndex = targetIndex;
+        this.targetModule = targetModule;
     }
 
     @Override
--- src/main/java/seedu/address/logic/parser/gradetrackerparsers/DeleteAssignmentParser.java
+++ src/main/java/seedu/address/logic/parser/gradetrackerparsers/DeleteAssignmentParser.java
@@ -2,10 +2,7 @@ package seedu.address.logic.parser.gradetrackerparsers;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_INDEX;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-
-import java.util.stream.Stream;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.gradetrackercommands.DeleteAssignmentCommand;
@@ -13,31 +10,25 @@ import seedu.address.logic.parser.ArgumentMultimap;
 import seedu.address.logic.parser.ArgumentTokenizer;
 import seedu.address.logic.parser.Parser;
 import seedu.address.logic.parser.ParserUtil;
-import seedu.address.logic.parser.Prefix;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 public class DeleteAssignmentParser implements Parser<DeleteAssignmentCommand> {
+
     @Override
     public DeleteAssignmentCommand parse(String args) throws ParseException {
         requireNonNull(args);
         String moduleName;
-        ArgumentTokenizer tokenizer = new ArgumentTokenizer(args, PREFIX_NAME, PREFIX_INDEX);
+        Index index;
+
+        ArgumentTokenizer tokenizer = new ArgumentTokenizer(args, PREFIX_NAME);
         ArgumentMultimap argMultimap = tokenizer.tokenize();
-        if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_INDEX)
-                || !argMultimap.getPreamble().isEmpty()) {
+        try {
+            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+        } catch (ParseException pe) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                    DeleteAssignmentCommand.MESSAGE_USAGE));
+                    DeleteAssignmentCommand.MESSAGE_USAGE), pe);
         }
         moduleName = ParserUtil.parseModuleName(argMultimap.getValue(PREFIX_NAME).get()).fullName;
-        Index targetIndex = ParserUtil.parseIndex(argMultimap.getValue(PREFIX_INDEX).get());
-        return new DeleteAssignmentCommand(moduleName, targetIndex);
-    }
-
-    /**
-     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
-     * {@code ArgumentMultimap}.
-     */
-    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
-        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+        return new DeleteAssignmentCommand(index, moduleName);
     }
 }
