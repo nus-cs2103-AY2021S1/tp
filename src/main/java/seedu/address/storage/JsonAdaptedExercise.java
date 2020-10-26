b1dc15fe9a61fd193e28d6361871e6910ca93f30
leeweiminsg <lee.wei.min@u.nus.edu>
2020-10-26 23:09:13 +0800

    Fix naming errors
    From persons to exercises

--- src/main/java/seedu/address/storage/JsonAdaptedExercise.java
+++ src/main/java/seedu/address/storage/JsonAdaptedExercise.java
@@ -1,5 +1,10 @@
 package seedu.address.storage;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -8,6 +13,7 @@ import seedu.address.model.exercise.Calories;
 import seedu.address.model.exercise.Date;
 import seedu.address.model.exercise.Description;
 import seedu.address.model.exercise.Exercise;
+import seedu.address.model.exercise.Muscle;
 import seedu.address.model.exercise.Name;
 
 /**
@@ -15,24 +21,26 @@ import seedu.address.model.exercise.Name;
  */
 class JsonAdaptedExercise {
 
-    public static final String MISSING_FIELD_MESSAGE_FORMAT = "Person's %s field is missing!";
+    public static final String MISSING_FIELD_MESSAGE_FORMAT = "Exercise's %s field is missing!";
 
     private final String name;
     private final String description;
     private final String date;
     private final String calories;
+    private final String musclesWorked;
 
     /**
      * Constructs a {@code JsonAdaptedExercise} with the given person details.
      */
     @JsonCreator
     public JsonAdaptedExercise(@JsonProperty("name") String name, @JsonProperty("phone") String description,
-                               @JsonProperty("email") String date, @JsonProperty("address") String calories) {
+                               @JsonProperty("email") String date, @JsonProperty("address") String calories,
+                               @JsonProperty("musclesWorked") String musclesWorked) {
         this.name = name;
         this.description = description;
         this.date = date;
         this.calories = calories;
-
+        this.musclesWorked = musclesWorked;
     }
 
     /**
@@ -43,12 +51,13 @@ class JsonAdaptedExercise {
         description = source.getDescription().value;
         date = source.getDate().value;
         calories = source.getCalories().value;
+        musclesWorked = source.getMusclesWorkedDescription();
     }
 
     /**
-     * Converts this Jackson-friendly adapted person object into the model's {@code Person} object.
+     * Converts this Jackson-friendly adapted exercise object into the model's {@code Exercise} object.
      *
-     * @throws IllegalValueException if there were any data constraints violated in the adapted person.
+     * @throws IllegalValueException if there were any data constraints violated in the adapted exercise.
      */
     public Exercise toModelType() throws IllegalValueException {
 
@@ -61,37 +70,34 @@ class JsonAdaptedExercise {
         final Name modelName = new Name(name);
 
         if (description == null) {
-            throw new IllegalValueException("Person's Description field is missing!");
-            //throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Phone.class.getSimpleName()));
+            throw new IllegalValueException("Exercise's Description field is missing!");
         }
         if (!Description.isValidDescription(description)) {
             throw new IllegalValueException("Invalid description");
-            //throw new IllegalValueException(Phone.MESSAGE_CONSTRAINTS);
         }
         final Description modelDescription = new Description(description);
 
         if (date == null) {
-            throw new IllegalValueException("Person's Date field is missing!");
-            //throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Email.class.getSimpleName()));
+            throw new IllegalValueException("Exercise's Date field is missing!");
         }
         if (!Date.isValidDate(date)) {
             throw new IllegalValueException("Invalid Date");
-            //throw new IllegalValueException(Email.MESSAGE_CONSTRAINTS);
         }
         final Date modelDate = new Date(date);
 
         if (calories == null) {
             throw new IllegalValueException("Person's Calories field is missing!");
-            //throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT,
-            // Address.class.getSimpleName()));
         }
         if (!Calories.isValidCalories(calories)) {
             throw new IllegalValueException("Invalid Calories");
-            //throw new IllegalValueException(Email.MESSAGE_CONSTRAINTS);
         }
         final Calories modelCalories = new Calories(calories);
 
-        return new Exercise(modelName, modelDescription, modelDate, modelCalories);
-    }
+        if (musclesWorked == null) {
+            throw new IllegalValueException("Exercise's muscles field is missing!");
+        }
+        List<Muscle> musclesWorkedLst = Muscle.stringToMuscleList(musclesWorked);
 
+        return new Exercise(modelName, modelDescription, modelDate, modelCalories, musclesWorkedLst);
+    }
 }
