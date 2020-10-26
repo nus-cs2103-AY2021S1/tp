b8ed2c3ceea6143d7c46f369c138ab4fc844a275
MorningLit <57279935+MorningLit@users.noreply.github.com>
2020-10-26 13:19:06 +0800

    Update MenuCommand.java

--- src/main/java/seedu/address/logic/commands/MenuCommand.java
+++ src/main/java/seedu/address/logic/commands/MenuCommand.java
@@ -1,6 +1,8 @@
 package seedu.address.logic.commands;
 
 import seedu.address.commons.core.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.parser.ParserUtil;
 import seedu.address.model.Model;
 
 public class MenuCommand extends Command {
@@ -10,7 +12,7 @@ public class MenuCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows the whole menu again.";
 
     @Override
-    public CommandResult execute(Model model) {
+    public CommandResult execute(Model model) throws CommandException {
         if (!model.isSelected()) {
             throw new CommandException(ParserUtil.MESSAGE_VENDOR_NOT_SELECTED);
         }
