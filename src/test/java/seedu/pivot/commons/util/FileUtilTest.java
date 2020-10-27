93874a54052ecd6156d046aad3abe4ba534bf5cc
CodyChew <chewchengyap@gmail.com>
2020-10-27 17:33:14 +0800

    Add OpenDocumentCommandTest
    - unable to test for execute success without touching user references
    directory.
    - tried to create temp test directory but command uses fileUtil which
    does not check from temporary test file path

--- src/test/java/seedu/pivot/commons/util/FileUtilTest.java
+++ src/test/java/seedu/pivot/commons/util/FileUtilTest.java
@@ -1,12 +1,30 @@
 package seedu.pivot.commons.util;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.pivot.testutil.Assert.assertThrows;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 public class FileUtilTest {
+
+
+
+
+    @TempDir
+    public static Path testFolder;
+
+    private static final String SINGLE_FILE = "test.txt";
+    private static Path getTempFilePath(String fileName) {
+        return testFolder.resolve(fileName);
+    }
+
 
     @Test
     public void isValidPath() {
@@ -18,6 +36,14 @@ public class FileUtilTest {
 
         // null path -> throws NullPointerException
         assertThrows(NullPointerException.class, () -> FileUtil.isValidPath(null));
+    }
+
+    @Test
+    public void openDocument() throws IOException {
+        Files.createFile(getTempFilePath(SINGLE_FILE));
+
+        //assuming Desktop is supported
+        assertDoesNotThrow(() -> FileUtil.openFile(getTempFilePath(SINGLE_FILE)));
     }
 
 }
