599b377aa352554a6e8be617b9205186211c98cb
Ma-Yueran <mayuerantom@outlook.com>
2020-10-27 21:53:16 +0800

    Make File Explorer clickable
    Add icons for files in the list

--- src/main/java/seedu/address/ui/theme/ThemeSet.java
+++ src/main/java/seedu/address/ui/theme/ThemeSet.java
@@ -7,7 +7,6 @@ public class ThemeSet {
 
     private static final String DEFAULT_THEME_NAME = "Default Theme";
     private static final String DARK_THEME_NAME = "Dark Theme";
-    private static final String GALAXY_THEME_NAME = "Galaxy Theme";
 
     public static final Theme DEFAULT_THEME =
             new Theme(DEFAULT_THEME_NAME, "view/DefaultTheme.css", "/images/default_theme.png");
@@ -15,15 +14,10 @@ public class ThemeSet {
     public static final Theme DARK_THEME =
             new Theme(DARK_THEME_NAME, "view/HelloFileDarkTheme.css", "/images/dark_theme.png");
 
-    public static final Theme GALAXY_THEME =
-            new Theme(GALAXY_THEME_NAME, "view/GalaxyTheme.css", "/images/galaxy_theme.png");
-
     public static final Theme getTheme(String themeName) {
         switch (themeName) {
         case DARK_THEME_NAME:
             return DARK_THEME;
-        case GALAXY_THEME_NAME:
-            return GALAXY_THEME;
         default:
             return DEFAULT_THEME;
         }
