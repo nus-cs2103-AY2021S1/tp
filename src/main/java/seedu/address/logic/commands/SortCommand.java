1271613cd2b4197a2e982e8a8058b8d114cbc6ed
yongyan <cs623329235@gmail.com>
2020-10-27 16:11:05 +0800

    update sort command

--- src/main/java/seedu/address/logic/commands/SortCommand.java
+++ src/main/java/seedu/address/logic/commands/SortCommand.java
@@ -4,10 +4,8 @@ import static java.util.Objects.requireNonNull;
 
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.task.Deadline;
-import seedu.address.model.task.Event;
 import seedu.address.model.task.Task;
-import seedu.address.model.task.TaskDate;
+import seedu.address.model.task.TaskDateComparator;
 
 import java.util.Comparator;
 import java.util.List;
@@ -22,35 +20,22 @@ public class SortCommand extends Command {
 
     public static final String MESSAGE_DELETE_TASK_SUCCESS = "All results sorted";
 
-    class SortByDate implements Comparator<Task> {
-        private TaskDate getDate(Task t) {
-            if (t instanceof Deadline)
-                return ((Deadline) t).getDeadlineDate();
-            else if (t instanceof Event)
-                return ((Event) t).getEventDate();
-            else
-                return null;
-        }
-
-        @Override
-        public int compare(Task task1, Task task2) {
-            TaskDate date1 = getDate(task1), date2 = getDate(task2);
-            if (date1 != null && date2 != null) return date1.compareTo(date2);
-            else if (date1 == null) return -1;
-            else return 0;
-        }
-    }
-
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
         List<Task> lastShownList = model.getFilteredTaskList();
-        lastShownList.sort(new SortByDate());
+        lastShownList.sort(new TaskDateComparator());
+        TaskDateComparator tdc = new TaskDateComparator();
+        System.out.println(lastShownList.get(0).getTitle());
+        System.out.println(lastShownList.get(2).getTitle());
+        System.out.println(tdc.getDate(lastShownList.get(0)));
+        System.out.println(tdc.getDate(lastShownList.get(2)));
+        System.out.println(tdc.compare(lastShownList.get(0), lastShownList.get(2)));
 
         for (Task t : lastShownList){
             System.out.println(t.getTitle());
         }
 
-        return null;
+        return  new CommandResult(MESSAGE_DELETE_TASK_SUCCESS);
     }
 }
