0ee5dc09b6a7083b268bd99007e7b47ae443aac3
CodyChew <chewchengyap@gmail.com>
2020-10-27 21:15:04 +0800

    Add support for EditCommand

---
+++ src/main/java/seedu/pivot/logic/commands/casecommands/EditTitleCommand.java
@@ -0,0 +1,13 @@
+package seedu.pivot.logic.commands.casecommands;
+
+import seedu.pivot.logic.commands.CommandResult;
+import seedu.pivot.logic.commands.EditCommand;
+import seedu.pivot.logic.commands.exceptions.CommandException;
+import seedu.pivot.model.Model;
+
+public class EditTitleCommand extends EditCommand {
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        return null;
+    }
+}
---
+++ src/main/java/seedu/pivot/logic/parser/EditTitleCommandParser.java
@@ -0,0 +1,11 @@
+package seedu.pivot.logic.parser;
+
+import seedu.pivot.logic.commands.casecommands.EditTitleCommand;
+import seedu.pivot.logic.parser.exceptions.ParseException;
+
+public class EditTitleCommandParser implements Parser<EditTitleCommand> {
+    @Override
+    public EditTitleCommand parse(String userInput) throws ParseException {
+        return null;
+    }
+}
