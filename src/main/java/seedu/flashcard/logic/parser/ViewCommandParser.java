70dc636319351aa1bf5eae4e02c96aba6ba75c6a
sgn00 <37658877+sgn00@users.noreply.github.com>
2020-10-27 20:54:15 +0800

    Add testcases to ViewCommand and change command syntax

--- src/main/java/seedu/flashcard/logic/parser/ViewCommandParser.java
+++ src/main/java/seedu/flashcard/logic/parser/ViewCommandParser.java
@@ -1,7 +1,7 @@
 package seedu.flashcard.logic.parser;
 
 import static seedu.flashcard.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.flashcard.logic.parser.CliSyntax.PREFIX_ANSWER;
+import static seedu.flashcard.logic.parser.CliSyntax.FLAG_ANSWER;
 
 import seedu.flashcard.commons.core.index.Index;
 import seedu.flashcard.logic.commands.ViewCommand;
@@ -14,13 +14,13 @@ public class ViewCommandParser implements Parser<ViewCommand> {
 
     /**
      * Parses the given {@code String} of arguments in the context of the ViewCommand
-     * and returns a VieweCommand object for execution.
+     * and returns a ViewCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
     public ViewCommand parse(String args) throws ParseException {
         Index index;
         ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(args, PREFIX_ANSWER);
+                ArgumentTokenizer.tokenize(args, FLAG_ANSWER);
 
         try {
             index = ParserUtil.parseIndex(argMultimap.getPreamble());
@@ -30,7 +30,7 @@ public class ViewCommandParser implements Parser<ViewCommand> {
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, ViewCommand.MESSAGE_USAGE), pe);
         }
 
-        if (argMultimap.getValue(PREFIX_ANSWER).isPresent()) {
+        if (argMultimap.getValue(FLAG_ANSWER).isPresent()) {
             return new ViewCommand(index, true);
         } else {
             return new ViewCommand(index, false);
---
+++ src/test/java/seedu/flashcard/logic/commands/ViewCommandTest.java
@@ -0,0 +1,104 @@
+package seedu.flashcard.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.flashcard.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.flashcard.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.flashcard.logic.commands.CommandTestUtil.showFlashcardAtIndex;
+import static seedu.flashcard.testutil.TypicalFlashcards.getTypicalFlashcardDeck;
+import static seedu.flashcard.testutil.TypicalIndexes.INDEX_FIRST_FLASHCARD;
+import static seedu.flashcard.testutil.TypicalIndexes.INDEX_SECOND_FLASHCARD;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.flashcard.commons.core.Messages;
+import seedu.flashcard.commons.core.index.Index;
+import seedu.flashcard.model.Model;
+import seedu.flashcard.model.ModelManager;
+import seedu.flashcard.model.UserPrefs;
+import seedu.flashcard.model.flashcard.Flashcard;
+
+class ViewCommandTest {
+
+    private Model model = new ModelManager(getTypicalFlashcardDeck(), new UserPrefs());
+
+    @Test
+    public void execute_validIndexUnfilteredList_success() {
+        Flashcard flashcardToView = model.getFilteredFlashcardList()
+                .get(INDEX_FIRST_FLASHCARD.getZeroBased());
+        ViewCommand viewCommand = new ViewCommand(INDEX_FIRST_FLASHCARD, false);
+        CommandResult expectedCommandResult = new CommandResult(
+                String.format(ViewCommand.MESSAGE_VIEW_FLASHCARD_SUCCESS,
+                        flashcardToView), INDEX_FIRST_FLASHCARD.getZeroBased(), false);
+        ModelManager expectedModel = new ModelManager(model.getFlashcardDeck(), new UserPrefs());
+        assertCommandSuccess(viewCommand, model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_validIndexUnfilteredList_failure() {
+        Flashcard flashcardToView = model.getFilteredFlashcardList()
+                .get(INDEX_FIRST_FLASHCARD.getZeroBased());
+        ViewCommand viewCommand = new ViewCommand(INDEX_FIRST_FLASHCARD, true);
+        CommandResult expectedCommandResult = new CommandResult(
+                String.format(ViewCommand.MESSAGE_VIEW_FLASHCARD_SUCCESS,
+                        flashcardToView), INDEX_FIRST_FLASHCARD.getZeroBased(), false);
+        ModelManager expectedModel = new ModelManager(model.getFlashcardDeck(), new UserPrefs());
+        assertCommandSuccess(viewCommand, model, expectedCommandResult, expectedModel);
+    }
+
+
+    @Test
+    public void execute_invalidIndexUnfilteredList_throwsCommandException() {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredFlashcardList().size() + 1);
+        ViewCommand viewCommand = new ViewCommand(outOfBoundIndex, false);
+        assertCommandFailure(viewCommand, model, Messages.MESSAGE_INVALID_FLASHCARD_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void execute_validIndexFilteredList_success() {
+        showFlashcardAtIndex(model, INDEX_FIRST_FLASHCARD);
+        Flashcard flashcardToView = model.getFilteredFlashcardList()
+                .get(INDEX_FIRST_FLASHCARD.getZeroBased());
+        ViewCommand viewCommand = new ViewCommand(INDEX_FIRST_FLASHCARD, false);
+        CommandResult expectedCommandResult = new CommandResult(
+                String.format(ViewCommand.MESSAGE_VIEW_FLASHCARD_SUCCESS, flashcardToView),
+                INDEX_FIRST_FLASHCARD.getZeroBased(), false);
+        assertCommandSuccess(viewCommand, model, expectedCommandResult, model);
+    }
+
+    @Test
+    public void execute_invalidIndexFilteredList_throwsCommandException() {
+        showFlashcardAtIndex(model, INDEX_FIRST_FLASHCARD);
+        Index outOfBoundIndex = INDEX_SECOND_FLASHCARD;
+        assertTrue(outOfBoundIndex.getZeroBased() < model.getFlashcardDeck().getFlashcardList().size());
+        ViewCommand viewCommand = new ViewCommand(outOfBoundIndex, false);
+        assertCommandFailure(viewCommand, model, Messages.MESSAGE_INVALID_FLASHCARD_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void equals() {
+        ViewCommand viewFirstCommand = new ViewCommand(INDEX_FIRST_FLASHCARD, false);
+        ViewCommand viewSecondCommand = new ViewCommand(INDEX_SECOND_FLASHCARD, false);
+        ViewCommand viewThirdCommand = new ViewCommand(INDEX_FIRST_FLASHCARD, true);
+
+        // same object -> returns true
+        assertTrue(viewFirstCommand.equals(viewFirstCommand));
+
+        // same values -> returns true
+        ViewCommand viewFirstCommandCopy = new ViewCommand(INDEX_FIRST_FLASHCARD, false);
+        assertTrue(viewFirstCommand.equals(viewFirstCommandCopy));
+
+        // different types -> returns false
+        assertFalse(viewFirstCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(viewFirstCommand.equals(null));
+
+        // different flashcard -> returns false
+        assertFalse(viewFirstCommand.equals(viewSecondCommand));
+
+        // same flashcard but different showAnswer -> returns false
+        assertFalse(viewFirstCommand.equals(viewThirdCommand));
+    }
+
+}
