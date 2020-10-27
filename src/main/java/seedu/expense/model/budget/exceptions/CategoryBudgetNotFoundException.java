f7423e192a5e359acc4f85c31ac80701c756b320
Jaylenlee <e0424677@u.nus.edu>
2020-10-27 18:07:03 +0800

    Add message to Exception

--- src/main/java/seedu/expense/model/budget/exceptions/CategoryBudgetNotFoundException.java
+++ src/main/java/seedu/expense/model/budget/exceptions/CategoryBudgetNotFoundException.java
@@ -4,4 +4,7 @@ package seedu.expense.model.budget.exceptions;
  * Signals that the operation is unable to find the specified budget.
  */
 public class CategoryBudgetNotFoundException extends RuntimeException{
+    public CategoryBudgetNotFoundException() {
+        super("The given CategoryBudget does not exist in expense book");
+    }
 }
--- src/main/java/seedu/expense/model/expense/exceptions/ExpenseNotFoundException.java
+++ src/main/java/seedu/expense/model/expense/exceptions/ExpenseNotFoundException.java
@@ -4,4 +4,7 @@ package seedu.expense.model.expense.exceptions;
  * Signals that the operation is unable to find the specified expense.
  */
 public class ExpenseNotFoundException extends RuntimeException {
+    public ExpenseNotFoundException() {
+        super("The given Expense does not exist in expense book");
+    }
 }
