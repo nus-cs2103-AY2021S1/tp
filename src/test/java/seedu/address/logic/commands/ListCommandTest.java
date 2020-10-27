950a20de3e6be68737a5f5611f11f900a11220d8
luo-git <lluoyi.lu@gmail.com>
2020-10-27 15:41:13 +0800

    Update ListCommandTest

--- src/test/java/seedu/address/logic/commands/ListCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListCommandTest.java
@@ -1,5 +1,7 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.CommandTestUtil.showTagAtIndex;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_TAG;
@@ -11,6 +13,10 @@ import org.junit.jupiter.api.Test;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.label.Label;
+import seedu.address.model.tag.TagName;
+
+import java.util.HashSet;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for ListCommand.
@@ -35,5 +41,19 @@ public class ListCommandTest {
     public void execute_listIsFiltered_showsEverything() {
         showTagAtIndex(model, INDEX_FIRST_TAG);
         assertCommandSuccess(new ListCommand(), model, ListCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    @Test
+    public void equals() {
+        ListCommand listCommand = new ListCommand();
+
+        // same object -> returns true
+        assertTrue(listCommand.equals(listCommand));
+
+        // different types -> returns false
+        assertFalse(listCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(listCommand.equals(null));
     }
 }
