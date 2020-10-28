d6f0e736d3488818b58ef84fe07bf16960a4bbc9
lucastai98 <e0406424@u.nus.edu>
2020-10-28 16:39:26 +0800

    merging

--- src/main/java/seedu/address/model/project/Project.java
+++ src/main/java/seedu/address/model/project/Project.java
@@ -9,7 +9,6 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
