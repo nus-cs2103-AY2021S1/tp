8e28909cb1f4502d7a8b2b99e80d2a7860bda79d
AudreyFelicio <audreyanwar@gmail.com>
2020-10-28 13:23:45 +0800

    Add help suggestion test

--- src/test/java/seedu/stock/logic/parser/SuggestionCommandParserTest.java
+++ src/test/java/seedu/stock/logic/parser/SuggestionCommandParserTest.java
@@ -1,14 +1,13 @@
 package seedu.stock.logic.parser;
 
 import static seedu.stock.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
-import static seedu.stock.logic.commands.CommandTestUtil.NAME_DESC_APPLE;
-import static seedu.stock.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.stock.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.stock.logic.parser.SuggestionCommandParser.MESSAGE_SUGGESTION;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.stock.logic.commands.CommandWords;
+import seedu.stock.logic.commands.HelpCommand;
 import seedu.stock.logic.commands.SuggestionCommand;
 
 public class SuggestionCommandParserTest {
@@ -26,6 +25,24 @@ public class SuggestionCommandParserTest {
         parser = new SuggestionCommandParser("exit", "error message");
         expectedSuggestionMessage = "error message" + "\n"
                 + MESSAGE_SUGGESTION + CommandWords.EXIT_COMMAND_WORD;
+        expectedCommand = new SuggestionCommand(expectedSuggestionMessage);
+        assertParseSuccess(parser, userInput, expectedCommand);
+    }
+
+    @Test
+    public void parse_helpCommandSuggestion_success() {
+        // EP: incorrect command word
+        String userInput = "";
+        SuggestionCommandParser parser = new SuggestionCommandParser("hel");
+        String expectedSuggestionMessage = MESSAGE_UNKNOWN_COMMAND + "\n"
+                + MESSAGE_SUGGESTION + CommandWords.HELP_COMMAND_WORD + "\n" + HelpCommand.MESSAGE_USAGE;
+        SuggestionCommand expectedCommand = new SuggestionCommand(expectedSuggestionMessage);
+        assertParseSuccess(parser, userInput, expectedCommand);
+
+        // EP: correct command word
+        parser = new SuggestionCommandParser("help", "error message");
+        expectedSuggestionMessage = "error message" + "\n"
+                + MESSAGE_SUGGESTION + CommandWords.HELP_COMMAND_WORD + "\n" + HelpCommand.MESSAGE_USAGE;
         expectedCommand = new SuggestionCommand(expectedSuggestionMessage);
         assertParseSuccess(parser, userInput, expectedCommand);
     }
