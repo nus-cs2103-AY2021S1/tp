a65f7eb78dd6be5abc8a4be044c50cf088860efb
gohyeeloon <goh.yeeloon@gmail.com>
2020-10-28 17:23:49 +0800

    Add ToggleCommand

---
+++ src/main/java/seedu/address/logic/commands/ToggleCommand.java
@@ -0,0 +1,15 @@
+package seedu.address.logic.commands;
+
+import seedu.address.model.Model;
+
+public class ToggleCommand extends Command {
+    public static final String COMMAND_WORD = "toggle";
+
+    public static final String MESSAGE_SUCCESS = "The view of the app has been toggled as requested";
+
+    @Override
+    public CommandResult execute(Model model) {
+        return new CommandResult(MESSAGE_TOGGLE_ACKNOWLEDGEMENT, false, false,
+                false, true);
+    }
+}
