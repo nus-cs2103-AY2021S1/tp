5d97459ab14e92bae9e2c5ce814e294607cc686d
ureshiiYing <tan.ying.hui@u.nus.edu>
2020-10-28 17:10:56 +0800

    Update documentation to reflect the changes made

--- src/main/java/seedu/address/commons/util/StringUtil.java
+++ src/main/java/seedu/address/commons/util/StringUtil.java
@@ -67,7 +67,7 @@ public class StringUtil {
     }
 
     /**
-     * Returns true if {@code s} represents an unsigned integer greater than or equal to 0.
+     * Returns true if {@code s} represents an unsigned integer.
      * e.g. 0, 1, 2, 3, ..., {@code Integer.MAX_VALUE} <br>
      * Will return false for any other non-null string input
      * e.g. empty string, "-1", "+1", and " 2 " (untrimmed), "3 0" (contains whitespace), "1 a" (contains letters)
