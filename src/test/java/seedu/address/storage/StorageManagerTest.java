cf9551a5c3adbb19e4b04b537e7988e09da53164
davidcaiqifan <e0406982@u.nus.edu>
2020-10-29 01:27:02 +0800

    Add test cases for StorageManagerTest

--- src/test/java/seedu/address/storage/StorageManagerTest.java
+++ src/test/java/seedu/address/storage/StorageManagerTest.java
@@ -1,8 +1,8 @@
 package seedu.address.storage;
 
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-// import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static seedu.address.testutil.TypicalModules.getTypicalModuleList;
 
 import java.nio.file.Path;
 
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.io.TempDir;
 
 import seedu.address.commons.core.GuiSettings;
 import seedu.address.model.ModuleList;
-// import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.ReadOnlyModuleList;
 import seedu.address.model.UserPrefs;
 
 public class StorageManagerTest {
@@ -24,9 +24,11 @@ public class StorageManagerTest {
 
     @BeforeEach
     public void setUp() {
-        JsonModuleListStorage addressBookStorage = new JsonModuleListStorage(getTempFilePath("ab"));
+        JsonModuleListStorage moduleListStorage = new JsonModuleListStorage(getTempFilePath("ab"));
+        JsonContactListStorage contactListStorage = new JsonContactListStorage(getTempFilePath("cd"));
+        JsonTodoListStorage todoListStorage = new JsonTodoListStorage(getTempFilePath("ef"));
         JsonUserPrefsStorage userPrefsStorage = new JsonUserPrefsStorage(getTempFilePath("prefs"));
-        // storageManager = new StorageManager(addressBookStorage, userPrefsStorage);
+        storageManager = new StorageManager(moduleListStorage, contactListStorage, todoListStorage, userPrefsStorage);
     }
 
     private Path getTempFilePath(String fileName) {
@@ -42,27 +44,25 @@ public class StorageManagerTest {
          */
         UserPrefs original = new UserPrefs();
         original.setGuiSettings(new GuiSettings(300, 600, 4, 6));
-        // storageManager.saveUserPrefs(original);
-        // UserPrefs retrieved = storageManager.readUserPrefs().get();
-        // assertEquals(original, retrieved);
+        storageManager.saveUserPrefs(original);
+        UserPrefs retrieved = storageManager.readUserPrefs().get();
+        assertEquals(original, retrieved);
     }
 
     @Test
-    public void addressBookReadSave() throws Exception {
+    public void moduleListReadSave() throws Exception {
         /*
          * Note: This is an integration test that verifies the StorageManager is properly wired to the
-         * {@link JsonAddressBookStorage} class.
-         * More extensive testing of UserPref saving/reading is done in {@link JsonAddressBookStorageTest} class.
+         * {@link JsonModuleListStorage} class.
+         * More extensive testing of UserPref saving/reading is done in {@link JsonModuleListStorageTest} class.
          */
-        ModuleList original = getTypicalAddressBook();
-        // storageManager.saveAddressBook(original);
-        // ReadOnlyAddressBook retrieved = storageManager.readAddressBook().get();
-        // assertEquals(original, new ModuleList(retrieved));
+        ModuleList original = getTypicalModuleList();
+        storageManager.saveModuleList(original);
+        ReadOnlyModuleList retrieved = storageManager.readModuleList().get();
+        assertEquals(original, new ModuleList(retrieved));
     }
-
     @Test
-    public void getAddressBookFilePath() {
-        // assertNotNull(storageManager.getAddressBookFilePath());
+    public void getModuleListFilePath() {
+        assertNotNull(storageManager.getModuleListFilePath());
     }
-
 }
