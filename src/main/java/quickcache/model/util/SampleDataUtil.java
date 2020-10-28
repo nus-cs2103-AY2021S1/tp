1ab561bf01def4329b4d1a396f8b4f55dfc28564
Xingjian <xingjian.chen@yahoo.com.sg>
2020-10-28 16:14:02 +0800

    Edit SampleDataUtil, changed starting flashcards

--- src/main/java/quickcache/model/util/SampleDataUtil.java
+++ src/main/java/quickcache/model/util/SampleDataUtil.java
@@ -11,6 +11,7 @@ import quickcache.model.flashcard.Choice;
 import quickcache.model.flashcard.Difficulty;
 import quickcache.model.flashcard.Flashcard;
 import quickcache.model.flashcard.MultipleChoiceQuestion;
+import quickcache.model.flashcard.OpenEndedQuestion;
 import quickcache.model.flashcard.Tag;
 
 /**
@@ -22,36 +23,32 @@ public class SampleDataUtil {
         Choice choice2 = new Choice("A non-compulsory module for NUS Computer Science students");
         Choice choice3 = new Choice("A module that can be S/Ued");
         Choice choice4 = new Choice("A module that doesn't involve a brown field project");
-        Choice choice5 = new Choice("Computer Organization module for NUS students");
-        Choice choice6 = new Choice("A non-compulsory module for NUS Computer Science students");
-        Choice choice7 = new Choice("A module that can be S/Ued");
-        Choice choice8 = new Choice("A module that doesn't involve assembly language");
-        Choice choice9 = new Choice("Introductory module to computer networks for NUS students");
-        Choice choice10 = new Choice("A non-compulsory module for NUS Computer Science students");
-        Choice choice11 = new Choice("A module that can be S/Ued");
-        Choice choice12 = new Choice("A module that doesn't involve TCP");
+        Choice choice5 = new Choice("bubble tea");
+        Choice choice6 = new Choice("oxygen");
+        Choice choice7 = new Choice("iphone");
+        Choice choice8 = new Choice("pepsi");
         return new Flashcard[]{
-            new Flashcard(new MultipleChoiceQuestion("What is CS2103T?",
+            new Flashcard(new OpenEndedQuestion("Sample OEQ 1 (ans: SampleAns1)",
+                    new Answer("SampleAns1")
+            ), getTagSet("OEQ", "ExampleTag")),
+            new Flashcard(new OpenEndedQuestion("Complete the following sentence. "
+                    + "National University of ______ . (ans: Singapore)",
+                    new Answer("Singapore")
+            ), getTagSet("OEQ", "General"), new Difficulty("low")),
+            new Flashcard(new MultipleChoiceQuestion("All mammals need ______ to survive. (option: 2)",
+                    new Answer("oxygen"),
+                    choice5,
+                    choice6,
+                    choice7,
+                    choice8
+            ), getTagSet("MCQ", "Biology"), new Difficulty("Medium")),
+            new Flashcard(new MultipleChoiceQuestion("What is CS2103? (option: 1)",
                     new Answer("Software Engineering module for NUS students"),
-                choice1,
-                choice2,
-                choice3,
-                choice4
-            ), getTagSet("MCQ", "GoodQuestion"), new Difficulty("Low")),
-            new Flashcard(new MultipleChoiceQuestion("What is CS2100?",
-                    new Answer("Computer Organization module for NUS students"),
-                choice5,
-                choice6,
-                choice7,
-                choice8
-            ), getTagSet("MCQ", "Assembly"), new Difficulty("Low")),
-            new Flashcard(new MultipleChoiceQuestion("What is CS2105?",
-                    new Answer("Introductory module to computer networks for NUS students"),
-                choice9,
-                choice10,
-                choice11,
-                choice12
-            ), getTagSet("MCQ", "TCPforLife"))
+                    choice1,
+                    choice2,
+                    choice3,
+                    choice4
+            ), getTagSet("MCQ", "CS2103"), new Difficulty("High"))
         };
     }
 
