4c5c9201fe9d261ea0cd5761a0d184405537ed03
afroneth <knjh30@gmail.com>
2020-10-28 23:56:23 +0800

    Add test coverage for ProfilePicture and CommandResult

--- src/test/java/seedu/address/model/patient/ProfilePictureTest.java
+++ src/test/java/seedu/address/model/patient/ProfilePictureTest.java
@@ -1,6 +1,7 @@
 package seedu.address.model.patient;
 
 import static seedu.address.testutil.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -14,6 +15,13 @@ public class ProfilePictureTest {
     public void isValidFilePath() {
         // null file path
         assertThrows(NullPointerException.class, () -> ProfilePicture.isValidFilePath(null));
+    }
+
+    @Test
+    public void toStringTest() {
+        ProfilePicture profilePicture = new ProfilePicture("data/pictures");
+        String filePath = profilePicture.toString();
+        assertEquals("data/pictures", filePath);
     }
 }
 
