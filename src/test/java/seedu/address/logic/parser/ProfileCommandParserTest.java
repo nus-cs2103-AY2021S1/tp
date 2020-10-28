8c4bce0754290470cc7b5ee80de11584137873fa
eugene3231 <47620989+eugene3231@users.noreply.github.com>
2020-10-28 23:33:51 +0800

    Fix DeleteVisitCommand constructors

--- src/test/java/seedu/address/logic/parser/ProfileCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ProfileCommandParserTest.java
@@ -12,26 +12,26 @@ import java.time.format.DateTimeFormatter;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.Messages;
-import seedu.address.logic.commands.AddVisitCommand;
+import seedu.address.logic.commands.ProfileCommand;
 
-class AddVisitCommandParserTest {
-    private final AddVisitCommandParser parser = new AddVisitCommandParser();
+class ProfileCommandParserTest {
+    private final ProfileCommandParser parser = new ProfileCommandParser();
 
     @Test
-    void parse_validArgs_returnsAddVisitCommand() {
+    void parse_validArgs_returnsProfileCommand() {
         String visitDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-        assertParseSuccess(parser, "1", new AddVisitCommand(visitDate, INDEX_FIRST_PATIENT));
-        assertParseSuccess(parser, "2", new AddVisitCommand(visitDate, INDEX_SECOND_PATIENT));
-        assertParseSuccess(parser, "3", new AddVisitCommand(visitDate, INDEX_THIRD_PATIENT));
+        assertParseSuccess(parser, "1", new ProfileCommand(INDEX_FIRST_PATIENT));
+        assertParseSuccess(parser, "2", new ProfileCommand(INDEX_SECOND_PATIENT));
+        assertParseSuccess(parser, "3", new ProfileCommand(INDEX_THIRD_PATIENT));
     }
 
     @Test
     void parse_invalidArgs_throwsParseException() {
         assertParseFailure(parser, "-1" , String.format(Messages.MESSAGE_INVALID_COMMAND_FORMAT,
-            AddVisitCommand.MESSAGE_USAGE));
+            ProfileCommand.MESSAGE_USAGE));
         assertParseFailure(parser, "0", String.format(Messages.MESSAGE_INVALID_COMMAND_FORMAT,
-            AddVisitCommand.MESSAGE_USAGE));
+            ProfileCommand.MESSAGE_USAGE));
         assertParseFailure(parser, "a", String.format(Messages.MESSAGE_INVALID_COMMAND_FORMAT,
-            AddVisitCommand.MESSAGE_USAGE));
+            ProfileCommand.MESSAGE_USAGE));
     }
 }
