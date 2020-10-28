9488aa93279132bb9f17bbc5cd9530c3b37fbf0f
MatthiasLHK <matthiasli1998@hotmail.com>
2020-10-29 00:44:50 +0800

    Add DeleteEvent parser and command

--- src/main/java/seedu/address/logic/parser/schedulerparsers/DeleteEventParser.java
+++ src/main/java/seedu/address/logic/parser/schedulerparsers/DeleteEventParser.java
@@ -1,12 +1,27 @@
 package seedu.address.logic.parser.schedulerparsers;
 
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.modulelistcommands.DeleteModuleCommand;
+import seedu.address.logic.commands.schedulercommands.AddEventCommand;
 import seedu.address.logic.commands.schedulercommands.DeleteEventCommand;
+import seedu.address.logic.parser.ArgumentMultimap;
+import seedu.address.logic.parser.ArgumentTokenizer;
 import seedu.address.logic.parser.Parser;
+import seedu.address.logic.parser.ParserUtil;
 import seedu.address.logic.parser.exceptions.ParseException;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 public class DeleteEventParser implements Parser<DeleteEventCommand> {
     @Override
     public DeleteEventCommand parse(String userInput) throws ParseException {
-        return null;
+        try {
+            Index index = ParserUtil.parseIndex(userInput);
+            return new DeleteEventCommand(index);
+        } catch (ParseException e) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteEventCommand.MESSAGE_USAGE), e);
+        }
+
     }
 }
--- src/main/java/seedu/address/logic/parser/schedulerparsers/EditEventParser.java
+++ src/main/java/seedu/address/logic/parser/schedulerparsers/EditEventParser.java
@@ -1,12 +1,30 @@
 package seedu.address.logic.parser.schedulerparsers;
 
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+
 import seedu.address.logic.commands.schedulercommands.EditEventCommand;
+import seedu.address.logic.parser.ArgumentMultimap;
+import seedu.address.logic.parser.ArgumentTokenizer;
 import seedu.address.logic.parser.Parser;
+import seedu.address.logic.parser.Prefix;
 import seedu.address.logic.parser.exceptions.ParseException;
+
+import java.util.stream.Stream;
 
 public class EditEventParser implements Parser<EditEventCommand> {
     @Override
     public EditEventCommand parse(String userInput) throws ParseException {
+        ArgumentTokenizer tokenizer = new ArgumentTokenizer(userInput, PREFIX_NAME, PREFIX_DATE);
+        ArgumentMultimap argMultiMap = tokenizer.tokenize();
         return null;
+    }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
     }
 }
