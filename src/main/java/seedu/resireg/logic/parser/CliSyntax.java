f6ecf7a3484a91460d2620372067101932c90195
chloelee767 <echloelee@gmail.com>
2020-10-28 19:10:35 +0800

    Make prefixes more consistent

--- src/main/java/seedu/resireg/logic/parser/CliSyntax.java
+++ src/main/java/seedu/resireg/logic/parser/CliSyntax.java
@@ -12,14 +12,14 @@ public class CliSyntax {
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
     public static final Prefix PREFIX_FACULTY = new Prefix("f/");
     public static final Prefix PREFIX_STUDENT_ID = new Prefix("i/");
-    public static final Prefix PREFIX_TAG = new Prefix("t/");
     public static final Prefix PREFIX_STUDENT_INDEX = new Prefix("si/");
     // rooms
     public static final Prefix PREFIX_ROOM_INDEX = new Prefix("ri/");
-    public static final Prefix PREFIX_ROOM_FLOOR = new Prefix("floor/");
-    public static final Prefix PREFIX_ROOM_NUMBER = new Prefix("number/");
-    public static final Prefix PREFIX_ROOM_TYPE = new Prefix("type/");
-
+    public static final Prefix PREFIX_ROOM_FLOOR = new Prefix("f/");
+    public static final Prefix PREFIX_ROOM_NUMBER = new Prefix("n/");
+    public static final Prefix PREFIX_ROOM_TYPE = new Prefix("t/");
+    // both rooms and students
+    public static final Prefix PREFIX_TAG = new Prefix("tag/");
     /**
      * Prefix for --style keywords eg. --vacant
      */
