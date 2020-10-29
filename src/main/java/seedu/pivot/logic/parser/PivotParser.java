82476b895fa40457f57cfe720a771a643c56b8a4
Constance <constancensq@gmail.com>
2020-10-30 00:41:35 +0800

    Update UserGuide

--- src/main/java/seedu/pivot/logic/parser/PivotParser.java
+++ src/main/java/seedu/pivot/logic/parser/PivotParser.java
@@ -59,8 +59,8 @@ public class PivotParser {
         case DeleteCommand.COMMAND_WORD:
             return new DeleteCommandParser().parse(arguments);
 
-        case ClearCommand.COMMAND_WORD:
-            return new ClearCommand();
+//        case ClearCommand.COMMAND_WORD:
+//            return new ClearCommand();
 
         case FindCommand.COMMAND_WORD:
             return new FindCommandParser().parse(arguments);
