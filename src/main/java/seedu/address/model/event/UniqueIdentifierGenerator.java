472dedcace469ae5d2e31cd3d955d6fd8eedbf03
AlexCQY <alex_chua@u.nus.edu>
2020-10-29 14:47:23 +0800

    add uniqueIdentifierGenerator

--- src/main/java/seedu/address/model/event/UniqueIdentifierGenerator.java
+++ src/main/java/seedu/address/model/event/UniqueIdentifierGenerator.java
@@ -1,2 +1,23 @@
-package seedu.address.model.event;public class UniqueIdentifierGenerator {
+package seedu.address.model.event;
+
+import java.time.LocalDateTime;
+
+/**
+ * A utility for generating unique identifier strings for Event as used by VEvent.
+ */
+public class UniqueIdentifierGenerator {
+    /**
+     * Generates a unique string identifier for the Events object as required by VEvent.
+     */
+    public static String generateUid(String eventName, String eventStartDateTime, String eventEndDateTime) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(LocalDateTime.now().toString())
+                .append("/")
+                .append(eventName)
+                .append("/")
+                .append(eventStartDateTime)
+                .append("/")
+                .append(eventEndDateTime);
+        return builder.toString();
+    }
 }
