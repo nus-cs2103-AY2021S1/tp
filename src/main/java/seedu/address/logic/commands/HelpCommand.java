17ab04421a44f865a24bd126f1c00be16d198adf
leeweiminsg <lee.wei.min@u.nus.edu>
2020-10-29 11:31:42 +0800

    Add help command

--- src/main/java/seedu/address/logic/commands/HelpCommand.java
+++ src/main/java/seedu/address/logic/commands/HelpCommand.java
@@ -1,19 +1,21 @@
-//package seedu.address.logic.commands;
-//
-///**
-// * Format full help instructions for every command for display.
-// */
-//public class HelpCommand extends Command {
-//
-//    public static final String COMMAND_WORD = "help";
-//
-//    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows program usage instructions.\n"
-//            + "Example: " + COMMAND_WORD;
-//
-//    public static final String SHOWING_HELP_MESSAGE = "Opened help window.";
-//
-//    @Override
-//    public CommandResult execute(Model model) {
-//        return new CommandResult(SHOWING_HELP_MESSAGE, true, false);
-//    }
-//}
+package seedu.address.logic.commands;
+
+import seedu.address.model.ExerciseModel;
+
+/**
+ * Format full help instructions for every command for display.
+ */
+public class HelpCommand extends CommandForExercise {
+
+    public static final String COMMAND_WORD = "help";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows program usage instructions.\n"
+            + "Example: " + COMMAND_WORD;
+
+    public static final String SHOWING_HELP_MESSAGE = "Opened help window.";
+
+    @Override
+    public CommandResult execute(ExerciseModel model) {
+        return new CommandResult(SHOWING_HELP_MESSAGE, true, false);
+    }
+}
