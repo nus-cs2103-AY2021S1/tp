faf91c413442421f78cb67a310bf5cd306281815
AlexCQY <alex_chua@u.nus.edu>
2020-10-29 14:47:46 +0800

    add DeleteEvent

--- src/main/java/seedu/address/model/event/DeleteEvent.java
+++ src/main/java/seedu/address/model/event/DeleteEvent.java
@@ -1,2 +1,25 @@
-package seedu.address.model.event;public class deleteEvent {
+package seedu.address.model.event;
+
+import java.time.LocalDateTime;
+
+/**
+ * This class is used to support deleting events in the schedule.
+ *
+ */
+public class DeleteEvent extends Event {
+
+    private static final String placeHolderUniqueIdentifier = LocalDateTime.now().toString();
+    private static final EventRecurrence placeHolderEventRecur = EventRecurrence.NONE;
+
+    public DeleteEvent(String eventName, LocalDateTime eventStartDateTime, LocalDateTime eventEndDateTime) {
+        this(eventName, eventStartDateTime, eventEndDateTime, placeHolderUniqueIdentifier, placeHolderEventRecur);
+    }
+
+    /**
+     * Creates an deleteEvent object that will be used to check with the schedule's event.
+     */
+    private DeleteEvent(String eventName, LocalDateTime eventStartDateTime, LocalDateTime eventEndDateTime,
+                       String uniqueIdentifier, EventRecurrence recurrence) {
+        super(eventName, eventStartDateTime, eventEndDateTime, uniqueIdentifier, recurrence);
+    }
 }
