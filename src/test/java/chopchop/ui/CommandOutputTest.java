bb8cb5107fbe168dc75b2625b22c9ffe10b6b0b5
Xue Yong <xueyong26@hotmail.com>
2020-10-29 11:22:09 +0800

    Add test for CommandResult

--- src/test/java/chopchop/ui/CommandOutputTest.java
+++ src/test/java/chopchop/ui/CommandOutputTest.java
@@ -1,4 +1,59 @@
 package chopchop.ui;
 
-public class CommandOutputTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import chopchop.logic.commands.CommandResult;
+import guitests.guihandles.CommandOutputHandle;
+import javafx.scene.Node;
+import javafx.scene.text.Text;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class CommandOutputTest extends GuiUnitTest {
+
+    private CommandOutput commandOutput;
+    private CommandOutputHandle commandOutputHandle;
+
+    @BeforeEach
+    public void setup() {
+        commandOutput = new CommandOutput();
+        uiPartExtension.setUiPart(commandOutput);
+
+        commandOutputHandle = new CommandOutputHandle(getChildNode(commandOutput.getRoot(),
+                CommandOutputHandle.RESULT_DISPLAY_ID));
+    }
+
+    @Test
+    public void displayValidMessage() {
+        guiRobot.pauseForHuman();
+        assertEquals("", parseTextToString(commandOutputHandle.getText()));
+
+        // Command output received
+        String testFeedback = "test feedback to user";
+        guiRobot.interact(() -> commandOutput.setFeedbackToUser(CommandResult.message(testFeedback)));
+        guiRobot.pauseForHuman();
+        // Command output adds a newline to the end of each text
+        assertEquals(testFeedback + "\n", parseTextToString(commandOutputHandle.getText()));
+    }
+
+    @Test
+    public void displayErrorMessage() {
+        String testError = "test error to user.";
+        guiRobot.interact(() -> commandOutput.setFeedbackToUser(CommandResult.message(testError)));
+        guiRobot.pauseForHuman();
+        // Command output adds a newline to the end of each text
+        assertEquals(testError + "\n", parseTextToString(commandOutputHandle.getText()));
+    }
+
+    private String parseTextToString(List<Node> nodeList) {
+        StringBuilder str = new StringBuilder();
+        for (Node node : nodeList) {
+            if (node instanceof Text) {
+                str.append(((Text) node).getText());
+            }
+        }
+        return str.toString();
+    }
 }
--- src/test/java/guitests/guihandles/CommandOutputHandle.java
+++ src/test/java/guitests/guihandles/CommandOutputHandle.java
@@ -1,25 +1,28 @@
 package guitests.guihandles;
 
-import javafx.scene.control.TextArea;
+import javafx.scene.Node;
+import javafx.scene.text.TextFlow;
+
+import java.util.List;
 
 /**
  * A handle to the {@code CommandOutput} in the GUI.
  */
-public class CommandOutputHandle extends NodeHandle<TextArea> {
+public class CommandOutputHandle extends NodeHandle<TextFlow> {
 
     public static final String RESULT_DISPLAY_ID = "#displayBox";
 
     /**
      * Constructs a {@code commandOutputHandle} with the given {@code commandOutputNode}.
      */
-    public CommandOutputHandle(TextArea commandOutputNode) {
+    public CommandOutputHandle(TextFlow commandOutputNode) {
         super(commandOutputNode);
     }
 
     /**
      * Returns the text in the display box.
      */
-    public String getText() {
-        return getRootNode().getText();
+    public List<Node> getText() {
+        return getRootNode().getChildren();
     }
 }
