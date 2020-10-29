40f9b4917f3d73a69f2423a6dabbacf282e577dc
davidcaiqifan <e0406982@u.nus.edu>
2020-10-30 02:32:48 +0800

    Fix bugs during capcalculator integration

--- src/main/java/seedu/address/logic/commands/modulelistcommands/AddCompletedModuleCommand.java
+++ src/main/java/seedu/address/logic/commands/modulelistcommands/AddCompletedModuleCommand.java
@@ -1,8 +1,7 @@
 package seedu.address.logic.commands.modulelistcommands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_ZOOM_LINK;
+import static seedu.address.logic.parser.CliSyntax.*;
 
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
@@ -13,13 +12,17 @@ import seedu.address.model.module.Module;
 public class AddCompletedModuleCommand extends Command {
     public static final String COMMAND_WORD = "addcmodule";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a module to the module list. "
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a completed module to the module list. "
             + "Parameters: "
             + PREFIX_NAME + "NAME "
-            + PREFIX_ZOOM_LINK + "ZOOM LINK "
+            + PREFIX_MODULAR_CREDITS + "MODULAR CREDITS "
+            + PREFIX_GRADE_POINT + "GRADE POINT "
+            + "[" + PREFIX_TAG + "TAG]...\n"
             + "Example: " + COMMAND_WORD + " "
             + PREFIX_NAME + "CS2100 "
-            + PREFIX_ZOOM_LINK + "www.zoom.com";
+            + PREFIX_MODULAR_CREDITS + "4.0 "
+            + PREFIX_GRADE_POINT + "5.0 "
+            + PREFIX_TAG + "year1";
 
     public static final String MESSAGE_SUCCESS = "New module added: %1$s";
     public static final String MESSAGE_DUPLICATE_MODULE = "This module already exists in the module list";
