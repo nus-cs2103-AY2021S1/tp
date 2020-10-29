cc8f3f9e127f53faf1d2c085f4c881db38ca0467
Sean Tay <seantaysl@gmail.com>
2020-10-29 11:29:06 +0800

    Fix naming error

--- src/main/java/seedu/address/logic/commands/FindTutorialGroupCommand.java
+++ src/main/java/seedu/address/logic/commands/FindTutorialGroupCommand.java
@@ -1,24 +1,25 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import seedu.address.commons.core.Messages;
-import seedu.address.logic.commands.exceptions.CommandException;
+
 import seedu.address.model.Model;
 import seedu.address.model.tutorialgroup.TutorialContainsKeywordsPredicate;
+import seedu.address.commons.core.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
 
 public class FindTutorialGroupCommand extends Command {
 
     public static final String COMMAND_WORD = "findTG";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all Tutorial Groups whose Tutorial Group Id " +
-        "contain any of the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all Tutorial Groups whose Tutorial Group Id "
+        + "contain any of the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
         + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
         + "Example: " + COMMAND_WORD + " B014 B015";
 
-    public static final String MESSAGE_NOT_IN_TUTORIAL_VIEW = "You are currently not in the Tutorial view. " +
-        "Run listMod to go back to the module view.";
-    public static final String MESSAGE_IN_MODULE_VIEW = "You are currently in Module View. " +
-        "Use viewTG MOUDLE_INDEX to view the Tutorial Groups of the Module you want";
+    public static final String MESSAGE_NOT_IN_TUTORIAL_VIEW = "You are currently not in the Tutorial view. "
+        + "Run listMod to go back to the module view.";
+    public static final String MESSAGE_IN_MODULE_VIEW = "You are currently in Module View. "
+        + "Use viewTG MOUDLE_INDEX to view the Tutorial Groups of the Module you want";
 
     private final TutorialContainsKeywordsPredicate predicate;
 
@@ -38,7 +39,9 @@ public class FindTutorialGroupCommand extends Command {
 
         model.updateFilteredTutorialGroupList(predicate);
         return new CommandResult(
-            String.format(Messages.MESSAGE_TUTORIALS_LISTED_OVERVIEW, model.getFilteredTutorialGroupList().size()), false, false, true, false, false);
+            String.format(Messages.MESSAGE_TUTORIAL_GROUPS_LISTED_OVERVIEW,
+                    model.getFilteredTutorialGroupList().size()),
+                false, false, true, false, false);
     }
 
     @Override
