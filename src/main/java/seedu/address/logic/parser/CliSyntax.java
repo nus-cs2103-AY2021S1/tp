6d5206a8a62cc73e55f51ea535616aecc596852a
AlexCQY <alex_chua@u.nus.edu>
2020-10-29 14:54:18 +0800

    add cliSyntax for schedule commands

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -30,11 +30,11 @@ public class CliSyntax {
     public static final Prefix PREFIX_VIEW_MODE = new Prefix("mode/");
     public static final Prefix PREFIX_VIEW_DATE = new Prefix("date/");
     public static final Prefix PREFIX_ADD_EVENT = new Prefix("add");
-    public static final Prefix PREFIX_EVENT_NAME = new Prefix("eventName/");
-    public static final Prefix PREFIX_EVENT_START_DATE_TIME = new Prefix("startDateTime/");
-    public static final Prefix PREFIX_EVENT_END_DATE_TIME = new Prefix("endDateTime/");
-    public static final Prefix PREFIX_EVENT_DESCRIPTION = new Prefix("description/");
-    public static final Prefix PREFIX_DELETE_EVENT = new Prefix("delete/");
+    public static final Prefix PREFIX_EVENT_NAME = new Prefix("n/");
+    public static final Prefix PREFIX_EVENT_START_DATE_TIME = new Prefix("start/");
+    public static final Prefix PREFIX_EVENT_END_DATE_TIME = new Prefix("end/");
+    public static final Prefix PREFIX_DELETE_EVENT = new Prefix("delete");
+    public static final Prefix PREFIX_RECURRENCE = new Prefix("r/");
 
     public static final Prefix[] COMPULSORY_PREFIXES = new Prefix[] {PREFIX_NAME, PREFIX_PHONE,
         PREFIX_SCHOOL, PREFIX_YEAR, PREFIX_VENUE, PREFIX_TIME, PREFIX_FEE, PREFIX_PAYMENT};
@@ -53,5 +53,9 @@ public class CliSyntax {
 
     public static final Prefix[] SCHEDULE_COMMAND_PREFIXES = new Prefix[] {PREFIX_VIEW, PREFIX_VIEW_MODE,
             PREFIX_VIEW_DATE, PREFIX_EVENT_NAME, PREFIX_EVENT_START_DATE_TIME, PREFIX_EVENT_END_DATE_TIME,
-        PREFIX_EVENT_DESCRIPTION, PREFIX_ADD_EVENT};
+        PREFIX_ADD_EVENT, PREFIX_RECURRENCE, PREFIX_DELETE_EVENT};
+
+    public  static final Prefix[] SCHEDULE_ADD_COMPULSORY_PREFIXES = new Prefix[] {PREFIX_ADD_EVENT,
+            PREFIX_EVENT_NAME, PREFIX_EVENT_START_DATE_TIME, PREFIX_EVENT_END_DATE_TIME,
+            PREFIX_RECURRENCE};
 }
