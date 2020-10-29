67913bd9bbef1c50515fc996a5458e77cc63a727
Jing Yen Loh <lohjingyen@gmail.com>
2020-10-30 01:25:15 +0800

    Remove find-students alias

--- src/test/java/seedu/resireg/testutil/TypicalCommandWordAliases.java
+++ src/test/java/seedu/resireg/testutil/TypicalCommandWordAliases.java
@@ -19,31 +19,30 @@ import seedu.resireg.model.alias.CommandWordAlias;
 public class TypicalCommandWordAliases {
 
     public static final CommandWordAlias ROOMS_R = new CommandWordAliasBuilder().withCommandWord("rooms")
-        .withAlias("r").build();
+            .withAlias("r").build();
     public static final CommandWordAlias STUDENTS_STU = new CommandWordAliasBuilder().withCommandWord("students")
-        .withAlias("stu").build();
+            .withAlias("stu").build();
     public static final CommandWordAlias ALLOCATE_AL = new CommandWordAliasBuilder().withCommandWord("allocate")
-        .withAlias("al").build();
-    public static final CommandWordAlias FIND_FI = new CommandWordAliasBuilder().withCommandWord("find-students")
-        .withAlias("fi").build();
+            .withAlias("al").build();
     public static final CommandWordAlias REALLOCATE_REL = new CommandWordAliasBuilder().withCommandWord("reallocate")
-        .withAlias("rel").build();
+            .withAlias("rel").build();
 
     // Manually added
     public static final CommandWordAlias EDIT_E = new CommandWordAliasBuilder().withCommandWord("edit-student")
-        .withAlias("e").build();
+            .withAlias("e").build();
     public static final CommandWordAlias DELETE_D = new CommandWordAliasBuilder().withCommandWord("delete-student")
-        .withAlias("d").build();
+            .withAlias("d").build();
 
     // Manually added - Student's details found in {@code CommandTestUtil}
     public static final CommandWordAlias ROOMS_RO = new CommandWordAliasBuilder()
-        .withCommandWord(VALID_COMMAND_ROOMS_RO)
-        .withAlias(VALID_ALIAS_ROOMS_RO).build();
+            .withCommandWord(VALID_COMMAND_ROOMS_RO)
+            .withAlias(VALID_ALIAS_ROOMS_RO).build();
     public static final CommandWordAlias STUDENTS_ST = new CommandWordAliasBuilder()
-        .withCommandWord(VALID_COMMAND_STUDENTS_ST)
-        .withAlias(VALID_ALIAS_STUDENTS_ST).build();
+            .withCommandWord(VALID_COMMAND_STUDENTS_ST)
+            .withAlias(VALID_ALIAS_STUDENTS_ST).build();
 
-    private TypicalCommandWordAliases() {} // prevents instantiation
+    private TypicalCommandWordAliases() {
+    } // prevents instantiation
 
     /**
      * Returns an {@code UserPrefs} with all the typical command word aliases.
@@ -57,6 +56,6 @@ public class TypicalCommandWordAliases {
     }
 
     public static List<CommandWordAlias> getTypicalCommandWordAliases() {
-        return new ArrayList<>(Arrays.asList(ROOMS_R, STUDENTS_STU, ALLOCATE_AL, FIND_FI, REALLOCATE_REL));
+        return new ArrayList<>(Arrays.asList(ROOMS_R, STUDENTS_STU, ALLOCATE_AL, REALLOCATE_REL));
     }
 }
