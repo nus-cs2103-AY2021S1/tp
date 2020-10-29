87d03b5a77365f8c0b25fea0a72ae2c2581dc8f0
Elgoh <elgoh3@gmail.com>
2020-10-29 20:08:20 +0800

    Update inventory json test cases

--- src/test/java/nustorage/testutil/TypicalInventoryRecords.java
+++ src/test/java/nustorage/testutil/TypicalInventoryRecords.java
@@ -1,6 +1,9 @@
 package nustorage.testutil;
 
 
+import static nustorage.logic.commands.CommandTestUtil.COST_1;
+import static nustorage.logic.commands.CommandTestUtil.COST_2;
+import static nustorage.logic.commands.CommandTestUtil.COST_3;
 import static nustorage.logic.commands.CommandTestUtil.DATE_TIME_A;
 import static nustorage.logic.commands.CommandTestUtil.DATE_TIME_B;
 import static nustorage.logic.commands.CommandTestUtil.DATE_TIME_C;
@@ -25,11 +28,11 @@ import nustorage.model.record.InventoryRecord;
 public class TypicalInventoryRecords {
 
     public static final InventoryRecord INVENTORY_RECORD_A =
-            new InventoryRecord(ITEM_NAME_1, QUANTITY_1, DATE_TIME_A, ID_A);
+            new InventoryRecord(ITEM_NAME_1, QUANTITY_1, COST_1, DATE_TIME_A, ID_A);
     public static final InventoryRecord INVENTORY_RECORD_B =
-            new InventoryRecord(ITEM_NAME_2, QUANTITY_2, DATE_TIME_B, ID_B);
+            new InventoryRecord(ITEM_NAME_2, QUANTITY_2, COST_2, DATE_TIME_B, ID_B);
     public static final InventoryRecord INVENTORY_RECORD_C =
-            new InventoryRecord(ITEM_NAME_3, QUANTITY_3, DATE_TIME_C, ID_C);
+            new InventoryRecord(ITEM_NAME_3, QUANTITY_3, COST_3, DATE_TIME_C, ID_C);
 
     private TypicalInventoryRecords() {} // prevents instantiation
 
