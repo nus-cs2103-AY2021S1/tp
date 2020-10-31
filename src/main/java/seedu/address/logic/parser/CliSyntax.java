28a16a463be6d3964ee818c32c2dafee23e6e4ca
luo-git <lluoyi.lu@gmail.com>
2020-10-31 22:58:03 +0800

    Change CLI syntax of commands
    t/ -> t>
    o/ -> o>
    f/ -> f>
    l/ -> l>

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -6,11 +6,10 @@ package seedu.address.logic.parser;
 public class CliSyntax {
 
     /* Prefix definitions */
-    public static final Prefix PREFIX_TAG_NAME = new Prefix("t/");
-    public static final Prefix PREFIX_OLD_TAG_NAME = new Prefix("o/");
-    public static final Prefix PREFIX_FILE_ADDRESS = new Prefix("f/");
-    public static final Prefix PREFIX_COMMAND = new Prefix("c/");
+    public static final Prefix PREFIX_TAG_NAME = new Prefix("t>");
+    public static final Prefix PREFIX_OLD_TAG_NAME = new Prefix("o>");
+    public static final Prefix PREFIX_FILE_ADDRESS = new Prefix("f>");
     public static final Prefix PREFIX_CHILD_PATH = new Prefix("./");
     public static final Prefix PREFIX_PARENT_PATH = new Prefix("../");
-    public static final Prefix PREFIX_LABEL_NAME = new Prefix("l/");
+    public static final Prefix PREFIX_LABEL_NAME = new Prefix("l>");
 }
