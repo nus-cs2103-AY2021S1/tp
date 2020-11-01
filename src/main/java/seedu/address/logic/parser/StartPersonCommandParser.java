003ecc81d243d4ea301af69dd30547cdbbbaa849
lll-jy <E0376997@u.nus.edu>
2020-11-01 17:34:27 +0800

    Format parsers

--- src/main/java/seedu/address/logic/parser/StartPersonCommandParser.java
+++ src/main/java/seedu/address/logic/parser/StartPersonCommandParser.java
@@ -14,6 +14,7 @@ public class StartPersonCommandParser implements Parser<StartPersonCommand> {
     /**
      * Parses the given {@code String} of arguments in the context of the StartPersonCommand
      * and returns a StartPersonCommand object for execution.
+     *
      * @throws ParseException if the user input does not conform the expected format
      */
     public StartPersonCommand parse(String args) throws ParseException {
