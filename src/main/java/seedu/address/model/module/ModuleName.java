c8211fd53d505e1155a7550177f145717fa4efe7
murtubak <murtubakchickenss@gmail.com>
2020-11-01 18:32:05 +0800

    Fix for incorrect regex for module name

--- src/main/java/seedu/address/model/module/ModuleName.java
+++ src/main/java/seedu/address/model/module/ModuleName.java
@@ -16,7 +16,7 @@ public class ModuleName {
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "[A-Z]{2,3}[\\d]{4,4}\\D?";
+    public static final String VALIDATION_REGEX = "[A-Z]{2,3}[\\d]{4,4}[A-Z]?";
 
     public final String fullName;
 
