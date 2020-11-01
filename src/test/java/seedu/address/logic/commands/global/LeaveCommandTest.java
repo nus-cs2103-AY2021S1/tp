c6d1955080dba2131c2b3976268f8efedff65b12
lll-jy <E0376997@u.nus.edu>
2020-11-01 16:45:33 +0800

    Fix checkstyle fails in test

--- src/test/java/seedu/address/logic/commands/global/LeaveCommandTest.java
+++ src/test/java/seedu/address/logic/commands/global/LeaveCommandTest.java
@@ -2,7 +2,6 @@ package seedu.address.logic.commands.global;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import seedu.address.logic.commands.global.LeaveCommand;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PROJECT;
 import static seedu.address.testutil.TypicalProjects.getTypicalMainCatalogue;
 
