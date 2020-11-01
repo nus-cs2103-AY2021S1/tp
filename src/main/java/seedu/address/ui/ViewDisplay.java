0419b65493b6f242e52037e34ae09baa32a554c9
Sean Tay <seantaysl@gmail.com>
2020-11-01 17:16:49 +0800

    Update ViewDisplay

--- src/main/java/seedu/address/ui/ViewDisplay.java
+++ src/main/java/seedu/address/ui/ViewDisplay.java
@@ -1,5 +1,6 @@
 package seedu.address.ui;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.Region;
@@ -8,7 +9,7 @@ import javafx.scene.layout.Region;
  * A ui for the current view display that is displayed at the header of the application.
  */
 public class ViewDisplay extends UiPart<Region> {
-    private static final String FXML = "viewDisplay.fxml";
+    private static final String FXML = "ViewDisplay.fxml";
 
     @FXML
     private TextArea viewDisplay;
@@ -23,6 +24,26 @@ public class ViewDisplay extends UiPart<Region> {
     }
 
     public void setCurrentView(String view) {
+        ObservableList<String> viewDisplayStyle = viewDisplay.getStyleClass();
+        viewDisplayStyle.clear();
+
+        switch (view) {
+            case "MODULES":
+                viewDisplayStyle.add("view-display-module");
+                System.out.println("moduel add");
+                break;
+
+            case "TUTORIAL GROUPS":
+                viewDisplayStyle.add("view-display-tg");
+                System.out.println("tg add");
+                break;
+
+            case "STUDENTS":
+                viewDisplayStyle.add("view-display-student");
+                System.out.println("student add");
+                break;
+
+        }
         viewDisplay.setText(view);
     }
 }
