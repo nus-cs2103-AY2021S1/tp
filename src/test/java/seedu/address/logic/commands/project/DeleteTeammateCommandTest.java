d3ef368741f36153549c92239892baa0689d3319
GeNiaaz <geniaaz1009@gmail.com>
2020-11-01 16:11:48 +0800

    Fix checkstyle

--- src/test/java/seedu/address/logic/commands/project/DeleteTeammateCommandTest.java
+++ src/test/java/seedu/address/logic/commands/project/DeleteTeammateCommandTest.java
@@ -2,12 +2,14 @@ package seedu.address.logic.commands.project;
 
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalGitIndexes.GIT_USERINDEX_FIRST_TEAMMATE;
 import static seedu.address.testutil.TypicalGitIndexes.GIT_USERINDEX_SECOND_TEAMMATE;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PROJECT;
-import static seedu.address.testutil.TypicalGitIndexes.GIT_USERINDEX_FIRST_TEAMMATE;
 import static seedu.address.testutil.TypicalPersons.DESC_A;
 import static seedu.address.testutil.TypicalProjects.getTypicalMainCatalogue;
+
 import org.junit.jupiter.api.Test;
+
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
@@ -36,8 +38,7 @@ public class DeleteTeammateCommandTest {
         model.addPerson(person);
         model.addParticipation(participation);
 
-        DeleteTeammateCommand deleteTeammateCommand = new DeleteTeammateCommand
-            (GIT_USERINDEX_FIRST_TEAMMATE);
+        DeleteTeammateCommand deleteTeammateCommand = new DeleteTeammateCommand(GIT_USERINDEX_FIRST_TEAMMATE);
 
         String expectedMessage = String.format(DeleteTeammateCommand.MESSAGE_DELETE_TEAMMATE_SUCCESS,
             person);
--- src/test/java/seedu/address/testutil/TypicalGitIndexes.java
+++ src/test/java/seedu/address/testutil/TypicalGitIndexes.java
@@ -2,14 +2,15 @@ package seedu.address.testutil;
 
 import static seedu.address.logic.commands.TeammateTestUtil.VALID_TEAMMATE_GIT_USERNAME_A;
 import static seedu.address.logic.commands.TeammateTestUtil.VALID_TEAMMATE_GIT_USERNAME_B;
+
 import seedu.address.commons.core.index.GitUserIndex;
 
 /**
  * A utility class containing a list of {@code GitUserIndex} objects to be used in tests.
  */
 public class TypicalGitIndexes {
-    public static final GitUserIndex GIT_USERINDEX_FIRST_TEAMMATE = new GitUserIndex
-        (VALID_TEAMMATE_GIT_USERNAME_A);
-    public static final GitUserIndex GIT_USERINDEX_SECOND_TEAMMATE = new GitUserIndex
-        (VALID_TEAMMATE_GIT_USERNAME_B);
+    public static final GitUserIndex GIT_USERINDEX_FIRST_TEAMMATE = new GitUserIndex(
+        VALID_TEAMMATE_GIT_USERNAME_A);
+    public static final GitUserIndex GIT_USERINDEX_SECOND_TEAMMATE = new GitUserIndex(
+        VALID_TEAMMATE_GIT_USERNAME_B);
 }
