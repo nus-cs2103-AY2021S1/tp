0ad7cc9617a77265b77a82e6d9fa5a7770865384
lll-jy <E0376997@u.nus.edu>
2020-11-02 09:16:12 +0800

    Fix problems in importing order

--- src/main/java/seedu/address/logic/parser/MainCatalogueParser.java
+++ src/main/java/seedu/address/logic/parser/MainCatalogueParser.java
@@ -30,9 +30,9 @@ import seedu.address.logic.commands.project.TaskFilterCommand;
 import seedu.address.logic.commands.project.TaskSorterCommand;
 import seedu.address.logic.commands.project.ViewTaskCommand;
 import seedu.address.logic.commands.project.ViewTeammateCommand;
+import seedu.address.logic.parser.exceptions.InvalidScopeException;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.Status;
-import seedu.address.logic.parser.exceptions.InvalidScopeException;
 
 /**
  * Parses user input.
