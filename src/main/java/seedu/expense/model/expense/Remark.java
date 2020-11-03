00f09d6f00f40c4cd2fc5bee373c84d27d366240
Jaylenlee <e0424677@u.nus.edu>
2020-11-03 23:42:54 +0800

    Restrict Description and Remark length

--- src/main/java/seedu/expense/model/expense/Remark.java
+++ src/main/java/seedu/expense/model/expense/Remark.java
@@ -1,12 +1,17 @@
 package seedu.expense.model.expense;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.expense.commons.core.Messages.MESSAGE_INPUT_OVERLIMIT;
+import static seedu.expense.commons.util.AppUtil.checkArgument;
 
 /**
  * Represents an Expense's remark in the expense book.
  * Guarantees: immutable; is always valid
  */
 public class Remark {
+
+    public static final int MAX_CHARACTERS = 200;
+    public static final String CLASS_NAME = "Remark";
     public final String value;
 
     /**
@@ -16,7 +21,16 @@ public class Remark {
      */
     public Remark(String remark) {
         requireNonNull(remark);
+        checkArgument(isValidLength(remark), String.format(MESSAGE_INPUT_OVERLIMIT,
+            CLASS_NAME, MAX_CHARACTERS));
         value = remark;
+    }
+
+    /**
+     * Returns true if a given string is of valid length.
+     */
+    public static boolean isValidLength(String test) {
+        return test.length() <= MAX_CHARACTERS;
     }
 
     @Override
