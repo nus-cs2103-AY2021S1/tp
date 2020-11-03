35e0e299f1e75004fb197dde8bb48f15f5765455
Constance <constancensq@gmail.com>
2020-11-03 22:11:40 +0800

    Edit testing for Email to fit new validation regex

--- src/main/java/seedu/pivot/model/investigationcase/caseperson/Email.java
+++ src/main/java/seedu/pivot/model/investigationcase/caseperson/Email.java
@@ -10,24 +10,17 @@ import static seedu.pivot.commons.util.AppUtil.checkArgument;
 public class Email {
 
     private static final String SPECIAL_CHARACTERS = "!#$%&'*+/=?`{|}~^.-";
-    public static final String MESSAGE_CONSTRAINTS = "Emails should be of the format local-part@domain.top-level-domain "
-            + "and adhere to the following constraints:\n"
+    public static final String MESSAGE_CONSTRAINTS = "Emails should be of the format "
+            + "local-part@domain.top-level-domain and adhere to the following constraints:\n"
             + "1. The local-part should only contain alphanumeric characters and these special characters, excluding "
             + "the parentheses, (" + SPECIAL_CHARACTERS + ") . The local part should not start with a '.'. \n"
-            + "2. This is followed by a '@' and then a domain name and then followed by a top-level domain name. "
+            + "2. This is followed by a '@' and then a domain name and then followed by a top-level domain "
+            + "(e.g. '.com'). "
             + "The domain name must:\n"
             + "    - start and end with alphanumeric characters\n"
             + "    - consist of alphanumeric characters, a period or a hyphen for the characters in between, if any.";
 
-    // alphanumeric and special characters
-    private static final String LOCAL_PART_REGEX = "^[\\w" + SPECIAL_CHARACTERS + "]+";
-    private static final String DOMAIN_FIRST_CHARACTER_REGEX = "[^\\W_]"; // alphanumeric characters except underscore
-    private static final String DOMAIN_MIDDLE_REGEX = "[a-zA-Z0-9.-]*"; // alphanumeric, period and hyphen
-    private static final String DOMAIN_LAST_CHARACTER_REGEX = "[^\\W_]$";
-    public static final String VALIDATION_REGEX = LOCAL_PART_REGEX + "@"
-            + DOMAIN_FIRST_CHARACTER_REGEX + DOMAIN_MIDDLE_REGEX + DOMAIN_LAST_CHARACTER_REGEX;
-
-    public static final String VALIDATION_REGEX_CHECK = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\."
+    private static final String VALIDATION_REGEX_CHECK = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\."
             + "[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f"
             + "]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9]"
             + "(?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]"
@@ -43,17 +36,17 @@ public class Email {
      */
     public Email(String email) {
         requireNonNull(email);
-        String trimmedEmail = email.trim();
-        checkArgument(isValidEmail(trimmedEmail), MESSAGE_CONSTRAINTS);
-        value = trimmedEmail;
+        String trimmedLowerCaseEmail = email.trim().toLowerCase();
+        checkArgument(isValidEmail(trimmedLowerCaseEmail), MESSAGE_CONSTRAINTS);
+        value = trimmedLowerCaseEmail;
     }
 
     /**
      * Returns if a given string is a valid email.
      */
     public static boolean isValidEmail(String test) {
-        String trimmedTest = test.trim();
-        return trimmedTest.isBlank() || trimmedTest.matches(VALIDATION_REGEX_CHECK);
+        String trimmedLowerCaseTest = test.trim().toLowerCase();
+        return trimmedLowerCaseTest.isBlank() || trimmedLowerCaseTest.matches(VALIDATION_REGEX_CHECK);
     }
 
     @Override
