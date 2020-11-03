7feccbd706386a33247fd4b1cabe3dd1e6a0c15b
EthanTheGoondu <ethannoahrozario@gmail.com>
2020-11-04 02:46:26 +0800

    Add testcases for JsonAdaptedWeekNumber

--- src/test/java/seedu/address/storage/JsonAdaptedAttendanceTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedAttendanceTest.java
@@ -7,6 +7,7 @@ import static seedu.address.testutil.TypicalStudents.ALEX;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 
@@ -16,6 +17,11 @@ import seedu.address.model.student.Attendance;
 public class JsonAdaptedAttendanceTest {
 
     private static final String INVALID_PARTICIPATION_SCORE = "4524324";
+    private static final String INVALID_WEEK_NUMBER = "365";
+
+    private static final String VALID_PARTICIPATION_SCORE = ALEX.getAttendance().getParticipationScoreAsString();
+    private static final List<JsonAdaptedWeekNumber> VALID_WEEK_NUMBERS = List.of("1", "2", "5", "11")
+            .stream().map(JsonAdaptedWeekNumber::new).collect(Collectors.toList());
 
     private static final List<JsonAdaptedWeekNumber> DUMMY_WEEK_NUMBERS = new ArrayList<>();
 
@@ -23,6 +29,17 @@ public class JsonAdaptedAttendanceTest {
     public void toModelType_validAttendanceDetails_returnsAttendance() throws Exception {
         JsonAdaptedAttendance attendance = new JsonAdaptedAttendance(ALEX.getAttendance());
         assertEquals(ALEX.getAttendance(), attendance.toModelType());
+    }
+
+    @Test
+    public void toModelType_invalidWeekNumbers_throwsIllegalValueException() {
+        List<JsonAdaptedWeekNumber> invalidWeekNumbers = new ArrayList<>(VALID_WEEK_NUMBERS);
+        invalidWeekNumbers.add(new JsonAdaptedWeekNumber(INVALID_WEEK_NUMBER));
+        JsonAdaptedAttendance attendance =
+                new JsonAdaptedAttendance(invalidWeekNumbers, VALID_PARTICIPATION_SCORE);
+        String expectedMessage = String.format((Attendance.WEEK_NUMBER_CONSTRAINTS),
+                Attendance.MIN_WEEK, Attendance.MAX_WEEK);
+        assertThrows(IllegalValueException.class, expectedMessage, attendance::toModelType);
     }
 
     @Test
