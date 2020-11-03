48dda8edddd6b33f75828835cc39cf79a7ef2c95
yanbingtao <yanbingtao963@Gmail.com>
2020-11-03 13:38:55 +0800

    Rename addressbook json file.

--- src/main/java/seedu/address/model/UserPrefs.java
+++ src/main/java/seedu/address/model/UserPrefs.java
@@ -14,7 +14,7 @@ import seedu.address.commons.core.GuiSettings;
 public class UserPrefs implements ReadOnlyUserPrefs {
 
     private GuiSettings guiSettings = new GuiSettings();
-    private Path addressBookFilePath = Paths.get("data" , "addressbook.json");
+    private Path addressBookFilePath = Paths.get("data" , "employeebook.json");
     private Path salesBookFilePath = Paths.get("data" , "salesbook.json");
     private Path ingredientBookFilePath = Paths.get("data" , "ingredientbook.json");
 
