1eb2c2f64b24bc6d489acfe424afee557411c793
Jay <Nopenotj@gmail.com>
2020-11-04 02:28:34 +0800

    Refactored fzf code

--- src/main/java/seedu/address/ui/FzfModule.java
+++ src/main/java/seedu/address/ui/FzfModule.java
@@ -8,83 +8,71 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 
-import java.awt.event.ActionEvent;
-import java.security.Key;
 import java.util.List;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 public class FzfModule {
 
-    private Supplier<List<String>> suggestionGen;
+    private Supplier<List<String>> optionSupplier;
     private boolean isFzfMode = false;
-    private int fzfStartPos;
-    private double fzfX;
-    private double fzfY;
+    private int fzfStartPos; // caret position where fzf was triggered
+    private double menuX;
+    private double menuY;
 
     private TextField query = new TextField() {
         {
             setSkin(createDefaultSkin());
         }
     };
-
-    private TextField textField;
-
-    ContextMenu fzfMenu;
+    private TextField attachedTextField;
+    private ContextMenu menu;
 
     private FzfModule(TextField tf, Supplier<List<String>> supplier) {
-        fzfMenu = new ContextMenu();
-        this.suggestionGen = supplier;
-        this.textField = tf;
+        this.optionSupplier = supplier;
+        this.attachedTextField = tf;
+        menu = new ContextMenu();
+
         setupFzf();
     }
+
     public static FzfModule attachTo(TextField tf, Supplier<List<String>> supplier) {
         return new FzfModule(tf, supplier);
     }
-    private MenuItem buildFzfMenuItem(String option, String query) {
-        MenuItem mi = new MenuItem("", buildTextFlow(option, query));
-        mi.setOnAction(e -> {
-            textField.replaceText(fzfStartPos, fzfStartPos + this.query.getText().length(), option);
-            toggleFzfOff();
-        });
-        return mi;
-    }
-
-    public void toggleFzfOn(int caretPos, double x, double y) {
-        if (!isFzfMode) {
-            System.out.println("Fzf Toggled On");
-            fzfStartPos = caretPos;
-            isFzfMode = true;
-            fzfX = x;
-            fzfY = y;
-        }
-    }
-
-    public void toggleFzfOff() {
-        if(isFzfMode){
-            System.out.println("Fzf Toggled Off");
-            isFzfMode = false;
-            query.setText("");
-        }
-    }
 
     private void setupFzf() {
-        textField.caretPositionProperty().addListener((unused1, unused2, newPosition) -> {
-            if (newPosition.intValue() < fzfStartPos || newPosition.intValue() > fzfStartPos + query.getText().length()) {
-                toggleFzfOff();
+        setupExitPoints();
+        setupEntryPoints();
+        setupMainLogic();
+
+    }
+
+    private void setupMainLogic() {
+        attachedTextField.addEventFilter(KeyEvent.ANY, (event) -> {
+            if (isFzfMode) {
+                if(event.getCode() == KeyCode.DOWN || event.getCode() == KeyCode.UP) {
+                    System.out.println(event.getCode());
+
+                    menu.fireEvent(event.copyFor(menu, menu));
+                    event.consume();
+                    return;
+                }
+
+                // Redirect Event to query TextField
+                query.fireEvent(event.copyFor(query,query));
+                refreshMenu(query.getText());
+
+                if (menu.getItems().isEmpty()) {
+                    menu.hide();
+                }
+                if(!menu.isShowing()) {
+                    menu.show(attachedTextField, menuX, menuY);
+                }
             }
         });
-
-        textField.addEventFilter(KeyEvent.ANY, (event) -> {
-                    if (event.getCode() == KeyCode.ESCAPE) {
-                        if (isFzfMode) {
-                            toggleFzfOff();
-                        }
-                    }
-                }
-        );
-
-        textField.addEventFilter(KeyEvent.KEY_PRESSED, (event) -> {
+    }
+    private void setupEntryPoints() {
+        attachedTextField.addEventFilter(KeyEvent.KEY_PRESSED, (event) -> {
                     if (event.isControlDown() && event.getCode() == KeyCode.SPACE) {
                         TextInputControl control = (TextInputControl) event.getSource();
                         Point2D pos = control.getInputMethodRequests().getTextLocation(0);
@@ -92,51 +80,42 @@ public class FzfModule {
                         toggleFzfOff();
                         toggleFzfOn(control.getCaretPosition(), pos.getX(), pos.getY());
                     }
-                    if(!isFzfMode) {
-                        fzfMenu.hide();
-                    }
                 }
         );
-
-        textField.addEventFilter(KeyEvent.ANY, (event) -> {
-                    if (isFzfMode) {
-                        if(event.getCode() == KeyCode.DOWN
-                                || event.getCode() == KeyCode.UP
-                        ) {
-                            System.out.println(event.getCode());
-
-                            fzfMenu.fireEvent(event.copyFor(fzfMenu,fzfMenu));
-                            event.consume();
-                            return;
-                        }
-
-                        // Redirect Event to TextField
-                        query.fireEvent(event.copyFor(query,query));
-                        String queryText = query.getText();
-
-                        fzfMenu.getItems().clear();
-                        fzfMenu.getItems().addAll(
-                                suggestionGen.get().stream()
-                                        .filter( x -> x.toLowerCase().contains(queryText.toLowerCase()))
-                                        .map( x -> buildFzfMenuItem(x, queryText) )
-                                        .collect(Collectors.toList())
-                        );
-                        if (fzfMenu.getItems().isEmpty()) {
-                            fzfMenu.hide();
-                        }
-
-                        if(!fzfMenu.isShowing()) {
-                            fzfMenu.show(textField, fzfX, fzfY);
-                        }
-
-                    }
-                }
-        );
-
-
-
+    }
+    private void setupExitPoints() {
+        attachedTextField.caretPositionProperty().addListener((unused1, unused2, newPosition) -> {
+            if (newPosition.intValue() < fzfStartPos || newPosition.intValue() > fzfStartPos + query.getText().length()) {
+                toggleFzfOff();
+            }
+        });
+        attachedTextField.addEventFilter(KeyEvent.ANY, (event) -> {
+            if (isFzfMode && event.getCode() == KeyCode.ESCAPE) {
+                toggleFzfOff();
+            }
+            if(!isFzfMode) {
+                menu.hide();
+            }
+        });
     }
 
+    private void refreshMenu(String queryText) {
+        menu.getItems().clear();
+        menu.getItems().addAll(
+                optionSupplier.get().stream()
+                        .filter( x -> x.toLowerCase().contains(queryText.toLowerCase()))
+                        .map( x -> buildMenuItem(x, queryText) )
+                        .collect(Collectors.toList())
+        );
+    }
+    private MenuItem buildMenuItem(String option, String query) {
+        MenuItem item = new MenuItem("", buildTextFlow(option, query));
+        item.setOnAction(e -> {
+            attachedTextField.replaceText(fzfStartPos, fzfStartPos + this.query.getText().length(), option);
+            toggleFzfOff();
+        });
+        return item;
+    }
     private TextFlow buildTextFlow(String text, String query) {
         if (query.length() == 0) {
             Text t = new Text(text);
@@ -144,6 +123,7 @@ public class FzfModule {
             return new TextFlow(t);
         }
         int filterIndex = text.toLowerCase().indexOf(query.toLowerCase());
+
         Text textBefore = new Text(text.substring(0, filterIndex));
         Text textAfter = new Text(text.substring(filterIndex + query.length()));
         Text textFilter = new Text(text.substring(filterIndex,  filterIndex + query.length()));
@@ -154,5 +134,20 @@ public class FzfModule {
 
         return new TextFlow(textBefore, textFilter, textAfter);
     }
-
+    private void toggleFzfOn(int caretPos, double x, double y) {
+        if (!isFzfMode) {
+            System.out.println("Fzf Toggled On");
+            fzfStartPos = caretPos;
+            isFzfMode = true;
+            menuX = x;
+            menuY = y;
+        }
+    }
+    private void toggleFzfOff() {
+        if(isFzfMode){
+            System.out.println("Fzf Toggled Off");
+            isFzfMode = false;
+            query.setText("");
+        }
+    }
 }
