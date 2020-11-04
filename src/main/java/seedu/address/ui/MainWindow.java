b288117f4d0873d37e8e5ff098e017c9ef00bd07
Jerryl Chong <jerrylchong@gmail.com>
2020-11-04 18:12:48 +0800

    Fix MainWindow not catching ParseExceptions

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -269,7 +269,7 @@ public class MainWindow extends UiPart<Stage> implements Observer {
             }
 
             return commandResult;
-        } catch (CommandException e) {
+        } catch (CommandException | ParseException e) {
             logger.info("Invalid command: " + commandText);
             resultDisplay.setFeedbackToUser(e.getMessage());
             throw e;
