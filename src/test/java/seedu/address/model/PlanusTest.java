6daed876f3bc0dc6fc4799fc3ced80011a5318b2
dearvae <lovevae7@gmail.com>
2020-11-05 02:49:42 +0800

    fix checkstyle error

--- src/test/java/seedu/address/model/PlanusTest.java
+++ src/test/java/seedu/address/model/PlanusTest.java
@@ -3,7 +3,6 @@ package seedu.address.model;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalTasks.DEADLINE1;
 import static seedu.address.testutil.TypicalTasks.getTypicalPlanus;
