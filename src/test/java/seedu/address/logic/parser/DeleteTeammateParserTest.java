4ed0cc6480b12901ff464cf7a81db8bc8b1f8507
GeNiaaz <geniaaz1009@gmail.com>
2020-11-04 23:19:36 +0800

    Add test for DeleteTeammateParserTest

--- src/test/java/seedu/address/logic/parser/DeleteTeammateParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteTeammateParserTest.java
@@ -2,6 +2,8 @@ package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalGitIndexes.GIT_USERINDEX_FIRST_TEAMMATE;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,6 +12,12 @@ import seedu.address.logic.commands.project.DeleteTeammateCommand;
 public class DeleteTeammateParserTest {
 
     private DeleteTeammateCommandParser parser = new DeleteTeammateCommandParser();
+
+    @Test
+    public void parse_validArgs_returnsDeleteTeammateCommand() {
+        assertParseSuccess(parser, "Sparrow32",
+            new DeleteTeammateCommand(GIT_USERINDEX_FIRST_TEAMMATE));
+    }
 
     @Test
     public void parse_invalidArgs_throwsParseException() {
