2348b73642574da0e5bd650c08967ce341790c73
StopTakingAllTheNames <ong.yinggao@gmail.com>
2020-11-04 15:18:44 +0800

    Stage file i left out -_-

--- src/test/java/seedu/address/model/student/academic/exam/ExamTest.java
+++ src/test/java/seedu/address/model/student/academic/exam/ExamTest.java
@@ -1,61 +1,35 @@
 package seedu.address.model.student.academic.exam;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.commons.util.DateUtil.parseToDate;
 import static seedu.address.testutil.Assert.assertThrows;
+
+import java.time.LocalDate;
 
 import org.junit.jupiter.api.Test;
 
 public class ExamTest {
-    static final String VALID_EXAM_NAME = "CA2";
-    static final String VALID_EXAM_DATE_DEF = "13/03/2020";
-    static final String VALID_EXAM_DATE_ALT = "13/3/20";
-    static final String INVALID_EXAM_DATE_FORMAT = "2020-02-12";
-    static final String INVALID_EXAM_DATE = "31/02/2020";
-    static final String VALID_SCORE = "25/50";
-    static final Exam VALID_EXAM = new Exam(VALID_EXAM_NAME, VALID_EXAM_DATE_DEF,
-            new Score(VALID_SCORE));
+    private static final String VALID_EXAM_NAME = "CA2";
+    private static final LocalDate VALID_EXAM_DATE_DEF = parseToDate("13/03/2020");
+    private static final LocalDate VALID_EXAM_DATE_ALT = parseToDate("13/3/20");
+    private static final Score VALID_SCORE = new Score("25/50");
+    private static final Exam VALID_EXAM = new Exam(VALID_EXAM_NAME, VALID_EXAM_DATE_DEF, VALID_SCORE);
 
     @Test
     public void constructor_null_throwsNullPointerException() {
         // All fields null
-        assertThrows(NullPointerException.class, () -> new Exam(null, null,
-                new Score(null)));
+        assertThrows(NullPointerException.class, () -> new Exam(null, null, null));
 
         // Only one non-null
-        assertThrows(NullPointerException.class, () -> new Exam(VALID_EXAM_NAME, null,
-                new Score(null)));
-        assertThrows(NullPointerException.class, () -> new Exam(null, VALID_EXAM_DATE_DEF,
-                new Score(null)));
-        assertThrows(NullPointerException.class, () -> new Exam(null, null,
-                new Score(VALID_SCORE)));
+        assertThrows(NullPointerException.class, () -> new Exam(VALID_EXAM_NAME, null, null));
+        assertThrows(NullPointerException.class, () -> new Exam(null, VALID_EXAM_DATE_DEF, null));
+        assertThrows(NullPointerException.class, () -> new Exam(null, null, VALID_SCORE));
 
         // Only one null
-        assertThrows(NullPointerException.class, () -> new Exam(VALID_EXAM_NAME, VALID_EXAM_DATE_DEF,
-                new Score(null)));
-        assertThrows(NullPointerException.class, () -> new Exam(null, VALID_EXAM_DATE_DEF,
-                new Score(VALID_SCORE)));
-        assertThrows(NullPointerException.class, () -> new Exam(VALID_EXAM_NAME, null,
-                new Score(VALID_SCORE)));
-    }
-
-    @Test
-    public void isValidDate_test() {
-        // null Date should throw exception
-        assertThrows(NullPointerException.class, () -> Exam.isValidDate(null));
-
-        // emptyDate is invalid
-        assertFalse(Exam.isValidDate(" "));
-
-        // Invalid format
-        assertFalse(Exam.isValidDate(INVALID_EXAM_DATE_FORMAT));
-        assertFalse(Exam.isValidDate(INVALID_EXAM_DATE));
-
-        // valid format date
-        assertTrue(Exam.isValidDate(VALID_EXAM_DATE_DEF));
-        assertTrue(Exam.isValidDate(VALID_EXAM_DATE_ALT));
+        assertThrows(NullPointerException.class, () -> new Exam(VALID_EXAM_NAME, VALID_EXAM_DATE_DEF, null));
+        assertThrows(NullPointerException.class, () -> new Exam(null, VALID_EXAM_DATE_DEF, VALID_SCORE));
+        assertThrows(NullPointerException.class, () -> new Exam(VALID_EXAM_NAME, null, VALID_SCORE));
     }
 
     @Test
@@ -64,19 +38,15 @@ public class ExamTest {
         assertNotEquals(VALID_EXAM, "not an exam");
 
         // diff fields - name is the only field that differentiates exams
-        assertNotEquals(VALID_EXAM, new Exam("Mid Terms 19/20", VALID_EXAM_DATE_DEF,
-                new Score(VALID_SCORE)));
+        assertNotEquals(VALID_EXAM, new Exam("Mid Terms 19/20", VALID_EXAM_DATE_DEF, VALID_SCORE));
 
-        assertEquals(VALID_EXAM, new Exam(VALID_EXAM_NAME, "25/12/2020",
-                new Score(VALID_SCORE)));
-        assertEquals(VALID_EXAM, new Exam(VALID_EXAM_NAME, VALID_EXAM_DATE_ALT,
-                new Score("10/100")));
+        assertEquals(VALID_EXAM, new Exam(VALID_EXAM_NAME, parseToDate("25/12/2020"), VALID_SCORE));
+        assertEquals(VALID_EXAM, new Exam(VALID_EXAM_NAME, VALID_EXAM_DATE_ALT, new Score("10/100")));
 
         // same object
         assertEquals(VALID_EXAM, VALID_EXAM);
 
         // same fields
-        assertEquals(VALID_EXAM, new Exam(VALID_EXAM_NAME, VALID_EXAM_DATE_ALT,
-                new Score(VALID_SCORE)));
+        assertEquals(VALID_EXAM, new Exam(VALID_EXAM_NAME, VALID_EXAM_DATE_ALT, VALID_SCORE));
     }
 }
