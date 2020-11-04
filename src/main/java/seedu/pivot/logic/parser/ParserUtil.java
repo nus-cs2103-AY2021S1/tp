d9af1e5551e8b74b70fe9d09fc47647c43d2cd77
Jargonx <neilgupta98@yahoo.com.sg>
2020-11-04 15:08:50 +0800

    Add, Edit and Delete Description

--- src/main/java/seedu/pivot/logic/parser/ParserUtil.java
+++ src/main/java/seedu/pivot/logic/parser/ParserUtil.java
@@ -160,7 +160,7 @@ public class ParserUtil {
     public static Description parseDescription(String description) throws ParseException {
         requireNonNull(description);
         String trimmedDescription = description.trim();
-        if (!Description.isValidDescription(trimmedDescription)) {
+        if (!Description.isValidDescriptionToAdd(trimmedDescription)) {
             throw new ParseException(Description.MESSAGE_CONSTRAINTS);
         }
         return new Description(trimmedDescription);
