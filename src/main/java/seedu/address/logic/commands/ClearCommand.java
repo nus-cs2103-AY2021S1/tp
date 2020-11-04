8c4b19f78ff51b2f941b938d20b93a285b4940e2
Jerry Ho <jerryho@u.nus.edu>
2020-11-04 16:11:54 +0800

    Bug commit

---
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -0,0 +1,23 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.model.Model;
+import seedu.address.model.ProductiveNus;
+
+/**
+ * Clears ProductiveNus.
+ */
+public class ClearCommand extends Command {
+
+    public static final String COMMAND_WORD = "clear";
+    public static final String MESSAGE_SUCCESS = "ProductiveNus has been cleared!";
+
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.setProductiveNus(new ProductiveNus());
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+}
