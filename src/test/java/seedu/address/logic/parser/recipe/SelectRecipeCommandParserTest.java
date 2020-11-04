24abbf12bf5d1f92fd2456b5be27daecba033114
jiaqi20 <tjq888@gmail.com>
2020-11-04 19:23:26 +0800

    Update checkstyle

--- src/test/java/seedu/address/logic/parser/recipe/SelectRecipeCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/recipe/SelectRecipeCommandParserTest.java
@@ -1,12 +1,13 @@
 package seedu.address.logic.parser.recipe;
 
-import org.junit.jupiter.api.Test;
-import seedu.address.logic.commands.recipe.SelectRecipeCommand;
-
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_RECIPE;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.recipe.SelectRecipeCommand;
 
 /**
  * As we are only doing white-box testing, our test cases do not cover path variations
