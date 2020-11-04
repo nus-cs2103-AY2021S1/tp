574ddc2d6746efd2f0f41f8758f0afcf7c444921
Dora Heng <e0425696@u.nus.edu>
2020-11-04 18:20:24 +0800

    Edit test case

--- src/test/java/nustorage/logic/parser/FindFinanceCommandParserTest.java
+++ src/test/java/nustorage/logic/parser/FindFinanceCommandParserTest.java
@@ -1,9 +1,7 @@
 package nustorage.logic.parser;
 
 import static nustorage.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static nustorage.logic.commands.CommandTestUtil.DATE_A;
 import static nustorage.logic.parser.CommandParserTestUtil.assertParseFailure;
-import static nustorage.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
 import org.junit.jupiter.api.Test;
 
@@ -16,12 +14,5 @@ public class FindFinanceCommandParserTest {
     public void parse_emptyArg_throwsParseException() {
         assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
                 FindFinanceCommand.MESSAGE_USAGE));
-    }
-
-    @Test
-    public void parse_validArgs_returnsFindCommand() {
-        FindFinanceCommand expectedFindFinanceCommand = new FindFinanceCommand();
-        expectedFindFinanceCommand.setAfterDatetime(DATE_A);
-        assertParseSuccess(parser, "2020-10-01", expectedFindFinanceCommand);
     }
 }
