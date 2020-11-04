03d394f4b7114fa74aad3818c5b2b673d1062410
Junhua <wenjh1998@gmail.com>
2020-11-04 21:59:35 +0800

    Update FoodContainsKeywordsPredicateTest

--- src/test/java/jimmy/mcgymmy/logic/predicate/FoodContainsKeywordsPredicateTest.java
+++ src/test/java/jimmy/mcgymmy/logic/predicate/FoodContainsKeywordsPredicateTest.java
@@ -1,6 +1,8 @@
 package jimmy.mcgymmy.logic.predicate;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
@@ -14,56 +16,70 @@ import jimmy.mcgymmy.model.food.Name;
 import jimmy.mcgymmy.testutil.FoodBuilder;
 
 class FoodContainsKeywordsPredicateTest {
+
+    private static final List<String> CHICKEN_LUNCH_LIST = Arrays.asList("Chicken", "Lunch");
+    private static final List<String> CHICKEN_DINNER_LIST = Arrays.asList("Chicken", "Dinner");
+    private static final List<String> SECOND_PREDICATE_KEYWORD_LIST = Arrays.asList("first", "second");
+    private static final List<String> FIRST_PREDICATE_KEYWORD_LIST = Collections.singletonList("first");
+    private static final FoodContainsKeywordsPredicate CHICKEN_DINNER_PREDICATE =
+            new FoodContainsKeywordsPredicate(CHICKEN_DINNER_LIST);
+
     @Test
     public void equals() {
-        List<String> firstPredicateKeywordList = Collections.singletonList("first");
-        List<String> secondPredicateKeywordList = Arrays.asList("first", "second");
 
-        FoodContainsKeywordsPredicate firstPredicate = new FoodContainsKeywordsPredicate(firstPredicateKeywordList);
-        FoodContainsKeywordsPredicate secondPredicate = new FoodContainsKeywordsPredicate(secondPredicateKeywordList);
+        FoodContainsKeywordsPredicate firstPredicate = new FoodContainsKeywordsPredicate(FIRST_PREDICATE_KEYWORD_LIST);
+        FoodContainsKeywordsPredicate secondPredicate =
+                new FoodContainsKeywordsPredicate(SECOND_PREDICATE_KEYWORD_LIST);
 
         // same object -> returns true
-        assertTrue(firstPredicate.equals(firstPredicate));
+        assertEquals(firstPredicate, firstPredicate);
 
         // same values -> returns true
-        FoodContainsKeywordsPredicate firstPredicateCopy = new FoodContainsKeywordsPredicate(firstPredicateKeywordList);
-        assertTrue(firstPredicate.equals(firstPredicateCopy));
+        FoodContainsKeywordsPredicate firstPredicateCopy =
+                new FoodContainsKeywordsPredicate(FIRST_PREDICATE_KEYWORD_LIST);
+        assertEquals(firstPredicateCopy, firstPredicate);
 
         // different types -> returns false
-        assertFalse(firstPredicate.equals(1));
+        assertNotEquals(firstPredicate, 1);
 
         // null -> returns false
-        assertFalse(firstPredicate.equals(null));
+        assertNotEquals(firstPredicate, null);
 
         // different food -> returns false
-        assertFalse(firstPredicate.equals(secondPredicate));
+        assertNotEquals(secondPredicate, firstPredicate);
     }
 
     @Test
     public void test_nameFoundTagNotFound_returnsTrue() throws IllegalValueException {
-        FoodContainsKeywordsPredicate predicate =
-                new FoodContainsKeywordsPredicate(Arrays.asList("Chicken", "Dinner"));
-        assertTrue(predicate.test(new FoodBuilder().withName(new Name("Chicken McWing")).withTags("Lunch").build()));
+        assertTrue(CHICKEN_DINNER_PREDICATE
+                .test(new FoodBuilder()
+                        .withName(new Name("Chicken McWing"))
+                        .withTags("Lunch")
+                        .build()));
     }
 
     @Test
     public void test_nameFoundTagFound_returnsTrue() throws IllegalValueException {
-        FoodContainsKeywordsPredicate predicate =
-                new FoodContainsKeywordsPredicate(Arrays.asList("Chicken", "Dinner"));
-        assertTrue(predicate.test(new FoodBuilder().withName(new Name("Chicken McWing")).withTags("Dinner").build()));
+        assertTrue(CHICKEN_DINNER_PREDICATE
+                .test(new FoodBuilder()
+                        .withName(new Name("Chicken McWing"))
+                        .withTags("Dinner")
+                        .build()));
     }
 
     @Test
     public void test_nameNotFoundTagFound_returnsTrue() throws IllegalValueException {
-        FoodContainsKeywordsPredicate predicate =
-                new FoodContainsKeywordsPredicate(Arrays.asList("Chicken", "Dinner"));
-        assertTrue(predicate.test(new FoodBuilder().withName(new Name("Big Mac")).withTags("Dinner").build()));
+        assertTrue(CHICKEN_DINNER_PREDICATE
+                .test(new FoodBuilder()
+                        .withName(new Name("Big Mac"))
+                        .withTags("Dinner")
+                        .build()));
     }
 
     @Test
     public void test_nameNotFoundTagNotFound_returnsFalse() throws IllegalValueException {
         FoodContainsKeywordsPredicate predicate =
-                new FoodContainsKeywordsPredicate(Arrays.asList("Chicken", "Lunch"));
+                new FoodContainsKeywordsPredicate(CHICKEN_LUNCH_LIST);
         assertFalse(predicate.test(new FoodBuilder().withName(new Name("Big Mac")).withTags("Dinner").build()));
     }
 }
