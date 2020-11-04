a2d8252322a16fcbfdad888ca2ed3b20207c17b0
lll-jy <E0376997@u.nus.edu>
2020-11-04 09:04:39 +0800

    Fix bug of not outputting error message for new teammate

--- src/main/java/seedu/address/logic/commands/project/NewTeammateCommand.java
+++ src/main/java/seedu/address/logic/commands/project/NewTeammateCommand.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands.project;
 
 import static java.util.Objects.requireNonNull;
+import seedu.address.logic.commands.exceptions.CommandException;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TEAMMATE_ADDRESS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TEAMMATE_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TEAMMATE_GIT_USERNAME;
@@ -34,6 +35,7 @@ public class NewTeammateCommand extends Command {
         + "Example: " + COMMAND_WORD + " mn/Lucas mg/LucasTai98 mp/93824823 me/lucas@gmail.com ma/18 Evelyn Road";
 
     public static final String MESSAGE_NEW_TEAMMATE_SUCCESS = "New Teammate added: %1$s";
+    public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the main catalogue";
 
     private static final Logger logger = Logger.getLogger("NewTeammateCommandLogger");
     private final Person toAdd;
@@ -53,9 +55,12 @@ public class NewTeammateCommand extends Command {
      * @return feedback message of the operation result for display
      */
     @Override
-    public CommandResult execute(Model model) {
+    public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
         Project project = model.getProjectToBeDisplayedOnDashboard().get();
+        if (model.hasPerson(toAdd)) {
+            throw new CommandException(MESSAGE_DUPLICATE_PERSON);
+        }
         model.addPerson(toAdd);
         toAdd.addProject(project);
         project.addParticipation(toAdd);
