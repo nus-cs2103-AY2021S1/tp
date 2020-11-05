73dea461e8cbb7be27872b23a6f9bc6802d2abc5
jiaqi20 <tjq888@gmail.com>
2020-11-05 21:22:48 +0800

    Update CI

--- src/test/java/seedu/address/logic/parser/data/ImageParserTest.java
+++ src/test/java/seedu/address/logic/parser/data/ImageParserTest.java
@@ -1,7 +1,6 @@
 package seedu.address.logic.parser.data;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.IOException;
