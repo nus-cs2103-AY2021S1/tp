35ce1879cecd5800ee324118ae443397ede23679
GilbertTan19 <Gilberttan95.gt@gmail.com>
2020-11-05 17:33:26 +0800

    Fixed checkstyle

--- src/test/java/quickcache/testutil/FlashcardUtil.java
+++ src/test/java/quickcache/testutil/FlashcardUtil.java
@@ -1,16 +1,20 @@
 package quickcache.testutil;
 
 import static quickcache.logic.commands.EditCommand.EditFlashcardDescriptor;
-import static quickcache.logic.parser.CliSyntax.*;
+import static quickcache.logic.parser.CliSyntax.PREFIX_ANSWER;
+import static quickcache.logic.parser.CliSyntax.PREFIX_CHOICE;
+import static quickcache.logic.parser.CliSyntax.PREFIX_DIFFICULTY;
+import static quickcache.logic.parser.CliSyntax.PREFIX_QUESTION;
+import static quickcache.logic.parser.CliSyntax.PREFIX_TAG;
+
+import java.util.Optional;
+import java.util.Set;
 
 import quickcache.logic.commands.AddMultipleChoiceQuestionCommand;
 import quickcache.logic.commands.AddOpenEndedQuestionCommand;
 import quickcache.model.flashcard.Choice;
 import quickcache.model.flashcard.Flashcard;
 import quickcache.model.flashcard.Tag;
-
-import java.util.Optional;
-import java.util.Set;
 
 /**
  * A utility class for Flashcard.
