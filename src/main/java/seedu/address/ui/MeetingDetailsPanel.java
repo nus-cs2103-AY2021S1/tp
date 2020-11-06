fa492cf827d2290f69d9a8e8dcf4f156f46a71b0
royleochan <royleochan@gmail.com>
2020-11-07 02:55:12 +0800

    Remove unused import

--- src/main/java/seedu/address/ui/MeetingDetailsPanel.java
+++ src/main/java/seedu/address/ui/MeetingDetailsPanel.java
@@ -3,7 +3,6 @@ package seedu.address.ui;
 import java.util.Comparator;
 
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
