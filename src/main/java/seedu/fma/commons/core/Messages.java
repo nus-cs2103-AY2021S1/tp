fd23f581b0038bda374ae5cedd4b939fa0662e92
jillzyt <jillzyt25@gmail.com>
2020-11-06 14:48:59 +0800

    fix: convert logs to log(s)
    fix: convert logs to log(s)

--- src/main/java/seedu/fma/commons/core/Messages.java
+++ src/main/java/seedu/fma/commons/core/Messages.java
@@ -8,7 +8,7 @@ public class Messages {
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_LOG_DISPLAYED_INDEX = "Invalid Log index. Please enter an index found "
             + "in the logs below.";
-    public static final String MESSAGE_LOGS_LISTED_OVERVIEW = "%1$d logs listed!";
+    public static final String MESSAGE_LOGS_LISTED_OVERVIEW = "%1$d log(s) listed!";
     public static final String MESSAGE_INVALID_EXERCISE_DISPLAYED_INDEX = "Invalid Exercise index. Please enter an "
             + "index found in the exercises below.";
 }
