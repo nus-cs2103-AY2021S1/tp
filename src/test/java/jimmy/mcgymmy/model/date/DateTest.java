37d6f51641e823f32b1a47dfbbde64433e5c24e9
Junhua <wenjh1998@gmail.com>
2020-11-06 18:04:21 +0800

    Add Unit Test to check for leap year and non leap years

--- src/test/java/jimmy/mcgymmy/model/date/DateTest.java
+++ src/test/java/jimmy/mcgymmy/model/date/DateTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import org.junit.jupiter.api.Test;
 
@@ -17,8 +18,8 @@ class DateTest {
     private static final String EXPECTED_DATE_STRING_1_DD_MMM_YYYY = "20 Apr 2020";
     private static final String EXPECTED_DATE_STRING_2_D_MMM_YYYY = "2 Sep 2020";
     private static final String EXPECTED_DATE_STRING_1_YYYY_MM_YYYY = "2020-04-20";
-    private static final String EXPECTED_DATE_STRING_3_LEAP_YEAR_FEB = "2020-02-29";
-    private static final String EXPECTED_DATE_STRING_3_NON_LEAP_YEAR_FEB = "2019-02-30";
+    private static final String EXPECTED_DATE_STRING_3_LEAP_YEAR_FEB = "29 Feb 2020";
+    private static final String EXPECTED_DATE_STRING_3_NON_LEAP_YEAR_FEB = "29 Feb 2019";
     private static final String EXPECTED_DATE_STRING_1_NON_EXISTENT_DATE = "2019-04-31";
 
     @Test
@@ -29,7 +30,7 @@ class DateTest {
     @Test
     void leapYearValidTest() throws IllegalValueException {
         //Valid leap year Date
-        new Date(EXPECTED_DATE_STRING_3_LEAP_YEAR_FEB);
+        assertEquals(new Date(EXPECTED_DATE_STRING_3_LEAP_YEAR_FEB).toString(), EXPECTED_DATE_STRING_3_LEAP_YEAR_FEB);
 
         //Invalid leap year date
         assertThrows(IllegalValueException.class, () -> new Date(EXPECTED_DATE_STRING_3_NON_LEAP_YEAR_FEB));
@@ -38,24 +39,24 @@ class DateTest {
 
     @Test
     void constructorAndToString_supportedFormat_createsDateObject() throws IllegalValueException {
-        // format: "yyyy-M-dd"
-        assertEquals(new Date("2020-4-20").toString(), EXPECTED_DATE_STRING_1_DD_MMM_YYYY);
-        // format: "yyyy-M-d"
-        assertEquals(new Date("2020-9-2").toString(), EXPECTED_DATE_STRING_2_D_MMM_YYYY);
-        // format: "dd-MM-yyyy"
-        assertEquals(new Date("20-04-2020").toString(), EXPECTED_DATE_STRING_1_DD_MMM_YYYY);
-        // format: "dd-M-yyyy"
-        assertEquals(new Date("20-4-2020").toString(), EXPECTED_DATE_STRING_1_DD_MMM_YYYY);
+        // format: "d/M/yyyy",
+        assertEquals(new Date("2/9/2020").toString(), EXPECTED_DATE_STRING_2_D_MMM_YYYY);
         // format: "d-M-yyyy"
         assertEquals(new Date("2-9-2020").toString(), EXPECTED_DATE_STRING_2_D_MMM_YYYY);
+        // format: "yyyy-M-d"
+        assertEquals(new Date("2020-9-2").toString(), EXPECTED_DATE_STRING_2_D_MMM_YYYY);
+        // format: "yyyy-M-dd"
+        assertEquals(new Date("2020-4-20").toString(), EXPECTED_DATE_STRING_1_DD_MMM_YYYY);
+        // format: "dd-M-yyyy"
+        assertEquals(new Date("20-4-2020").toString(), EXPECTED_DATE_STRING_1_DD_MMM_YYYY);
+        // format: "dd/M/yyyy"
+        assertEquals(new Date("20/4/2020").toString(), EXPECTED_DATE_STRING_1_DD_MMM_YYYY);
+        // format: "dd-MM-yyyy"
+        assertEquals(new Date("20-04-2020").toString(), EXPECTED_DATE_STRING_1_DD_MMM_YYYY);
         // format: "yyyy/MM/dd"
         assertEquals(new Date("2020/04/20").toString(), EXPECTED_DATE_STRING_1_DD_MMM_YYYY);
         // format: "dd/MM/yyyy"
         assertEquals(new Date("20/04/2020").toString(), EXPECTED_DATE_STRING_1_DD_MMM_YYYY);
-        // format: "dd/M/yyyy"
-        assertEquals(new Date("20/4/2020").toString(), EXPECTED_DATE_STRING_1_DD_MMM_YYYY);
-        // format: "d/M/yyyy",
-        assertEquals(new Date("2/9/2020").toString(), EXPECTED_DATE_STRING_2_D_MMM_YYYY);
         // format: "d MMM yyyy"
         assertEquals(new Date(EXPECTED_DATE_STRING_1_DD_MMM_YYYY).toString(), EXPECTED_DATE_STRING_1_DD_MMM_YYYY);
         // format: "yyyy-MM-dd"
@@ -73,20 +74,21 @@ class DateTest {
 
     @Test
     void equals_returnsCorrectComparison() throws IllegalValueException {
-        Date testDate1 = new Date(EXPECTED_DATE_STRING_1_DD_MMM_YYYY);
-        Date testDate2 = new Date(EXPECTED_DATE_STRING_2_D_MMM_YYYY);
+        Date testDate1 = new Date(EXPECTED_DATE_STRING_2_D_MMM_YYYY);
+        Date testDate2 = new Date(EXPECTED_DATE_STRING_1_DD_MMM_YYYY);
+
 
         // identical -> returns true
-        assertEquals(testDate1, testDate1);
+        assertEquals(testDate2, testDate2);
 
         // same date -> returns true
-        assertEquals(new Date(EXPECTED_DATE_STRING_1_YYYY_MM_YYYY), testDate1);
+        assertEquals(new Date(EXPECTED_DATE_STRING_1_YYYY_MM_YYYY), testDate2);
 
         // different date -> returns false
-        assertNotEquals(testDate2, testDate1);
+        assertNotEquals(testDate1, testDate2);
 
         // not instance of Date -> returns false
-        assertNotEquals(EXPECTED_DATE_STRING_1_DD_MMM_YYYY, testDate1);
+        assertNotEquals(EXPECTED_DATE_STRING_1_DD_MMM_YYYY, testDate2);
     }
 
     @Test
