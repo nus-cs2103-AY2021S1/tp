cbbdc6e0a486814b5798806a549ef314bd2b04e2
michael-setia <michaelsetia55@gmail.com>
2020-11-07 03:48:14 +0800

    SortContactParser class: add exception to parse() method

--- src/main/java/seedu/address/logic/parser/contactlistparsers/SortContactParser.java
+++ src/main/java/seedu/address/logic/parser/contactlistparsers/SortContactParser.java
@@ -8,6 +8,8 @@ import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.contact.Contact;
 import seedu.address.model.contact.ContactComparatorByName;
 
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
 /**
  * Parses input arguments and creates a new SortContactCommand object
  */
@@ -17,11 +19,16 @@ public class SortContactParser implements Parser<SortContactCommand> {
      * and returns an SortContactCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
-    public SortContactCommand parse(String args) {
+    public SortContactCommand parse(String args) throws ParseException {
         Comparator<Contact> comparator = new ContactComparatorByName();
-        if (args.trim().equals("r")) {
-            comparator = comparator.reversed();
+        String trimmedArgs = args.trim();
+        if (trimmedArgs.isEmpty()) {
+            return new SortContactCommand(comparator);
         }
-        return new SortContactCommand(comparator);
+        if (args.trim().equals("r")) {
+            return new SortContactCommand(comparator.reversed());
+        }
+        throw new ParseException(
+            String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortContactCommand.MESSAGE_USAGE));
     }
 }
