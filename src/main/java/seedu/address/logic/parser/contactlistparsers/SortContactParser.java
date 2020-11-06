8e3b5102c9b25f6447c6cfb15a1979c052f563f5
michael-setia <michaelsetia55@gmail.com>
2020-11-07 03:58:28 +0800

    SortContactParser class: Fix import statements order

--- src/main/java/seedu/address/logic/parser/contactlistparsers/SortContactParser.java
+++ src/main/java/seedu/address/logic/parser/contactlistparsers/SortContactParser.java
@@ -2,13 +2,13 @@ package seedu.address.logic.parser.contactlistparsers;
 
 import java.util.Comparator;
 
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
 import seedu.address.logic.commands.contactlistcommands.SortContactCommand;
 import seedu.address.logic.parser.Parser;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.contact.Contact;
 import seedu.address.model.contact.ContactComparatorByName;
-
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 /**
  * Parses input arguments and creates a new SortContactCommand object
