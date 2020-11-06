8890702908484479addff756efeaef122bd38ad6
ChooJiaXin <e0426255@u.nus.edu>
2020-11-06 18:37:30 +0800

    Amend message usage for clear, undo and exit

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -2,6 +2,7 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import seedu.address.commons.core.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ProductiveNus;
@@ -12,7 +13,7 @@ import seedu.address.model.ProductiveNus;
 public class ClearCommand extends Command {
 
     public static final String COMMAND_WORD = "clear";
-    public static final String MESSAGE_INCORRECT_FORMAT = "Invalid command format! \nFormat: clear";
+    public static final String MESSAGE_USAGE = "Format: " + COMMAND_WORD;
     public static final String MESSAGE_SUCCESS = "ProductiveNus has been cleared!";
 
     private final String userInput;
@@ -30,7 +31,7 @@ public class ClearCommand extends Command {
             model.setProductiveNus(new ProductiveNus());
             return new CommandResult(MESSAGE_SUCCESS);
         } else {
-            throw new CommandException(MESSAGE_INCORRECT_FORMAT);
+            throw new CommandException(String.format(Messages.MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
         }
     }
 }
