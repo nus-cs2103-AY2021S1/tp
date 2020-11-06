fd96643428a3e1fe330f44fb77623fc2e5893800
IronBiscuit <limyuyang.98@gmail.com>
2020-11-07 03:00:26 +0800

    Add more update inventory cases

--- src/test/java/nustorage/logic/commands/UpdateInventoryCommandTest.java
+++ src/test/java/nustorage/logic/commands/UpdateInventoryCommandTest.java
@@ -6,6 +6,7 @@ import static nustorage.logic.commands.UpdateInventoryCommand.MESSAGE_INVALID_UP
 import static nustorage.testutil.Assert.assertThrows;
 import static nustorage.testutil.TypicalIndexes.INDEX_FIRST;
 
+import nustorage.logic.commands.exceptions.CommandException;
 import org.junit.jupiter.api.Test;
 
 import nustorage.logic.commands.UpdateInventoryCommand.UpdateInventoryDescriptor;
@@ -47,6 +48,26 @@ public class UpdateInventoryCommandTest {
     public void execute_decreaseInQuantityParsed_success() {
         InventoryRecord oldInventoryRecord = model.getFilteredInventory().get(0);
         InventoryRecord updatedInventoryRecord = new InventoryRecordBuilder(oldInventoryRecord)
+                .withQuantity(5)
+                .build();
+        int changeInQuantity = updatedInventoryRecord.getQuantity() - oldInventoryRecord.getQuantity();
+        FinanceRecord newFinanceRecord =
+                new FinanceRecord(oldInventoryRecord.getFinanceId(),
+                        updatedInventoryRecord.getQuantity() * updatedInventoryRecord.getUnitCost(),
+                        updatedInventoryRecord.getDateTime(),
+                        true);
+        updatedInventoryRecord.setFinanceRecord(newFinanceRecord);
+        UpdateInventoryDescriptor descriptor = new UpdateInventoryDescriptorBuilder(changeInQuantity).build();
+        UpdateInventoryCommand command = new UpdateInventoryCommand(INDEX_FIRST, descriptor);
+        String expectedMessage = String.format(MESSAGE_UPDATE_INVENTORY_SUCCESS, updatedInventoryRecord);
+        Model expectedModel = model;
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_decreaseInQuantityToZeroParsed_success() {
+        InventoryRecord oldInventoryRecord = model.getFilteredInventory().get(0);
+        InventoryRecord updatedInventoryRecord = new InventoryRecordBuilder(oldInventoryRecord)
                 .withQuantity(0)
                 .build();
         int changeInQuantity = updatedInventoryRecord.getQuantity() - oldInventoryRecord.getQuantity();
@@ -72,6 +93,11 @@ public class UpdateInventoryCommandTest {
         UpdateInventoryDescriptor descriptor = new UpdateInventoryDescriptorBuilder(changeInQuantity).build();
         UpdateInventoryCommand command = new UpdateInventoryCommand(INDEX_FIRST, descriptor);
         String expectedMessage = MESSAGE_INVALID_UPDATE_OPERATION;
-        Model expectedModel = model;
+        Model modelCopy = new ModelManager(TypicalFinanceRecords
+                .getTypicalFinanceWithInventory(),
+                TypicalInventoryRecords.getTypicalInventory(),
+                new UserPrefs());
+        assertThrows(CommandException.class, () -> command.execute(modelCopy));
     }
+
 }
