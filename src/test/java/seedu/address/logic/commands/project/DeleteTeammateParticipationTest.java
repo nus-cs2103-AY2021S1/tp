402ebd13f1b9aad8dd819392304f8fab62f14c11
GeNiaaz <geniaaz1009@gmail.com>
2020-11-06 19:42:31 +0800

    Fix github tests, attempt #19

--- src/test/java/seedu/address/logic/commands/project/DeleteTeammateParticipationTest.java
+++ src/test/java/seedu/address/logic/commands/project/DeleteTeammateParticipationTest.java
@@ -2,11 +2,13 @@ package seedu.address.logic.commands.project;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalGitIndexes.GIT_USERINDEX_FIRST_TEAMMATE;
 import static seedu.address.testutil.TypicalGitIndexes.GIT_USERINDEX_INVALID_TEAMMATE;
 import static seedu.address.testutil.TypicalGitIndexes.GIT_USERINDEX_SECOND_TEAMMATE;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PROJECT;
+import static seedu.address.testutil.TypicalPersons.DESC_A;
 import static seedu.address.testutil.TypicalProjects.getTypicalMainCatalogue;
 
 import org.junit.jupiter.api.Test;
@@ -18,7 +20,6 @@ import seedu.address.model.project.Project;
 
 public class DeleteTeammateParticipationTest {
 
-    /**
     @Test
     public void execute_validGitUserIndex_success() {
         //Person.getAllPeople().clear();
@@ -42,7 +43,7 @@ public class DeleteTeammateParticipationTest {
 
         assertCommandSuccess(deleteTeammateParticipationCommand, model, expectedMessage, expectedModel);
     }
-     */
+
 
     @Test
     public void execute_invalidGitUserIndex_throwsNullPointerException() {
