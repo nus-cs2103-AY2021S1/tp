cc2e71493cdbc76fd3b39970281774657157be65
jillzyt <jillzyt25@gmail.com>
2020-11-06 13:24:19 +0800

    fix: fix the message for invalid indexes
    fix: fix the message for invalid indexes

--- src/main/java/seedu/fma/commons/core/Messages.java
+++ src/main/java/seedu/fma/commons/core/Messages.java
@@ -6,7 +6,9 @@ package seedu.fma.commons.core;
 public class Messages {
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
-    public static final String MESSAGE_INVALID_LOG_DISPLAYED_INDEX = "The log index provided is invalid";
+    public static final String MESSAGE_INVALID_LOG_DISPLAYED_INDEX = "Invalid Log index. Please enter an index found " +
+            "in the logs below.";
     public static final String MESSAGE_LOGS_LISTED_OVERVIEW = "%1$d logs listed!";
-    public static final String MESSAGE_INVALID_EXERCISE_DISPLAYED_INDEX = "The exercise index provided is invalid";
+    public static final String MESSAGE_INVALID_EXERCISE_DISPLAYED_INDEX = "Invalid Exercise index. Please enter an " +
+            "index found in the exercises below.";
 }
