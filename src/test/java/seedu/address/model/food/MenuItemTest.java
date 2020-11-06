c733f2e8f8e5407b6c1b5a7f68c233311b4c6b94
Lim Wei Quan, Ernest <ernestlim8@gmail.com>
2020-11-06 20:39:51 +0800

    Add tests for tag and untag command

--- src/test/java/seedu/address/model/food/MenuItemTest.java
+++ src/test/java/seedu/address/model/food/MenuItemTest.java
@@ -8,24 +8,23 @@ import static seedu.address.testutil.TypicalMenuItems.CHEESE_PRATA;
 import static seedu.address.testutil.TypicalMenuItems.PRATA;
 import static seedu.address.testutil.TypicalMenuItems.getTypicalMenuItems;
 
+import java.util.HashSet;
+
 import org.junit.jupiter.api.Test;
 
+import seedu.address.model.tag.Tag;
 import seedu.address.testutil.MenuItemBuilder;
 
 
-public class MenuItemTest {
 
-    //    @Test
-    //    public void asObservableList_modifyList_throwsUnsupportedOperationException() {
-    //        MenuItem item = new MenuItemBuilder().build();
-    //        assertThrows(UnsupportedOperationException.class, () -> item.getTags().remove(0));
-    //    }
+
+public class MenuItemTest {
 
     @Test
     public void equals() {
         getTypicalMenuItems();
         // same values -> returns true
-        Food prataCopy = new MenuItemBuilder(PRATA).build();
+        MenuItem prataCopy = new MenuItemBuilder(PRATA).build();
         assertTrue(PRATA.equals(prataCopy));
 
         // same object -> returns true
@@ -40,7 +39,7 @@ public class MenuItemTest {
         // different food -> returns false
         assertFalse(PRATA.equals(CHEESE_PRATA));
         // different name -> returns false
-        Food editedPrata = new MenuItemBuilder(PRATA).withName("Cheese Prata").build();
+        MenuItem editedPrata = new MenuItemBuilder(PRATA).withName("Cheese Prata").build();
         assertFalse(PRATA.equals(editedPrata));
 
         // different price -> returns false
@@ -64,11 +63,27 @@ public class MenuItemTest {
         // one equivalence value is 0
         assertFalse(Food.isValidPrice(0));
 
+        // Test prices less than $1.00
+        assertTrue(Food.isValidPrice(.40));
+
         assertTrue(Food.isValidPrice(2));
         assertTrue(Food.isValidPrice(2.54));
         assertTrue(Food.isValidPrice(1.2));
 
         // Price should have only 2 decimal points
         assertFalse(Food.isValidPrice(2.555));
+    }
+
+    @Test
+    public void setTags_validTags_success() {
+        MenuItem prataCopy = new MenuItemBuilder().withName("Prata").withPrice(1).build();
+        MenuItem expectedPrata = new MenuItemBuilder().withName("Prata").withPrice(1)
+                .withTags("small", "medium", "large").build();
+        HashSet<Tag> tags = new HashSet<>();
+        tags.add(new Tag("small"));
+        tags.add(new Tag("medium"));
+        tags.add(new Tag("large"));
+        prataCopy.setTags(tags);
+        assertEquals(prataCopy, expectedPrata);
     }
 }
