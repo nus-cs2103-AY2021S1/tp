36d6a834e8d69ee0e34ee6127bd18b67e066e3a6
Lee Ming De <leemingde@gmail.com>
2020-11-08 01:45:10 +0800

    Fix checkstyle error

--- src/test/java/seedu/address/logic/parser/CovigentAppParserTest.java
+++ src/test/java/seedu/address/logic/parser/CovigentAppParserTest.java
@@ -120,7 +120,8 @@ public class CovigentAppParserTest {
 
     @Test
     public void parseCommand_unrecognisedInput_throwsParseException() {
-        assertThrows(ParseException.class, String.format(MESSAGE_INVALID_COMMAND_FORMAT, HelpCommand.MESSAGE_USAGE), () -> parser.parseCommand(""));
+        assertThrows(ParseException.class, String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+            HelpCommand.MESSAGE_USAGE), () -> parser.parseCommand(""));
     }
 
     @Test
