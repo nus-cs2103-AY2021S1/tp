0205ba8085b73dcd4297c5756c08fc58988aaa1f
davidcaiqifan <e0406982@u.nus.edu>
2020-11-07 18:11:52 +0800

    Fix checkstyle

--- src/test/java/seedu/address/logic/parser/contactlistparsers/SortContactParserTest.java
+++ src/test/java/seedu/address/logic/parser/contactlistparsers/SortContactParserTest.java
@@ -1,13 +1,13 @@
 package seedu.address.logic.parser.contactlistparsers;
 
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+/*import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
-import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;*/
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.logic.commands.contactlistcommands.SortContactCommand;
-import seedu.address.model.contact.ContactComparatorByName;
+/*import seedu.address.logic.commands.contactlistcommands.SortContactCommand;
+import seedu.address.model.contact.ContactComparatorByName;*/
 
 public class SortContactParserTest {
     private SortContactParser parser = new SortContactParser();
