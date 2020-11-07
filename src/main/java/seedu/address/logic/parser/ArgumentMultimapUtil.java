36f98ab5338848bc31161fbd10379f9c533f39a7
Tian Fang <tianfangsg@gmail.com>
2020-11-07 11:44:54 +0800

    Review ArgumentMultimapUtil.java

--- src/main/java/seedu/address/logic/parser/ArgumentMultimapUtil.java
+++ src/main/java/seedu/address/logic/parser/ArgumentMultimapUtil.java
@@ -2,10 +2,16 @@ package seedu.address.logic.parser;
 
 import java.util.stream.Stream;
 
+/**
+ * Facilitates checking prefixes inside an {@code ArgumentMultimap}.
+ */
 public class ArgumentMultimapUtil {
     /**
-     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
-     * {@code ArgumentMultimap}.
+     * Returns true if all the prefixes are present in the given {@code ArgumentMultimap}.
+     *
+     * @param argumentMultimap the map containing the prefixes and the corresponding Strings.
+     * @param prefixes prefixes to check.
+     * @return true if all the prefixes are present in the given ArgumentMultimap, and false otherwise.
      */
     public static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
         return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
@@ -13,20 +19,25 @@ public class ArgumentMultimapUtil {
 
     /**
      * Returns true if only one of the given prefixes is present.
-     * @param argumentMultimap  the map containing the prefixes and the corresponding Strings
-     * @param prefixes  prefixes to check
+     *
+     * @param argumentMultimap the map containing the prefixes and the corresponding Strings.
+     * @param prefixes prefixes to check.
+     * @return true if only one of the given prefixes is present.
      */
     public static boolean isOnlyOneGivenPrefixPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
         return Stream.of(prefixes)
-            .filter(prefix -> argumentMultimap.getValue(prefix).isPresent())
-            .count() == 1;
+                .filter(prefix -> argumentMultimap.getValue(prefix).isPresent())
+                .count() == 1;
     }
 
     /**
      * Returns true if there is one and only one prefix in the argumentMultimap,
      * and that prefix is one of the given prefixes.
-     * @param argumentMultimap  the map containing the prefixes and the corresponding Strings
-     * @param prefixes  prefixes to check
+     *
+     * @param argumentMultimap the map containing the prefixes and the corresponding Strings.
+     * @param prefixes prefixes to check.
+     * @return true if there is one and only one prefix in the argumentMultimap,
+     *          and that prefix is one of the given prefixes.
      */
     public static boolean isOnlyOnePrefixPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
         if (argumentMultimap.numOfPrefixes() != 1) {
@@ -35,11 +46,12 @@ public class ArgumentMultimapUtil {
         return isOnlyOneGivenPrefixPresent(argumentMultimap, prefixes);
     }
     /**
-     * Checks if the given two prefixes are the only prefixes that are present.
-     * @param argumentMultimap  the map containing the prefixes and the corresponding Strings
-     * @param prefix1  the first prefix to check
-     * @param prefix2  the second prefix to check
-     * @return  true if the given two prefixes are the only prefixes presented, and false otherwise
+     * Returns true if the given two prefixes are the only prefixes presented.
+     *
+     * @param argumentMultimap the map containing the prefixes and the corresponding Strings.
+     * @param prefix1 the first prefix to check.
+     * @param prefix2 the second prefix to check.
+     * @return true if the given two prefixes are the only prefixes presented, and false otherwise.
      */
     public static boolean areOnlyTheseTwoPrefixesPresent(ArgumentMultimap argumentMultimap,
                                                          Prefix prefix1, Prefix prefix2) {
