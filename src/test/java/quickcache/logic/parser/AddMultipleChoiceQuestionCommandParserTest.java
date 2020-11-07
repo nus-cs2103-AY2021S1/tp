508a4d679944b19dc485de684aabb3805eb3297e
joshtyf <tan.yin.feng@hotmail.com>
2020-11-07 15:23:51 +0800

    Update test method name Replace 'Qns' with 'Questions' to prevent ambiguity

--- src/test/java/quickcache/logic/parser/AddMultipleChoiceQuestionCommandParserTest.java
+++ src/test/java/quickcache/logic/parser/AddMultipleChoiceQuestionCommandParserTest.java
@@ -40,7 +40,7 @@ public class AddMultipleChoiceQuestionCommandParserTest {
     }
 
     @Test
-    public void parse_multipleQnsPresent_failure() {
+    public void parse_multipleQuestionsPresent_failure() {
         // multiple questions - not accepted
         CommandParserTestUtil.assertParseFailure(parser,
                 QUESTION_DESC_TWO + QUESTION_DESC_ONE + ANSWER_DESC_ONE + CHOICE_DESC,
