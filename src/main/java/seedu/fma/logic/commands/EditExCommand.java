65a5a53a4099c8fa40a7b391d8a4a0f1d1d5d540
rizhaow <rizhaow@gmail.com>
2020-11-07 18:19:54 +0800

    fix: fix edit and edit ex format

--- src/main/java/seedu/fma/logic/commands/EditExCommand.java
+++ src/main/java/seedu/fma/logic/commands/EditExCommand.java
@@ -24,8 +24,8 @@ public class EditExCommand extends Command {
     public static final String COMMAND_WORD = "editex";
 
     public static final String AC_SUGGESTION = COMMAND_WORD + " <index> "
-            + PREFIX_E + "<exercise name> "
-            + PREFIX_C + "<calories>";
+            + "[" + PREFIX_E + "exercise name] "
+            + "[" + PREFIX_C + "calories]";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the details of the exercise identified "
             + "by the index number used in the displayed exercise list. "
