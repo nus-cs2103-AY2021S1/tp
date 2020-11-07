4c40e0558aee1b83dc3ab549fb21afaaeffad5ee
rizhaow <rizhaow@gmail.com>
2020-11-07 19:18:25 +0800

    fix: fix tests

--- src/test/java/seedu/fma/model/exercise/ExerciseTest.java
+++ src/test/java/seedu/fma/model/exercise/ExerciseTest.java
@@ -51,6 +51,6 @@ public class ExerciseTest {
 
     @Test
     void testToString() {
-        assertEquals("Exercise: Flying kicks\nCaloriesPerRep: 15", EXERCISE_A.toString());
+        assertEquals("Flying kicks\nCalories per rep: 15", EXERCISE_A.toString());
     }
 }
