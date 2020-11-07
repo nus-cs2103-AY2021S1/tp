b82bae5f8035fb923606a9295dbdaa64b5409fa6
michael-setia <michaelsetia55@gmail.com>
2020-11-08 05:43:50 +0800

    TaskComparatorByPriorityTest class: Increase test line coverage

--- src/test/java/seedu/address/model/task/comparator/TaskComparatorByPriorityTest.java
+++ src/test/java/seedu/address/model/task/comparator/TaskComparatorByPriorityTest.java
@@ -6,6 +6,9 @@ import static seedu.address.testutil.todolist.TypicalTasks.LAB_02;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.model.task.Task;
+import seedu.address.testutil.todolist.TaskBuilder;
+
 public class TaskComparatorByPriorityTest {
 
     private TaskComparatorByPriority taskComparatorByPriority = new TaskComparatorByPriority();
@@ -15,5 +18,15 @@ public class TaskComparatorByPriorityTest {
         assertEquals(-1, taskComparatorByPriority.compare(LAB_01, LAB_02));
         assertEquals(0, taskComparatorByPriority.compare(LAB_01, LAB_01));
         assertEquals(1, taskComparatorByPriority.compare(LAB_02, LAB_01));
+
+        Task editedLab01 = new TaskBuilder().withPriority(null).build();
+        Task editedLab02 = new TaskBuilder().withPriority(null).build();
+
+        // only one has priority
+        assertEquals(1, taskComparatorByPriority.compare(editedLab01, LAB_02));
+        assertEquals(-1, taskComparatorByPriority.compare(LAB_02, editedLab01));
+
+        // both don't have priority
+        assertEquals(0, taskComparatorByPriority.compare(editedLab01, editedLab02));
     }
 }
