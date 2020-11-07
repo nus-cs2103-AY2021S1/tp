ac18cdc4df17ae5d2fb4315ca1e3145321e41f93
michael-setia <michaelsetia55@gmail.com>
2020-11-08 05:37:24 +0800

    TaskComparatorByDateTest: Increase test line coverage

--- src/test/java/seedu/address/model/task/comparator/TaskComparatorByDateTest.java
+++ src/test/java/seedu/address/model/task/comparator/TaskComparatorByDateTest.java
@@ -1,10 +1,13 @@
 package seedu.address.model.task.comparator;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.testutil.todolist.TypicalTasks.LAB_01;
 import static seedu.address.testutil.todolist.TypicalTasks.LAB_02;
 
 import org.junit.jupiter.api.Test;
+import seedu.address.model.task.Task;
+import seedu.address.testutil.todolist.TaskBuilder;
 
 public class TaskComparatorByDateTest {
 
@@ -12,8 +15,17 @@ public class TaskComparatorByDateTest {
 
     @Test
     public void compare() {
+        // both have dates
         assertEquals(-1, taskComparatorByDate.compare(LAB_01, LAB_02));
         assertEquals(0, taskComparatorByDate.compare(LAB_01, LAB_01));
         assertEquals(1, taskComparatorByDate.compare(LAB_02, LAB_01));
+
+        // only one has date
+        Task editedLab01 = new TaskBuilder().withDate(null).build();
+        Task editedLab02 = new TaskBuilder().withDate(null).build();
+
+        assertEquals(1, taskComparatorByDate.compare(editedLab01, LAB_02));
+        assertEquals(-1, taskComparatorByDate.compare(LAB_02, editedLab01));
+        assertEquals(0, taskComparatorByDate.compare(editedLab01, editedLab02));
     }
 }
