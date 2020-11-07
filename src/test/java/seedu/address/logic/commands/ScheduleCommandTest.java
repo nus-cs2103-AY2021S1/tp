25e18071f336abebcfbb379f58906f7b62b9631f
Minh Nguyen <minh.hh.nguyen.2000@gmail.com>
2020-11-08 05:37:09 +0800

    Implements ScheduleTest.java

--- src/test/java/seedu/address/logic/commands/ScheduleCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ScheduleCommandTest.java
@@ -7,8 +7,8 @@ import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.showAssignmentAtIndex;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalAssignments.getTypicalProductiveNus;
-import static seedu.address.testutil.TypicalDeadline.EARLY_DEADLINE;
-import static seedu.address.testutil.TypicalDeadline.LATE_DEADLINE;
+import static seedu.address.testutil.TypicalTime.EARLY_TIME;
+import static seedu.address.testutil.TypicalTime.LATE_TIME;
 import static seedu.address.testutil.TypicalExpectedHours.EXPECTED_HOUR_MAX;
 import static seedu.address.testutil.TypicalExpectedHours.EXPECTED_HOUR_MIN;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_ASSIGNMENT;
@@ -53,7 +53,7 @@ public class ScheduleCommandTest {
         Assignment assignmentToSchedule = model.getFilteredAssignmentList().get(INDEX_THIRD_ASSIGNMENT.getZeroBased());
 
         ScheduleCommand scheduleCommand = new ScheduleCommand(INDEX_THIRD_ASSIGNMENT, EXPECTED_HOUR_MIN,
-                EARLY_DEADLINE, LATE_DEADLINE);
+                EARLY_TIME, LATE_TIME);
 
         String expectedMessage = String.format(ScheduleCommand.MESSAGE_SCHEDULE_ASSIGNMENT_SUCCESS,
                 assignmentToSchedule);
@@ -64,7 +64,7 @@ public class ScheduleCommandTest {
     @Test
     public void execute_validIndexUnfilteredList_overdueFail() {
         ScheduleCommand scheduleCommand = new ScheduleCommand(INDEX_FIRST_ASSIGNMENT, EXPECTED_HOUR_MIN,
-                EARLY_DEADLINE, LATE_DEADLINE);
+                EARLY_TIME, LATE_TIME);
 
         String expectedMessage = ScheduleCommand.MESSAGE_ASSIGNMENT_DUE;
 
@@ -74,7 +74,7 @@ public class ScheduleCommandTest {
     @Test
     public void execute_validIndexUnfilteredList_noScheduleFail() {
         ScheduleCommand scheduleCommand = new ScheduleCommand(INDEX_THIRD_ASSIGNMENT, EXPECTED_HOUR_MIN,
-                LATE_DEADLINE, EARLY_DEADLINE);
+                LATE_TIME, EARLY_TIME);
 
         String expectedMessage = ScheduleCommand.MESSAGE_SCHEDULE_ASSIGNMENT_FAIL;
 
@@ -85,7 +85,7 @@ public class ScheduleCommandTest {
     public void execute_invalidIndexUnfilteredList_throwsCommandException() {
         Index outOfBoundIndex = Index.fromOneBased(model.getFilteredAssignmentList().size() + 1);
         ScheduleCommand scheduleCommand = new ScheduleCommand(outOfBoundIndex, EXPECTED_HOUR_MIN,
-                EARLY_DEADLINE, LATE_DEADLINE);
+                EARLY_TIME, LATE_TIME);
         assertCommandFailure(scheduleCommand, model, Messages.MESSAGE_INVALID_ASSIGNMENT_DISPLAYED_INDEX);
     }
 
@@ -95,7 +95,7 @@ public class ScheduleCommandTest {
 
         Assignment assignmentToSchedule = model.getFilteredAssignmentList().get(INDEX_FIRST_ASSIGNMENT.getZeroBased());
         ScheduleCommand scheduleCommand = new ScheduleCommand(INDEX_FIRST_ASSIGNMENT, EXPECTED_HOUR_MIN,
-                EARLY_DEADLINE, LATE_DEADLINE);
+                EARLY_TIME, LATE_TIME);
 
         String expectedMessage = String.format(ScheduleCommand.MESSAGE_SCHEDULE_ASSIGNMENT_SUCCESS,
                 assignmentToSchedule);
@@ -109,7 +109,7 @@ public class ScheduleCommandTest {
         showAssignmentAtIndex(model, INDEX_FIRST_ASSIGNMENT);
 
         ScheduleCommand scheduleCommand = new ScheduleCommand(INDEX_FIRST_ASSIGNMENT, EXPECTED_HOUR_MIN,
-                EARLY_DEADLINE, LATE_DEADLINE);
+                EARLY_TIME, LATE_TIME);
 
         String expectedMessage = ScheduleCommand.MESSAGE_ASSIGNMENT_DUE;
 
@@ -121,7 +121,7 @@ public class ScheduleCommandTest {
         showAssignmentAtIndex(model, INDEX_THIRD_ASSIGNMENT);
 
         ScheduleCommand scheduleCommand = new ScheduleCommand(INDEX_FIRST_ASSIGNMENT, EXPECTED_HOUR_MIN,
-                LATE_DEADLINE, EARLY_DEADLINE);
+                LATE_TIME, EARLY_TIME);
 
         String expectedMessage = ScheduleCommand.MESSAGE_SCHEDULE_ASSIGNMENT_FAIL;
 
@@ -138,23 +138,23 @@ public class ScheduleCommandTest {
         assertTrue(outOfBoundIndex.getZeroBased() < model.getProductiveNus().getAssignmentList().size());
 
         ScheduleCommand scheduleCommand = new ScheduleCommand(outOfBoundIndex, EXPECTED_HOUR_MIN,
-                EARLY_DEADLINE, LATE_DEADLINE);
+                EARLY_TIME, LATE_TIME);
         assertCommandFailure(scheduleCommand, model, Messages.MESSAGE_INVALID_ASSIGNMENT_DISPLAYED_INDEX);
     }
 
     @Test
     public void equals() {
         ScheduleCommand scheduleFirstCommand = new ScheduleCommand(INDEX_FIRST_ASSIGNMENT, EXPECTED_HOUR_MIN,
-                EARLY_DEADLINE, LATE_DEADLINE);
+                EARLY_TIME, LATE_TIME);
         ScheduleCommand scheduleSecondCommand = new ScheduleCommand(INDEX_SECOND_ASSIGNMENT, EXPECTED_HOUR_MAX,
-                EARLY_DEADLINE, LATE_DEADLINE);
+                EARLY_TIME, LATE_TIME);
 
         // same object -> returns true
         assertTrue(scheduleFirstCommand.equals(scheduleFirstCommand));
 
         // same values -> returns true
         ScheduleCommand scheduleFirstCommandCopy = new ScheduleCommand(INDEX_FIRST_ASSIGNMENT, EXPECTED_HOUR_MIN,
-                EARLY_DEADLINE, LATE_DEADLINE);
+                EARLY_TIME, LATE_TIME);
         assertTrue(scheduleFirstCommand.equals(scheduleFirstCommandCopy));
 
         // different types -> returns false
---
+++ src/test/java/seedu/address/testutil/TypicalTime.java
@@ -0,0 +1,8 @@
+package seedu.address.testutil;
+
+import seedu.address.model.task.Time;
+
+public class TypicalTime {
+    public static final Time EARLY_TIME = new Time("01-01-2020 0000");
+    public static final Time LATE_TIME = new Time("30-12-2020 2359");
+}
