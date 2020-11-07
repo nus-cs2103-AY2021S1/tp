bdeacbf9404a2aaf72c127cbe580a80e62d51a6b
Jay <Nopenotj@gmail.com>
2020-11-07 16:04:56 +0800

    Add more tests for FzfModule

--- src/test/java/seedu/address/logic/textfieldmodules/FzfModuleTest.java
+++ src/test/java/seedu/address/logic/textfieldmodules/FzfModuleTest.java
@@ -1,10 +1,9 @@
 package seedu.address.logic.textfieldmodules;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import java.util.Arrays;
 import java.util.List;
 
+import javafx.scene.control.ContextMenu;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.testfx.api.FxRobot;
@@ -16,6 +15,8 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(ApplicationExtension.class)
 class FzfModuleTest extends HeadlessTestBase {
@@ -34,7 +35,19 @@ class FzfModuleTest extends HeadlessTestBase {
         stage.show();
 
     }
-
+    @Test
+    public void attachTo_nullTextField_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> FzfModule.attachTo(null, () -> sampleNameList));
+    }
+    @Test
+    public void attachTo_nullListSupplier_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> FzfModule.attachTo(textField, null));
+    }
+    @Test
+    public void textFieldWithFzfModule_triggerFzf_showFzfMenu(FxRobot robot) {
+        robot.press(KeyCode.CONTROL, KeyCode.SPACE);
+        assertTrue(robot.listWindows().stream().anyMatch(win -> win instanceof ContextMenu));
+    }
     @Test
     public void textFieldWithFzfModule_lockInFirstSuggestion_success(FxRobot robot) {
 
@@ -47,7 +60,24 @@ class FzfModuleTest extends HeadlessTestBase {
 
         assertEquals(expected, actual);
     }
+    @Test
+    public void textFieldWithFzfModule_navigateAndLockInLastSuggestion_success(FxRobot robot) {
+        robot.press(KeyCode.CONTROL, KeyCode.SPACE);
 
+        // navigate down the list to last element
+        for(int i = 0; i < sampleNameList.size() - 1; i ++) {
+            robot.press(KeyCode.DOWN);
+            robot.release(KeyCode.DOWN);
+        }
+
+        robot.press(KeyCode.ENTER)
+                .release(KeyCode.ENTER);
+
+        String expected = sampleNameList.get(sampleNameList.size() - 1);
+        String actual = textField.getText();
+
+        assertEquals(expected, actual);
+    }
     @Test
     public void textFieldWithFzfModule_nonEmptyQuery_validNumberOfSuggestions(FxRobot robot) {
 
@@ -62,4 +92,26 @@ class FzfModuleTest extends HeadlessTestBase {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void textFieldWithFzfModule_exitFzfMode_hideFzfMenu(FxRobot robot) {
+        robot.press(KeyCode.CONTROL, KeyCode.SPACE)
+                .press(KeyCode.ESCAPE);
+        assertTrue(robot.listWindows().stream().allMatch(win -> !(win instanceof ContextMenu)));
+    }
+    @Test
+    public void textFieldWithFzfModule_queryHasNoValidSuggestions_hideFzfMenu(FxRobot robot) {
+        String query = "Debra";
+
+        robot.press(KeyCode.CONTROL, KeyCode.SPACE)
+                .write(query);
+        robot.release(new KeyCode[]{});
+
+        long expected = sampleNameList.stream().filter(x -> x.toLowerCase().contains(query)).count();
+        long actual = fzfModule.getMenu().getItems().stream().count();
+
+        assertEquals(expected, actual);
+        assertTrue(robot.listWindows().stream().allMatch(win -> !(win instanceof ContextMenu)));
+    }
+
 }
