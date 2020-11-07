c16b81419851bb2236fe50d5c1b9112eaec4ab7f
michael-setia <michaelsetia55@gmail.com>
2020-11-08 05:16:55 +0800

    Update TodoListTest class

--- src/test/java/seedu/address/model/TodoListTest.java
+++ src/test/java/seedu/address/model/TodoListTest.java
@@ -23,76 +23,93 @@ import seedu.address.testutil.todolist.TaskBuilder;
 
 public class TodoListTest {
 
-    private final TodoList addressBook = new TodoList();
+    private final TodoList todoList = new TodoList();
 
     @Test
     public void constructor() {
-        assertEquals(Collections.emptyList(), addressBook.getTodoList());
+        assertEquals(Collections.emptyList(), todoList.getTodoList());
     }
 
     @Test
     public void resetData_null_throwsNullPointerException() {
-        assertThrows(NullPointerException.class, () -> addressBook.resetData(null));
+        assertThrows(NullPointerException.class, () -> todoList.resetData(null));
     }
 
     @Test
     public void resetData_withValidReadOnlyTodoList_replacesData() {
         TodoList newData = getTypicalTodoList();
-        addressBook.resetData(newData);
-        assertEquals(newData, addressBook);
+        todoList.resetData(newData);
+        assertEquals(newData, todoList);
     }
 
     @Test
     public void resetData_withDuplicateTasks_throwsDuplicateTaskException() {
-        // Two persons with the same name
+        // Two tasks with the same name
         Task editedLab01 = new TaskBuilder(LAB_01).withStatus(VALID_STATUS_COMPLETED).build();
         List<Task> newTasks = Arrays.asList(LAB_01, editedLab01);
         TodoListStub newData = new TodoListStub(newTasks);
 
-        assertThrows(DuplicateTaskException.class, () -> addressBook.resetData(newData));
+        assertThrows(DuplicateTaskException.class, () -> todoList.resetData(newData));
+    }
+
+    @Test
+    public void addTask() {
+        assertThrows(NullPointerException.class, () -> todoList.addTask(null));
+    }
+
+    @Test
+    public void setTask() {
+        assertThrows(NullPointerException.class, () -> todoList.setTask(LAB_01, null));
+        assertThrows(NullPointerException.class, () -> todoList.setTask(null, LAB_01));
+        assertThrows(NullPointerException.class, () -> todoList.setTask(null, null));
+    }
+
+    @Test
+    public void removeTask() {
+        assertThrows(NullPointerException.class, () -> todoList.removeTask(null));
     }
 
     @Test
     public void hasTask_nullTask_throwsNullPointerException() {
-        assertThrows(NullPointerException.class, () -> addressBook.hasTask(null));
+        assertThrows(NullPointerException.class, () -> todoList.hasTask(null));
     }
 
     @Test
-    public void hasTask_personNotInTodoList_returnsFalse() {
-        assertFalse(addressBook.hasTask(LAB_01));
+    public void hasTask_taskNotInTodoList_returnsFalse() {
+        assertFalse(todoList.hasTask(LAB_01));
     }
 
     @Test
-    public void hasTask_personInTodoList_returnsTrue() {
-        addressBook.addTask(LAB_01);
-        assertTrue(addressBook.hasTask(LAB_01));
+    public void hasTask_taskInTodoList_returnsTrue() {
+        todoList.addTask(LAB_01);
+        assertTrue(todoList.hasTask(LAB_01));
     }
 
     @Test
-    public void hasTask_personWithSameNameInTodoList_returnsTrue() {
-        addressBook.addTask(LAB_01);
+    public void hasTask_taskWithSameNameInTodoList_returnsTrue() {
+        todoList.addTask(LAB_01);
         Task editedLab01 = new TaskBuilder(LAB_01).withStatus(VALID_STATUS_COMPLETED).build();
-        assertTrue(addressBook.hasTask(editedLab01));
+        assertTrue(todoList.hasTask(editedLab01));
     }
 
     @Test
     public void getTodoList_modifyList_throwsUnsupportedOperationException() {
-        assertThrows(UnsupportedOperationException.class, () -> addressBook.getTodoList().remove(0));
+        assertThrows(UnsupportedOperationException.class, () -> todoList.getTodoList().remove(0));
     }
 
     /**
-     * A stub ReadOnlyTodoList whose persons list can violate interface constraints.
+     * A stub ReadOnlyTodoList whose tasks list can violate interface constraints.
      */
     private static class TodoListStub implements ReadOnlyTodoList {
-        private final ObservableList<Task> persons = FXCollections.observableArrayList();
+        private final ObservableList<Task> tasks = FXCollections.observableArrayList();
 
-        TodoListStub(Collection<Task> persons) {
-            this.persons.setAll(persons);
+        TodoListStub(Collection<Task> tasks) {
+            this.tasks.setAll(tasks);
         }
 
         @Override
         public ObservableList<Task> getTodoList() {
-            return persons;
+            return tasks;
         }
     }
 
