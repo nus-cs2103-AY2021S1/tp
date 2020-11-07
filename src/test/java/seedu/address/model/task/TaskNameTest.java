d60259ac670801242011da93c0ebae378307c312
michael-setia <michaelsetia55@gmail.com>
2020-11-08 02:33:02 +0800

    Add TaskNameTest class

---
+++ src/test/java/seedu/address/model/task/TaskNameTest.java
@@ -0,0 +1,59 @@
+package seedu.address.model.task;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_LAB05;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_LAB07;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class TaskNameTest {
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new TaskName(null));
+    }
+
+    @Test
+    public void constructor_invalidTaskName_throwsIllegalArgumentException() {
+        String invalidTaskName = "";
+        assertThrows(IllegalArgumentException.class, () -> new TaskName(invalidTaskName));
+    }
+
+    @Test
+    public void isValidTaskName() {
+        // null name
+        assertThrows(NullPointerException.class, () -> TaskName.isValidTaskName(null));
+
+        // invalid name
+        assertFalse(TaskName.isValidTaskName("")); // empty string
+        assertFalse(TaskName.isValidTaskName(" ")); // spaces only
+        assertFalse(TaskName.isValidTaskName("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")); // boundary value (length = 31)
+
+        // valid name
+        assertTrue(TaskName.isValidTaskName("Laundry")); // single word
+        assertTrue(TaskName.isValidTaskName("Finish Lab01")); // with whitespace
+        assertTrue(TaskName.isValidTaskName("ASSIGNMENT01")); // all uppercase
+        assertTrue(TaskName.isValidTaskName("Prepare team meeting")); // 3 words
+        assertTrue(TaskName.isValidTaskName("Review Chapter8 for final exam")); // boundary value (length = 30)
+    }
+
+    @Test
+    public void equals() {
+        TaskName first = new TaskName(VALID_NAME_LAB05);
+        TaskName second = new TaskName(VALID_NAME_LAB07);
+
+        // null value -> false
+        assertFalse(first.equals(null));
+
+        // other type -> false
+        assertFalse(first.equals(10));
+
+        // same task name -> true
+        assertTrue(first.equals(first));
+
+        // different task name -> false
+        assertFalse(first.equals(second));
+    }
+}
