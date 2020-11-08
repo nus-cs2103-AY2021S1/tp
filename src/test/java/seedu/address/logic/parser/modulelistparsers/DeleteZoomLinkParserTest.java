a2cbc7544098c17856fe2e204c1fa9353c2c3bc8
jonasngs <jonasngs30@gmail.com>
2020-11-08 13:32:06 +0800

    Implement junit tests for AddZoomLinkParser class

---
+++ src/test/java/seedu/address/logic/parser/modulelistparsers/DeleteZoomLinkParserTest.java
@@ -0,0 +1,12 @@
+package seedu.address.logic.parser.modulelistparsers;
+
+
+import org.junit.jupiter.api.Test;
+
+public class DeleteZoomLinkParserTest {
+
+    private DeleteZoomLinkParser parser = new DeleteZoomLinkParser();
+
+    @Test
+
+}
