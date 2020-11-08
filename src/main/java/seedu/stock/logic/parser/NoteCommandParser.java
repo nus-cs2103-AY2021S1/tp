37aa8372eac0720d9e08bf837b597409e736fad3
amylzting <amylimzting@gmail.com>
2020-11-08 21:11:51 +0800

    Do abstraction for some parts of code

--- src/main/java/seedu/stock/logic/parser/NoteCommandParser.java
+++ src/main/java/seedu/stock/logic/parser/NoteCommandParser.java
@@ -29,14 +29,14 @@ public class NoteCommandParser implements Parser<NoteCommand> {
         ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, allPossiblePrefixes);
 
         // Check if command format is correct
-        if (!areAllPrefixesPresent(argMultimap, validPrefixesForNote)
+        if (!ParserUtil.areAllPrefixesPresent(argMultimap, validPrefixesForNote)
                 || ParserUtil.isInvalidPrefixPresent(argMultimap, validPrefixesForNote)
                 || !argMultimap.getPreamble().isEmpty()) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
                     NoteCommand.MESSAGE_USAGE));
         }
 
-        if (isDuplicatePrefixPresent(argMultimap, validPrefixesForNote)) {
+        if (ParserUtil.isDuplicatePrefixPresent(argMultimap, validPrefixesForNote)) {
             throw new ParseException(String.format(MESSAGE_DUPLICATE_HEADER_FIELD,
                     NoteCommand.MESSAGE_USAGE));
         }
@@ -49,34 +49,6 @@ public class NoteCommandParser implements Parser<NoteCommand> {
 
         logger.log(Level.INFO, "Finished parsing note command successfully");
         return new NoteCommand(serialNumber, noteToAdd);
-    }
-
-    /**
-     * Returns true if all prefixes specified does not contain an empty {@code Optional} value
-     * in the given {@code ArgumentMultimap}.
-     * @param argumentMultimap map of prefix to keywords entered by user
-     * @param prefixes prefixes to parse
-     * @return boolean true if all prefixes specified is present
-     */
-    private static boolean areAllPrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
-        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
-    }
-
-    /**
-     * Returns true if duplicate prefixes are present when parsing command.
-     * @param argumentMultimap map of prefix to keywords entered by user
-     * @param prefixes prefixes to parse
-     * @return boolean true if duplicate prefix is present
-     */
-    private static boolean isDuplicatePrefixPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
-
-        // Check for duplicate prefixes
-        for (Prefix prefix: prefixes) {
-            if (argumentMultimap.getAllValues(prefix).size() >= 2) {
-                return true;
-            }
-        }
-        return false;
     }
 
 }
