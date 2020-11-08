1612ae1cc41227a7e4d2bd8fbe973776c4b389ee
Nahoyhp <phyohan@hotmail.com>
2020-11-08 16:25:06 +0800

    Update Clear Command Test Cases.

--- src/test/java/seedu/address/logic/commands/ClearCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ClearCommandTest.java
@@ -3,17 +3,28 @@ package seedu.address.logic.commands;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalExercise.getTypicalExerciseBook;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.junit.jupiter.api.Test;
 
 import seedu.address.model.ExerciseBook;
 import seedu.address.model.ExerciseModel;
 import seedu.address.model.ExerciseModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.exercise.Template;
 
 public class ClearCommandTest {
-
     @Test
     public void execute_emptyExerciseBook_success() {
+        File testFile = new File(Template.getFilePath());
+        if (!testFile.exists()) {
+            try {
+                testFile.createNewFile();
+            } catch (IOException err) {
+                throw new AssertionError("Problem creating file");
+            }
+        }
         ExerciseModel model = new ExerciseModelManager();
         ExerciseModel expectedModel = new ExerciseModelManager();
 
@@ -22,6 +33,14 @@ public class ClearCommandTest {
 
     @Test
     public void execute_nonEmptyExerciseBook_success() {
+        File testFile = new File(Template.getFilePath());
+        if (!testFile.exists()) {
+            try {
+                testFile.createNewFile();
+            } catch (IOException err) {
+                throw new AssertionError("Problem creating file");
+            }
+        }
         ExerciseModel model = new ExerciseModelManager(getTypicalExerciseBook(), new UserPrefs());
         ExerciseModel expectedModel = new ExerciseModelManager(getTypicalExerciseBook(), new UserPrefs());
         expectedModel.setExerciseBook(new ExerciseBook());
