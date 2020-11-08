f9c849d7404af03c6927ff60921d6dfb973d50e7
e0316059 <e0316059@u.nus.edu>
2020-11-08 23:18:23 +0800

    add undo command test, redo command test

--- src/test/java/seedu/schedar/logic/commands/RedoCommandTest.java
+++ src/test/java/seedu/schedar/logic/commands/RedoCommandTest.java
@@ -13,7 +13,7 @@ import seedu.schedar.model.Model;
 import seedu.schedar.model.ModelManager;
 import seedu.schedar.model.UserPrefs;
 
-public class UndoCommandTest {
+public class RedoCommandTest {
 
     private final Model model = new ModelManager(getTypicalTaskManager(), new UserPrefs());
     private final Model expectedModel = new ModelManager(getTypicalTaskManager(), new UserPrefs());
@@ -21,27 +21,29 @@ public class UndoCommandTest {
 
     @BeforeEach
     public void setUp() {
-        // set up of models' undo/redo history
+        // set up of both models' undo/redo history
         deleteFirstTask(model);
         deleteFirstTask(model);
+        model.undoTaskManager();
+        model.undoTaskManager();
 
         deleteFirstTask(expectedModel);
         deleteFirstTask(expectedModel);
+        expectedModel.undoTaskManager();
+        expectedModel.undoTaskManager();
     }
 
     @Test
     public void execute() {
-        // multiple undoable states in model
-        expectedModel.undoTaskManager();
-        assertCommandSuccess(new UndoCommand(), model, commandHistory, UndoCommand.MESSAGE_SUCCESS, expectedModel);
+        // multiple redoable states in model
+        expectedModel.redoTaskManager();
+        assertCommandSuccess(new RedoCommand(), model, commandHistory, RedoCommand.MESSAGE_SUCCESS, expectedModel);
 
-        // single undoable state in model
-        expectedModel.undoTaskManager();
-        assertCommandSuccess(new UndoCommand(), model, commandHistory, UndoCommand.MESSAGE_SUCCESS, expectedModel);
+        // single redoable state in model
+        expectedModel.redoTaskManager();
+        assertCommandSuccess(new RedoCommand(), model, commandHistory, RedoCommand.MESSAGE_SUCCESS, expectedModel);
 
-        // no undoable states in model
-        assertCommandFailure(new UndoCommand(), model, commandHistory, UndoCommand.MESSAGE_FAILURE);
+        // no redoable state in model
+        assertCommandFailure(new RedoCommand(), model, commandHistory, RedoCommand.MESSAGE_FAILURE);
     }
 }
-
-
