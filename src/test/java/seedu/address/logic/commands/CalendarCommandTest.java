7cc6d7efb8240505a85cc93dc77de7fea30ce137
nuovonatura <60500235+nuovonatura@users.noreply.github.com>
2020-11-09 10:15:17 +0800

    Change default app window size

---
+++ src/test/java/seedu/address/logic/commands/CalendarCommandTest.java
@@ -0,0 +1,32 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalTasks.getTypicalPlanus;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.Planus;
+import seedu.address.model.UserPrefs;
+
+public class CalendarCommandTest {
+
+    @Test
+    public void execute_emptyPlanus_success() {
+        Model model = new ModelManager();
+        Model expectedModel = new ModelManager();
+
+        assertCommandSuccess(new CalendarCommand(), model, CalendarCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    @Test
+    public void execute_nonEmptyPlanus_success() {
+        Model model = new ModelManager(getTypicalPlanus(), new UserPrefs());
+        Model expectedModel = new ModelManager(getTypicalPlanus(), new UserPrefs());
+        expectedModel.setPlanus(new Planus());
+
+        assertCommandSuccess(new CalendarCommand(), model, CalendarCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+}
