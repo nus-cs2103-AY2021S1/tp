56dabf5558a447a13a773652aa36444cd5b67880
csiongn <>
2020-11-09 17:29:13 +0800

    Fix sort command message

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -10,7 +10,7 @@ public class Messages {
     public static final String MESSAGE_INVALID_STUDENT_DISPLAYED_INDEX = "There is no student at the given index!";
     public static final String MESSAGE_STUDENTS_LISTED_OVERVIEW = "%1$d students listed!";
     public static final String MESSAGE_CLASHING_LESSON = "You have another lesson that clashes with this lesson!";
-    public static final String MESSAGE_STUDENTS_SORTED = "Students sorted by %1%s!";
+    public static final String MESSAGE_STUDENTS_SORTED = "Students sorted by %1$s!";
 
     public static final String MESSAGE_INVALID_NOTE_DISPLAYED_INDEX = "There is no note at the given index!";
 }
