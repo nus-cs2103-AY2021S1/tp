9ce9859e16ecdd907f9b773e0fb46b3001a39456
nicholasyeo <65457870+nicholasyeo@users.noreply.github.com>
2020-11-09 22:37:04 +0800

    Add sample data

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -1,12 +1,15 @@
 package seedu.address.model.util;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import seedu.address.model.FitNus;
 import seedu.address.model.ReadOnlyFitNus;
+import seedu.address.model.exercise.Exercise;
 import seedu.address.model.lesson.Lesson;
+import seedu.address.model.routine.Routine;
 import seedu.address.model.tag.Tag;
 
 /**
@@ -15,12 +18,28 @@ import seedu.address.model.tag.Tag;
 public class SampleDataUtil {
     public static Lesson[] getSampleLessons() {
         return new Lesson[] {
-            new Lesson(new Name("GER1000"), getTagSet("easy")),
-            new Lesson(new Name("GEQ2000"), getTagSet("chill", "webcasted")),
-            new Lesson(new Name("GEH3000"), getTagSet("hard")),
-            new Lesson(new Name("GET4000"), getTagSet("priority")),
-            new Lesson(new Name("GES5000"), getTagSet("fun")),
-            new Lesson(new Name("GEX6000"), getTagSet("hard"))
+            new Lesson(new Name("MA1101R"), getTagSet("easy")),
+            new Lesson(new Name("MA1102R"), getTagSet("chill", "webcast")),
+            new Lesson(new Name("CS2103T"), getTagSet("project")),
+            new Lesson(new Name("CS2100"), getTagSet("priority")),
+            new Lesson(new Name("CS3243"), getTagSet("fun")),
+            new Lesson(new Name("CS3244"), getTagSet("hard"))
+        };
+    }
+
+    public static Routine[] getSampleRoutines() {
+        return new Routine[] {
+                new Routine(new Name("Leg Day")),
+                new Routine(new Name("Upper Body Workout")),
+                new Routine(new Name("Arm Routine"))
+        };
+    }
+
+    public static Exercise[] getSampleExercise() {
+        return new Exercise[] {
+                new Exercise(new Name("Bench Press"), new HashSet<>()),
+                new Exercise(new Name("Shoulder Press"), new HashSet<>()),
+                new Exercise(new Name("Squats"), new HashSet<>())
         };
     }
 
@@ -29,6 +48,15 @@ public class SampleDataUtil {
         for (Lesson sampleLesson : getSampleLessons()) {
             sampleFitNus.addLesson(sampleLesson);
         }
+
+        for (Exercise sampleExercise : getSampleExercise()) {
+            sampleFitNus.addExercise(sampleExercise);
+        }
+
+        for (Routine sampleRoutine : getSampleRoutines()) {
+            sampleFitNus.addRoutine(sampleRoutine);
+        }
+
         return sampleFitNus;
     }
 
