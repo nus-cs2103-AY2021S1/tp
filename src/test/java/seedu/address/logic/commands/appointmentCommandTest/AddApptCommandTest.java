16cd3c80e91e4f21daff7288414126fd9e4ce518
GabrielTeo <gabrieltyx@gmail.com>
2020-11-09 21:06:56 +0800

    Add test cases for Appt commands

--- src/test/java/seedu/address/logic/commands/appointmentCommandTest/AddApptCommandTest.java
+++ src/test/java/seedu/address/logic/commands/appointmentCommandTest/AddApptCommandTest.java
@@ -2,6 +2,10 @@ package seedu.address.logic.commands.appointmentCommandTest;
 
 import static seedu.address.testutil.Assert.assertThrows;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.appointmentcommand.AddApptCommand;
@@ -15,5 +19,93 @@ public class AddApptCommandTest {
         assertThrows(NullPointerException.class, () -> new AddApptCommand(null, null));
         assertThrows(NullPointerException.class, () -> new AddApptCommand(null, new Appointment()));
         assertThrows(NullPointerException.class, () -> new AddApptCommand(new Nric("S0000001A"), null));
+    }
+
+    @Test
+    public void constructor_invalidNric_throwsIllegalArgumentException() {
+        // NRIC missing 1 digit
+        assertThrows(IllegalArgumentException.class, () -> new AddApptCommand(
+                new Nric("S123456A"), new Appointment()));
+
+        // NRIC missing 5 digits
+        assertThrows(IllegalArgumentException.class, () -> new AddApptCommand(
+                new Nric("S12A"), new Appointment()));
+
+        // NRIC no digits
+        assertThrows(IllegalArgumentException.class, () -> new AddApptCommand(
+                new Nric("SA"), new Appointment()));
+
+        // NRIC extra digit
+        assertThrows(IllegalArgumentException.class, () -> new AddApptCommand(
+                new Nric("S12345678A"), new Appointment()));
+
+        // NRIC extra 5 digits
+        assertThrows(IllegalArgumentException.class, () -> new AddApptCommand(
+                new Nric("S123456789012A"), new Appointment()));
+    }
+
+    @Test
+    public void constructor_invalidAppointmentTime_throwsDateTimeParseException() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy HH:mm");
+
+        // invalid format
+        assertThrows(DateTimeParseException.class, () -> new AddApptCommand(
+                new Nric("S1234567A"), new Appointment("", LocalDateTime.parse("1-12-2020", formatter))));
+
+        // missing time
+        assertThrows(DateTimeParseException.class, () -> new AddApptCommand(
+                new Nric("S1234567A"), new Appointment("", LocalDateTime.parse("10/12/2020", formatter))));
+
+        // missing date
+        assertThrows(DateTimeParseException.class, () -> new AddApptCommand(
+                new Nric("S1234567A"), new Appointment("", LocalDateTime.parse("18:00", formatter))));
+
+        // missing date and time
+        assertThrows(DateTimeParseException.class, () -> new AddApptCommand(
+                new Nric("S1234567A"), new Appointment("", LocalDateTime.parse("", formatter))));
+
+        // invalid day
+        assertThrows(DateTimeParseException.class, () -> new AddApptCommand(
+                new Nric("S1234567A"), new Appointment("", LocalDateTime.parse("32/12/2020", formatter))));
+
+        // invalid day
+        assertThrows(DateTimeParseException.class, () -> new AddApptCommand(
+                new Nric("S1234567A"), new Appointment("", LocalDateTime.parse("50/20/2020", formatter))));
+
+        // invalid day
+        assertThrows(DateTimeParseException.class, () -> new AddApptCommand(
+                new Nric("S1234567A"), new Appointment("", LocalDateTime.parse("-1/20/2020", formatter))));
+
+        // invalid month
+        assertThrows(DateTimeParseException.class, () -> new AddApptCommand(
+                new Nric("S1234567A"), new Appointment("", LocalDateTime.parse("1/JAN/2020", formatter))));
+
+        // invalid month
+        assertThrows(DateTimeParseException.class, () -> new AddApptCommand(
+                new Nric("S1234567A"), new Appointment("", LocalDateTime.parse("10/13/2020", formatter))));
+
+        // invalid month
+        assertThrows(DateTimeParseException.class, () -> new AddApptCommand(
+                new Nric("S1234567A"), new Appointment("", LocalDateTime.parse("10/20/2020", formatter))));
+
+        // invalid time
+        assertThrows(DateTimeParseException.class, () -> new AddApptCommand(
+                new Nric("S1234567A"), new Appointment("", LocalDateTime.parse("10/12/2020 23:60", formatter))));
+
+        // invalid time
+        assertThrows(DateTimeParseException.class, () -> new AddApptCommand(
+                new Nric("S1234567A"), new Appointment("", LocalDateTime.parse("10/12/2020 24:01", formatter))));
+
+        // invalid time
+        assertThrows(DateTimeParseException.class, () -> new AddApptCommand(
+                new Nric("S1234567A"), new Appointment("", LocalDateTime.parse("10/12/2020 24:59", formatter))));
+
+        // invalid time
+        assertThrows(DateTimeParseException.class, () -> new AddApptCommand(
+                new Nric("S1234567A"), new Appointment("", LocalDateTime.parse("10/12/2020 2359", formatter))));
+
+        // invalid time
+        assertThrows(DateTimeParseException.class, () -> new AddApptCommand(
+                new Nric("S1234567A"), new Appointment("", LocalDateTime.parse("10/12/2020 233:59", formatter))));
     }
 }
--- src/test/java/seedu/address/logic/commands/appointmentCommandTest/EditApptCommandTest.java
+++ src/test/java/seedu/address/logic/commands/appointmentCommandTest/EditApptCommandTest.java
@@ -2,6 +2,10 @@ package seedu.address.logic.commands.appointmentCommandTest;
 
 import static seedu.address.testutil.Assert.assertThrows;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.appointmentcommand.EditApptCommand;
@@ -15,5 +19,123 @@ public class EditApptCommandTest {
         assertThrows(NullPointerException.class, () -> new EditApptCommand(null, null, null));
         assertThrows(NullPointerException.class, () -> new EditApptCommand(null, new Appointment(), new Appointment()));
         assertThrows(NullPointerException.class, () -> new EditApptCommand(new Nric("S0000001A"), null, null));
+    }
+
+    @Test
+    public void constructor_invalidNric_throwsIllegalArgumentException() {
+        // NRIC missing 1 digit
+        assertThrows(IllegalArgumentException.class, () -> new EditApptCommand(
+                new Nric("S123456A"), new Appointment(), new Appointment()));
+
+        // NRIC missing 5 digits
+        assertThrows(IllegalArgumentException.class, () -> new EditApptCommand(
+                new Nric("S12A"), new Appointment(), new Appointment()));
+
+        // NRIC no digits
+        assertThrows(IllegalArgumentException.class, () -> new EditApptCommand(
+                new Nric("SA"), new Appointment(), new Appointment()));
+
+        // NRIC extra digit
+        assertThrows(IllegalArgumentException.class, () -> new EditApptCommand(
+                new Nric("S12345678A"), new Appointment(), new Appointment()));
+
+        // NRIC extra 5 digits
+        assertThrows(IllegalArgumentException.class, () -> new EditApptCommand(
+                new Nric("S123456789012A"), new Appointment(), new Appointment()));
+    }
+
+    @Test
+    public void constructor_invalidAppointmentTime_throwsDateTimeParseException() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy HH:mm");
+
+        // invalid format
+        assertThrows(DateTimeParseException.class, () -> new EditApptCommand(
+                new Nric("S1234567A"),
+                new Appointment("", LocalDateTime.parse("1-12-2020", formatter)),
+                new Appointment()));
+
+        // missing time
+        assertThrows(DateTimeParseException.class, () -> new EditApptCommand(
+                new Nric("S1234567A"),
+                new Appointment("", LocalDateTime.parse("10/12/2020", formatter)),
+                new Appointment()));
+
+        // missing date
+        assertThrows(DateTimeParseException.class, () -> new EditApptCommand(
+                new Nric("S1234567A"),
+                new Appointment("", LocalDateTime.parse("18:00", formatter)),
+                new Appointment()));
+
+        // missing date and time
+        assertThrows(DateTimeParseException.class, () -> new EditApptCommand(
+                new Nric("S1234567A"),
+                new Appointment("", LocalDateTime.parse("", formatter)),
+                new Appointment()));
+
+        // invalid day
+        assertThrows(DateTimeParseException.class, () -> new EditApptCommand(
+                new Nric("S1234567A"),
+                new Appointment("", LocalDateTime.parse("32/12/2020", formatter)),
+                new Appointment()));
+
+        // invalid day
+        assertThrows(DateTimeParseException.class, () -> new EditApptCommand(
+                new Nric("S1234567A"),
+                new Appointment("", LocalDateTime.parse("50/20/2020", formatter)),
+                new Appointment()));
+
+        // invalid day
+        assertThrows(DateTimeParseException.class, () -> new EditApptCommand(
+                new Nric("S1234567A"),
+                new Appointment("", LocalDateTime.parse("-1/20/2020", formatter)),
+                new Appointment()));
+
+        // invalid month
+        assertThrows(DateTimeParseException.class, () -> new EditApptCommand(
+                new Nric("S1234567A"),
+                new Appointment(),
+                new Appointment("", LocalDateTime.parse("1/JAN/2020", formatter))));
+
+        // invalid month
+        assertThrows(DateTimeParseException.class, () -> new EditApptCommand(
+                new Nric("S1234567A"),
+                new Appointment(),
+                new Appointment("", LocalDateTime.parse("10/13/2020", formatter))));
+
+        // invalid month
+        assertThrows(DateTimeParseException.class, () -> new EditApptCommand(
+                new Nric("S1234567A"),
+                new Appointment(),
+                new Appointment("", LocalDateTime.parse("10/20/2020", formatter))));
+
+        // invalid time
+        assertThrows(DateTimeParseException.class, () -> new EditApptCommand(
+                new Nric("S1234567A"),
+                new Appointment(),
+                new Appointment("", LocalDateTime.parse("10/12/2020 23:60", formatter))));
+
+        // invalid time
+        assertThrows(DateTimeParseException.class, () -> new EditApptCommand(
+                new Nric("S1234567A"),
+                new Appointment(),
+                new Appointment("", LocalDateTime.parse("10/12/2020 24:01", formatter))));
+
+        // invalid time
+        assertThrows(DateTimeParseException.class, () -> new EditApptCommand(
+                new Nric("S1234567A"),
+                new Appointment(),
+                new Appointment("", LocalDateTime.parse("10/12/2020 24:59", formatter))));
+
+        // invalid time
+        assertThrows(DateTimeParseException.class, () -> new EditApptCommand(
+                new Nric("S1234567A"),
+                new Appointment(),
+                new Appointment("", LocalDateTime.parse("10/12/2020 2359", formatter))));
+
+        // invalid time
+        assertThrows(DateTimeParseException.class, () -> new EditApptCommand(
+                new Nric("S1234567A"),
+                new Appointment(),
+                new Appointment("", LocalDateTime.parse("10/12/2020 233:59", formatter))));
     }
 }
